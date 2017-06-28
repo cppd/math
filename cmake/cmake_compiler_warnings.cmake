471b6df8382395e1fa5671bba2a085312e75212f
cppd <cppd@users.noreply.github.com>
2017-06-28 20:06:23 +0300

    edit CMake configuration

--- cmake/cmake_compiler_warnings.cmake
+++ cmake/cmake_compiler_warnings.cmake
@@ -2,124 +2,99 @@ target_compile_options(${PROJECT_NAME} PRIVATE
         $<$<CXX_COMPILER_ID:GNU>:
                 -Wall
                 -Wextra
+                -Wabi
+                -Wcast-align
+                -Wcast-qual
+                -Wdate-time
+                -Wdisabled-optimization
+                -Wduplicated-cond
+                -Wenum-compare
+                -Wformat=2
+                -Wformat-signedness
+                -Wframe-larger-than=10000
+                -Winit-self
+                -Wlarger-than=1000000
+                -Wlogical-op
+                -Wmissing-declarations
+                -Wmissing-format-attribute
+                -Wmissing-include-dirs
+                -Wpacked
+                -Wredundant-decls
+                -Wshadow
+                -Wstack-usage=10000
+                -Wstrict-aliasing=3
+                -Wstrict-overflow=1
+                -Wsuggest-attribute=format
+                -Wsuggest-attribute=noreturn
+                -Wsuggest-final-methods
+                -Wsuggest-final-types
+                -Wswitch-enum
+                -Wtrampolines
+                -Wundef
+                -Wunreachable-code
+                -Wunused
+                -Wunused-parameter
+                -Wvla
+                -Wwrite-strings
                 $<$<COMPILE_LANGUAGE:CXX>:
-                        -Wabi
-                        -Wcast-align
-                        -Wcast-qual
-                        -Wconditionally-supported
-                        -Wctor-dtor-privacy
-                        -Wdate-time
-                        -Wdisabled-optimization
-                        -Wduplicated-cond
-                        -Wenum-compare
-                        -Wformat=2
-                        -Wformat-signedness
-                        -Wframe-larger-than=10000
-                        -Winit-self
-                        -Wlarger-than=1000000
-                        -Wlogical-op
-                        -Wmissing-declarations
-                        -Wmissing-format-attribute
-                        -Wmissing-include-dirs
-                        -Wnoexcept
-                        -Wold-style-cast
-                        -Woverloaded-virtual
-                        -Wpacked
-                        -Wredundant-decls
-                        -Wshadow
-                        -Wsign-promo
-                        -Wstack-usage=10000
-                        -Wstrict-aliasing=3
-                        -Wstrict-overflow=1
-                        -Wsuggest-attribute=format
-                        -Wsuggest-attribute=noreturn
-                        -Wsuggest-final-methods
-                        -Wsuggest-final-types
-                        -Wsuggest-override
-                        -Wswitch-enum
-                        -Wtrampolines
-                        -Wundef
-                        -Wunreachable-code
-                        -Wunused
-                        -Wunused-parameter
-                        -Wvirtual-inheritance
-                        -Wvla
-                        -Wwrite-strings
-                        -Wzero-as-null-pointer-constant
+                -Wconditionally-supported
+                -Wctor-dtor-privacy
+                -Wnoexcept
+                -Wnon-virtual-dtor
+                -Wold-style-cast
+                -Woverloaded-virtual
+                -Wsign-promo
+                -Wsuggest-override
+                -Wvirtual-inheritance
+                -Wzero-as-null-pointer-constant
                 >
                 $<$<COMPILE_LANGUAGE:C>:
-                        -Wabi
-                        -Wbad-function-cast
-                        -Wcast-align
-                        -Wcast-qual
-                        -Wc++-compat
-                        -Wdate-time
-                        -Wdisabled-optimization
-                        -Wduplicated-cond
-                        -Wenum-compare
-                        -Wformat=2
-                        -Wformat-signedness
-                        -Wframe-larger-than=10000
-                        -Winit-self
-                        -Wjump-misses-init
-                        -Wlarger-than=10000
-                        -Wlogical-op
-                        -Wmissing-declarations
-                        -Wmissing-include-dirs
-                        -Wmissing-prototypes
-                        -Wnested-externs
-                        -Wold-style-definition
-                        -Wpacked
-                        -Wredundant-decls
-                        -Wshadow
-                        -Wstack-usage=10000
-                        -Wstrict-aliasing=3
-                        -Wstrict-overflow=1
-                        -Wstrict-prototypes
-                        -Wswitch-enum
-                        -Wtrampolines
-                        -Wundef
-                        -Wunreachable-code
-                        -Wunsuffixed-float-constants
-                        -Wunused
-                        -Wunused-parameter
-                        -Wvla
-                        -Wwrite-strings
+                -Wbad-function-cast
+                -Wc++-compat
+                -Wjump-misses-init
+                -Wmissing-prototypes
+                -Wnested-externs
+                -Wold-style-definition
+                -Wstrict-prototypes
+                -Wunsuffixed-float-constants
                 >
         >
         $<$<CXX_COMPILER_ID:Clang>:
                 -Weverything
                 $<$<COMPILE_LANGUAGE:CXX>:
-                        -Wno-c++98-compat
-                        -Wno-c++98-compat-pedantic
-                        -Wno-conversion
-                        -Wno-double-promotion
-                        -Wno-exit-time-destructors
-                        -Wno-float-equal
-                        -Wno-padded
-                        -Wno-weak-vtables
+                -Wno-c++98-compat
+                -Wno-c++98-compat-pedantic
+                -Wno-conversion
+                -Wno-double-promotion
+                -Wno-exit-time-destructors
+                -Wno-float-equal
+                -Wno-padded
+                -Wno-weak-vtables
                 >
         >
 )
 
 #-Waggregate-return
 #-Wconversion
-#-Weffc++
+#-Wdouble-promotion
 #-Werror
 #-Wfatal-errors
 #-Wfloat-conversion
 #-Wfloat-equal
 #-Winline
-#-Wmultiple-inheritance
-#-Wnon-virtual-dtor
 #-Wpadded
 #-Wsign-conversion
 #-Wsuggest-attribute=const
 #-Wsuggest-attribute=pure
 #-Wswitch-default
+#-Wunsafe-loop-optimizations
+
+#C++:
+#-Weffc++
+#-Wmultiple-inheritance
 #-Wuseless-cast
 
 #C:
 #-Wtraditional
 #-Wtraditional-conversion
-#-Wdouble-promotion
+
