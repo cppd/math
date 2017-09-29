b90c55e55531d195b17946b2e7cec51999720cb6
cppd <cppd@users.noreply.github.com>
2017-09-29 20:12:02 +0300

    color spaces

---
+++ src/path_tracing/colors.h
@@ -0,0 +1,76 @@
+/*
+Copyright (C) 2017 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "vec3.h"
+
+#include <cmath>
+
+inline double rgb_to_srgb(double c)
+{
+        if (c > 1.0)
+        {
+                return 1.0;
+        }
+        if (c >= 0.0031308)
+        {
+                return 1.055 * std::pow(c, 1.0 / 2.4) - 0.055;
+        }
+        if (c >= 0.0)
+        {
+                return c * 12.92;
+        }
+        return 0.0;
+}
+
+inline double srgb_to_rgb(double c)
+{
+        if (c > 1.0)
+        {
+                return 1.0;
+        }
+        if (c >= 0.04045)
+        {
+                return std::pow((c + 0.055) / 1.055, 2.4);
+        }
+        if (c >= 0.0)
+        {
+                return c / 12.92;
+        }
+        return 0.0;
+}
+
+inline unsigned char rgb_float_to_srgb_int8(double c)
+{
+        return static_cast<unsigned char>(rgb_to_srgb(c) * 255.0 + 0.5);
+}
+
+inline double srgb_int8_to_rgb_float(unsigned char c)
+{
+        return srgb_to_rgb(c / 255.0);
+}
+
+inline vec3 srgb_to_rgb(double r, double g, double b)
+{
+        return vec3(srgb_to_rgb(r), srgb_to_rgb(g), srgb_to_rgb(b));
+}
+
+inline vec3 srgb_to_rgb(const vec3& c)
+{
+        return srgb_to_rgb(c[0], c[1], c[2]);
+}
