39e08fe88af16f213945f31d5993316afc8235e8
cppd <cppd@users.noreply.github.com>
2018-07-01 09:19:19 +0300

    span class

---
+++ src/com/span.h
@@ -0,0 +1,59 @@
+/*
+Copyright (C) 2017, 2018 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <iterator>
+
+template <typename T>
+class Span
+{
+        T* const m_pointer;
+        const size_t m_size;
+
+public:
+        constexpr Span(T* pointer, size_t size) noexcept : m_pointer(pointer), m_size(size)
+        {
+        }
+
+        template <class Container>
+        constexpr Span(Container& c) noexcept(noexcept(std::data(c)) && noexcept(std::size(c)))
+                : m_pointer(std::data(c)), m_size(std::size(c))
+        {
+        }
+
+        template <class Container>
+        constexpr Span(const Container& c) noexcept(noexcept(std::data(c)) && noexcept(std::size(c)))
+                : m_pointer(std::data(c)), m_size(std::size(c))
+        {
+        }
+
+        constexpr T* data() const noexcept
+        {
+                return m_pointer;
+        }
+
+        constexpr size_t size() const noexcept
+        {
+                return m_size;
+        }
+
+        constexpr bool empty() const noexcept
+        {
+                return m_size == 0;
+        }
+};
