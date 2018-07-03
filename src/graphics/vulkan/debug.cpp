0c9427ee86f396472a2d59f723ffb03f03f4b009
cppd <cppd@users.noreply.github.com>
2018-07-03 09:43:35 +0300

    refactoring

---
+++ src/graphics/vulkan/debug.cpp
@@ -0,0 +1,103 @@
+/*
+Copyright (C) 2017, 2018 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#if defined(VULKAN_FOUND)
+
+#include "debug.h"
+
+#include "com/error.h"
+#include "com/log.h"
+
+namespace
+{
+void add_to_debug_message(std::string* str, const char* text)
+{
+        if (!str->empty())
+        {
+                *str += ", ";
+        }
+        *str += text;
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT /*objectType*/,
+                                              uint64_t /*object*/, size_t /*location*/, int32_t /*messageCode*/,
+                                              const char* /*pLayerPrefix*/, const char* pMessage, void* /*pUserData*/)
+{
+        std::string s;
+
+        if (flags & VK_DEBUG_REPORT_INFORMATION_BIT_EXT)
+        {
+                add_to_debug_message(&s, "information");
+        }
+        if (flags & VK_DEBUG_REPORT_WARNING_BIT_EXT)
+        {
+                add_to_debug_message(&s, "warning");
+        }
+        if (flags & VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT)
+        {
+                add_to_debug_message(&s, "performance warning");
+        }
+        if (flags & VK_DEBUG_REPORT_ERROR_BIT_EXT)
+        {
+                add_to_debug_message(&s, "error");
+        }
+        if (flags & VK_DEBUG_REPORT_DEBUG_BIT_EXT)
+        {
+                add_to_debug_message(&s, "debug");
+        }
+
+        if (s.size() > 0)
+        {
+                LOG("Validation layer message (" + s + "): " + std::string(pMessage));
+        }
+        else
+        {
+                LOG("Validation layer message: " + std::string(pMessage));
+        }
+
+        return VK_FALSE;
+}
+}
+
+namespace vulkan
+{
+DebugReportCallback create_debug_report_callback(VkInstance instance)
+{
+        if (instance == VK_NULL_HANDLE)
+        {
+                error("No VkInstance for DebugReportCallback");
+        }
+
+        VkDebugReportCallbackCreateInfoEXT create_info = {};
+
+        create_info.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+
+        create_info.flags |= VK_DEBUG_REPORT_ERROR_BIT_EXT;
+        create_info.flags |= VK_DEBUG_REPORT_WARNING_BIT_EXT;
+        create_info.flags |= VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT;
+#if 0
+        create_info.flags |= VK_DEBUG_REPORT_DEBUG_BIT_EXT;
+        create_info.flags |= VK_DEBUG_REPORT_INFORMATION_BIT_EXT;
+#endif
+
+        create_info.pfnCallback = debug_callback;
+
+        return DebugReportCallback(instance, create_info);
+}
+}
+
+#endif
--- src/graphics/vulkan/instance.cpp
+++ src/graphics/vulkan/instance.cpp
@@ -20,11 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "instance.h"
 
 #include "common.h"
+#include "debug.h"
 #include "query.h"
 
 #include "application/application_name.h"
 #include "com/error.h"
-#include "com/log.h"
 
 namespace
 {
@@ -269,53 +269,6 @@ std::vector<VkPipelineShaderStageCreateInfo> pipeline_shader_stage_create_info(c
         return res;
 }
 
-VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT /*objectType*/,
-                                              uint64_t /*object*/, size_t /*location*/, int32_t /*messageCode*/,
-                                              const char* /*pLayerPrefix*/, const char* pMessage, void* /*pUserData*/)
-{
-        const auto add_to_debug_message = [](std::string* str, const char* text) {
-                if (!str->empty())
-                {
-                        *str += ", ";
-                }
-                *str += text;
-        };
-
-        std::string s;
-
-        if (flags & VK_DEBUG_REPORT_INFORMATION_BIT_EXT)
-        {
-                add_to_debug_message(&s, "information");
-        }
-        if (flags & VK_DEBUG_REPORT_WARNING_BIT_EXT)
-        {
-                add_to_debug_message(&s, "warning");
-        }
-        if (flags & VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT)
-        {
-                add_to_debug_message(&s, "performance warning");
-        }
-        if (flags & VK_DEBUG_REPORT_ERROR_BIT_EXT)
-        {
-                add_to_debug_message(&s, "error");
-        }
-        if (flags & VK_DEBUG_REPORT_DEBUG_BIT_EXT)
-        {
-                add_to_debug_message(&s, "debug");
-        }
-
-        if (s.size() > 0)
-        {
-                LOG("Validation layer message (" + s + "): " + std::string(pMessage));
-        }
-        else
-        {
-                LOG("Validation layer message: " + std::string(pMessage));
-        }
-
-        return VK_FALSE;
-}
-
 vulkan::Instance create_instance(int api_version_major, int api_version_minor, std::vector<std::string> required_extensions,
                                  const std::vector<std::string>& required_validation_layers)
 {
@@ -357,30 +310,6 @@ vulkan::Instance create_instance(int api_version_major, int api_version_minor, s
         }
 
         return vulkan::Instance(create_info);
-}
-
-vulkan::DebugReportCallback create_debug_report_callback(VkInstance instance)
-{
-        if (instance == VK_NULL_HANDLE)
-        {
-                error("No VkInstance for DebugReportCallback");
-        }
-
-        VkDebugReportCallbackCreateInfoEXT create_info = {};
-
-        create_info.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
-
-        create_info.flags |= VK_DEBUG_REPORT_ERROR_BIT_EXT;
-        create_info.flags |= VK_DEBUG_REPORT_WARNING_BIT_EXT;
-        create_info.flags |= VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT;
-#if 0
-        create_info.flags |= VK_DEBUG_REPORT_DEBUG_BIT_EXT;
-        create_info.flags |= VK_DEBUG_REPORT_INFORMATION_BIT_EXT;
-#endif
-
-        create_info.pfnCallback = debug_callback;
-
-        return vulkan::DebugReportCallback(instance, create_info);
 }
 
 vulkan::Device create_device(VkPhysicalDevice physical_device, const std::vector<unsigned>& family_indices,
