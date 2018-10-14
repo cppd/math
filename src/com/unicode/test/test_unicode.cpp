20423aa88577dc3123b64149f882da8db551a68f
cppd <cppd@users.noreply.github.com>
2018-10-14 09:33:57 +0300

    edit Unicode tests

--- src/com/unicode/test/test_unicode.cpp
+++ src/com/unicode/test/test_unicode.cpp
@@ -18,11 +18,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "test_unicode.h"
 
 #include "com/error.h"
+#include "com/log.h"
 #include "com/print.h"
 #include "com/unicode/unicode.h"
 
-void test_unicode()
+namespace
 {
+void test_utf32_to_utf8_to_utf32()
+{
+        LOG("UTF-32 to UTF-8 to UTF-32");
+
         std::string utf8;
 
         for (char32_t c1 = 0; c1 <= 0x10FFFF; ++c1)
@@ -35,43 +40,127 @@ void test_unicode()
                 {
                         std::string e;
                         e += "Error Unicode converting.\n";
-                        e += "UTF-32: " + to_string(static_cast<long long>(c1)) + "\n";
-                        e += "UTF-8: " + utf8;
-                        e += "UTF-32: " + to_string(static_cast<long long>(c2)) + "\n";
+                        e += "UTF-32: " + unicode::utf32_to_number_string(c1) + "\n";
+                        e += "UTF-8: " + unicode::utf8_to_number_string(utf8) + "\n";
+                        e += "UTF-32: " + unicode::utf32_to_number_string(c2) + "\n";
                         error(e);
                 }
         }
+}
 
-        //
+void test_utf32_replacement_character()
+{
+        LOG("UTF-32 replacement character");
 
-        if ("\xEF\xBF\xBD" != unicode::utf32_to_utf8(char32_t(0xFFFFFF)))
+        if (u8"\U0000FFFD" != unicode::utf32_to_utf8(char32_t(0xFFFFFF)))
         {
                 error("Error UTF-8 replacement character");
         }
+}
 
-        size_t i;
+void test_utf8_replacement_character_and_self_synchronizing()
+{
+        LOG("UTF-8 replacement character and self-synchronizing");
 
-        i = 0;
-        if (0xFFFD != unicode::utf8_to_utf32("\x96\x96", i))
         {
-                error("Error UTF-32 replacement character");
+                std::string s = u8"\U0000222B\U00002211";
+                s.erase(0, 1);
+
+                ASSERT(s.size() == 5);
+
+                size_t i = 0;
+
+                if (0xFFFD != unicode::utf8_to_utf32(s, i))
+                {
+                        error("Error UTF-32 replacement character");
+                }
+                if (i != 1)
+                {
+                        error("Error UTF-8 string index " + to_string(i));
+                }
+
+                if (0xFFFD != unicode::utf8_to_utf32(s, i))
+                {
+                        error("Error UTF-32 replacement character");
+                }
+                if (i != 2)
+                {
+                        error("Error UTF-8 string index " + to_string(i));
+                }
+
+                if (0x2211 != unicode::utf8_to_utf32(s, i))
+                {
+                        error("Error reading UTF-32");
+                }
+                if (i != s.size())
+                {
+                        error("Error UTF-8 string index " + to_string(i));
+                }
         }
 
-        i = 0;
-        if (0xFFFD != unicode::utf8_to_utf32("\xE2\x88", i))
         {
-                error("Error UTF-32 replacement character");
+                std::string s = u8"\U0000222B\U00002211";
+                s.erase(2, 1);
+
+                ASSERT(s.size() == 5);
+
+                size_t i = 0;
+
+                if (0xFFFD != unicode::utf8_to_utf32(s, i))
+                {
+                        error("Error UTF-32 replacement character");
+                }
+                if (i != 1)
+                {
+                        error("Error UTF-8 string index " + to_string(i));
+                }
+
+                if (0xFFFD != unicode::utf8_to_utf32(s, i))
+                {
+                        error("Error UTF-32 replacement character");
+                }
+                if (i != 2)
+                {
+                        error("Error UTF-8 string index " + to_string(i));
+                }
+
+                if (0x2211 != unicode::utf8_to_utf32(s, i))
+                {
+                        error("Error reading UTF-32");
+                }
+                if (i != s.size())
+                {
+                        error("Error UTF-8 string index " + to_string(i));
+                }
         }
+}
 
-        //
+void test_utf32_to_utf8()
+{
+        LOG("UTF-32 to UTF-8");
 
-        if (0x222B != unicode::utf8_to_utf32("\xE2\x88\xAB"))
-        {
-                error("Error UTF-8 to UTF-32");
-        }
-
-        if ("\xE2\x88\xAB" != unicode::utf32_to_utf8(char32_t(0x222B)))
+        if (u8"\U0000222B" != unicode::utf32_to_utf8(char32_t(0x222B)))
         {
                 error("Error UTF-32 to UTF-8");
         }
+}
+
+void test_utf8_to_utf32()
+{
+        LOG("UTF-8 to UTF-32");
+
+        if (0x222B != unicode::utf8_to_utf32(u8"\U0000222B"))
+        {
+                error("Error UTF-8 to UTF-32");
+        }
+}
+}
+
+void test_unicode()
+{
+        test_utf32_to_utf8_to_utf32();
+        test_utf32_replacement_character();
+        test_utf8_replacement_character_and_self_synchronizing();
+        test_utf32_to_utf8();
+        test_utf8_to_utf32();
 }
