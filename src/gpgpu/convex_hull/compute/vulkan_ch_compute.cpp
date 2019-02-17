aab7cda687574885baf5c4b430ea0b24c5c0c22d
cppd <cppd@users.noreply.github.com>
2019-02-17 09:24:54 +0300

    Vulkan convex hull compute pipelines

--- src/gpgpu/convex_hull/compute/vulkan_ch_compute.cpp
+++ src/gpgpu/convex_hull/compute/vulkan_ch_compute.cpp
@@ -62,6 +62,18 @@ using namespace gpgpu_convex_hull_compute_implementation;
 using namespace gpgpu_convex_hull_compute_vulkan_implementation;
 }
 
+int group_size_prepare(int width, const VkPhysicalDeviceLimits& limits)
+{
+        return impl::group_size_prepare(width, limits.maxComputeWorkGroupSize[0], limits.maxComputeWorkGroupInvocations,
+                                        limits.maxComputeSharedMemorySize);
+}
+
+int group_size_merge(int height, const VkPhysicalDeviceLimits& limits)
+{
+        return impl::group_size_merge(height, limits.maxComputeWorkGroupSize[0], limits.maxComputeWorkGroupInvocations,
+                                      limits.maxComputeSharedMemorySize);
+}
+
 class Impl final : public gpgpu_vulkan::ConvexHullCompute
 {
         const std::thread::id m_thread_id = std::this_thread::get_id();
@@ -162,22 +174,75 @@ class Impl final : public gpgpu_vulkan::ConvexHullCompute
                 m_points_buffer = points_buffer;
                 m_point_count_buffer = point_count_buffer;
 
-                m_debug_memory.set_object_image(objects);
-                m_debug_memory.set_points(points_buffer);
-                m_debug_memory.set_point_count(point_count_buffer);
-                m_debug_constant.set_local_size_x(4);
-                m_debug_constant.set_local_size_y(1);
-                m_debug_constant.set_local_size_z(1);
-                m_debug_constant.set_buffer_size(1);
+                const VkPhysicalDeviceLimits& limits = m_instance.physical_device().properties().limits;
 
-                vulkan::ComputePipelineCreateInfo info;
-                info.device = &m_device;
-                info.pipeline_layout = m_debug_pipeline_layout;
-                info.shader = &m_debug_shader;
-                info.specialization_map_entries = m_debug_constant.entries();
-                info.specialization_data = m_debug_constant.data();
-                info.specialization_data_size = m_debug_constant.size();
-                m_debug_pipeline = create_compute_pipeline(info);
+                {
+                        m_debug_memory.set_object_image(objects);
+                        m_debug_memory.set_points(points_buffer);
+                        m_debug_memory.set_point_count(point_count_buffer);
+
+                        m_debug_constant.set_local_size_x(4);
+                        m_debug_constant.set_local_size_y(1);
+                        m_debug_constant.set_local_size_z(1);
+                        m_debug_constant.set_buffer_size(1);
+
+                        vulkan::ComputePipelineCreateInfo info;
+                        info.device = &m_device;
+                        info.pipeline_layout = m_debug_pipeline_layout;
+                        info.shader = &m_debug_shader;
+                        info.specialization_map_entries = m_debug_constant.entries();
+                        info.specialization_data = m_debug_constant.data();
+                        info.specialization_data_size = m_debug_constant.size();
+                        m_debug_pipeline = create_compute_pipeline(info);
+                }
+                {
+                        m_prepare_memory.set_object_image(objects);
+                        m_prepare_memory.set_lines(*m_lines_buffer);
+
+                        m_prepare_constant.set_line_size(objects.height());
+                        m_prepare_constant.set_buffer_and_group_size(group_size_prepare(objects.width(), limits));
+
+                        vulkan::ComputePipelineCreateInfo info;
+                        info.device = &m_device;
+                        info.pipeline_layout = m_prepare_pipeline_layout;
+                        info.shader = &m_prepare_shader;
+                        info.specialization_map_entries = m_prepare_constant.entries();
+                        info.specialization_data = m_prepare_constant.data();
+                        info.specialization_data_size = m_prepare_constant.size();
+                        m_prepare_pipeline = create_compute_pipeline(info);
+                }
+                {
+                        m_merge_memory.set_lines(*m_lines_buffer);
+
+                        m_merge_constant.set_line_size(objects.height());
+                        m_merge_constant.set_local_size_x(group_size_merge(objects.height(), limits));
+                        m_merge_constant.set_iteration_count(impl::iteration_count_merge(objects.height()));
+
+                        vulkan::ComputePipelineCreateInfo info;
+                        info.device = &m_device;
+                        info.pipeline_layout = m_merge_pipeline_layout;
+                        info.shader = &m_merge_shader;
+                        info.specialization_map_entries = m_merge_constant.entries();
+                        info.specialization_data = m_merge_constant.data();
+                        info.specialization_data_size = m_merge_constant.size();
+                        m_merge_pipeline = create_compute_pipeline(info);
+                }
+                {
+                        m_filter_memory.set_lines(*m_lines_buffer);
+                        m_filter_memory.set_points(points_buffer);
+                        m_filter_memory.set_point_count(point_count_buffer);
+
+                        m_filter_constant.set_line_size(objects.height());
+
+                        vulkan::ComputePipelineCreateInfo info;
+                        info.device = &m_device;
+                        info.pipeline_layout = m_filter_pipeline_layout;
+                        info.shader = &m_filter_shader;
+                        info.specialization_map_entries = m_filter_constant.entries();
+                        info.specialization_data = m_filter_constant.data();
+                        info.specialization_data_size = m_filter_constant.size();
+                        m_filter_pipeline = create_compute_pipeline(info);
+                }
         }
 
         void delete_buffers() override
