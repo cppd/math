a3de65f9d1cf703c9178bb615e27fd8e7ff8c063
cppd <cppd@users.noreply.github.com>
2019-09-07 12:03:03 +0300

    use event window class

--- src/show/show.cpp
+++ src/show/show.cpp
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "show.h"
 
 #include "camera.h"
+#include "event_window.h"
 #include "frame_rate.h"
 #include "show_thread.h"
 
@@ -113,6 +114,7 @@ class Impl final : public Show, public WindowEvent
         //
 
         EventQueue& m_event_queue;
+        EventWindow<Window> m_event_window;
         ShowCallback* const m_callback;
         const WindowID m_parent_window;
         const double m_parent_window_ppi;
@@ -132,19 +134,8 @@ class Impl final : public Show, public WindowEvent
 
         //
 
-        int m_window_width = limits<int>::lowest();
-        int m_window_height = limits<int>::lowest();
-
         int m_draw_width = limits<int>::lowest();
         int m_draw_height = limits<int>::lowest();
-
-        int m_mouse_x = 0;
-        int m_mouse_y = 0;
-
-        int m_mouse_pressed_x = 0;
-        int m_mouse_pressed_y = 0;
-        bool m_mouse_pressed = false;
-        MouseButton m_mouse_pressed_button;
 
         bool m_fullscreen_active = false;
 
@@ -441,37 +432,42 @@ class Impl final : public Show, public WindowEvent
                 }
         }
 
-        void window_mouse_pressed(MouseButton button) override
+        void window_mouse_pressed(MouseButton /*button*/) override
         {
                 ASSERT(std::this_thread::get_id() == m_thread_id);
-
-                if (!m_mouse_pressed && m_mouse_x < m_draw_width && m_mouse_y < m_draw_height)
-                {
-                        m_mouse_pressed = true;
-                        m_mouse_pressed_button = button;
-                        m_mouse_pressed_x = m_mouse_x;
-                        m_mouse_pressed_y = m_mouse_y;
-                }
         }
 
-        void window_mouse_released(MouseButton button) override
+        void window_mouse_released(MouseButton /*button*/) override
         {
                 ASSERT(std::this_thread::get_id() == m_thread_id);
-
-                if (m_mouse_pressed && button == m_mouse_pressed_button)
-                {
-                        m_mouse_pressed = false;
-                }
         }
 
-        void window_mouse_moved(int x, int y) override
+        void window_mouse_moved(int /*x*/, int /*y*/) override
         {
                 ASSERT(std::this_thread::get_id() == m_thread_id);
 
-                m_mouse_x = x;
-                m_mouse_y = y;
+                bool changed = false;
 
-                mouse_move_handler();
+                const PressedMouseButton& right = m_event_window.pressed_mouse_button(MouseButton::Right);
+                if (right.pressed && right.pressed_x < m_draw_width && right.pressed_y < m_draw_height &&
+                    (right.delta_x != 0 || right.delta_y != 0))
+                {
+                        m_camera.rotate(-right.delta_x, -right.delta_y);
+                        changed = true;
+                }
+
+                const PressedMouseButton& left = m_event_window.pressed_mouse_button(MouseButton::Left);
+                if (left.pressed && left.pressed_x < m_draw_width && left.pressed_y < m_draw_height &&
+                    (left.delta_x != 0 || left.delta_y != 0))
+                {
+                        m_camera.move(vec2(-left.delta_x, left.delta_y));
+                        changed = true;
+                }
+
+                if (changed)
+                {
+                        m_renderer->set_camera(m_camera.rasterization_info());
+                }
         }
 
         void window_mouse_wheel(int delta) override
@@ -487,17 +483,9 @@ class Impl final : public Show, public WindowEvent
                 }
         }
 
-        void window_resized(int width, int height) override
+        void window_resized(int /*width*/, int /*height*/) override
         {
                 ASSERT(std::this_thread::get_id() == m_thread_id);
-
-                if (width <= 0 || height <= 0)
-                {
-                        error("Window resize error: width = " + to_string(width) + ", height = " + to_string(height));
-                }
-
-                m_window_width = width;
-                m_window_height = height;
 
                 window_resize_handler();
         }
@@ -508,40 +496,7 @@ class Impl final : public Show, public WindowEvent
         {
                 ASSERT(std::this_thread::get_id() == m_thread_id);
 
-                m_camera.scale(m_mouse_x, m_mouse_y, delta);
-
-                m_renderer->set_camera(m_camera.rasterization_info());
-        }
-
-        void mouse_move_handler()
-        {
-                ASSERT(std::this_thread::get_id() == m_thread_id);
-
-                if (!m_mouse_pressed)
-                {
-                        return;
-                }
-
-                int delta_x = m_mouse_x - m_mouse_pressed_x;
-                int delta_y = m_mouse_y - m_mouse_pressed_y;
-
-                if (delta_x == 0 && delta_y == 0)
-                {
-                        return;
-                }
-
-                m_mouse_pressed_x = m_mouse_x;
-                m_mouse_pressed_y = m_mouse_y;
-
-                switch (m_mouse_pressed_button)
-                {
-                case MouseButton::Right:
-                        m_camera.rotate(-delta_x, -delta_y);
-                        break;
-                case MouseButton::Left:
-                        m_camera.move(vec2(-delta_x, delta_y));
-                        break;
-                }
+                m_camera.scale(m_event_window.mouse_x(), m_event_window.mouse_y(), delta);
 
                 m_renderer->set_camera(m_camera.rasterization_info());
         }
@@ -559,7 +514,7 @@ class Impl final : public Show, public WindowEvent
         {
                 ASSERT(std::this_thread::get_id() == m_thread_id);
 
-                if (m_window_width <= 0 || m_window_height <= 0)
+                if (m_event_window.window_width() != m_window->width() || m_event_window.window_height() != m_window->height())
                 {
                         // Вызов не из обработчика измерения окна (там есть проверка),
                         // а вызов из обработчика включения-выключения ДПФ в то время,
@@ -567,8 +522,8 @@ class Impl final : public Show, public WindowEvent
                         return;
                 }
 
-                m_draw_width = m_canvas->dft_active() ? m_window_width / 2 : m_window_width;
-                m_draw_height = m_window_height;
+                m_draw_width = m_canvas->dft_active() ? m_window->width() / 2 : m_window->width();
+                m_draw_height = m_window->height();
 
                 resize();
 
@@ -585,7 +540,7 @@ class Impl final : public Show, public WindowEvent
                 // Вначале команды, потом сообщения окна, потому что в командах
                 // могут быть действия с окном, а в событиях окна нет комманд
                 m_event_queue.pull_and_dispatch_events(*this);
-                m_window->pull_and_dispath_events(*this);
+                m_event_window.pull_and_dispath_events(*this);
         }
 
         void init_window_and_view()
@@ -594,7 +549,9 @@ class Impl final : public Show, public WindowEvent
 
                 move_window_to_parent(m_window->system_handle(), m_parent_window);
 
-                for (int i = 1; m_window_width != m_window->width() && m_window_height != m_window->height(); ++i)
+                for (int i = 1;
+                     m_event_window.window_width() != m_window->width() && m_event_window.window_height() != m_window->height();
+                     ++i)
                 {
                         if (i > 10)
                         {
@@ -657,6 +614,7 @@ void Impl<GraphicsAndComputeAPI::OpenGL>::loop(std::atomic_bool& stop)
 
         //
 
+        m_event_window.set_window(*window);
         m_window.set(window);
         m_renderer.set(renderer);
         m_canvas.set(canvas);
@@ -691,12 +649,12 @@ void Impl<GraphicsAndComputeAPI::OpenGL>::resize()
 {
         m_renderer->set_size(m_draw_width, m_draw_height);
 
-        int dft_dst_x = (m_window_width & 1) ? (m_draw_width + 1) : m_draw_width;
+        int dft_dst_x = (m_window->width() & 1) ? (m_draw_width + 1) : m_draw_width;
         int dft_dst_y = 0;
 
-        m_canvas->create_objects(m_window_width, m_window_height, m_renderer->color_buffer(), m_renderer->color_buffer_is_srgb(),
-                                 m_renderer->objects(), m_draw_width, m_draw_height, dft_dst_x, dft_dst_y,
-                                 m_renderer->frame_buffer_is_srgb());
+        m_canvas->create_objects(m_window->width(), m_window->height(), m_renderer->color_buffer(),
+                                 m_renderer->color_buffer_is_srgb(), m_renderer->objects(), m_draw_width, m_draw_height,
+                                 dft_dst_x, dft_dst_y, m_renderer->frame_buffer_is_srgb());
 }
 
 //
@@ -835,6 +793,7 @@ void Impl<GraphicsAndComputeAPI::Vulkan>::loop(std::atomic_bool& stop)
 
         //
 
+        m_event_window.set_window(*window);
         m_window.set(window);
         m_renderer.set(renderer);
         m_canvas.set(canvas);
