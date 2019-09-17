9b06698022b568276e95eac953445ce7355eb2d7
cppd <cppd@users.noreply.github.com>
2019-09-17 07:20:04 +0300

    Vulkan pencil sketch compute memory

---
+++ src/gpu/pencil_sketch/vulkan/compute_memory.cpp
@@ -0,0 +1,233 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "compute_memory.h"
+
+namespace gpu_vulkan
+{
+std::vector<VkDescriptorSetLayoutBinding> PencilSketchComputeMemory::descriptor_set_layout_bindings()
+{
+        std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = INPUT_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = OUTPUT_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = OBJECTS_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+
+        return bindings;
+}
+
+PencilSketchComputeMemory::PencilSketchComputeMemory(const vulkan::Device& device)
+        : m_descriptor_set_layout(vulkan::create_descriptor_set_layout(device, descriptor_set_layout_bindings())),
+          m_descriptors(device, 1, m_descriptor_set_layout, descriptor_set_layout_bindings())
+{
+}
+
+unsigned PencilSketchComputeMemory::set_number()
+{
+        return SET_NUMBER;
+}
+
+VkDescriptorSetLayout PencilSketchComputeMemory::descriptor_set_layout() const
+{
+        return m_descriptor_set_layout;
+}
+
+const VkDescriptorSet& PencilSketchComputeMemory::descriptor_set() const
+{
+        return m_descriptors.descriptor_set(0);
+}
+
+void PencilSketchComputeMemory::set_input_image(const vulkan::StorageImage& storage_image) const
+{
+        ASSERT(storage_image.format() == VK_FORMAT_R32G32B32A32_SFLOAT);
+
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        image_info.imageView = storage_image.image_view();
+
+        m_descriptors.update_descriptor_set(0, INPUT_BINDING, image_info);
+}
+
+void PencilSketchComputeMemory::set_output_image(const vulkan::StorageImage& storage_image) const
+{
+        ASSERT(storage_image.format() == VK_FORMAT_R32G32B32A32_SFLOAT);
+
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        image_info.imageView = storage_image.image_view();
+
+        m_descriptors.update_descriptor_set(0, OUTPUT_BINDING, image_info);
+}
+
+void PencilSketchComputeMemory::set_object_image(const vulkan::StorageImage& storage_image) const
+{
+        ASSERT(storage_image.format() == VK_FORMAT_R32_UINT);
+
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        image_info.imageView = storage_image.image_view();
+
+        m_descriptors.update_descriptor_set(0, OBJECTS_BINDING, image_info);
+}
+
+//
+
+PencilSketchComputeConstant::PencilSketchComputeConstant()
+{
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 0;
+                entry.offset = offsetof(Data, local_size);
+                entry.size = sizeof(Data::local_size);
+                m_entries.push_back(entry);
+        }
+}
+
+void PencilSketchComputeConstant::set_group_size(uint32_t v)
+{
+        static_assert(std::is_same_v<decltype(m_data.local_size), decltype(v)>);
+        m_data.local_size = v;
+}
+
+const std::vector<VkSpecializationMapEntry>& PencilSketchComputeConstant::entries() const
+{
+        return m_entries;
+}
+
+const void* PencilSketchComputeConstant::data() const
+{
+        return &m_data;
+}
+
+size_t PencilSketchComputeConstant::size() const
+{
+        return sizeof(m_data);
+}
+
+//
+
+std::vector<VkDescriptorSetLayoutBinding> PencilSketchLuminanceMemory::descriptor_set_layout_bindings()
+{
+        std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = IMAGE_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+
+        return bindings;
+}
+
+PencilSketchLuminanceMemory::PencilSketchLuminanceMemory(const vulkan::Device& device)
+        : m_descriptor_set_layout(vulkan::create_descriptor_set_layout(device, descriptor_set_layout_bindings())),
+          m_descriptors(device, 1, m_descriptor_set_layout, descriptor_set_layout_bindings())
+{
+}
+
+unsigned PencilSketchLuminanceMemory::set_number()
+{
+        return SET_NUMBER;
+}
+
+VkDescriptorSetLayout PencilSketchLuminanceMemory::descriptor_set_layout() const
+{
+        return m_descriptor_set_layout;
+}
+
+const VkDescriptorSet& PencilSketchLuminanceMemory::descriptor_set() const
+{
+        return m_descriptors.descriptor_set(0);
+}
+
+void PencilSketchLuminanceMemory::set_image(const vulkan::StorageImage& storage_image) const
+{
+        ASSERT(storage_image.format() == VK_FORMAT_R32G32B32A32_SFLOAT);
+
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        image_info.imageView = storage_image.image_view();
+
+        m_descriptors.update_descriptor_set(0, IMAGE_BINDING, image_info);
+}
+
+//
+
+PencilSketchLuminanceConstant::PencilSketchLuminanceConstant()
+{
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 0;
+                entry.offset = offsetof(Data, local_size);
+                entry.size = sizeof(Data::local_size);
+                m_entries.push_back(entry);
+        }
+}
+
+void PencilSketchLuminanceConstant::set_group_size(uint32_t v)
+{
+        static_assert(std::is_same_v<decltype(m_data.local_size), decltype(v)>);
+        m_data.local_size = v;
+}
+
+const std::vector<VkSpecializationMapEntry>& PencilSketchLuminanceConstant::entries() const
+{
+        return m_entries;
+}
+
+const void* PencilSketchLuminanceConstant::data() const
+{
+        return &m_data;
+}
+
+size_t PencilSketchLuminanceConstant::size() const
+{
+        return sizeof(m_data);
+}
+}
---
+++ src/gpu/pencil_sketch/vulkan/compute_memory.h
@@ -0,0 +1,134 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "graphics/vulkan/buffers.h"
+#include "graphics/vulkan/constant.h"
+#include "graphics/vulkan/descriptor.h"
+#include "graphics/vulkan/objects.h"
+
+#include <vector>
+
+namespace gpu_vulkan
+{
+class PencilSketchComputeMemory final
+{
+        static constexpr int SET_NUMBER = 0;
+
+        static constexpr int INPUT_BINDING = 0;
+        static constexpr int OUTPUT_BINDING = 1;
+        static constexpr int OBJECTS_BINDING = 2;
+
+        static std::vector<VkDescriptorSetLayoutBinding> descriptor_set_layout_bindings();
+
+        vulkan::DescriptorSetLayout m_descriptor_set_layout;
+        vulkan::Descriptors m_descriptors;
+
+public:
+        PencilSketchComputeMemory(const vulkan::Device& device);
+
+        PencilSketchComputeMemory(const PencilSketchComputeMemory&) = delete;
+        PencilSketchComputeMemory& operator=(const PencilSketchComputeMemory&) = delete;
+        PencilSketchComputeMemory& operator=(PencilSketchComputeMemory&&) = delete;
+
+        PencilSketchComputeMemory(PencilSketchComputeMemory&&) = default;
+        ~PencilSketchComputeMemory() = default;
+
+        //
+
+        static unsigned set_number();
+        VkDescriptorSetLayout descriptor_set_layout() const;
+        const VkDescriptorSet& descriptor_set() const;
+
+        //
+
+        void set_input_image(const vulkan::StorageImage& storage_image) const;
+        void set_output_image(const vulkan::StorageImage& storage_image) const;
+        void set_object_image(const vulkan::StorageImage& storage_image) const;
+};
+
+class PencilSketchComputeConstant final : public vulkan::SpecializationConstant
+{
+        struct Data
+        {
+                uint32_t local_size;
+        } m_data;
+
+        std::vector<VkSpecializationMapEntry> m_entries;
+
+        const std::vector<VkSpecializationMapEntry>& entries() const override;
+        const void* data() const override;
+        size_t size() const override;
+
+public:
+        PencilSketchComputeConstant();
+
+        void set_group_size(uint32_t v);
+};
+
+class PencilSketchLuminanceMemory final
+{
+        static constexpr int SET_NUMBER = 0;
+
+        static constexpr int IMAGE_BINDING = 0;
+
+        static std::vector<VkDescriptorSetLayoutBinding> descriptor_set_layout_bindings();
+
+        vulkan::DescriptorSetLayout m_descriptor_set_layout;
+        vulkan::Descriptors m_descriptors;
+
+public:
+        PencilSketchLuminanceMemory(const vulkan::Device& device);
+
+        PencilSketchLuminanceMemory(const PencilSketchLuminanceMemory&) = delete;
+        PencilSketchLuminanceMemory& operator=(const PencilSketchLuminanceMemory&) = delete;
+        PencilSketchLuminanceMemory& operator=(PencilSketchLuminanceMemory&&) = delete;
+
+        PencilSketchLuminanceMemory(PencilSketchLuminanceMemory&&) = default;
+        ~PencilSketchLuminanceMemory() = default;
+
+        //
+
+        static unsigned set_number();
+        VkDescriptorSetLayout descriptor_set_layout() const;
+        const VkDescriptorSet& descriptor_set() const;
+
+        //
+
+        void set_image(const vulkan::StorageImage& storage_image) const;
+};
+
+class PencilSketchLuminanceConstant final : public vulkan::SpecializationConstant
+{
+        struct Data
+        {
+                uint32_t local_size;
+        } m_data;
+
+        std::vector<VkSpecializationMapEntry> m_entries;
+
+        const std::vector<VkSpecializationMapEntry>& entries() const override;
+        const void* data() const override;
+        size_t size() const override;
+
+public:
+        PencilSketchLuminanceConstant();
+
+        void set_group_size(uint32_t v);
+};
+}
