d5c2b155fcf71d52226f159c01ad4af832190bee
cppd <cppd@users.noreply.github.com>
2019-09-29 11:21:06 +0300

    Vulkan DFT copy input memory

---
+++ src/gpu/dft/vulkan/compute_memory.h
@@ -0,0 +1,82 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "graphics/vulkan/buffers.h"
+#include "graphics/vulkan/constant.h"
+#include "graphics/vulkan/descriptor.h"
+#include "graphics/vulkan/objects.h"
+
+#include <vector>
+
+namespace gpu_vulkan
+{
+class DftCopyInputMemory final
+{
+        static constexpr int SET_NUMBER = 0;
+
+        static constexpr int INPUT_BINDING = 2;
+        static constexpr int OUTPUT_BINDING = 0;
+
+        static std::vector<VkDescriptorSetLayoutBinding> descriptor_set_layout_bindings();
+
+        vulkan::DescriptorSetLayout m_descriptor_set_layout;
+        vulkan::Descriptors m_descriptors;
+
+public:
+        DftCopyInputMemory(const vulkan::Device& device);
+
+        DftCopyInputMemory(const DftCopyInputMemory&) = delete;
+        DftCopyInputMemory& operator=(const DftCopyInputMemory&) = delete;
+        DftCopyInputMemory& operator=(DftCopyInputMemory&&) = delete;
+
+        DftCopyInputMemory(DftCopyInputMemory&&) = default;
+        ~DftCopyInputMemory() = default;
+
+        //
+
+        static unsigned set_number();
+        VkDescriptorSetLayout descriptor_set_layout() const;
+        const VkDescriptorSet& descriptor_set() const;
+
+        //
+
+        void set_input(VkSampler sampler, const vulkan::ImageWithMemory& image) const;
+        void set_output(const vulkan::BufferWithMemory& buffer) const;
+};
+
+class DftCopyInputConstant final : public vulkan::SpecializationConstant
+{
+        struct Data
+        {
+                uint32_t local_size_x;
+                uint32_t local_size_y;
+        } m_data;
+
+        std::vector<VkSpecializationMapEntry> m_entries;
+
+        const std::vector<VkSpecializationMapEntry>& entries() const override;
+        const void* data() const override;
+        size_t size() const override;
+
+public:
+        DftCopyInputConstant();
+
+        void set_group_size(uint32_t x, uint32_t y);
+};
+}
