207e9b6a717a30a641e7ba98f8f9f65002ae544f
cppd <cppd@users.noreply.github.com>
2019-10-04 06:56:15 +0300

    Vulkan DFT mul d memory

--- src/gpu/dft/vulkan/compute_memory.cpp
+++ src/gpu/dft/vulkan/compute_memory.cpp
@@ -837,4 +837,142 @@ size_t DftMulConstant::size() const
 {
         return sizeof(m_data);
 }
+
+//
+
+std::vector<VkDescriptorSetLayoutBinding> DftMulDMemory::descriptor_set_layout_bindings()
+{
+        std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = DIAGONAL_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = DATA_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+
+        return bindings;
+}
+
+DftMulDMemory::DftMulDMemory(const vulkan::Device& device)
+        : m_descriptor_set_layout(vulkan::create_descriptor_set_layout(device, descriptor_set_layout_bindings())),
+          m_descriptors(device, 1, m_descriptor_set_layout, descriptor_set_layout_bindings())
+{
+}
+
+unsigned DftMulDMemory::set_number()
+{
+        return SET_NUMBER;
+}
+
+VkDescriptorSetLayout DftMulDMemory::descriptor_set_layout() const
+{
+        return m_descriptor_set_layout;
+}
+
+const VkDescriptorSet& DftMulDMemory::descriptor_set() const
+{
+        return m_descriptors.descriptor_set(0);
+}
+
+void DftMulDMemory::set_diagonal(const vulkan::BufferWithMemory& diagonal) const
+{
+        ASSERT(diagonal.usage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT));
+
+        VkDescriptorBufferInfo buffer_info = {};
+        buffer_info.buffer = diagonal;
+        buffer_info.offset = 0;
+        buffer_info.range = diagonal.size();
+
+        m_descriptors.update_descriptor_set(0, DIAGONAL_BINDING, buffer_info);
+}
+
+void DftMulDMemory::set_data(const vulkan::BufferWithMemory& data) const
+{
+        ASSERT(data.usage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT));
+
+        VkDescriptorBufferInfo buffer_info = {};
+        buffer_info.buffer = data;
+        buffer_info.offset = 0;
+        buffer_info.range = data.size();
+
+        m_descriptors.update_descriptor_set(0, DATA_BINDING, buffer_info);
+}
+
+//
+
+DftMulDConstant::DftMulDConstant()
+{
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 0;
+                entry.offset = offsetof(Data, group_size_x);
+                entry.size = sizeof(Data::group_size_x);
+                m_entries.push_back(entry);
+        }
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 1;
+                entry.offset = offsetof(Data, group_size_y);
+                entry.size = sizeof(Data::group_size_y);
+                m_entries.push_back(entry);
+        }
+
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 2;
+                entry.offset = offsetof(Data, rows);
+                entry.size = sizeof(Data::rows);
+                m_entries.push_back(entry);
+        }
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 3;
+                entry.offset = offsetof(Data, columns);
+                entry.size = sizeof(Data::columns);
+                m_entries.push_back(entry);
+        }
+}
+
+void DftMulDConstant::set(uint32_t group_size_x, uint32_t group_size_y, int32_t rows, int32_t columns)
+{
+        static_assert(std::is_same_v<decltype(m_data.group_size_x), decltype(group_size_x)>);
+        m_data.group_size_x = group_size_x;
+        static_assert(std::is_same_v<decltype(m_data.group_size_y), decltype(group_size_y)>);
+        m_data.group_size_y = group_size_y;
+        static_assert(std::is_same_v<decltype(m_data.rows), decltype(rows)>);
+        m_data.rows = rows;
+        static_assert(std::is_same_v<decltype(m_data.columns), decltype(columns)>);
+        m_data.columns = columns;
+}
+
+const std::vector<VkSpecializationMapEntry>& DftMulDConstant::entries() const
+{
+        return m_entries;
+}
+
+const void* DftMulDConstant::data() const
+{
+        return &m_data;
+}
+
+size_t DftMulDConstant::size() const
+{
+        return sizeof(m_data);
+}
 }
--- src/gpu/dft/vulkan/compute_memory.h
+++ src/gpu/dft/vulkan/compute_memory.h
@@ -378,4 +378,62 @@ public:
         void set(int32_t function_index, int32_t n1, int32_t n2, int32_t m1, int32_t m2, bool inverse, uint32_t group_size_x,
                  uint32_t group_size_y);
 };
+
+//
+
+class DftMulDMemory final
+{
+        static constexpr int SET_NUMBER = 0;
+
+        static constexpr int DIAGONAL_BINDING = 0;
+        static constexpr int DATA_BINDING = 1;
+
+        static std::vector<VkDescriptorSetLayoutBinding> descriptor_set_layout_bindings();
+
+        vulkan::DescriptorSetLayout m_descriptor_set_layout;
+        vulkan::Descriptors m_descriptors;
+
+public:
+        DftMulDMemory(const vulkan::Device& device);
+
+        DftMulDMemory(const DftMulDMemory&) = delete;
+        DftMulDMemory& operator=(const DftMulDMemory&) = delete;
+        DftMulDMemory& operator=(DftMulDMemory&&) = delete;
+
+        DftMulDMemory(DftMulDMemory&&) = default;
+        ~DftMulDMemory() = default;
+
+        //
+
+        static unsigned set_number();
+        VkDescriptorSetLayout descriptor_set_layout() const;
+        const VkDescriptorSet& descriptor_set() const;
+
+        //
+
+        void set_diagonal(const vulkan::BufferWithMemory& diagonal) const;
+        void set_data(const vulkan::BufferWithMemory& data) const;
+};
+
+class DftMulDConstant final : public vulkan::SpecializationConstant
+{
+        struct Data
+        {
+                uint32_t group_size_x;
+                uint32_t group_size_y;
+                int32_t rows;
+                int32_t columns;
+        } m_data;
+
+        std::vector<VkSpecializationMapEntry> m_entries;
+
+        const std::vector<VkSpecializationMapEntry>& entries() const override;
+        const void* data() const override;
+        size_t size() const override;
+
+public:
+        DftMulDConstant();
+
+        void set(uint32_t group_size_x, uint32_t group_size_y, int32_t rows, int32_t columns);
+};
 }
