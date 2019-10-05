82d48a0deed3515883ef142edacf27e566e7fafa
cppd <cppd@users.noreply.github.com>
2019-10-05 11:17:27 +0300

    Vulkan DFT show vertex

---
+++ src/gpu/dft/vulkan/show_vertex.cpp
@@ -0,0 +1,63 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "show_vertex.h"
+
+namespace gpu_vulkan
+{
+std::vector<VkVertexInputBindingDescription> DftShowVertex::binding_descriptions()
+{
+        std::vector<VkVertexInputBindingDescription> descriptions;
+
+        {
+                VkVertexInputBindingDescription d = {};
+                d.binding = 0;
+                d.stride = sizeof(DftShowVertex);
+                d.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+                descriptions.push_back(d);
+        }
+
+        return descriptions;
+}
+
+std::vector<VkVertexInputAttributeDescription> DftShowVertex::attribute_descriptions()
+{
+        std::vector<VkVertexInputAttributeDescription> descriptions;
+
+        {
+                VkVertexInputAttributeDescription d = {};
+                d.binding = 0;
+                d.location = 0;
+                d.format = VK_FORMAT_R32G32B32A32_SFLOAT;
+                d.offset = offsetof(DftShowVertex, position);
+
+                descriptions.push_back(d);
+        }
+        {
+                VkVertexInputAttributeDescription d = {};
+                d.binding = 0;
+                d.location = 1;
+                d.format = VK_FORMAT_R32G32_SFLOAT;
+                d.offset = offsetof(DftShowVertex, texture_coordinates);
+
+                descriptions.push_back(d);
+        }
+
+        return descriptions;
+}
+}
