9b4c13be5a304cda8c8641e296cdd3a1436c7774
cppd <cppd@users.noreply.github.com>
2019-11-28 09:09:28 +0300

    Vulkan optical flow compute program

---
+++ src/gpu/optical_flow/vulkan/compute_flow.cpp
@@ -0,0 +1,368 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "compute_flow.h"
+
+#include "shader_source.h"
+
+#include "graphics/vulkan/create.h"
+#include "graphics/vulkan/pipeline.h"
+
+namespace gpu_vulkan
+{
+std::vector<VkDescriptorSetLayoutBinding> OpticalFlowFlowMemory::descriptor_set_layout_bindings()
+{
+        std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = TOP_POINTS_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = POINTS_FLOW_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = POINTS_FLOW_GUESS_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = DATA_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = DX_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = DY_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = I_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = J_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+                b.pImmutableSamplers = nullptr;
+
+                bindings.push_back(b);
+        }
+
+        return bindings;
+}
+
+OpticalFlowFlowMemory::OpticalFlowFlowMemory(const vulkan::Device& device, VkDescriptorSetLayout descriptor_set_layout,
+                                             const std::unordered_set<uint32_t>& family_indices)
+        : m_descriptors(device, 1, descriptor_set_layout, descriptor_set_layout_bindings())
+{
+        std::vector<Variant<VkDescriptorBufferInfo, VkDescriptorImageInfo>> infos;
+        std::vector<uint32_t> bindings;
+
+        {
+                m_uniform_buffers.emplace_back(vulkan::BufferMemoryType::HostVisible, device, family_indices,
+                                               VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, sizeof(Data));
+
+                VkDescriptorBufferInfo buffer_info = {};
+                buffer_info.buffer = m_uniform_buffers.back();
+                buffer_info.offset = 0;
+                buffer_info.range = m_uniform_buffers.back().size();
+
+                infos.push_back(buffer_info);
+
+                bindings.push_back(DATA_BINDING);
+        }
+
+        m_descriptors.update_descriptor_set(0, bindings, infos);
+}
+
+unsigned OpticalFlowFlowMemory::set_number()
+{
+        return SET_NUMBER;
+}
+
+const VkDescriptorSet& OpticalFlowFlowMemory::descriptor_set() const
+{
+        return m_descriptors.descriptor_set(0);
+}
+
+void OpticalFlowFlowMemory::set_data(int point_count_x, int point_count_y, bool use_all_points, bool use_guess, int guess_kx,
+                                     int guess_ky, int guess_width) const
+{
+        Data d;
+        d.point_count_x = point_count_x;
+        d.point_count_y = point_count_y;
+        d.use_all_points = use_all_points ? 1 : 0;
+        d.use_guess = use_guess ? 1 : 0;
+        d.guess_kx = guess_kx;
+        d.guess_ky = guess_ky;
+        d.guess_width = guess_width;
+        vulkan::map_and_write_to_buffer(m_uniform_buffers[0], d);
+}
+
+void OpticalFlowFlowMemory::set_dx(const vulkan::ImageWithMemory& image) const
+{
+        ASSERT(image.usage() & VK_IMAGE_USAGE_STORAGE_BIT);
+        ASSERT(image.format() == VK_FORMAT_R32_SFLOAT);
+
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        image_info.imageView = image.image_view();
+
+        m_descriptors.update_descriptor_set(0, DX_BINDING, image_info);
+}
+
+void OpticalFlowFlowMemory::set_dy(const vulkan::ImageWithMemory& image) const
+{
+        ASSERT(image.usage() & VK_IMAGE_USAGE_STORAGE_BIT);
+        ASSERT(image.format() == VK_FORMAT_R32_SFLOAT);
+
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        image_info.imageView = image.image_view();
+
+        m_descriptors.update_descriptor_set(0, DY_BINDING, image_info);
+}
+
+void OpticalFlowFlowMemory::set_i(const vulkan::ImageWithMemory& image) const
+{
+        ASSERT(image.usage() & VK_IMAGE_USAGE_STORAGE_BIT);
+        ASSERT(image.format() == VK_FORMAT_R32_SFLOAT);
+
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
+        image_info.imageView = image.image_view();
+
+        m_descriptors.update_descriptor_set(0, I_BINDING, image_info);
+}
+
+void OpticalFlowFlowMemory::set_j(VkSampler sampler, const vulkan::ImageWithMemory& image) const
+{
+        ASSERT(image.usage() & VK_IMAGE_USAGE_SAMPLED_BIT);
+        VkDescriptorImageInfo image_info = {};
+        image_info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+        image_info.imageView = image.image_view();
+        image_info.sampler = sampler;
+
+        m_descriptors.update_descriptor_set(0, I_BINDING, image_info);
+}
+
+void OpticalFlowFlowMemory::set_top_points(const vulkan::BufferWithMemory& buffer) const
+{
+        ASSERT(buffer.usage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT));
+        VkDescriptorBufferInfo buffer_info = {};
+        buffer_info.buffer = buffer;
+        buffer_info.offset = 0;
+        buffer_info.range = buffer.size();
+
+        m_descriptors.update_descriptor_set(0, TOP_POINTS_BINDING, buffer_info);
+}
+
+void OpticalFlowFlowMemory::set_points_flow(const vulkan::BufferWithMemory& buffer) const
+{
+        ASSERT(buffer.usage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT));
+        VkDescriptorBufferInfo buffer_info = {};
+        buffer_info.buffer = buffer;
+        buffer_info.offset = 0;
+        buffer_info.range = buffer.size();
+
+        m_descriptors.update_descriptor_set(0, POINTS_FLOW_BINDING, buffer_info);
+}
+
+void OpticalFlowFlowMemory::set_points_flow_guess(const vulkan::BufferWithMemory& buffer) const
+{
+        ASSERT(buffer.usage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT));
+        VkDescriptorBufferInfo buffer_info = {};
+        buffer_info.buffer = buffer;
+        buffer_info.offset = 0;
+        buffer_info.range = buffer.size();
+
+        m_descriptors.update_descriptor_set(0, POINTS_FLOW_GUESS_BINDING, buffer_info);
+}
+
+//
+
+OpticalFlowFlowConstant::OpticalFlowFlowConstant()
+{
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 0;
+                entry.offset = offsetof(Data, local_size_x);
+                entry.size = sizeof(Data::local_size_x);
+                m_entries.push_back(entry);
+        }
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 1;
+                entry.offset = offsetof(Data, local_size_y);
+                entry.size = sizeof(Data::local_size_y);
+                m_entries.push_back(entry);
+        }
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 2;
+                entry.offset = offsetof(Data, radius);
+                entry.size = sizeof(Data::radius);
+                m_entries.push_back(entry);
+        }
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 3;
+                entry.offset = offsetof(Data, iteration_count);
+                entry.size = sizeof(Data::iteration_count);
+                m_entries.push_back(entry);
+        }
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 4;
+                entry.offset = offsetof(Data, stop_move_square);
+                entry.size = sizeof(Data::stop_move_square);
+                m_entries.push_back(entry);
+        }
+        {
+                VkSpecializationMapEntry entry = {};
+                entry.constantID = 5;
+                entry.offset = offsetof(Data, min_determinant);
+                entry.size = sizeof(Data::min_determinant);
+                m_entries.push_back(entry);
+        }
+}
+
+void OpticalFlowFlowConstant::set(uint32_t local_size_x, uint32_t local_size_y, int32_t radius, int32_t iteration_count,
+                                  float stop_move_square, float min_determinant)
+{
+        static_assert(std::is_same_v<decltype(m_data.local_size_x), decltype(local_size_x)>);
+        m_data.local_size_x = local_size_x;
+        static_assert(std::is_same_v<decltype(m_data.local_size_y), decltype(local_size_y)>);
+        m_data.local_size_y = local_size_y;
+        static_assert(std::is_same_v<decltype(m_data.radius), decltype(radius)>);
+        m_data.radius = radius;
+        static_assert(std::is_same_v<decltype(m_data.iteration_count), decltype(iteration_count)>);
+        m_data.iteration_count = iteration_count;
+        static_assert(std::is_same_v<decltype(m_data.stop_move_square), decltype(stop_move_square)>);
+        m_data.stop_move_square = stop_move_square;
+        static_assert(std::is_same_v<decltype(m_data.min_determinant), decltype(min_determinant)>);
+        m_data.min_determinant = min_determinant;
+}
+
+const std::vector<VkSpecializationMapEntry>& OpticalFlowFlowConstant::entries() const
+{
+        return m_entries;
+}
+
+const void* OpticalFlowFlowConstant::data() const
+{
+        return &m_data;
+}
+
+size_t OpticalFlowFlowConstant::size() const
+{
+        return sizeof(m_data);
+}
+
+//
+
+OpticalFlowFlowProgram::OpticalFlowFlowProgram(const vulkan::Device& device)
+        : m_device(device),
+          m_descriptor_set_layout(
+                  vulkan::create_descriptor_set_layout(device, OpticalFlowFlowMemory::descriptor_set_layout_bindings())),
+          m_pipeline_layout(
+                  vulkan::create_pipeline_layout(device, {OpticalFlowFlowMemory::set_number()}, {m_descriptor_set_layout})),
+          m_shader(device, optical_flow_flow_comp(), "main")
+{
+}
+
+VkDescriptorSetLayout OpticalFlowFlowProgram::descriptor_set_layout() const
+{
+        return m_descriptor_set_layout;
+}
+
+VkPipelineLayout OpticalFlowFlowProgram::pipeline_layout() const
+{
+        return m_pipeline_layout;
+}
+
+VkPipeline OpticalFlowFlowProgram::pipeline() const
+{
+        ASSERT(m_pipeline != VK_NULL_HANDLE);
+        return m_pipeline;
+}
+
+void OpticalFlowFlowProgram::create_pipeline(uint32_t local_size_x, uint32_t local_size_y, int32_t radius,
+                                             int32_t iteration_count, float stop_move_square, float min_determinant)
+{
+        m_constant.set(local_size_x, local_size_y, radius, iteration_count, stop_move_square, min_determinant);
+
+        vulkan::ComputePipelineCreateInfo info;
+        info.device = &m_device;
+        info.pipeline_layout = m_pipeline_layout;
+        info.shader = &m_shader;
+        info.constants = &m_constant;
+        m_pipeline = create_compute_pipeline(info);
+}
+
+void OpticalFlowFlowProgram::delete_pipeline()
+{
+        m_pipeline = vulkan::Pipeline();
+}
+}
---
+++ src/gpu/optical_flow/vulkan/compute_flow.h
@@ -0,0 +1,143 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "graphics/vulkan/buffers.h"
+#include "graphics/vulkan/constant.h"
+#include "graphics/vulkan/descriptor.h"
+#include "graphics/vulkan/objects.h"
+#include "graphics/vulkan/shader.h"
+
+#include <vector>
+
+namespace gpu_vulkan
+{
+class OpticalFlowFlowMemory final
+{
+        static constexpr int SET_NUMBER = 0;
+
+        static constexpr int TOP_POINTS_BINDING = 0;
+        static constexpr int POINTS_FLOW_BINDING = 1;
+        static constexpr int POINTS_FLOW_GUESS_BINDING = 2;
+        static constexpr int DATA_BINDING = 3;
+        static constexpr int DX_BINDING = 4;
+        static constexpr int DY_BINDING = 5;
+        static constexpr int I_BINDING = 6;
+        static constexpr int J_BINDING = 7;
+
+        vulkan::Descriptors m_descriptors;
+        std::vector<vulkan::BufferWithMemory> m_uniform_buffers;
+
+        struct Data
+        {
+                int32_t point_count_x;
+                int32_t point_count_y;
+                uint32_t use_all_points;
+                uint32_t use_guess;
+                int32_t guess_kx;
+                int32_t guess_ky;
+                int32_t guess_width;
+        };
+
+public:
+        static std::vector<VkDescriptorSetLayoutBinding> descriptor_set_layout_bindings();
+        static unsigned set_number();
+
+        OpticalFlowFlowMemory(const vulkan::Device& device, VkDescriptorSetLayout descriptor_set_layout,
+                              const std::unordered_set<uint32_t>& family_indices);
+
+        OpticalFlowFlowMemory(const OpticalFlowFlowMemory&) = delete;
+        OpticalFlowFlowMemory& operator=(const OpticalFlowFlowMemory&) = delete;
+        OpticalFlowFlowMemory& operator=(OpticalFlowFlowMemory&&) = delete;
+
+        OpticalFlowFlowMemory(OpticalFlowFlowMemory&&) = default;
+        ~OpticalFlowFlowMemory() = default;
+
+        //
+
+        const VkDescriptorSet& descriptor_set() const;
+
+        //
+
+        void set_data(int point_count_x, int point_count_y, bool use_all_points, bool use_guess, int guess_kx, int guess_ky,
+                      int guess_width) const;
+
+        void set_dx(const vulkan::ImageWithMemory& image) const;
+        void set_dy(const vulkan::ImageWithMemory& image) const;
+        void set_i(const vulkan::ImageWithMemory& image) const;
+        void set_j(VkSampler sampler, const vulkan::ImageWithMemory& image) const;
+
+        void set_top_points(const vulkan::BufferWithMemory& buffer) const;
+        void set_points_flow(const vulkan::BufferWithMemory& buffer) const;
+        void set_points_flow_guess(const vulkan::BufferWithMemory& buffer) const;
+};
+
+class OpticalFlowFlowConstant final : public vulkan::SpecializationConstant
+{
+        struct Data
+        {
+                uint32_t local_size_x;
+                uint32_t local_size_y;
+                int32_t radius;
+                int32_t iteration_count;
+                float stop_move_square;
+                float min_determinant;
+        } m_data;
+
+        std::vector<VkSpecializationMapEntry> m_entries;
+
+        const std::vector<VkSpecializationMapEntry>& entries() const override;
+        const void* data() const override;
+        size_t size() const override;
+
+public:
+        OpticalFlowFlowConstant();
+
+        void set(uint32_t local_size_x, uint32_t local_size_y, int32_t radius, int32_t iteration_count, float stop_move_square,
+                 float min_determinant);
+};
+
+class OpticalFlowFlowProgram final
+{
+        const vulkan::Device& m_device;
+
+        vulkan::DescriptorSetLayout m_descriptor_set_layout;
+        vulkan::PipelineLayout m_pipeline_layout;
+        OpticalFlowFlowConstant m_constant;
+        vulkan::ComputeShader m_shader;
+        vulkan::Pipeline m_pipeline;
+
+public:
+        OpticalFlowFlowProgram(const vulkan::Device& device);
+
+        OpticalFlowFlowProgram(const OpticalFlowFlowProgram&) = delete;
+        OpticalFlowFlowProgram& operator=(const OpticalFlowFlowProgram&) = delete;
+        OpticalFlowFlowProgram& operator=(OpticalFlowFlowProgram&&) = delete;
+
+        OpticalFlowFlowProgram(OpticalFlowFlowProgram&&) = default;
+        ~OpticalFlowFlowProgram() = default;
+
+        void create_pipeline(uint32_t local_size_x, uint32_t local_size_y, int32_t radius, int32_t iteration_count,
+                             float stop_move_square, float min_determinant);
+        void delete_pipeline();
+
+        VkDescriptorSetLayout descriptor_set_layout() const;
+        VkPipelineLayout pipeline_layout() const;
+        VkPipeline pipeline() const;
+};
+}
