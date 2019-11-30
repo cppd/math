7768b10af595b17ebaf120ffee9f3c9cf6b70edd
cppd <cppd@users.noreply.github.com>
2019-11-30 10:58:07 +0300

    Vulkan optical flow show shader

---
+++ src/gpu/optical_flow/vulkan/show_shader.cpp
@@ -0,0 +1,182 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "show_shader.h"
+
+#include "shader_source.h"
+
+#include "graphics/vulkan/create.h"
+#include "graphics/vulkan/pipeline.h"
+
+namespace gpu_vulkan
+{
+std::vector<VkDescriptorSetLayoutBinding> OpticalFlowShowMemory::descriptor_set_layout_bindings()
+{
+        std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = POINTS_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = FLOW_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+
+                bindings.push_back(b);
+        }
+        {
+                VkDescriptorSetLayoutBinding b = {};
+                b.binding = DATA_BINDING;
+                b.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+                b.descriptorCount = 1;
+                b.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+
+                bindings.push_back(b);
+        }
+
+        return bindings;
+}
+
+OpticalFlowShowMemory::OpticalFlowShowMemory(const vulkan::Device& device, VkDescriptorSetLayout descriptor_set_layout,
+                                             const std::unordered_set<uint32_t>& family_indices)
+        : m_descriptors(device, 1, descriptor_set_layout, descriptor_set_layout_bindings())
+{
+        std::vector<Variant<VkDescriptorBufferInfo, VkDescriptorImageInfo>> infos;
+        std::vector<uint32_t> bindings;
+
+        {
+                m_uniform_buffers.emplace_back(vulkan::BufferMemoryType::HostVisible, device, family_indices,
+                                               VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, sizeof(Data));
+
+                VkDescriptorBufferInfo buffer_info = {};
+                buffer_info.buffer = m_uniform_buffers.back();
+                buffer_info.offset = 0;
+                buffer_info.range = m_uniform_buffers.back().size();
+
+                infos.push_back(buffer_info);
+
+                bindings.push_back(DATA_BINDING);
+        }
+
+        m_descriptors.update_descriptor_set(0, bindings, infos);
+}
+
+unsigned OpticalFlowShowMemory::set_number()
+{
+        return SET_NUMBER;
+}
+
+const VkDescriptorSet& OpticalFlowShowMemory::descriptor_set() const
+{
+        return m_descriptors.descriptor_set(0);
+}
+
+void OpticalFlowShowMemory::set_points(const vulkan::BufferWithMemory& buffer) const
+{
+        ASSERT(buffer.usage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT));
+        VkDescriptorBufferInfo buffer_info = {};
+        buffer_info.buffer = buffer;
+        buffer_info.offset = 0;
+        buffer_info.range = buffer.size();
+
+        m_descriptors.update_descriptor_set(0, POINTS_BINDING, buffer_info);
+}
+
+void OpticalFlowShowMemory::set_flow(const vulkan::BufferWithMemory& buffer) const
+{
+        ASSERT(buffer.usage(VK_BUFFER_USAGE_STORAGE_BUFFER_BIT));
+        VkDescriptorBufferInfo buffer_info = {};
+        buffer_info.buffer = buffer;
+        buffer_info.offset = 0;
+        buffer_info.range = buffer.size();
+
+        m_descriptors.update_descriptor_set(0, FLOW_BINDING, buffer_info);
+}
+
+void OpticalFlowShowMemory::set_matrix(const mat4& matrix) const
+{
+        Data data;
+        data.matrix = transpose(to_matrix<float>(matrix));
+        vulkan::map_and_write_to_buffer(m_uniform_buffers[0], 0, data);
+}
+
+//
+
+OpticalFlowShowProgram::OpticalFlowShowProgram(const vulkan::Device& device)
+        : m_device(device),
+          m_descriptor_set_layout(
+                  vulkan::create_descriptor_set_layout(device, OpticalFlowShowMemory::descriptor_set_layout_bindings())),
+          m_pipeline_layout(
+                  vulkan::create_pipeline_layout(device, {OpticalFlowShowMemory::set_number()}, {m_descriptor_set_layout})),
+          m_vertex_shader(m_device, optical_flow_show_vert(), "main"),
+          m_fragment_shader(m_device, optical_flow_show_frag(), "main")
+{
+}
+
+VkDescriptorSetLayout OpticalFlowShowProgram::descriptor_set_layout() const
+{
+        return m_descriptor_set_layout;
+}
+
+VkPipelineLayout OpticalFlowShowProgram::pipeline_layout() const
+{
+        return m_pipeline_layout;
+}
+
+vulkan::Pipeline OpticalFlowShowProgram::create_pipeline(VkRenderPass render_pass, VkSampleCountFlagBits sample_count,
+                                                         VkPrimitiveTopology primitive_topology, unsigned x, unsigned y,
+                                                         unsigned width, unsigned height) const
+{
+        vulkan::GraphicsPipelineCreateInfo info;
+
+        info.device = &m_device;
+        info.render_pass = render_pass;
+        info.sub_pass = 0;
+        info.sample_count = sample_count;
+        info.sample_shading = false;
+        info.pipeline_layout = m_pipeline_layout;
+        info.viewport_x = x;
+        info.viewport_y = y;
+        info.viewport_width = width;
+        info.viewport_height = height;
+        info.primitive_topology = primitive_topology;
+        info.depth_bias = false;
+        info.color_blend = false;
+
+        const std::vector<const vulkan::Shader*> shaders = {&m_vertex_shader, &m_fragment_shader};
+        info.shaders = &shaders;
+
+        const std::vector<const vulkan::SpecializationConstant*> constants = {nullptr, nullptr};
+        info.constants = &constants;
+
+        const std::vector<VkVertexInputBindingDescription> binding_descriptions;
+        info.binding_descriptions = &binding_descriptions;
+
+        const std::vector<VkVertexInputAttributeDescription> attribute_descriptions;
+        info.attribute_descriptions = &attribute_descriptions;
+
+        return vulkan::create_graphics_pipeline(info);
+}
+}
---
+++ src/gpu/optical_flow/vulkan/show_shader.h
@@ -0,0 +1,99 @@
+/*
+Copyright (C) 2017-2019 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "com/matrix.h"
+#include "com/vec.h"
+#include "graphics/vulkan/buffers.h"
+#include "graphics/vulkan/descriptor.h"
+#include "graphics/vulkan/objects.h"
+#include "graphics/vulkan/shader.h"
+
+#include <unordered_set>
+#include <vector>
+
+namespace gpu_vulkan
+{
+class OpticalFlowShowMemory final
+{
+        static constexpr int SET_NUMBER = 0;
+
+        static constexpr int POINTS_BINDING = 0;
+        static constexpr int FLOW_BINDING = 1;
+        static constexpr int DATA_BINDING = 2;
+
+        vulkan::Descriptors m_descriptors;
+        std::vector<vulkan::BufferWithMemory> m_uniform_buffers;
+
+        struct Data
+        {
+                Matrix<4, 4, float> matrix;
+        };
+
+public:
+        static std::vector<VkDescriptorSetLayoutBinding> descriptor_set_layout_bindings();
+        static unsigned set_number();
+
+        OpticalFlowShowMemory(const vulkan::Device& device, VkDescriptorSetLayout descriptor_set_layout,
+                              const std::unordered_set<uint32_t>& family_indices);
+
+        OpticalFlowShowMemory(const OpticalFlowShowMemory&) = delete;
+        OpticalFlowShowMemory& operator=(const OpticalFlowShowMemory&) = delete;
+        OpticalFlowShowMemory& operator=(OpticalFlowShowMemory&&) = delete;
+
+        OpticalFlowShowMemory(OpticalFlowShowMemory&&) = default;
+        ~OpticalFlowShowMemory() = default;
+
+        //
+
+        const VkDescriptorSet& descriptor_set() const;
+
+        //
+
+        void set_points(const vulkan::BufferWithMemory& buffer) const;
+        void set_flow(const vulkan::BufferWithMemory& buffer) const;
+        void set_matrix(const mat4& matrix) const;
+};
+
+class OpticalFlowShowProgram final
+{
+        const vulkan::Device& m_device;
+
+        vulkan::DescriptorSetLayout m_descriptor_set_layout;
+        vulkan::PipelineLayout m_pipeline_layout;
+        vulkan::VertexShader m_vertex_shader;
+        vulkan::FragmentShader m_fragment_shader;
+
+public:
+        OpticalFlowShowProgram(const vulkan::Device& device);
+
+        OpticalFlowShowProgram(const OpticalFlowShowProgram&) = delete;
+        OpticalFlowShowProgram& operator=(const OpticalFlowShowProgram&) = delete;
+        OpticalFlowShowProgram& operator=(OpticalFlowShowProgram&&) = delete;
+
+        OpticalFlowShowProgram(OpticalFlowShowProgram&&) = default;
+        ~OpticalFlowShowProgram() = default;
+
+        vulkan::Pipeline create_pipeline(VkRenderPass render_pass, VkSampleCountFlagBits sample_count,
+                                         VkPrimitiveTopology primitive_topology, unsigned x, unsigned y, unsigned width,
+                                         unsigned height) const;
+
+        VkDescriptorSetLayout descriptor_set_layout() const;
+        VkPipelineLayout pipeline_layout() const;
+};
+}
