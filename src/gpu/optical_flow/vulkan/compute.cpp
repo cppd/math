08617b05b82400829396f71a83a92bd5e8036434
cppd <cppd@users.noreply.github.com>
2019-12-05 08:07:23 +0300

    Vulkan optical flow compute memory

--- src/gpu/optical_flow/vulkan/compute.cpp
+++ src/gpu/optical_flow/vulkan/compute.cpp
@@ -42,6 +42,8 @@ Chapter 5. Tracking Objects in Videos.
 
 #include <thread>
 
+constexpr VkFormat IMAGE_FORMAT = VK_FORMAT_R32_SFLOAT;
+
 // clang-format off
 constexpr std::initializer_list<vulkan::PhysicalDeviceFeatures> REQUIRED_DEVICE_FEATURES =
 {
@@ -59,15 +61,22 @@ class Impl final : public OpticalFlowCompute
         const vulkan::VulkanInstance& m_instance;
         const vulkan::Device& m_device;
 
-        // const vulkan::CommandPool& m_compute_command_pool;
-        // const vulkan::Queue& m_compute_queue;
+        const vulkan::CommandPool& m_compute_command_pool;
+        const vulkan::Queue& m_compute_queue;
         // const vulkan::CommandPool& m_transfer_command_pool;
         // const vulkan::Queue& m_transfer_queue;
 
+        std::array<std::vector<vulkan::ImageWithMemory>, 2> m_images;
+        std::vector<vulkan::ImageWithMemory> m_dx;
+        std::vector<vulkan::ImageWithMemory> m_dy;
+        std::vector<vulkan::BufferWithMemory> m_flow_buffers;
+
         OpticalFlowGrayscaleProgram m_grayscale_program;
+        std::array<OpticalFlowGrayscaleMemory, 2> m_grayscale_memory;
         vec2i m_grayscale_groups;
 
         OpticalFlowDownsampleProgram m_downsample_program;
+        std::array<std::vector<OpticalFlowDownsampleMemory>, 2> m_downsample_memory;
         std::vector<vec2i> m_downsample_groups;
 
         OpticalFlowSobelProgram m_sobel_program;
@@ -75,6 +84,69 @@ class Impl final : public OpticalFlowCompute
 
         OpticalFlowFlowProgram m_flow_program;
         std::vector<vec2i> m_flow_groups;
+
+        std::vector<vulkan::ImageWithMemory> create_images(const std::vector<vec2i>& sizes) const
+        {
+                std::vector<vulkan::ImageWithMemory> images;
+                images.reserve(sizes.size());
+
+                const bool storage = true;
+                const std::unordered_set<uint32_t> family_indices({m_compute_command_pool.family_index()});
+                const std::vector<VkFormat> formats({IMAGE_FORMAT});
+                for (const vec2i& s : sizes)
+                {
+                        images.emplace_back(m_device, m_compute_command_pool, m_compute_queue, family_indices, formats, s[0],
+                                            s[1], VK_IMAGE_LAYOUT_GENERAL, storage);
+                }
+
+                return images;
+        }
+
+        std::vector<vulkan::BufferWithMemory> create_flow_buffers(const std::vector<vec2i>& sizes) const
+        {
+                std::vector<vulkan::BufferWithMemory> buffers;
+                if (sizes.size() <= 1)
+                {
+                        return {};
+                }
+                buffers.reserve(sizes.size() - 1);
+
+                const std::unordered_set<uint32_t> family_indices({m_compute_command_pool.family_index()});
+                for (const vec2i& s : sizes)
+                {
+                        const int buffer_size = s[0] * s[1] * sizeof(vec2f);
+                        buffers.emplace_back(vulkan::BufferMemoryType::DeviceLocal, m_device, family_indices,
+                                             VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, buffer_size);
+                }
+
+                return buffers;
+        }
+
+        static std::array<OpticalFlowGrayscaleMemory, 2> create_grayscale_memory(const vulkan::Device& device,
+                                                                                 VkDescriptorSetLayout descriptor_set_layout)
+        {
+                return {OpticalFlowGrayscaleMemory(device, descriptor_set_layout),
+                        OpticalFlowGrayscaleMemory(device, descriptor_set_layout)};
+        }
+
+        static std::array<std::vector<OpticalFlowDownsampleMemory>, 2> create_downsample_memory(
+                const vulkan::Device& device, VkDescriptorSetLayout descriptor_set_layout,
+                const std::array<std::vector<vulkan::ImageWithMemory>, 2>& images)
+        {
+                ASSERT(images[0].size() == images[1].size());
+
+                std::array<std::vector<OpticalFlowDownsampleMemory>, 2> downsample_images;
+
+                for (unsigned i = 1; i < images[0].size(); ++i)
+                {
+                        downsample_images[0].emplace_back(device, descriptor_set_layout);
+                        downsample_images[1].emplace_back(device, descriptor_set_layout);
+                        downsample_images[0].back().set(images[0][i - 1], images[0][i]);
+                        downsample_images[1].back().set(images[1][i - 1], images[1][i]);
+                }
+
+                return downsample_images;
+        }
 
         void compute_commands(VkCommandBuffer /*command_buffer*/) const override
         {
@@ -98,15 +170,26 @@ class Impl final : public OpticalFlowCompute
                 const std::vector<vec2i> sizes =
                         optical_flow_pyramid_sizes(input.width(), input.height(), OPTICAL_FLOW_BOTTOM_IMAGE_SIZE);
 
+                m_images[0] = create_images(sizes);
+                m_images[1] = create_images(sizes);
+                m_dx = create_images(sizes);
+                m_dy = create_images(sizes);
+                m_flow_buffers = create_flow_buffers(sizes);
+
                 constexpr vec2i GROUPS = OPTICAL_FLOW_GROUP_SIZE;
                 constexpr int GROUPS_X = OPTICAL_FLOW_GROUP_SIZE[0];
                 constexpr int GROUPS_Y = OPTICAL_FLOW_GROUP_SIZE[1];
 
                 m_grayscale_groups = optical_flow_grayscale_groups(GROUPS, sizes);
                 m_grayscale_program.create_pipeline(GROUPS_X, GROUPS_Y, x, y, width, height);
+                m_grayscale_memory[0].set_src(sampler, input);
+                m_grayscale_memory[0].set_dst(m_images[0][0]);
+                m_grayscale_memory[1].set_src(sampler, input);
+                m_grayscale_memory[1].set_dst(m_images[1][0]);
 
                 m_downsample_groups = optical_flow_downsample_groups(GROUPS, sizes);
                 m_downsample_program.create_pipeline(GROUPS_X, GROUPS_Y);
+                m_downsample_memory = create_downsample_memory(m_device, m_downsample_program.descriptor_set_layout(), m_images);
 
                 m_sobel_groups = optical_flow_sobel_groups(GROUPS, sizes);
                 m_sobel_program.create_pipeline(GROUPS_X, GROUPS_Y);
@@ -126,6 +209,14 @@ class Impl final : public OpticalFlowCompute
                 m_downsample_program.delete_pipeline();
                 m_sobel_program.delete_pipeline();
                 m_flow_program.delete_pipeline();
+
+                m_images[0].clear();
+                m_images[1].clear();
+                m_dx.clear();
+                m_dy.clear();
+                m_flow_buffers.clear();
+                m_downsample_memory[0].clear();
+                m_downsample_memory[1].clear();
         }
 
         void reset() override
@@ -138,11 +229,12 @@ public:
              const vulkan::Queue& transfer_queue)
                 : m_instance(instance),
                   m_device(instance.device()),
-                  // m_compute_command_pool(compute_command_pool),
-                  // m_compute_queue(compute_queue),
+                  m_compute_command_pool(compute_command_pool),
+                  m_compute_queue(compute_queue),
                   // m_transfer_command_pool(transfer_command_pool),
                   // m_transfer_queue(transfer_queue),
                   m_grayscale_program(instance.device()),
+                  m_grayscale_memory(create_grayscale_memory(m_device, m_grayscale_program.descriptor_set_layout())),
                   m_downsample_program(instance.device()),
                   m_sobel_program(instance.device()),
                   m_flow_program(instance.device())
