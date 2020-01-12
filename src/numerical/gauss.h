3d8dae68566c60cb9286841904565a7de9db7481
cppd <cppd@users.noreply.github.com>
2020-01-12 12:07:53 +0300

    matrix and vector parameters

--- src/numerical/gauss.h
+++ src/numerical/gauss.h
@@ -26,18 +26,14 @@ Cambridge University Press.
 
 #pragma once
 
-#include "matrix.h"
-
+#include "com/math.h"
 #include "com/type/trait.h"
-#include "com/vec.h"
-
-#include <cmath>
 
 namespace numerical
 {
 namespace gauss_implementation
 {
-template <size_t N, typename T>
+template <size_t N, typename T, template <size_t, size_t, typename> typename Matrix>
 int find_pivot(const Matrix<N, N, T>& A, int column, int from_row)
 {
         T max = abs(A(from_row, column));
@@ -57,7 +53,8 @@ int find_pivot(const Matrix<N, N, T>& A, int column, int from_row)
 
 // input: A * x = b.
 // output: b = x; A = upper triangular.
-template <size_t Size, typename T>
+template <size_t Size, typename T, template <size_t, size_t, typename> typename Matrix,
+          template <size_t, typename> typename Vector>
 void solve_gauss(Matrix<Size, Size, T>* A_p, Vector<Size, T>* b_p)
 {
         static_assert(is_floating_point<T>);
@@ -103,7 +100,7 @@ void solve_gauss(Matrix<Size, Size, T>* A_p, Vector<Size, T>* b_p)
 
 // Тоже самое, что и для одного столбца b, только сразу для SizeB столбцов B.
 // Если B является единичной матрицей, то в B будет обратная к A матрица.
-template <size_t SizeA, size_t SizeB, typename T>
+template <size_t SizeA, size_t SizeB, typename T, template <size_t, size_t, typename> typename Matrix>
 void solve_gauss(Matrix<SizeA, SizeA, T>* A_p, Matrix<SizeA, SizeB, T>* B_p)
 {
         static_assert(is_floating_point<T>);
@@ -161,7 +158,7 @@ void solve_gauss(Matrix<SizeA, SizeA, T>* A_p, Matrix<SizeA, SizeB, T>* B_p)
         }
 }
 
-template <size_t Size, typename T>
+template <size_t Size, typename T, template <size_t, size_t, typename> typename Matrix>
 T determinant_gauss(Matrix<Size, Size, T>* A_p)
 {
         static_assert(is_floating_point<T>);
--- src/numerical/linear.h
+++ src/numerical/linear.h
@@ -18,6 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include "gauss.h"
+#include "matrix.h"
+
+#include "com/vec.h"
 
 namespace numerical
 {
