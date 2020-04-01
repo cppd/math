8f05e33ccc27323f6053eece1059bb6bcd11713f
cppd <cppd@users.noreply.github.com>
2020-04-01 08:01:08 +0300

    binary STL

--- src/model/mesh_utility/file/load_stl.cpp
+++ src/model/mesh_utility/file/load_stl.cpp
@@ -27,12 +27,61 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/utility/file/read.h>
 #include <src/utility/string/ascii.h>
 
+#include <bit>
+#include <cstring>
 #include <unordered_map>
 
 namespace mesh::file
 {
 namespace
 {
+static_assert(std::endian::native == std::endian::little, "Binary STL numbers must be little-endian");
+
+constexpr std::uintmax_t BINARY_HEADER_SIZE = 80 * sizeof(uint8_t);
+constexpr std::uintmax_t BINARY_NUMBER_OF_TRIANGLES_SIZE = sizeof(uint32_t);
+constexpr std::uintmax_t BINARY_BEGIN_SIZE = BINARY_HEADER_SIZE + BINARY_NUMBER_OF_TRIANGLES_SIZE;
+template <size_t N>
+constexpr std::uintmax_t BINARY_NORMAL_SIZE = N * sizeof(float);
+template <size_t N>
+constexpr std::uintmax_t BINARY_FACETS_SIZE = N* N * sizeof(float);
+
+template <size_t N>
+bool is_binary(const std::string& data)
+{
+        if (data.size() <= BINARY_BEGIN_SIZE)
+        {
+                return false;
+        }
+
+        uint32_t number_of_triangles;
+        std::memcpy(&number_of_triangles, &data[BINARY_HEADER_SIZE], sizeof(number_of_triangles));
+
+        const std::uintmax_t required_binary_size =
+                BINARY_BEGIN_SIZE + number_of_triangles * (BINARY_NORMAL_SIZE<N> + BINARY_FACETS_SIZE<N>);
+
+        if (data.size() < required_binary_size)
+        {
+                return false;
+        }
+
+        constexpr std::uintmax_t END_SIZE = sizeof(uint16_t);
+
+        if (data.size() > required_binary_size + END_SIZE)
+        {
+                return false;
+        }
+
+        for (char c : data)
+        {
+                if (!(ascii::is_print(c) || ascii::is_space(c)))
+                {
+                        return true;
+                }
+        }
+
+        return false;
+}
+
 template <typename Data, typename Word>
 void read_keyword(const Data& data, long long data_size, const Word& word, long long* i)
 {
@@ -130,6 +179,42 @@ void read_ascii_stl(
 }
 
 template <size_t N>
+void read_binary_stl(
+        const std::string& file_data,
+        ProgressRatio* progress,
+        const std::function<void(const std::array<Vector<N, float>, N>&)>& yield_facet)
+{
+        ASSERT(file_data.size() > BINARY_BEGIN_SIZE);
+
+        uint32_t facet_count;
+        std::memcpy(&facet_count, &file_data[BINARY_HEADER_SIZE], sizeof(facet_count));
+
+        ASSERT(BINARY_BEGIN_SIZE + facet_count * (BINARY_NORMAL_SIZE<N> + BINARY_FACETS_SIZE<N>) <= file_data.size());
+
+        const char* read_ptr = &file_data[BINARY_BEGIN_SIZE];
+        const double facet_count_reciprocal = 1.0 / facet_count;
+
+        std::array<Vector<N, float>, N> facet_vertices;
+        static_assert(sizeof(facet_vertices) == BINARY_FACETS_SIZE<N>);
+
+        read_ptr += BINARY_NORMAL_SIZE<N>;
+        for (unsigned facet = 0; facet < facet_count; ++facet)
+        {
+                if ((facet & 0xfff) == 0xfff)
+                {
+                        progress->set(facet * facet_count_reciprocal);
+                }
+
+                std::memcpy(&facet_vertices, read_ptr, sizeof(facet_vertices));
+                yield_facet(facet_vertices);
+
+                read_ptr += BINARY_NORMAL_SIZE<N> + BINARY_FACETS_SIZE<N>;
+        }
+
+        LOG("STL facet count: " + to_string(facet_count));
+}
+
+template <size_t N>
 std::unique_ptr<Mesh<N>> read_stl(const std::string& file_name, ProgressRatio* progress)
 {
         std::unordered_map<Vector<N, float>, unsigned> unique_vertices;
@@ -167,7 +252,14 @@ std::unique_ptr<Mesh<N>> read_stl(const std::string& file_name, ProgressRatio* p
         std::string file_data;
         read_binary_file(file_name, &file_data);
 
-        read_ascii_stl<N>(file_data, progress, yield_facet);
+        if (is_binary<N>(file_data))
+        {
+                read_binary_stl<N>(file_data, progress, yield_facet);
+        }
+        else
+        {
+                read_ascii_stl<N>(file_data, progress, yield_facet);
+        }
 
         set_center_and_length(&mesh);
 
