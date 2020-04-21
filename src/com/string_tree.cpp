27f7c56cb624efa7ffb40ae6b36d0e926d867687
cppd <cppd@users.noreply.github.com>
2020-04-21 21:38:16 +0300

    string tree

---
+++ src/com/string_tree.cpp
@@ -0,0 +1,70 @@
+/*
+Copyright (C) 2017-2020 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "string_tree.h"
+
+#include <stack>
+#include <tuple>
+
+constexpr size_t ROOT_NODE = 0;
+
+StringTree::StringTree()
+{
+        m_nodes.emplace_back("");
+}
+
+size_t StringTree::add(const std::string& s)
+{
+        m_nodes.emplace_back(s);
+        m_nodes[ROOT_NODE].children.push_back(m_nodes.size() - 1);
+        return m_nodes.size() - 1;
+}
+
+size_t StringTree::add(size_t parent, const std::string& s)
+{
+        m_nodes.emplace_back(s);
+        m_nodes[parent].children.push_back(m_nodes.size() - 1);
+        return m_nodes.size() - 1;
+}
+
+std::string StringTree::text(unsigned indent) const
+{
+        std::string s;
+
+        std::stack<std::tuple<size_t, unsigned>> stack({{ROOT_NODE, 0}});
+
+        while (!stack.empty())
+        {
+                auto index = std::get<0>(stack.top());
+                auto level = std::get<1>(stack.top());
+                stack.pop();
+
+                if (level > 0)
+                {
+                        s += '\n';
+                        s += std::string((level - 1) * indent, ' ');
+                        s += m_nodes[index].name;
+                }
+
+                for (auto iter = m_nodes[index].children.crbegin(); iter != m_nodes[index].children.crend(); ++iter)
+                {
+                        stack.push({*iter, level + 1});
+                }
+        }
+
+        return s;
+}
---
+++ src/com/string_tree.h
@@ -0,0 +1,43 @@
+/*
+Copyright (C) 2017-2020 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+class StringTree
+{
+        struct Node
+        {
+                std::string name;
+                std::vector<size_t> children;
+                explicit Node(const std::string& s) : name(s)
+                {
+                }
+        };
+
+        std::vector<Node> m_nodes;
+
+public:
+        StringTree();
+
+        size_t add(const std::string& s);
+        size_t add(size_t parent, const std::string& s);
+
+        std::string text(unsigned indent) const;
+};
