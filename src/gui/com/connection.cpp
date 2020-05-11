9e059185b4bae65f8f0e6745c9c2e5dbeeb15a0e
cppd <cppd@users.noreply.github.com>
2020-05-11 20:52:10 +0300

    connection class

---
+++ src/gui/com/connection.cpp
@@ -0,0 +1,61 @@
+/*
+Copyright (C) 2017-2020 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "connection.h"
+
+#include <QObject>
+
+namespace gui
+{
+void Connection::destroy() noexcept
+{
+        if (m_connection)
+        {
+                QObject::disconnect(*m_connection);
+        }
+}
+
+void Connection::move(Connection* from) noexcept
+{
+        m_connection = from->m_connection;
+        from->m_connection.reset();
+}
+
+Connection::Connection(QMetaObject::Connection&& connection) : m_connection(std::move(connection))
+{
+}
+
+Connection::~Connection()
+{
+        destroy();
+}
+
+Connection::Connection(Connection&& obj) noexcept
+{
+        move(&obj);
+}
+
+Connection& Connection::operator=(Connection&& obj) noexcept
+{
+        if (this != &obj)
+        {
+                destroy();
+                move(&obj);
+        }
+        return *this;
+}
+}
---
+++ src/gui/com/connection.h
@@ -0,0 +1,42 @@
+/*
+Copyright (C) 2017-2020 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <QMetaObject>
+#include <optional>
+
+namespace gui
+{
+class Connection final
+{
+        std::optional<QMetaObject::Connection> m_connection;
+
+        void destroy() noexcept;
+        void move(Connection* from) noexcept;
+
+public:
+        explicit Connection(QMetaObject::Connection&& connection);
+        ~Connection();
+
+        Connection(const Connection&) = delete;
+        Connection& operator=(const Connection&) = delete;
+
+        Connection(Connection&& obj) noexcept;
+        Connection& operator=(Connection&& obj) noexcept;
+};
+}
