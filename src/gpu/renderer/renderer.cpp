9ad874f581019aef82f5c43c3d4f4a5d216bfc2e
cppd <cppd@users.noreply.github.com>
2020-06-13 12:53:59 +0300

    renderer clear image

--- src/gpu/renderer/renderer.cpp
+++ src/gpu/renderer/renderer.cpp
@@ -81,6 +81,58 @@ ViewportTransform viewport_transform(const Region<2, int>& viewport)
         return t;
 }
 
+void clear_uint32_image_commands(const vulkan::ImageWithMemory& image, VkCommandBuffer command_buffer, uint32_t value)
+{
+        VkImageMemoryBarrier barrier = {};
+
+        barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.image = image.image();
+        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        barrier.subresourceRange.baseMipLevel = 0;
+        barrier.subresourceRange.levelCount = 1;
+        barrier.subresourceRange.baseArrayLayer = 0;
+        barrier.subresourceRange.layerCount = 1;
+
+        //
+
+        barrier.oldLayout = VK_IMAGE_LAYOUT_GENERAL;
+        barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+        barrier.srcAccessMask = 0;
+        barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+        vkCmdPipelineBarrier(
+                command_buffer, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, nullptr, 0,
+                nullptr, 1, &barrier);
+
+        //
+
+        VkClearColorValue clear_color;
+
+        ASSERT(image.format() == VK_FORMAT_R32_UINT);
+        clear_color.uint32[0] = value;
+
+        VkImageSubresourceRange range = barrier.subresourceRange;
+
+        // Для vkCmdClearColorImage нужно VK_IMAGE_USAGE_TRANSFER_DST_BIT
+        ASSERT((image.usage() & VK_IMAGE_USAGE_TRANSFER_DST_BIT) == VK_IMAGE_USAGE_TRANSFER_DST_BIT);
+
+        vkCmdClearColorImage(
+                command_buffer, image.image(), VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, &clear_color, 1, &range);
+
+        //
+
+        barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+        barrier.newLayout = VK_IMAGE_LAYOUT_GENERAL;
+        barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_SHADER_WRITE_BIT;
+
+        vkCmdPipelineBarrier(
+                command_buffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0, 0, nullptr, 0,
+                nullptr, 1, &barrier);
+}
+
 class Impl final : public Renderer
 {
         // Для получения текстуры для тени результат рисования находится в интервалах x(-1, 1) y(-1, 1) z(0, 1).
@@ -607,8 +659,8 @@ class Impl final : public Renderer
         std::function<void(VkCommandBuffer)> before_render_pass_commands()
         {
                 return [this](VkCommandBuffer command_buffer) {
-                        ASSERT(m_object_image->format() == VK_FORMAT_R32_UINT);
-                        m_object_image->clear_commands(command_buffer, VK_IMAGE_LAYOUT_GENERAL);
+                        uint32_t clear_value = 0;
+                        clear_uint32_image_commands(*m_object_image, command_buffer, clear_value);
                 };
         }
 
