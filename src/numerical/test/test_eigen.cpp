9d40a4be6481051a839516b00d62c70ec2ae4646
cppd <cppd@users.noreply.github.com>
2020-09-23 09:57:12 +0300

    edit tests

--- src/numerical/test/test_eigen.cpp
+++ src/numerical/test/test_eigen.cpp
@@ -21,6 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <src/com/error.h>
 #include <src/com/log.h>
+#include <src/com/type/limit.h>
+#include <src/utility/random/engine.h>
+
+#include <random>
 
 namespace numerical
 {
@@ -42,12 +46,28 @@ bool equal(const Vector<3, double>& a, const Vector<3, double>& b)
         }
         return true;
 }
+
+template <size_t N, typename T>
+Matrix<N, N, T> random_symmetric_matrix(T min, T max)
+{
+        Matrix<N, N, T> m;
+        RandomEngineWithSeed<std::mt19937_64> random_engine;
+        std::uniform_real_distribution<T> urd(min, max);
+        for (unsigned i = 0; i < N; ++i)
+        {
+                m(i, i) = urd(random_engine);
+                for (unsigned j = i + 1; j < N; ++j)
+                {
+                        T v = urd(random_engine);
+                        m(i, j) = v;
+                        m(j, i) = v;
+                }
+        }
+        return m;
 }
 
-void test_eigen()
+void test_eigen_defined()
 {
-        LOG("Test eigenvalues and eigenvectors");
-
         constexpr double TOLERANCE = 1e-10;
 
         Matrix<3, 3, double> a;
@@ -84,7 +104,66 @@ void test_eigen()
         {
                 error("Eigenvalues error");
         }
+}
 
+template <size_t N, typename T>
+void test_eigen_random()
+{
+        constexpr T TOLERANCE = limits<T>::epsilon() * 100;
+
+        Matrix<N, N, T> matrix = random_symmetric_matrix<N, T>(-1, 1);
+
+        Vector<N, T> eigenvalues;
+        std::array<Vector<N, T>, N> eigenvectors;
+        try
+        {
+                numerical::eigen(matrix, TOLERANCE, &eigenvalues, &eigenvectors);
+        }
+        catch (const numerical::EigenException& e)
+        {
+                error(e.what());
+        }
+
+        T trace = 0;
+        T sum = 0;
+        T product = 1;
+        T det = matrix.determinant();
+        for (unsigned i = 0; i < N; ++i)
+        {
+                trace += matrix(i, i);
+                sum += eigenvalues[i];
+                product *= eigenvalues[i];
+        }
+        if (std::abs((trace - sum) / std::max(std::abs(trace), std::abs(sum))) > T(0.01))
+        {
+                error("Eigenvalues error, trace " + to_string(trace) + " and sum " + to_string(sum) + " are not equal");
+        }
+        if (std::abs((det - product) / std::max(std::abs(det), std::abs(product))) > T(0.01))
+        {
+                error("Eigenvalues error, determinant " + to_string(det) + " and product " + to_string(product)
+                      + " are not equal");
+        }
+}
+
+template <typename T>
+void test_eigen_random()
+{
+        test_eigen_random<3, T>();
+        test_eigen_random<4, T>();
+        test_eigen_random<5, T>();
+}
+}
+
+void test_eigen()
+{
+        LOG("Test eigenvalues and eigenvectors");
+        test_eigen_defined();
+        for (int i = 0; i < 100; ++i)
+        {
+                test_eigen_random<float>();
+                test_eigen_random<double>();
+                test_eigen_random<long double>();
+        }
         LOG("Test eigenvalues and eigenvectors passed");
 }
 }
