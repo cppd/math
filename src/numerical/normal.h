a198e8d0e88a7b7a7f12607f32a4d7d000085f7c
cppd <cppd@users.noreply.github.com>
2020-09-24 09:51:27 +0300

    point normals

---
+++ src/numerical/normal.h
@@ -0,0 +1,110 @@
+/*
+Copyright (C) 2017-2020 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ Jakob Andreas Bærentzen, Jens Gravesen, François Anton, Henrik Aanæs.
+ Guide to Computational Geometry Processing. Foundations, Algorithms, and Methods.
+ Springer-Verlag London, 2012.
+
+ 17.1.1 Computing Point Normals.
+*/
+
+#pragma once
+
+#include "eigen.h"
+#include "matrix.h"
+#include "orthogonal.h"
+#include "vec.h"
+
+#include <src/com/error.h>
+#include <src/com/print.h>
+#include <src/com/type/limit.h>
+
+#include <vector>
+
+namespace numerical
+{
+template <size_t N, typename T>
+Vector<N, T> point_normal(const std::vector<Vector<N, T>>& points)
+{
+        if (points.size() < N)
+        {
+                error("At least " + to_string(N) + " points are required for point normal");
+        }
+
+        Vector<N, T> p0(0);
+        for (const Vector<N, T>& p : points)
+        {
+                p0 += p;
+        }
+        p0 /= points.size();
+
+        Matrix<N, N, T> covariance_matrix;
+        for (size_t i = 0; i < N; ++i)
+        {
+                covariance_matrix.row(i) = Vector<N, T>(0);
+        }
+        for (Vector<N, T> p : points)
+        {
+                p -= p0;
+                for (size_t i = 0; i < N; ++i)
+                {
+                        for (size_t j = i; j < N; ++j)
+                        {
+                                covariance_matrix(i, j) += p[i] * p[j];
+                        }
+                }
+        }
+        T max = limits<T>::lowest();
+        for (size_t i = 0; i < N - 1; ++i)
+        {
+                for (size_t j = i + 1; j < N; ++j)
+                {
+                        const T v = covariance_matrix(i, j);
+                        max = std::max(std::abs(v), max);
+                        covariance_matrix(j, i) = v;
+                }
+        }
+
+        Vector<N, T> eigenvalues;
+        std::array<Vector<N, T>, N> eigenvectors;
+        eigen(covariance_matrix, max * (100 * limits<T>::epsilon()), &eigenvalues, &eigenvectors);
+
+        size_t min_i = 0;
+        T min = eigenvalues[0];
+        for (size_t i = 1; i < N; ++i)
+        {
+                if (min > eigenvalues[i])
+                {
+                        min_i = i;
+                        min = eigenvalues[i];
+                }
+        }
+        std::array<Vector<N, T>, N - 1> vectors;
+        size_t n = 0;
+        for (size_t i = 0; i < min_i; ++i)
+        {
+                vectors[n++] = eigenvectors[i];
+        }
+        for (size_t i = min_i + 1; i < N; ++i)
+        {
+                vectors[n++] = eigenvectors[i];
+        }
+
+        return ortho_nn(vectors).normalized();
+}
+}
