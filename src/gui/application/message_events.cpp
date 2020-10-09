b6c25df64c46b87a8dbafbaa3b1e680047757bd2
cppd <cppd@users.noreply.github.com>
2020-10-09 09:53:52 +0300

    refactoring

--- src/gui/application/message_events.cpp
+++ src/gui/application/message_events.cpp
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../dialogs/message.h"
 
 #include <src/com/log.h>
-#include <src/com/variant.h>
 
 namespace gui::application
 {
