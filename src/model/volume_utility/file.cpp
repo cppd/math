c204f40fb503de4ceb16adc0d5723fb6b1782878
cppd <cppd@users.noreply.github.com>
2020-12-09 09:48:43 +0300

    volume load

--- src/model/volume_utility/file.cpp
+++ src/model/volume_utility/file.cpp
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/com/arrays.h>
 #include <src/com/error.h>
 #include <src/com/print.h>
-#include <src/image/alpha.h>
 #include <src/image/file.h>
 #include <src/image/flip.h>
 #include <src/utility/file/path.h>
@@ -352,12 +351,6 @@ std::enable_if_t<N >= 3, image::Image<N>> load(const std::filesystem::path& path
         load_from_images(path, image.color_format, image.size, image.pixels, progress, &current, image_count);
 
         image::flip_vertically(&image);
-
-        if (image::format_component_count(image.color_format) == 3)
-        {
-                constexpr float alpha = 1.0f / 255;
-                image::add_alpha(&image, alpha);
-        }
 
         return image;
 }
