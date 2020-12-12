eb934a508674f269e8af113a335b8f9ce0c8e3d9
cppd <cppd@users.noreply.github.com>
2020-12-12 10:34:45 +0300

    color conversion

--- src/color/conversion.cpp
+++ src/color/conversion.cpp
@@ -15,125 +15,21 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#if 0
+
 #include "conversion.h"
 
-#include <algorithm>
-#include <array>
-#include <cmath>
-
-#define USE_COLOR_LOOKUP_TABLES
+#include <iomanip>
+#include <sstream>
 
 namespace color
 {
 namespace
 {
-constexpr unsigned char MAX8 = (1u << 8) - 1;
-
-// Функции работают после преобразований с плавающей точкой,
-// поэтому параметр функций находится в интервале [0, 1]
-template <typename Float>
-constexpr unsigned char float_to_uint8(Float c)
-{
-        static_assert(std::is_floating_point_v<Float>);
-
-        ASSERT(c >= 0 && c <= 1);
-
-        return c * Float(MAX8) + Float(0.5);
-}
-
-// Функция работает до преобразований с плавающей точкой,
-// поэтому если параметр больше MAX8 при unsigned char не 8 бит,
-// то значение больше 1 будет далее правильно обработано
-template <typename Float, typename UInt8>
-constexpr Float uint8_to_float(UInt8 c)
-{
-        static_assert(std::is_same_v<UInt8, unsigned char>);
-        static_assert(std::is_floating_point_v<Float>);
-
-        ASSERT(limits<unsigned char>::max() == MAX8 || c <= MAX8);
-
-        return Float(c) / Float(MAX8);
-}
-
-#if defined(USE_COLOR_LOOKUP_TABLES)
-
-// clang-format off
 template <typename T>
-constexpr std::array<T, 256> SRGB_UINT8_TO_RGB_FLOAT_LOOKUP_TABLE =
+T srgb_float_to_linear_float(T c)
 {
-        0.000000000000000000000e+00L, 3.035269835488374933221e-04L, 6.070539670976749866442e-04L, 9.105809506465124800192e-04L,
-        1.214107934195349973288e-03L, 1.517634917744187466610e-03L, 1.821161901293024960038e-03L, 2.124688884841862453466e-03L,
-        2.428215868390699946577e-03L, 2.731742851939537439899e-03L, 3.035269835488374933221e-03L, 3.346535763899159701740e-03L,
-        3.676507324047436250104e-03L, 4.024717018496305703786e-03L, 4.391442037410294362864e-03L, 4.776953480693728742560e-03L,
-        5.181516702338386918977e-03L, 5.605391624202722681259e-03L, 6.048833022857054623069e-03L, 6.512090792594473683855e-03L,
-        6.995410187265387874825e-03L, 7.499032043226172490917e-03L, 8.023192985384995986786e-03L, 8.568125618069304729133e-03L,
-        9.134058702220789062427e-03L, 9.721217320237846692000e-03L, 1.032982302962694028285e-02L, 1.096009400648824435342e-02L,
-        1.161224517974388505224e-02L, 1.228648835691587084055e-02L, 1.298303234217301197175e-02L, 1.370208304728968735728e-02L,
-        1.444384359609254527088e-02L, 1.520851442291271006541e-02L, 1.599629336550963283536e-02L, 1.680737575288738151130e-02L,
-        1.764195448838408212447e-02L, 1.850022012837969387930e-02L, 1.938236095693572819910e-02L, 2.028856305665239681858e-02L,
-        2.121901037600356125583e-02L, 2.217388479338738207239e-02L, 2.315336617811041106959e-02L, 2.415763244850475524253e-02L,
-        2.518685962736162951214e-02L, 2.624122189484989761741e-02L, 2.732089163907489697962e-02L, 2.842603950442079611742e-02L,
-        2.955683443780880517082e-02L, 3.071344373299363014526e-02L, 3.189603307301152462105e-02L, 3.310476657088505491919e-02L,
-        3.433980680868217489978e-02L, 3.560131487502033128772e-02L, 3.688945040110002620648e-02L, 3.820437159534649299415e-02L,
-        3.954623527673284391143e-02L, 4.091519690685317913167e-02L, 4.231141062080966491429e-02L, 4.373502925697345946219e-02L,
-        4.518620438567555543287e-02L, 4.666508633688008942386e-02L, 4.817182422688941560764e-02L, 4.970656598412722908248e-02L,
-        5.126945837404323443024e-02L, 5.286064702318026650339e-02L, 5.448027644244236749921e-02L, 5.612849004960009094957e-02L,
-        5.780543019106722560816e-02L, 5.951123816298119766183e-02L, 6.124605423161760679287e-02L, 6.301001765316766992016e-02L,
-        6.480326669290577642395e-02L, 6.662593864377289174704e-02L, 6.847816984440017453270e-02L, 7.036009569659588876742e-02L,
-        7.227185068231748976935e-02L, 7.421356838014963640065e-02L, 7.618538148130782442928e-02L, 7.818742180518634387340e-02L,
-        8.021982031446833113364e-02L, 8.228270712981481068343e-02L, 8.437621154414879689061e-02L, 8.650046203654975106767e-02L,
-        8.865558628577295809613e-02L, 9.084171118340769874569e-02L, 9.305896284668744422298e-02L, 9.530746663096468708650e-02L,
-        9.758734714186244412718e-02L, 9.989872824711392009867e-02L, 1.022417330881013050533e-01L, 1.046164840911041891053e-01L,
-        1.070231029782676162888e-01L, 1.094617107782993612605e-01L, 1.119324278369055981831e-01L, 1.144353738269737377680e-01L,
-        1.169706677585108359527e-01L, 1.195384279883456238522e-01L, 1.221387722296018733253e-01L, 1.247718175609504930528e-01L,
-        1.274376804356474479510e-01L, 1.301364766903643059592e-01L, 1.328683215538179432261e-01L, 1.356333296552056775673e-01L,
-        1.384316150324518525182e-01L, 1.412632911402716578194e-01L, 1.441284708580577474785e-01L, 1.470272664975950021389e-01L,
-        1.499597898106085777051e-01L, 1.529261519961501878192e-01L, 1.559264637078273809881e-01L, 1.589608350608803962183e-01L,
-        1.620293756391110108928e-01L, 1.651321945016676331290e-01L, 1.682694001896907351956e-01L, 1.714411007328225774657e-01L,
-        1.746474036555850302985e-01L, 1.778884159836291657171e-01L, 1.811642442498601614188e-01L, 1.844749945004409353764e-01L,
-        1.878207723006778106096e-01L, 1.912016827407913955141e-01L, 1.946178304415757566442e-01L, 1.980693195599488554647e-01L,
-        2.015562537943971209453e-01L, 2.050787363903169335843e-01L, 2.086368701452557035690e-01L, 2.122307574140551380577e-01L,
-        2.158605001138992068193e-01L, 2.195261997292692340985e-01L, 2.232279573168084659831e-01L, 2.269658735100983873055e-01L,
-        2.307400485243489893618e-01L, 2.345505821610051203150e-01L, 2.383975738122709829689e-01L, 2.422811224655547800762e-01L,
-        2.462013267078354456538e-01L, 2.501582847299533404121e-01L, 2.541520943308267327344e-01L, 2.581828529215958305818e-01L,
-        2.622506575296960767431e-01L, 2.663556048028623683709e-01L, 2.704977910130658124170e-01L, 2.746773120603845802241e-01L,
-        2.788942634768103795363e-01L, 2.831487404299920172238e-01L, 2.874408377269174822680e-01L, 2.917706498175359396889e-01L,
-        2.961382707983209837800e-01L, 3.005437944157764616694e-01L, 3.049873140698861412258e-01L, 3.094689228175084608717e-01L,
-        3.139887133757175645247e-01L, 3.185467781250917916017e-01L, 3.231432091129507591979e-01L, 3.277780980565421425162e-01L,
-        3.324515363461792298344e-01L, 3.371636150483302978866e-01L, 3.419144249086608268343e-01L, 3.467040563550295452478e-01L,
-        3.515325995004392699284e-01L, 3.564001441459434795986e-01L, 3.613067797835095363895e-01L, 3.662525955988394455527e-01L,
-        3.712376804741490202708e-01L, 3.762621229909062960518e-01L, 3.813260114325300174270e-01L, 3.864294337870489986651e-01L,
-        3.915724777497231392381e-01L, 3.967552307256268558783e-01L, 4.019777798321956732296e-01L, 4.072402119017366959024e-01L,
-        4.125426134839036687238e-01L, 4.178850708481373115908e-01L, 4.232676699860716011876e-01L, 4.286904966139066531226e-01L,
-        4.341536361747488432974e-01L, 4.396571738409187916268e-01L, 4.452011945162278157761e-01L, 4.507857828382234482479e-01L,
-        4.564110231804045960779e-01L, 4.620769996544069082548e-01L, 4.677837961121589029664e-01L, 4.735314961480093933854e-01L,
-        4.793201831008267387805e-01L, 4.851499400560704343779e-01L, 4.910208498478355425277e-01L, 4.969329950608704555594e-01L,
-        5.028864580325684695717e-01L, 5.088813208549336376124e-01L, 5.149176653765213596469e-01L, 5.209955732043541569822e-01L,
-        5.271151257058130679438e-01L, 5.332764040105050926599e-01L, 5.394794890121071050202e-01L, 5.457244613701866394698e-01L,
-        5.520114015119999534135e-01L, 5.583403896342677554345e-01L, 5.647115057049289819978e-01L, 5.711248294648729958187e-01L,
-        5.775804404296505730095e-01L, 5.840784178911640359762e-01L, 5.906188409193368823178e-01L, 5.972017883637632535278e-01L,
-        6.038273388553375779753e-01L, 6.104955708078647171663e-01L, 6.172065624196509364251e-01L, 6.239603916750760149589e-01L,
-        6.307571363461468037575e-01L, 6.375968739940325323611e-01L, 6.444796819705821607502e-01L, 6.514056374198240653012e-01L,
-        6.583748172794483428114e-01L, 6.653872982822720099890e-01L, 6.724431569576873702157e-01L, 6.795424696330938147846e-01L,
-        6.866853124353133194725e-01L, 6.938717612919898923172e-01L, 7.011018919329732238107e-01L, 7.083757798916867844835e-01L,
-        7.156935005064806126860e-01L, 7.230551289219690262147e-01L, 7.304607400903534919143e-01L, 7.379104087727308788252e-01L,
-        7.454042095403873175721e-01L, 7.529422167760778855433e-01L, 7.605245046752923303106e-01L, 7.681511472475070425475e-01L,
-        7.758222183174234838458e-01L, 7.835377915261932717456e-01L, 7.912979403326301202216e-01L, 7.991027380144088296471e-01L,
-        8.069522576692515177573e-01L, 8.148465722161012783674e-01L, 8.227857543962834518340e-01L, 8.307698767746546871828e-01L,
-        8.387990117407399734959e-01L, 8.468732315098578138676e-01L, 8.549926081242337124735e-01L, 8.631572134541021422633e-01L,
-        8.713671191987971577494e-01L, 8.796223968878318141677e-01L, 8.879231178819665502725e-01L, 8.962693533742666941993e-01L,
-        9.046611743911492395744e-01L, 9.130986517934190457749e-01L, 9.215818562772946101686e-01L, 9.301108583754235543668e-01L,
-        9.386857284578879684692e-01L, 9.473065367331997523514e-01L, 9.559733532492860923939e-01L, 9.646862478944652077124e-01L,
-        9.734452903984124977300e-01L, 9.822505503331172218998e-01L, 9.911020971138298405433e-01L, 1.000000000000000000000e+00L
-};
-// clang-format on
-
-#else
-
-template <typename T>
-T srgb_to_linear(T c)
-{
-        static_assert(std::is_floating_point_v<T>);
+        static_assert(std::is_same_v<T, long double>);
 
         if (c >= 1)
         {
@@ -149,184 +45,45 @@ T srgb_to_linear(T c)
         }
         return 0;
 }
-
-#endif
-
-template <typename T>
-T linear_to_srgb(T c)
-{
-        static_assert(std::is_floating_point_v<T>);
-
-        if (c >= 1)
-        {
-                return 1;
-        }
-        if (c >= T(0.0031308))
-        {
-                return T(1.055) * std::pow(c, T(1) / T(2.4)) - T(0.055);
-        }
-        if (c > 0)
-        {
-                return c * T(12.92);
-        }
-        return 0;
 }
 
-template <typename T>
-T linear_luminance(T red, T green, T blue)
-{
-        static_assert(std::is_floating_point_v<T>);
-
-        return T(0.2126) * red + T(0.7152) * green + T(0.0722) * blue;
-}
-}
-
-#if defined(USE_COLOR_LOOKUP_TABLES)
-
-template <typename T, typename UInt8>
-T srgb_uint8_to_linear_float(UInt8 c)
-{
-        static_assert(std::is_same_v<UInt8, unsigned char>);
-        static_assert(std::is_floating_point_v<T>);
-
-        if constexpr (limits<unsigned char>::max() == MAX8)
-        {
-                return SRGB_UINT8_TO_RGB_FLOAT_LOOKUP_TABLE<T>[c];
-        }
-        else
-        {
-                return SRGB_UINT8_TO_RGB_FLOAT_LOOKUP_TABLE<T>[std::min(c, MAX8)];
-        }
-}
-
-#else
-
-template <typename T, typename UInt8>
-T srgb_uint8_to_linear_float(UInt8 c)
-{
-        return srgb_to_linear(uint8_to_float<T>(c));
-}
-
-#endif
-
-template <typename T, typename UInt8>
-T linear_uint8_to_linear_float(UInt8 c)
-{
-        static_assert(std::is_same_v<UInt8, unsigned char>);
-        if constexpr (limits<unsigned char>::max() != MAX8)
-        {
-                c = std::min(c, MAX8);
-        }
-        return uint8_to_float<T>(c);
-}
-
-//
-
-template <typename T>
-T linear_float_to_srgb_float(T c)
-{
-        return linear_to_srgb<T>(c);
-}
-
-//
-
-template <typename T>
-unsigned char linear_float_to_srgb_uint8(T c)
-{
-        return float_to_uint8(linear_to_srgb(c));
-}
-
-template <typename T>
-unsigned char linear_float_to_linear_uint8(T c)
-{
-        return float_to_uint8(c);
-}
-
-//
-
-template <typename T>
-T linear_float_to_linear_luminance(T red, T green, T blue)
-{
-        return linear_luminance(red, green, blue);
-}
-
-//
-
-template float srgb_uint8_to_linear_float(unsigned char c);
-template double srgb_uint8_to_linear_float(unsigned char c);
-template long double srgb_uint8_to_linear_float(unsigned char c);
-
-template float linear_uint8_to_linear_float(unsigned char c);
-template double linear_uint8_to_linear_float(unsigned char c);
-template long double linear_uint8_to_linear_float(unsigned char c);
-
-//
-
-template float linear_float_to_srgb_float(float c);
-template double linear_float_to_srgb_float(double c);
-template long double linear_float_to_srgb_float(long double c);
-
-//
-
-template unsigned char linear_float_to_srgb_uint8(float c);
-template unsigned char linear_float_to_srgb_uint8(double c);
-template unsigned char linear_float_to_srgb_uint8(long double c);
-
-template unsigned char linear_float_to_linear_uint8(float c);
-template unsigned char linear_float_to_linear_uint8(double c);
-template unsigned char linear_float_to_linear_uint8(long double c);
-
-//
-
-template float linear_float_to_linear_luminance(float red, float green, float blue);
-template double linear_float_to_linear_luminance(double red, double green, double blue);
-template long double linear_float_to_linear_luminance(long double red, long double green, long double blue);
-}
-
-#if !defined(USE_COLOR_LOOKUP_TABLES)
-//Функции для создания таблиц
-#include <src/utility/string/str.h>
-#include <src/com/type/name.h>
-#include <src/com/type/limit.h>
-#include <iomanip>
-#include <sstream>
-namespace color
-{
 std::string lookup_table_float()
 {
-        using T = long double;
-        std::string suffix = to_upper(floating_point_suffix<T>());
         std::ostringstream oss;
-        oss << std::setprecision(limits<T>::max_digits10);
+        oss << std::setprecision(limits<float>::max_digits10);
         oss << std::scientific;
         oss << "// clang-format off\n";
-        oss << "template <typename T>\n";
-        oss << "constexpr T SRGB_UINT8_TO_RGB_FLOAT_LOOKUP_TABLE[256] =\n";
+        oss << "inline constexpr std::array<float, 256> SRGB_UINT8_TO_RGB_FLOAT =\n";
         oss << "{";
-        for (unsigned i = 0; i <= MAX8; ++i)
+        for (unsigned i = 0; i <= 255; ++i)
         {
                 oss << ((i != 0) ? "," : "");
                 oss << (((i & 0b11) != 0) ? " " : "\n" + std::string(8, ' '));
-                oss << srgb_uint8_to_linear_float<T>(static_cast<unsigned char>(i)) << suffix;
+                const unsigned char srgb_int = i;
+                const long double srgb_float = implementation::uint8_to_float<long double>(srgb_int);
+                const float linear_float = srgb_float_to_linear_float(srgb_float);
+                oss << linear_float;
         }
         oss << "\n};\n";
         oss << "// clang-format on\n";
         return oss.str();
 }
+
 std::string lookup_table_uint16()
 {
         std::ostringstream oss;
         oss << std::setfill(' ');
         oss << "// clang-format off\n";
-        oss << "constexpr std::uint_least16_t SRGB_UINT8_TO_RGB_UINT16_LOOKUP_TABLE[256] =\n";
+        oss << "inline constexpr std::array<std::uint16_t, 256> SRGB_UINT8_TO_RGB_UINT16 =\n";
         oss << "{";
-        for (unsigned i = 0; i <= MAX8; ++i)
+        for (unsigned i = 0; i <= 255; ++i)
         {
                 oss << ((i != 0) ? "," : "");
                 oss << (((i % 16) != 0) ? " " : "\n" + std::string(8, ' '));
-                oss << std::setw(5)
-                    << static_cast<unsigned>(
-                               float_to_uint16(srgb_to_linear(uint8_to_float<float>(static_cast<unsigned char>(i)))));
+                const unsigned char srgb_int = i;
+                const long double srgb_float = implementation::uint8_to_float<long double>(srgb_int);
+                const float linear_float = srgb_float_to_linear_float(srgb_float);
+                oss << std::setw(5) << static_cast<unsigned>(float_to_uint16(linear_float));
         }
         oss << "\n};\n";
         oss << "// clang-format on\n";
--- src/color/conversion.h
+++ src/color/conversion.h
@@ -20,50 +20,189 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/com/error.h>
 #include <src/com/type/limit.h>
 
-#include <cstdint>
+#include <array>
+#include <cmath>
 
 namespace color
 {
+namespace implementation
+{
 template <typename T, typename UInt8>
-T srgb_uint8_to_linear_float(UInt8 c);
+constexpr T uint8_to_float(UInt8 c)
+{
+        static_assert(std::is_same_v<UInt8, std::uint8_t>);
+        static_assert(std::is_floating_point_v<T>);
+
+        constexpr T MAX = limits<std::uint8_t>::max();
+        return c / MAX;
+}
+
+template <typename T>
+constexpr std::uint8_t float_to_uint8(T c)
+{
+        static_assert(std::is_floating_point_v<T>);
+
+        constexpr T MAX = limits<std::uint8_t>::max();
+        ASSERT(c >= 0 && c <= 1);
+        return c * MAX + T(0.5);
+}
+
+// clang-format off
+inline constexpr std::array<float, 256> SRGB_UINT8_TO_RGB_FLOAT =
+{
+        0.000000000e+00, 3.035269910e-04, 6.070539821e-04, 9.105809731e-04,
+        1.214107964e-03, 1.517634955e-03, 1.821161946e-03, 2.124688821e-03,
+        2.428215928e-03, 2.731742803e-03, 3.035269910e-03, 3.346535843e-03,
+        3.676507389e-03, 4.024717025e-03, 4.391442053e-03, 4.776953254e-03,
+        5.181516521e-03, 5.605391692e-03, 6.048833020e-03, 6.512090564e-03,
+        6.995410193e-03, 7.499032188e-03, 8.023193106e-03, 8.568125777e-03,
+        9.134058841e-03, 9.721217677e-03, 1.032982301e-02, 1.096009370e-02,
+        1.161224488e-02, 1.228648797e-02, 1.298303250e-02, 1.370208338e-02,
+        1.444384363e-02, 1.520851441e-02, 1.599629410e-02, 1.680737548e-02,
+        1.764195412e-02, 1.850022003e-02, 1.938236132e-02, 2.028856240e-02,
+        2.121900953e-02, 2.217388526e-02, 2.315336652e-02, 2.415763214e-02,
+        2.518685907e-02, 2.624122240e-02, 2.732089162e-02, 2.842603996e-02,
+        2.955683507e-02, 3.071344458e-02, 3.189603239e-02, 3.310476616e-02,
+        3.433980793e-02, 3.560131416e-02, 3.688944876e-02, 3.820437193e-02,
+        3.954623640e-02, 4.091519862e-02, 4.231141135e-02, 4.373503104e-02,
+        4.518620297e-02, 4.666508734e-02, 4.817182571e-02, 4.970656708e-02,
+        5.126945674e-02, 5.286064744e-02, 5.448027700e-02, 5.612849072e-02,
+        5.780543014e-02, 5.951123685e-02, 6.124605238e-02, 6.301001459e-02,
+        6.480326504e-02, 6.662593782e-02, 6.847816706e-02, 7.036009431e-02,
+        7.227185369e-02, 7.421357185e-02, 7.618538290e-02, 7.818742096e-02,
+        8.021982014e-02, 8.228270710e-02, 8.437620848e-02, 8.650045842e-02,
+        8.865558356e-02, 9.084171057e-02, 9.305896610e-02, 9.530746937e-02,
+        9.758734703e-02, 9.989872575e-02, 1.022417322e-01, 1.046164855e-01,
+        1.070231050e-01, 1.094617099e-01, 1.119324267e-01, 1.144353747e-01,
+        1.169706658e-01, 1.195384264e-01, 1.221387759e-01, 1.247718185e-01,
+        1.274376810e-01, 1.301364750e-01, 1.328683197e-01, 1.356333345e-01,
+        1.384316087e-01, 1.412632912e-01, 1.441284716e-01, 1.470272690e-01,
+        1.499597877e-01, 1.529261470e-01, 1.559264660e-01, 1.589608341e-01,
+        1.620293707e-01, 1.651321948e-01, 1.682693958e-01, 1.714411080e-01,
+        1.746474057e-01, 1.778884232e-01, 1.811642498e-01, 1.844749898e-01,
+        1.878207773e-01, 1.912016869e-01, 1.946178377e-01, 1.980693191e-01,
+        2.015562505e-01, 2.050787359e-01, 2.086368650e-01, 2.122307569e-01,
+        2.158605009e-01, 2.195262015e-01, 2.232279629e-01, 2.269658744e-01,
+        2.307400554e-01, 2.345505804e-01, 2.383975685e-01, 2.422811240e-01,
+        2.462013215e-01, 2.501582801e-01, 2.541520894e-01, 2.581828535e-01,
+        2.622506618e-01, 2.663556039e-01, 2.704977989e-01, 2.746773064e-01,
+        2.788942754e-01, 2.831487358e-01, 2.874408364e-01, 2.917706370e-01,
+        2.961382568e-01, 3.005437851e-01, 3.049873114e-01, 3.094689250e-01,
+        3.139887154e-01, 3.185467720e-01, 3.231432140e-01, 3.277781010e-01,
+        3.324515224e-01, 3.371636271e-01, 3.419144154e-01, 3.467040658e-01,
+        3.515326083e-01, 3.564001322e-01, 3.613067865e-01, 3.662526011e-01,
+        3.712376952e-01, 3.762621284e-01, 3.813260198e-01, 3.864294291e-01,
+        3.915724754e-01, 3.967552185e-01, 4.019777775e-01, 4.072402120e-01,
+        4.125426114e-01, 4.178850651e-01, 4.232676625e-01, 4.286904931e-01,
+        4.341536462e-01, 4.396571815e-01, 4.452011883e-01, 4.507857859e-01,
+        4.564110339e-01, 4.620769918e-01, 4.677838087e-01, 4.735314846e-01,
+        4.793201685e-01, 4.851499498e-01, 4.910208583e-01, 4.969329834e-01,
+        5.028864741e-01, 5.088813305e-01, 5.149176717e-01, 5.209955573e-01,
+        5.271151066e-01, 5.332763791e-01, 5.394794941e-01, 5.457244515e-01,
+        5.520114303e-01, 5.583403707e-01, 5.647115111e-01, 5.711248517e-01,
+        5.775804520e-01, 5.840784311e-01, 5.906188488e-01, 5.972017646e-01,
+        6.038273573e-01, 6.104955673e-01, 6.172065735e-01, 6.239603758e-01,
+        6.307571530e-01, 6.375968456e-01, 6.444796920e-01, 6.514056325e-01,
+        6.583748460e-01, 6.653872728e-01, 6.724431515e-01, 6.795424819e-01,
+        6.866853237e-01, 6.938717365e-01, 7.011018991e-01, 7.083757520e-01,
+        7.156934738e-01, 7.230551243e-01, 7.304607630e-01, 7.379103899e-01,
+        7.454041839e-01, 7.529422045e-01, 7.605245113e-01, 7.681511641e-01,
+        7.758222222e-01, 7.835378051e-01, 7.912979126e-01, 7.991027236e-01,
+        8.069522381e-01, 8.148465753e-01, 8.227857351e-01, 8.307698965e-01,
+        8.387989998e-01, 8.468732238e-01, 8.549926281e-01, 8.631572127e-01,
+        8.713670969e-01, 8.796223998e-01, 8.879231215e-01, 8.962693810e-01,
+        9.046611786e-01, 9.130986333e-01, 9.215818644e-01, 9.301108718e-01,
+        9.386857152e-01, 9.473065138e-01, 9.559733272e-01, 9.646862745e-01,
+        9.734452963e-01, 9.822505713e-01, 9.911020994e-01, 1.000000000e+00
+};
+// clang-format on
+}
+
+//
 
 template <typename T, typename UInt8>
-T linear_uint8_to_linear_float(UInt8 c);
+constexpr T srgb_uint8_to_linear_float(UInt8 c)
+{
+        static_assert(std::is_same_v<UInt8, std::uint8_t>);
+        static_assert(std::is_same_v<T, float>);
+
+        return implementation::SRGB_UINT8_TO_RGB_FLOAT[c];
+}
+
+template <typename T, typename UInt8>
+constexpr T linear_uint8_to_linear_float(UInt8 c)
+{
+        static_assert(std::is_same_v<UInt8, std::uint8_t>);
+
+        return implementation::uint8_to_float<T>(c);
+}
 
 //
 
 template <typename T>
-T linear_float_to_srgb_float(T c);
-
-//
-
-template <typename T>
-unsigned char linear_float_to_srgb_uint8(T c);
-
-template <typename T>
-unsigned char linear_float_to_linear_uint8(T c);
-
-//
-
-template <typename T>
-T linear_float_to_linear_luminance(T red, T green, T blue);
-
-//
-
-template <typename T>
-std::uint16_t float_to_uint16(T c)
+T linear_float_to_srgb_float(T c)
 {
         static_assert(std::is_same_v<T, float>);
+
+        if (c >= 1)
+        {
+                return 1;
+        }
+        if (c >= T(0.0031308))
+        {
+                return T(1.055) * std::pow(c, T(1) / T(2.4)) - T(0.055);
+        }
+        if (c > 0)
+        {
+                return c * T(12.92);
+        }
+        return 0;
+}
+
+template <typename T>
+std::uint8_t linear_float_to_srgb_uint8(T c)
+{
+        static_assert(std::is_same_v<T, float>);
+
+        return implementation::float_to_uint8(linear_float_to_srgb_float(c));
+}
+
+template <typename T>
+constexpr std::uint8_t linear_float_to_linear_uint8(T c)
+{
+        static_assert(std::is_same_v<T, float>);
+
+        return implementation::float_to_uint8(c);
+}
+
+//
+
+template <typename T>
+constexpr std::uint16_t float_to_uint16(T c)
+{
+        static_assert(std::is_same_v<T, float>);
+
         constexpr float MAX = limits<std::uint16_t>::max();
         ASSERT(c >= 0.0f && c <= 1.0f);
         return c * MAX + 0.5f;
 }
 
 template <typename T>
-T uint16_to_float(T c)
+constexpr float uint16_to_float(T c)
 {
         static_assert(std::is_same_v<T, std::uint16_t>);
+
         constexpr float MAX = limits<std::uint16_t>::max();
         return c / MAX;
+}
+
+//
+
+template <typename T>
+constexpr T linear_float_to_linear_luminance(T red, T green, T blue)
+{
+        static_assert(std::is_same_v<T, float>);
+
+        return T(0.2126) * red + T(0.7152) * green + T(0.0722) * blue;
 }
 }
