75006e26da3a2ddd33a2be1b255c60acf4efc262
cppd <cppd@users.noreply.github.com>
2021-01-05 07:52:41 +0300

    random vectors tests

--- src/random/test/test_sphere_distribution.cpp
+++ src/random/test/test_sphere_distribution.cpp
@@ -41,14 +41,71 @@ namespace
 template <typename T>
 using RandomEngine = std::conditional_t<sizeof(T) <= 4, std::mt19937, std::mt19937_64>;
 
+template <typename T>
+T pdf_uniform(std::type_identity_t<T> angle)
+{
+        // ProbabilityDistribution[1, {x, 0, Pi},  Method -> "Normalize"]
+        if (angle >= 0 && angle < (PI<T>))
+        {
+                return 1 / PI<T>;
+        }
+        return 0;
+}
+
+template <typename T>
+T pdf_cosine(std::type_identity_t<T> angle)
+{
+        // ProbabilityDistribution[Cos[x], {x, 0, Pi/2}, Method -> "Normalize"]
+        if (angle >= 0 && angle < (PI<T> / 2))
+        {
+                return std::cos(angle);
+        }
+        return 0;
+}
+
+template <typename T>
+T pdf_power_cosine(std::type_identity_t<T> angle, std::type_identity_t<T> power)
+{
+        // Assuming[n >= 0,
+        //   ProbabilityDistribution[Cos[x]^n, {x, 0, Pi/2},
+        //   Method -> "Normalize"]]
+        if (angle >= 0 && angle < (PI<T> / 2))
+        {
+                T norm = 2 / std::sqrt(PI<T>);
+                norm *= std::exp(std::lgamma((2 + power) / 2) - std::lgamma((1 + power) / 2));
+                T v = norm * std::pow(std::cos(angle), power);
+                return v;
+        }
+        return 0;
+}
+
+template <typename T, typename F>
+T integrate(const F& f, std::type_identity_t<T> from, std::type_identity_t<T> to)
+{
+        static_assert(std::is_floating_point_v<T>);
+
+        // Composite Trapezoidal Rule
+        constexpr int COUNT = 100;
+        T sum = 0;
+        for (int i = 1; i < COUNT; ++i)
+        {
+                T x = std::lerp(from, to, T(i) / COUNT);
+                sum += f(x);
+        }
+        T h_2 = (to - from) / (2 * COUNT);
+        return (f(from) + 2 * sum + f(to)) * h_2;
+}
+
 template <std::size_t N, typename T>
 T sphere_surface_area(std::type_identity_t<T> a, std::type_identity_t<T> b)
 {
         static_assert(std::is_floating_point_v<T>);
+        static_assert(N >= 2);
 
-        // Integrate[Sin[x]^n, x]
+        // Assuming[Element[n,Integers]&&n>=0,Integrate[Sin[x]^n,x]]
         // -Cos[x] Hypergeometric2F1[1/2,(1-n)/2,3/2,Cos[x]^2] Sin[x]^(1+n) (Sin[x]^2)^(1/2 (-1-n))
-        // Integrate[Sin[x]^2, {x, a, b}]
+        // For[i=2,i<=10,++i,f=Integrate[Sin[x]^(i-2),{x, a, b}];Print[i];Print[f]]
+        // For[i=2,i<=10,++i,f=Simplify[Integrate[Sin[x]^(i-2),x]];Print[i];Print[f]]
 
         if constexpr (N == 2)
         {
@@ -60,7 +117,7 @@ T sphere_surface_area(std::type_identity_t<T> a, std::type_identity_t<T> b)
         }
         if constexpr (N == 4)
         {
-                return (-a + b + std::cos(a) * std::sin(a) - std::cos(b) * sin(b)) / 2;
+                return (2 * b - 2 * a - std::sin(2 * b) + std::sin(2 * a)) / 4;
         }
         if constexpr (N == 5)
         {
@@ -84,12 +141,34 @@ T sphere_surface_area(std::type_identity_t<T> a, std::type_identity_t<T> b)
                         - 45 * std::sin(2 * b) + 9 * std::sin(4 * b) - std::sin(6 * b))
                        / 192;
         }
+        if constexpr (N == 9)
+        {
+                return (1225 * std::cos(a) - 245 * std::cos(3 * a) + 49 * std::cos(5 * a) - 5 * std::cos(7 * a)
+                        - 1225 * std::cos(b) + 245 * std::cos(3 * b) - 49 * std::cos(5 * b) + 5 * std::cos(7 * b))
+                       / 2240;
+        }
+        if constexpr (N == 10)
+        {
+                return (-840 * a + 840 * b + 672 * std::sin(2 * a) - 168 * std::sin(4 * a) + 32 * std::sin(6 * a)
+                        - 3 * std::sin(8 * a) - 672 * std::sin(2 * b) + 168 * std::sin(4 * b) - 32 * std::sin(6 * b)
+                        + 3 * std::sin(8 * b))
+                       / 3072;
+        }
+        if constexpr (N > 10)
+        {
+                return integrate<T>(
+                        [](T x)
+                        {
+                                return std::pow(std::sin(x), N - 2);
+                        },
+                        a, b);
+        }
 }
 
 template <std::size_t N, typename T>
 class Buckets
 {
-        static constexpr unsigned SIZE = 3;
+        static constexpr unsigned SIZE = 2;
         static_assert(90 % SIZE == 0);
 
         static T to_bucket_angle(T angle)
@@ -107,28 +186,6 @@ class Buckets
         static T to_degrees(T angle)
         {
                 return angle * (180 / PI<T>);
-        }
-
-        template <typename PDF>
-        static T integrate(const PDF& pdf, T angle_from, T angle_to)
-        {
-                constexpr int COUNT = 100;
-                T offset = T(0.5) / COUNT * (angle_to - angle_from);
-                T from = angle_from + offset;
-                T to = angle_to + offset;
-                std::vector<T> values(COUNT);
-                for (int i = 0; i < COUNT; ++i)
-                {
-                        T x = std::lerp(from, to, T(i) / COUNT);
-                        values[i] = pdf(x);
-                }
-                std::sort(values.begin(), values.end());
-                T sum = 0;
-                for (int i = 0; i < COUNT; ++i)
-                {
-                        sum += values[i];
-                }
-                return sum / COUNT;
         }
 
         struct Bucket
@@ -171,27 +228,35 @@ public:
 
         void normalize()
         {
-                T max = limits<T>::lowest();
-
                 m_distribution.clear();
+
+                std::vector<T> distribution_values;
+                distribution_values.reserve(m_buckets.size());
 
                 for (auto& [angle, bucket] : m_buckets)
                 {
                         T bucket_surface_area = sphere_surface_area<N, T>(bucket.angle_from, bucket.angle_to);
 
-                        Distribution& n = m_distribution.emplace_back();
-                        n.angle_from = bucket.angle_from;
-                        n.angle_to = bucket.angle_to;
-                        n.distribution = bucket.sample_count / bucket_surface_area;
+                        Distribution& d = m_distribution.emplace_back();
+                        d.angle_from = bucket.angle_from;
+                        d.angle_to = bucket.angle_to;
+                        d.distribution = bucket.sample_count / bucket_surface_area;
 
-                        max = std::max(max, n.distribution);
+                        distribution_values.push_back(d.distribution);
                 }
 
                 m_buckets.clear();
 
+                std::sort(distribution_values.begin(), distribution_values.end());
+                T sum = 0;
+                for (T d : distribution_values)
+                {
+                        sum += d;
+                }
+                sum *= to_radians(SIZE);
                 for (Distribution& d : m_distribution)
                 {
-                        d.distribution /= max;
+                        d.distribution /= sum;
                 }
 
                 std::sort(
@@ -204,10 +269,18 @@ public:
 
         void print() const
         {
+                constexpr int BAR_SIZE = 100;
+                constexpr int DIVISION_SIZE = 10;
+
                 std::ostringstream oss;
                 oss << std::fixed;
-                oss << std::setprecision(1);
+
                 bool new_line = false;
+                T max = limits<T>::lowest();
+                for (const Distribution& d : m_distribution)
+                {
+                        max = std::max(max, d.distribution);
+                }
                 for (const Distribution& d : m_distribution)
                 {
                         if (new_line)
@@ -218,13 +291,13 @@ public:
                         {
                                 new_line = true;
                         }
-                        oss << std::setw(5) << d.bucket_angle();
-                        oss << ": ";
-                        int count = std::round(d.distribution * 100);
-                        for (int i = 0; i < count; i += 10)
+                        oss << std::setprecision(1) << std::setw(5) << d.bucket_angle();
+                        oss << ": " << std::setprecision(2) << std::setw(5) << d.distribution << ") ";
+                        int count = std::round(d.distribution / max * BAR_SIZE);
+                        for (int i = 0; i < count; i += DIVISION_SIZE)
                         {
                                 oss << '+';
-                                for (int j = i + 1; j < i + 10 && j < count; ++j)
+                                for (int j = i + 1; j < i + DIVISION_SIZE && j < count; ++j)
                                 {
                                         oss << '*';
                                 }
@@ -236,31 +309,39 @@ public:
         template <typename PDF>
         void compare_with_pdf(const PDF& pdf) const
         {
-                constexpr T LIMIT = 0.05;
-
                 for (const Distribution& d : m_distribution)
                 {
                         const T distribution_value = d.distribution;
-                        const T pdf_value = integrate(pdf, d.angle_from, d.angle_to);
 
-                        if (pdf_value == distribution_value)
+                        const T pdf_mean_value =
+                                integrate<T>(pdf, d.angle_from, d.angle_to) / (d.angle_to - d.angle_from);
+
+                        if (!(pdf_mean_value >= 0 && distribution_value >= 0))
+                        {
+                                error("Number is not positive and not zero: distribution = "
+                                      + to_string(distribution_value, 5) + ", PDF = " + to_string(pdf_mean_value, 5));
+                        }
+
+                        if (pdf_mean_value == distribution_value)
                         {
                                 continue;
                         }
 
-                        T discrepancy_abs = pdf_value - distribution_value;
-                        if (std::abs(discrepancy_abs) < LIMIT)
+                        T discrepancy_abs = std::abs(pdf_mean_value - distribution_value);
+                        if (discrepancy_abs <= T(0.05))
                         {
                                 continue;
                         }
 
-                        T discrepancy_rel = discrepancy_abs / (pdf_value != 0 ? pdf_value : distribution_value);
-                        if (!(std::abs(discrepancy_rel) <= LIMIT))
+                        T discrepancy_rel = discrepancy_abs / std::max(pdf_mean_value, distribution_value);
+                        if (discrepancy_rel <= T(0.05))
                         {
-                                error("Angle interval = [" + to_string(to_degrees(d.angle_from), 5) + ", "
-                                      + to_string(to_degrees(d.angle_to), 5) + "], distribution = "
-                                      + to_string(distribution_value, 5) + ", PDF = " + to_string(pdf_value, 5));
+                                continue;
                         }
+
+                        error("Angle interval = [" + to_string(to_degrees(d.angle_from), 5) + ", "
+                              + to_string(to_degrees(d.angle_to), 5) + "], distribution = "
+                              + to_string(distribution_value, 5) + ", PDF = " + to_string(pdf_mean_value, 5));
                 }
         }
 };
@@ -363,13 +444,9 @@ void test_uniform_on_sphere(long long count)
                 {
                         return random_on_sphere<N, T>(random_engine);
                 },
-                [](T angle) -> T
+                [](T angle)
                 {
-                        if (angle >= 0 && angle < (PI<T>))
-                        {
-                                return 1;
-                        }
-                        return T(0);
+                        return pdf_uniform<T>(angle);
                 });
 
         test_speed<N, T>(
@@ -400,13 +477,9 @@ void test_cosine_on_hemisphere(long long count)
                 {
                         return random_cosine_weighted_on_hemisphere(random_engine, normal);
                 },
-                [](T angle) -> T
+                [](T angle)
                 {
-                        if (angle >= 0 && angle < (PI<T> / 2))
-                        {
-                                return std::cos(angle);
-                        }
-                        return T(0);
+                        return pdf_cosine<T>(angle);
                 });
 
         test_speed<N, T>(
@@ -418,22 +491,60 @@ void test_cosine_on_hemisphere(long long count)
 }
 
 template <std::size_t N, typename T>
+void test_power_cosine_on_hemisphere(long long count)
+{
+        RandomEngine<T> random_engine = create_engine<RandomEngine<T>>();
+
+        const T POWER = std::uniform_real_distribution<T>(1, 100)(random_engine);
+
+        const std::string name = "power_" + to_string_fixed(POWER, 1) + "_cosine_weighted";
+
+        test_unit<N, T>(
+                name, count, random_engine,
+                [&](const Vector<N, T>& normal)
+                {
+                        return random_power_cosine_weighted_on_hemisphere(random_engine, normal, POWER);
+                });
+
+        test_distribution<N, T>(
+                name, count, random_engine,
+                [&](const Vector<N, T>& normal)
+                {
+                        return random_power_cosine_weighted_on_hemisphere(random_engine, normal, POWER);
+                },
+                [&](T angle)
+                {
+                        return pdf_power_cosine<T>(angle, POWER);
+                });
+
+        test_speed<N, T>(
+                name, count, random_engine,
+                [&](const Vector<N, T>& normal)
+                {
+                        return random_power_cosine_weighted_on_hemisphere(random_engine, normal, POWER);
+                });
+}
+
+template <std::size_t N, typename T>
 void test_distribution(long long count)
 {
         test_uniform_on_sphere<N, T>(count);
         LOG("");
         test_cosine_on_hemisphere<N, T>(count);
+        LOG("");
+        if constexpr (N == 3)
+        {
+                test_power_cosine_on_hemisphere<N, T>(count);
+                LOG("");
+        }
 }
 
 template <typename T>
 void test_distribution()
 {
         test_distribution<3, T>(50'000'000);
-        LOG("");
         test_distribution<4, T>(100'000'000);
-        LOG("");
         test_distribution<5, T>(200'000'000);
-        LOG("");
         test_distribution<6, T>(300'000'000);
 }
 }
@@ -441,7 +552,6 @@ void test_distribution()
 void test_sphere_distribution()
 {
         test_distribution<float>();
-        LOG("");
         test_distribution<double>();
 }
 }
