a226a09e62d1d12f5d14741e2d5851b31f1cd746
cppd <cppd@users.noreply.github.com>
2021-02-09 08:59:40 +0300

    refactoring

--- src/geometry/shapes/sphere_create.h
+++ src/geometry/shapes/sphere_create.h
@@ -75,57 +75,57 @@ bool equal_distance_from_point(const std::vector<Vector<N, T>>& vertices, const 
 }
 
 template <std::size_t N, typename T>
-struct Facet
+struct Simplex
 {
         std::array<Vector<N, T>, N> vertices;
 
-        explicit Facet(const std::array<Vector<N, T>, N>& vertices) : vertices(vertices)
+        explicit Simplex(const std::array<Vector<N, T>, N>& vertices) : vertices(vertices)
         {
         }
 
         template <typename... Ts>
-        explicit Facet(Ts... vertices) : vertices{vertices...}
+        explicit Simplex(Ts... vertices) : vertices{vertices...}
         {
                 static_assert(sizeof...(Ts) == N);
         }
 };
 
 template <std::size_t N>
-constexpr std::enable_if_t<N == 3, unsigned> divide_facet_count()
+constexpr std::enable_if_t<N == 3, unsigned> division_count()
 {
         return 4;
 }
 
 template <typename T>
-void divide_facet(const Facet<3, T>& facet, std::vector<Facet<3, T>>* facets)
+void divide_simplex(const Simplex<3, T>& simplex, std::vector<Simplex<3, T>>* simplices)
 {
-        Vector<3, T> p01 = (facet.vertices[0] + facet.vertices[1]).normalized();
-        Vector<3, T> p12 = (facet.vertices[1] + facet.vertices[2]).normalized();
-        Vector<3, T> p20 = (facet.vertices[2] + facet.vertices[0]).normalized();
-        facets->emplace_back(p01, p12, p20);
-        facets->emplace_back(facet.vertices[0], p01, p20);
-        facets->emplace_back(facet.vertices[1], p12, p01);
-        facets->emplace_back(facet.vertices[2], p20, p12);
+        Vector<3, T> p01 = (simplex.vertices[0] + simplex.vertices[1]).normalized();
+        Vector<3, T> p12 = (simplex.vertices[1] + simplex.vertices[2]).normalized();
+        Vector<3, T> p20 = (simplex.vertices[2] + simplex.vertices[0]).normalized();
+        simplices->emplace_back(p01, p12, p20);
+        simplices->emplace_back(simplex.vertices[0], p01, p20);
+        simplices->emplace_back(simplex.vertices[1], p12, p01);
+        simplices->emplace_back(simplex.vertices[2], p20, p12);
 }
 
 template <std::size_t N, typename T>
-std::vector<Facet<N, T>> divide_facets(unsigned count, std::vector<Facet<N, T>> facets)
+std::vector<Simplex<N, T>> divide_simplices(unsigned count, std::vector<Simplex<N, T>> simplices)
 {
-        while (facets.size() < count)
+        while (simplices.size() < count)
         {
-                std::vector<Facet<N, T>> tmp;
-                tmp.reserve(facets.size() * divide_facet_count<N>());
-                for (const Facet<N, T>& facet : facets)
+                std::vector<Simplex<N, T>> tmp;
+                tmp.reserve(simplices.size() * division_count<N>());
+                for (const Simplex<N, T>& facet : simplices)
                 {
-                        divide_facet(facet, &tmp);
+                        divide_simplex(facet, &tmp);
                 }
-                facets = std::move(tmp);
+                simplices = std::move(tmp);
         }
-        return facets;
+        return simplices;
 }
 
 template <std::size_t N, typename T>
-std::enable_if_t<N == 3, std::vector<Facet<N, T>>> create_sphere(unsigned count)
+std::enable_if_t<N == 3, std::vector<Simplex<N, T>>> create_sphere(unsigned count)
 {
         static_assert(N == 3);
 
@@ -142,59 +142,67 @@ std::enable_if_t<N == 3, std::vector<Facet<N, T>>> create_sphere(unsigned count)
 
         ASSERT(equal_distance_from_point(vertices, Vector<N, T>(0)));
 
-        std::vector<Facet<N, T>> facets = {Facet<N, T>(vertices[0], vertices[1], vertices[7]),
-                                           Facet<N, T>(vertices[0], vertices[5], vertices[1]),
-                                           Facet<N, T>(vertices[0], vertices[7], vertices[10]),
-                                           Facet<N, T>(vertices[0], vertices[10], vertices[11]),
-                                           Facet<N, T>(vertices[0], vertices[11], vertices[5]),
-                                           Facet<N, T>(vertices[1], vertices[5], vertices[9]),
-                                           Facet<N, T>(vertices[2], vertices[4], vertices[11]),
-                                           Facet<N, T>(vertices[3], vertices[2], vertices[6]),
-                                           Facet<N, T>(vertices[3], vertices[4], vertices[2]),
-                                           Facet<N, T>(vertices[3], vertices[6], vertices[8]),
-                                           Facet<N, T>(vertices[3], vertices[8], vertices[9]),
-                                           Facet<N, T>(vertices[3], vertices[9], vertices[4]),
-                                           Facet<N, T>(vertices[4], vertices[9], vertices[5]),
-                                           Facet<N, T>(vertices[5], vertices[11], vertices[4]),
-                                           Facet<N, T>(vertices[6], vertices[2], vertices[10]),
-                                           Facet<N, T>(vertices[7], vertices[1], vertices[8]),
-                                           Facet<N, T>(vertices[8], vertices[6], vertices[7]),
-                                           Facet<N, T>(vertices[9], vertices[8], vertices[1]),
-                                           Facet<N, T>(vertices[10], vertices[7], vertices[6]),
-                                           Facet<N, T>(vertices[11], vertices[10], vertices[2])};
+        std::vector<Simplex<N, T>> facets = {Simplex<N, T>(vertices[0], vertices[1], vertices[7]),
+                                             Simplex<N, T>(vertices[0], vertices[5], vertices[1]),
+                                             Simplex<N, T>(vertices[0], vertices[7], vertices[10]),
+                                             Simplex<N, T>(vertices[0], vertices[10], vertices[11]),
+                                             Simplex<N, T>(vertices[0], vertices[11], vertices[5]),
+                                             Simplex<N, T>(vertices[1], vertices[5], vertices[9]),
+                                             Simplex<N, T>(vertices[2], vertices[4], vertices[11]),
+                                             Simplex<N, T>(vertices[3], vertices[2], vertices[6]),
+                                             Simplex<N, T>(vertices[3], vertices[4], vertices[2]),
+                                             Simplex<N, T>(vertices[3], vertices[6], vertices[8]),
+                                             Simplex<N, T>(vertices[3], vertices[8], vertices[9]),
+                                             Simplex<N, T>(vertices[3], vertices[9], vertices[4]),
+                                             Simplex<N, T>(vertices[4], vertices[9], vertices[5]),
+                                             Simplex<N, T>(vertices[5], vertices[11], vertices[4]),
+                                             Simplex<N, T>(vertices[6], vertices[2], vertices[10]),
+                                             Simplex<N, T>(vertices[7], vertices[1], vertices[8]),
+                                             Simplex<N, T>(vertices[8], vertices[6], vertices[7]),
+                                             Simplex<N, T>(vertices[9], vertices[8], vertices[1]),
+                                             Simplex<N, T>(vertices[10], vertices[7], vertices[6]),
+                                             Simplex<N, T>(vertices[11], vertices[10], vertices[2])};
 
-        return divide_facets(count, std::move(facets));
+        return divide_simplices(count, std::move(facets));
+}
+
+template <std::size_t N, typename T>
+void create_mesh(
+        const std::vector<Simplex<N, T>>& simplices,
+        std::vector<Vector<N, T>>* vertices,
+        std::vector<std::array<int, N>>* facets)
+{
+        vertices->clear();
+        facets->clear();
+        facets->reserve(simplices.size());
+
+        std::unordered_map<Vector<N, T>, unsigned> map;
+        map.reserve(N * simplices.size());
+
+        for (const Simplex<N, T>& simplex : simplices)
+        {
+                std::array<int, N>& facet = facets->emplace_back();
+                for (unsigned i = 0; i < N; ++i)
+                {
+                        auto iter = map.find(simplex.vertices[i]);
+                        if (iter == map.cend())
+                        {
+                                iter = map.emplace(simplex.vertices[i], vertices->size()).first;
+                                vertices->push_back(simplex.vertices[i]);
+                        }
+                        facet[i] = iter->second;
+                }
+        }
 }
 }
 
 template <std::size_t N, typename T>
-void create_sphere(unsigned facet_count, std::vector<Vector<N, T>>* points, std::vector<std::array<int, N>>* facets)
+void create_sphere(unsigned facet_count, std::vector<Vector<N, T>>* vertices, std::vector<std::array<int, N>>* facets)
 {
         namespace impl = sphere_implementation;
 
-        std::vector<impl::Facet<N, T>> impl_facets = impl::create_sphere<N, T>(facet_count);
+        std::vector<impl::Simplex<N, T>> simplices = impl::create_sphere<N, T>(facet_count);
 
-        points->clear();
-
-        facets->clear();
-        facets->reserve(impl_facets.size());
-
-        std::unordered_map<Vector<N, T>, unsigned> map;
-        map.reserve(N * impl_facets.size());
-
-        for (const impl::Facet<N, T>& impl_f : impl_facets)
-        {
-                std::array<int, N>& vertices = facets->emplace_back();
-                for (unsigned i = 0; i < N; ++i)
-                {
-                        auto iter = map.find(impl_f.vertices[i]);
-                        if (iter == map.cend())
-                        {
-                                iter = map.emplace(impl_f.vertices[i], points->size()).first;
-                                points->push_back(impl_f.vertices[i]);
-                        }
-                        vertices[i] = iter->second;
-                }
-        }
+        impl::create_mesh(simplices, vertices, facets);
 }
 }
