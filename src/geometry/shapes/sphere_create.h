3e14674b8b41d8d70b45c7af70edfffe5535bd2b
cppd <cppd@users.noreply.github.com>
2021-02-10 09:53:16 +0300

    sphere

--- src/geometry/shapes/sphere_create.h
+++ src/geometry/shapes/sphere_create.h
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <src/com/arrays.h>
 #include <src/com/error.h>
+#include <src/com/math.h>
 #include <src/numerical/vec.h>
 
 #include <array>
@@ -29,44 +30,38 @@ namespace ns::geometry
 {
 namespace sphere_implementation
 {
-//template <std::size_t N, typename T>
-//bool equal_distances(const std::array<Vector<N, T>, N>& vertices)
-//{
-//        T d = -1;
-//        for (unsigned i = 0; i < vertices.size(); ++i)
-//        {
-//                for (unsigned j = i + 1; j < vertices.size(); ++j)
-//                {
-//                        T s = 0;
-//                        for (unsigned n = 0; n < N; ++n)
-//                        {
-//                                s += square(vertices[i][n] - vertices[j][n]);
-//                        }
-//                        s = std::sqrt(s);
-//                        d = (d < 0) ? s : d;
-//                        if (std::abs(s - d) > T(1e-2))
-//                        {
-//                                return false;
-//                        }
-//                }
-//        }
-//        return true;
-//}
-
 template <std::size_t N, typename T>
-bool equal_distance_from_point(const std::vector<Vector<N, T>>& vertices, const Vector<N, T>& p)
+bool equal_distances(const std::array<Vector<N, T>, N>& vertices)
 {
-        T d = -1;
+        if (vertices.size() < 2)
+        {
+                return true;
+        }
+        const T d = (vertices[0] - vertices[1]).norm();
         for (unsigned i = 0; i < vertices.size(); ++i)
         {
-                T s = 0;
-                for (unsigned n = 0; n < N; ++n)
+                for (unsigned j = i + 1; j < vertices.size(); ++j)
                 {
-                        s += square(vertices[i][n] - p[n]);
+                        if (std::abs(d - (vertices[j] - vertices[i]).norm()) > T(1e-3))
+                        {
+                                return false;
+                        }
                 }
-                s = std::sqrt(s);
-                d = (d < 0) ? s : d;
-                if (std::abs(s - d) > T(1e-5))
+        }
+        return true;
+}
+
+template <std::size_t N, typename T>
+bool equal_distance_from_origin(const std::array<Vector<N, T>, N>& vertices)
+{
+        if (vertices.empty())
+        {
+                return true;
+        }
+        const T d = vertices[0].norm();
+        for (unsigned i = 1; i < vertices.size(); ++i)
+        {
+                if (std::abs(d - vertices[i].norm()) > T(1e-3))
                 {
                         return false;
                 }
@@ -90,84 +85,176 @@ struct Simplex
         }
 };
 
-template <std::size_t N>
-constexpr std::enable_if_t<N == 3, unsigned> division_count()
-{
-        return 4;
-}
-
 template <typename T>
 void divide_simplex(const Simplex<3, T>& simplex, std::vector<Simplex<3, T>>* simplices)
 {
-        Vector<3, T> p01 = (simplex.vertices[0] + simplex.vertices[1]).normalized();
-        Vector<3, T> p12 = (simplex.vertices[1] + simplex.vertices[2]).normalized();
-        Vector<3, T> p20 = (simplex.vertices[2] + simplex.vertices[0]).normalized();
+        const Vector<3, T>& v0 = simplex.vertices[0];
+        const Vector<3, T>& v1 = simplex.vertices[1];
+        const Vector<3, T>& v2 = simplex.vertices[2];
+        Vector<3, T> p01 = (v0 + v1).normalized();
+        Vector<3, T> p12 = (v1 + v2).normalized();
+        Vector<3, T> p20 = (v2 + v0).normalized();
+        simplices->emplace_back(v0, p01, p20);
+        simplices->emplace_back(v1, p12, p01);
+        simplices->emplace_back(v2, p20, p12);
         simplices->emplace_back(p01, p12, p20);
-        simplices->emplace_back(simplex.vertices[0], p01, p20);
-        simplices->emplace_back(simplex.vertices[1], p12, p01);
-        simplices->emplace_back(simplex.vertices[2], p20, p12);
+}
+
+template <typename T>
+void divide_simplex(const Simplex<4, T>& simplex, std::vector<Simplex<4, T>>* simplices)
+{
+        const Vector<4, T>& v0 = simplex.vertices[0];
+        const Vector<4, T>& v1 = simplex.vertices[1];
+        const Vector<4, T>& v2 = simplex.vertices[2];
+        const Vector<4, T>& v3 = simplex.vertices[3];
+        const Vector<4, T>& p01 = (v0 + v1).normalized();
+        const Vector<4, T>& p02 = (v0 + v2).normalized();
+        const Vector<4, T>& p03 = (v0 + v3).normalized();
+        const Vector<4, T>& p12 = (v1 + v2).normalized();
+        const Vector<4, T>& p13 = (v1 + v3).normalized();
+        const Vector<4, T>& p23 = (v2 + v3).normalized();
+        simplices->emplace_back(v0, p01, p02, p03);
+        simplices->emplace_back(v1, p01, p12, p13);
+        simplices->emplace_back(v2, p02, p12, p23);
+        simplices->emplace_back(v3, p03, p13, p23);
+        simplices->emplace_back(p01, p23, p02, p03);
+        simplices->emplace_back(p01, p23, p12, p13);
+        simplices->emplace_back(p01, p23, p02, p12);
+        simplices->emplace_back(p01, p23, p13, p03);
 }
 
 template <std::size_t N, typename T>
-std::vector<Simplex<N, T>> divide_simplices(unsigned count, std::vector<Simplex<N, T>> simplices)
+std::vector<Simplex<N, T>> divide_simplices(unsigned min_count, std::vector<Simplex<N, T>> simplices)
 {
-        while (simplices.size() < count)
+        if (simplices.empty())
+        {
+                return {};
+        }
+        while (simplices.size() < min_count)
         {
                 std::vector<Simplex<N, T>> tmp;
-                tmp.reserve(simplices.size() * division_count<N>());
-                for (const Simplex<N, T>& facet : simplices)
+                auto iter = simplices.begin();
+                divide_simplex(*iter++, &tmp);
+                tmp.reserve(simplices.size() * tmp.size());
+                for (; iter != simplices.end(); ++iter)
                 {
-                        divide_simplex(facet, &tmp);
+                        divide_simplex(*iter, &tmp);
                 }
                 simplices = std::move(tmp);
         }
         return simplices;
 }
 
-template <std::size_t N, typename T>
-std::enable_if_t<N == 3, std::vector<Simplex<N, T>>> create_sphere(unsigned count)
+template <unsigned I, std::size_t N, typename T>
+void create_simplices(Vector<N, T>* point, std::vector<Simplex<N, T>>* simplices)
 {
-        static_assert(N == 3);
+        static_assert(I <= N);
+        if constexpr (I == N)
+        {
+                std::array<Vector<N, T>, N> vertices;
+                for (Vector<N, T>& v : vertices)
+                {
+                        v = Vector<N, T>(0);
+                }
+                for (unsigned i = 0; i < N; ++i)
+                {
+                        vertices[i][i] = (*point)[i];
+                }
+                simplices->emplace_back(vertices);
+        }
+        else
+        {
+                (*point)[I] = -1;
+                create_simplices<I + 1>(point, simplices);
+                (*point)[I] = 1;
+                create_simplices<I + 1>(point, simplices);
+        }
+}
+
+template <std::size_t N, typename T>
+std::enable_if_t<N >= 4, std::vector<Simplex<N, T>>> create_initial_shape()
+{
+        // Cross-polytope
+
+        std::vector<Simplex<N, T>> simplices;
+        simplices.reserve(power<N>(2));
+        Vector<N, T> point;
+        create_simplices<0>(&point, &simplices);
+        ASSERT(simplices.size() == power<N>(2));
+        return simplices;
+}
+
+template <std::size_t N, typename T>
+std::enable_if_t<N == 3, std::vector<Simplex<N, T>>> create_initial_shape()
+{
+        // Regular icosahedron
 
         const T p = (1 + std::sqrt(T(5))) / 2;
 
-        std::vector<Vector<N, T>> vertices = {{-1, p, 0}, {1, p, 0}, {-1, -p, 0}, {1, -p, 0},
-                                              {0, -1, p}, {0, 1, p}, {0, -1, -p}, {0, 1, -p},
-                                              {p, 0, -1}, {p, 0, 1}, {-p, 0, -1}, {-p, 0, 1}};
+        std::vector<Vector<N, T>> vertices;
+
+        vertices.emplace_back(-1, p, 0);
+        vertices.emplace_back(1, p, 0);
+        vertices.emplace_back(-1, -p, 0);
+        vertices.emplace_back(1, -p, 0);
+        vertices.emplace_back(0, -1, p);
+        vertices.emplace_back(0, 1, p);
+        vertices.emplace_back(0, -1, -p);
+        vertices.emplace_back(0, 1, -p);
+        vertices.emplace_back(p, 0, -1);
+        vertices.emplace_back(p, 0, 1);
+        vertices.emplace_back(-p, 0, -1);
+        vertices.emplace_back(-p, 0, 1);
 
         for (Vector<N, T>& v : vertices)
         {
                 v.normalize();
         }
 
-        ASSERT(equal_distance_from_point(vertices, Vector<N, T>(0)));
+        std::vector<Simplex<N, T>> facets;
 
-        std::vector<Simplex<N, T>> facets = {Simplex<N, T>(vertices[0], vertices[1], vertices[7]),
-                                             Simplex<N, T>(vertices[0], vertices[5], vertices[1]),
-                                             Simplex<N, T>(vertices[0], vertices[7], vertices[10]),
-                                             Simplex<N, T>(vertices[0], vertices[10], vertices[11]),
-                                             Simplex<N, T>(vertices[0], vertices[11], vertices[5]),
-                                             Simplex<N, T>(vertices[1], vertices[5], vertices[9]),
-                                             Simplex<N, T>(vertices[2], vertices[4], vertices[11]),
-                                             Simplex<N, T>(vertices[3], vertices[2], vertices[6]),
-                                             Simplex<N, T>(vertices[3], vertices[4], vertices[2]),
-                                             Simplex<N, T>(vertices[3], vertices[6], vertices[8]),
-                                             Simplex<N, T>(vertices[3], vertices[8], vertices[9]),
-                                             Simplex<N, T>(vertices[3], vertices[9], vertices[4]),
-                                             Simplex<N, T>(vertices[4], vertices[9], vertices[5]),
-                                             Simplex<N, T>(vertices[5], vertices[11], vertices[4]),
-                                             Simplex<N, T>(vertices[6], vertices[2], vertices[10]),
-                                             Simplex<N, T>(vertices[7], vertices[1], vertices[8]),
-                                             Simplex<N, T>(vertices[8], vertices[6], vertices[7]),
-                                             Simplex<N, T>(vertices[9], vertices[8], vertices[1]),
-                                             Simplex<N, T>(vertices[10], vertices[7], vertices[6]),
-                                             Simplex<N, T>(vertices[11], vertices[10], vertices[2])};
+        facets.emplace_back(vertices[0], vertices[1], vertices[7]);
+        facets.emplace_back(vertices[0], vertices[5], vertices[1]);
+        facets.emplace_back(vertices[0], vertices[7], vertices[10]);
+        facets.emplace_back(vertices[0], vertices[10], vertices[11]);
+        facets.emplace_back(vertices[0], vertices[11], vertices[5]);
+        facets.emplace_back(vertices[1], vertices[5], vertices[9]);
+        facets.emplace_back(vertices[2], vertices[4], vertices[11]);
+        facets.emplace_back(vertices[3], vertices[2], vertices[6]);
+        facets.emplace_back(vertices[3], vertices[4], vertices[2]);
+        facets.emplace_back(vertices[3], vertices[6], vertices[8]);
+        facets.emplace_back(vertices[3], vertices[8], vertices[9]);
+        facets.emplace_back(vertices[3], vertices[9], vertices[4]);
+        facets.emplace_back(vertices[4], vertices[9], vertices[5]);
+        facets.emplace_back(vertices[5], vertices[11], vertices[4]);
+        facets.emplace_back(vertices[6], vertices[2], vertices[10]);
+        facets.emplace_back(vertices[7], vertices[1], vertices[8]);
+        facets.emplace_back(vertices[8], vertices[6], vertices[7]);
+        facets.emplace_back(vertices[9], vertices[8], vertices[1]);
+        facets.emplace_back(vertices[10], vertices[7], vertices[6]);
+        facets.emplace_back(vertices[11], vertices[10], vertices[2]);
 
-        return divide_simplices(count, std::move(facets));
+        return facets;
 }
 
 template <std::size_t N, typename T>
-void create_mesh(
+std::vector<Simplex<N, T>> create_sphere(unsigned facet_min_count)
+{
+        std::vector<Simplex<N, T>> simplices = create_initial_shape<N, T>();
+
+        for (Simplex<N, T>& s : simplices)
+        {
+                if (!(equal_distances(s.vertices) && equal_distance_from_origin(s.vertices)))
+                {
+                        error("Error creating initial shape");
+                }
+        }
+
+        return divide_simplices(facet_min_count, std::move(simplices));
+}
+
+template <std::size_t N, typename T>
+void create_mesh_for_simplices(
         const std::vector<Simplex<N, T>>& simplices,
         std::vector<Vector<N, T>>* vertices,
         std::vector<std::array<int, N>>* facets)
@@ -197,12 +284,15 @@ void create_mesh(
 }
 
 template <std::size_t N, typename T>
-void create_sphere(unsigned facet_count, std::vector<Vector<N, T>>* vertices, std::vector<std::array<int, N>>* facets)
+void create_sphere(
+        unsigned facet_min_count,
+        std::vector<Vector<N, T>>* vertices,
+        std::vector<std::array<int, N>>* facets)
 {
         namespace impl = sphere_implementation;
 
-        std::vector<impl::Simplex<N, T>> simplices = impl::create_sphere<N, T>(facet_count);
+        std::vector<impl::Simplex<N, T>> simplices = impl::create_sphere<N, T>(facet_min_count);
 
-        impl::create_mesh(simplices, vertices, facets);
+        impl::create_mesh_for_simplices(simplices, vertices, facets);
 }
 }
