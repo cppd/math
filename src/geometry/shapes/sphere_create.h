a7dbece3ed0796ff3508f04316335cbd2775ab05
cppd <cppd@users.noreply.github.com>
2021-02-11 09:55:10 +0300

    sphere

--- src/geometry/shapes/sphere_create.h
+++ src/geometry/shapes/sphere_create.h
@@ -17,6 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#include "simplex.h"
+
+#include "../core/convex_hull.h"
+
 #include <src/com/arrays.h>
 #include <src/com/error.h>
 #include <src/com/math.h>
@@ -69,10 +73,61 @@ bool equal_distance_from_origin(const std::array<Vector<N, T>, N>& vertices)
         return true;
 }
 
+template <std::size_t N>
+class SimplexSubdivision
+{
+        std::vector<std::array<int, 2>> m_midpoints;
+        std::vector<std::array<int, N + 1>> m_simplices;
+
+public:
+        SimplexSubdivision()
+        {
+                std::array<Vector<N, float>, N + 1> vertices = create_origin_centered_simplex<N, float>();
+
+                std::vector<Vector<N, float>> source_points(vertices.cbegin(), vertices.cend());
+
+                for (unsigned i = 0; i < vertices.size(); ++i)
+                {
+                        for (unsigned j = i + 1; j < vertices.size(); ++j)
+                        {
+                                source_points.push_back((vertices[i] + vertices[j]) / 2.0f);
+                                source_points.back() *= 1.001f; // convex
+                                m_midpoints.push_back({static_cast<int>(i), static_cast<int>(j)});
+                        }
+                }
+
+                std::vector<vec<N>> points;
+                std::vector<DelaunaySimplex<N>> delaunay_simplices;
+                ProgressRatio progress(nullptr);
+
+                compute_delaunay(source_points, &points, &delaunay_simplices, &progress);
+                ASSERT(source_points.size() == points.size());
+
+                for (const DelaunaySimplex<N>& delaunay_simplex : delaunay_simplices)
+                {
+                        m_simplices.push_back(delaunay_simplex.vertices());
+                }
+        }
+
+        const std::vector<std::array<int, 2>>& midpoints() const
+        {
+                return m_midpoints;
+        }
+
+        const std::vector<std::array<int, N + 1>>& simplices() const
+        {
+                return m_simplices;
+        }
+};
+
 template <std::size_t N, typename T>
 struct Simplex
 {
         std::array<Vector<N, T>, N> vertices;
+
+        Simplex()
+        {
+        }
 
         explicit Simplex(const std::array<Vector<N, T>, N>& vertices) : vertices(vertices)
         {
@@ -85,60 +140,82 @@ struct Simplex
         }
 };
 
-template <typename T>
-void divide_simplex(const Simplex<3, T>& simplex, std::vector<Simplex<3, T>>* simplices)
-{
-        const Vector<3, T>& v0 = simplex.vertices[0];
-        const Vector<3, T>& v1 = simplex.vertices[1];
-        const Vector<3, T>& v2 = simplex.vertices[2];
-        Vector<3, T> p01 = (v0 + v1).normalized();
-        Vector<3, T> p12 = (v1 + v2).normalized();
-        Vector<3, T> p20 = (v2 + v0).normalized();
-        simplices->emplace_back(v0, p01, p20);
-        simplices->emplace_back(v1, p12, p01);
-        simplices->emplace_back(v2, p20, p12);
-        simplices->emplace_back(p01, p12, p20);
-}
+//template <typename T>
+//void divide_simplex(const Simplex<3, T>& simplex, std::vector<Simplex<3, T>>* simplices)
+//{
+//        const Vector<3, T>& v0 = simplex.vertices[0];
+//        const Vector<3, T>& v1 = simplex.vertices[1];
+//        const Vector<3, T>& v2 = simplex.vertices[2];
+//        Vector<3, T> p01 = (v0 + v1).normalized();
+//        Vector<3, T> p12 = (v1 + v2).normalized();
+//        Vector<3, T> p20 = (v2 + v0).normalized();
+//        simplices->emplace_back(v0, p01, p20);
+//        simplices->emplace_back(v1, p12, p01);
+//        simplices->emplace_back(v2, p20, p12);
+//        simplices->emplace_back(p01, p12, p20);
+//}
 
-template <typename T>
-void divide_simplex(const Simplex<4, T>& simplex, std::vector<Simplex<4, T>>* simplices)
-{
-        const Vector<4, T>& v0 = simplex.vertices[0];
-        const Vector<4, T>& v1 = simplex.vertices[1];
-        const Vector<4, T>& v2 = simplex.vertices[2];
-        const Vector<4, T>& v3 = simplex.vertices[3];
-        const Vector<4, T>& p01 = (v0 + v1).normalized();
-        const Vector<4, T>& p02 = (v0 + v2).normalized();
-        const Vector<4, T>& p03 = (v0 + v3).normalized();
-        const Vector<4, T>& p12 = (v1 + v2).normalized();
-        const Vector<4, T>& p13 = (v1 + v3).normalized();
-        const Vector<4, T>& p23 = (v2 + v3).normalized();
-        simplices->emplace_back(v0, p01, p02, p03);
-        simplices->emplace_back(v1, p01, p12, p13);
-        simplices->emplace_back(v2, p02, p12, p23);
-        simplices->emplace_back(v3, p03, p13, p23);
-        simplices->emplace_back(p01, p23, p02, p03);
-        simplices->emplace_back(p01, p23, p12, p13);
-        simplices->emplace_back(p01, p23, p02, p12);
-        simplices->emplace_back(p01, p23, p13, p03);
-}
+//template <typename T>
+//void divide_simplex(const Simplex<4, T>& simplex, std::vector<Simplex<4, T>>* simplices)
+//{
+//        const Vector<4, T>& v0 = simplex.vertices[0];
+//        const Vector<4, T>& v1 = simplex.vertices[1];
+//        const Vector<4, T>& v2 = simplex.vertices[2];
+//        const Vector<4, T>& v3 = simplex.vertices[3];
+//        const Vector<4, T>& p01 = (v0 + v1).normalized();
+//        const Vector<4, T>& p02 = (v0 + v2).normalized();
+//        const Vector<4, T>& p03 = (v0 + v3).normalized();
+//        const Vector<4, T>& p12 = (v1 + v2).normalized();
+//        const Vector<4, T>& p13 = (v1 + v3).normalized();
+//        const Vector<4, T>& p23 = (v2 + v3).normalized();
+//        simplices->emplace_back(v0, p01, p02, p03);
+//        simplices->emplace_back(v1, p01, p12, p13);
+//        simplices->emplace_back(v2, p02, p12, p23);
+//        simplices->emplace_back(v3, p03, p13, p23);
+//        simplices->emplace_back(p01, p23, p02, p03);
+//        simplices->emplace_back(p01, p23, p12, p13);
+//        simplices->emplace_back(p01, p23, p02, p12);
+//        simplices->emplace_back(p01, p23, p13, p03);
+//}
 
 template <std::size_t N, typename T>
-std::vector<Simplex<N, T>> divide_simplices(unsigned min_count, std::vector<Simplex<N, T>> simplices)
+std::vector<Simplex<N, T>> divide_simplices(
+        unsigned min_count,
+        const SimplexSubdivision<N - 1>& simplex_subdivision,
+        std::vector<Simplex<N, T>> simplices)
 {
         if (simplices.empty())
         {
                 return {};
         }
+
+        std::vector<Vector<N, T>> points(N + simplex_subdivision.midpoints().size());
+
         while (simplices.size() < min_count)
         {
                 std::vector<Simplex<N, T>> tmp;
-                auto iter = simplices.begin();
-                divide_simplex(*iter++, &tmp);
-                tmp.reserve(simplices.size() * tmp.size());
-                for (; iter != simplices.end(); ++iter)
+                tmp.reserve(simplices.size() * simplex_subdivision.simplices().size());
+
+                for (const Simplex<N, T>& simplex : simplices)
                 {
-                        divide_simplex(*iter, &tmp);
+                        for (unsigned i = 0; i < N; ++i)
+                        {
+                                points[i] = simplex.vertices[i];
+                        }
+                        for (unsigned i = 0; i < simplex_subdivision.midpoints().size(); ++i)
+                        {
+                                int v0 = simplex_subdivision.midpoints()[i][0];
+                                int v1 = simplex_subdivision.midpoints()[i][1];
+                                points[N + i] = (simplex.vertices[v0] + simplex.vertices[v1]).normalized();
+                        }
+                        for (const std::array<int, N>& indices : simplex_subdivision.simplices())
+                        {
+                                Simplex<N, T>& s = tmp.emplace_back();
+                                for (unsigned i = 0; i < N; ++i)
+                                {
+                                        s.vertices[i] = points[indices[i]];
+                                }
+                        }
                 }
                 simplices = std::move(tmp);
         }
@@ -250,7 +327,7 @@ std::vector<Simplex<N, T>> create_sphere(unsigned facet_min_count)
                 }
         }
 
-        return divide_simplices(facet_min_count, std::move(simplices));
+        return divide_simplices(facet_min_count, SimplexSubdivision<N - 1>(), std::move(simplices));
 }
 
 template <std::size_t N, typename T>
