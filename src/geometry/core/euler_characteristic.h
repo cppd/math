b77f6ff68fa7a2f880c93feadb9d2584d452c5d4
cppd <cppd@users.noreply.github.com>
2021-02-23 11:48:04 +0300

    Euler characteristic

---
+++ src/geometry/core/euler_characteristic.h
@@ -0,0 +1,100 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <src/com/combinatorics.h>
+#include <src/com/hash.h>
+#include <src/com/sort.h>
+
+#include <array>
+#include <unordered_set>
+#include <vector>
+
+namespace ns::geometry
+{
+template <std::size_t D, std::size_t N>
+long long simplex_count(const std::vector<std::array<int, N>>& facets)
+{
+        static_assert(D < N);
+
+        struct Hash
+        {
+                std::size_t operator()(const std::array<int, D + 1>& v) const
+                {
+                        return array_hash(v);
+                }
+        };
+
+        const std::array<std::array<unsigned char, D + 1>, binomial<N, D + 1>()> simplices = combinations<N, D + 1>();
+
+        std::unordered_set<std::array<int, D + 1>, Hash> simplex_set;
+
+        for (std::array<int, N> facet : facets)
+        {
+                facet = sort(std::move(facet));
+                for (const std::array<unsigned char, D + 1>& simplex : simplices)
+                {
+                        std::array<int, D + 1> simplex_vertex_indices;
+                        for (unsigned i = 0; i < D + 1; ++i)
+                        {
+                                simplex_vertex_indices[i] = facet[simplex[i]];
+                        }
+                        simplex_set.insert(simplex_vertex_indices);
+                }
+        }
+
+        return simplex_set.size();
+}
+
+namespace euler_characteristic_implementation
+{
+template <std::size_t N, std::size_t... I>
+int euler_characteristic(const std::vector<std::array<int, N>>& facets, std::integer_sequence<std::size_t, I...>)
+{
+        static_assert(sizeof...(I) == N);
+
+        return ((((I & 1) ? -1 : 1) * simplex_count<I>(facets)) + ...);
+}
+
+template <std::size_t N, std::size_t... I>
+std::array<long long, N> simplex_counts(
+        const std::vector<std::array<int, N>>& facets,
+        std::integer_sequence<std::size_t, I...>)
+{
+        static_assert(sizeof...(I) == N);
+
+        return {simplex_count<I>(facets)...};
+}
+}
+
+template <std::size_t N>
+int euler_characteristic(const std::vector<std::array<int, N>>& facets)
+{
+        namespace impl = euler_characteristic_implementation;
+
+        return impl::euler_characteristic(facets, std::make_integer_sequence<std::size_t, N>());
+}
+
+template <std::size_t N>
+std::array<long long, N> simplex_counts(const std::vector<std::array<int, N>>& facets)
+{
+        namespace impl = euler_characteristic_implementation;
+
+        return impl::simplex_counts(facets, std::make_integer_sequence<std::size_t, N>());
+}
+}
