d2fa6baf9cd00cddaf860fbe1609c1fa82e2f01a
cppd <cppd@users.noreply.github.com>
2021-03-10 09:59:10 +0300

    union-find test

---
+++ src/com/union_find.cpp
@@ -0,0 +1,62 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "union_find.h"
+
+#include "error.h"
+
+#include <src/test/test.h>
+
+namespace ns
+{
+namespace
+{
+void check_true(bool v)
+{
+        if (!v)
+        {
+                error("No connection found");
+        }
+}
+
+void check_false(bool v)
+{
+        if (v)
+        {
+                error("Unexpected connection");
+        }
+}
+
+void test()
+{
+        UnionFind<int> uf(10);
+
+        check_true(uf.add_connection(0, 1));
+        check_true(uf.add_connection(1, 2));
+        check_true(uf.add_connection(2, 3));
+        check_true(uf.add_connection(4, 5));
+        check_true(uf.add_connection(6, 7));
+        check_true(uf.add_connection(2, 7));
+
+        check_false(uf.add_connection(1, 3));
+        check_false(uf.add_connection(0, 6));
+        check_false(uf.add_connection(1, 7));
+}
+
+TEST_SMALL("Union-Find", test)
+}
+}
