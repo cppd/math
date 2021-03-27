65afc7c094a32619bdd78ac422207ae0d0a0ead7
cppd <cppd@users.noreply.github.com>
2021-03-27 12:41:13 +0300

    painter tests

--- src/painter/test/test_painter.cpp
+++ src/painter/test/test_painter.cpp
@@ -22,19 +22,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../shapes/mesh.h"
 
 #include <src/com/file/path.h>
-#include <src/com/global_index.h>
 #include <src/com/names.h>
 #include <src/com/random/engine.h>
 #include <src/com/string/str.h>
 #include <src/com/time.h>
 #include <src/geometry/shapes/sphere_create.h>
-#include <src/gui/com/support.h>
 #include <src/gui/painter_window/painter_window.h>
+#include <src/image/alpha.h>
+#include <src/image/file.h>
+#include <src/image/flip.h>
 #include <src/model/mesh_utility.h>
 #include <src/model/volume_utility.h>
 
-#include <array>
-#include <cmath>
 #include <filesystem>
 #include <random>
 
@@ -47,18 +46,34 @@ constexpr Color DEFAULT_COLOR = Color(Srgb8(150, 170, 150));
 constexpr Color::DataType METALNESS = 0;
 constexpr Color::DataType LIGHTING_INTENSITY = 1;
 
-template <std::size_t N>
-class Images final : public Notifier<N>
-{
-        static constexpr const char* DIRECTORY_NAME = "painter_test";
-        static constexpr const char* IMAGE_FILE_FORMAT = "png";
+constexpr const char* DIRECTORY_NAME = "painter_test";
+constexpr const char* IMAGE_FILE_FORMAT = "png";
+constexpr image::ColorFormat PIXEL_COLOR_FORMAT = image::ColorFormat::R8G8B8_SRGB;
 
-        const GlobalIndex<N, long long> m_global_index;
-        const std::array<int, N> m_screen_size;
+template <std::size_t N>
+void save_image(const std::filesystem::path& path, const Color& blend_color, image::Image<N>&& image)
+{
+        image::flip_vertically(&image);
+        image::blend_alpha(&image.color_format, image.pixels, blend_color);
+
+        {
+                std::vector<std::byte> pixels;
+                format_conversion(image.color_format, image.pixels, PIXEL_COLOR_FORMAT, &pixels);
+                image.color_format = PIXEL_COLOR_FORMAT;
+                image.pixels = std::move(pixels);
+        }
+
+        ProgressRatio progress(nullptr);
+        volume::save_to_images(path, IMAGE_FILE_FORMAT, image::ImageView(image), &progress);
+}
+
+template <std::size_t N>
+class Image final : public Notifier<N>
+{
+        const std::filesystem::path m_path;
         const Color m_background_color;
 
-        std::vector<std::byte> m_pixels;
-        std::filesystem::path m_directory;
+        std::optional<image::Image<N>> m_image;
 
         void thread_busy(unsigned, const std::array<int, N>&) override
         {
@@ -68,37 +83,13 @@ class Images final : public Notifier<N>
         {
         }
 
-        void pixel_set(const std::array<int, N>& pixel, const Color& color, Color::DataType alpha) override
+        void pixel_set(const std::array<int, N>&, const Color&, Color::DataType) override
         {
-                std::array<int, N> p = pixel;
-                p[1] = m_screen_size[1] - 1 - pixel[1];
-
-                const Color c = [&]
-                {
-                        if (alpha >= 1)
-                        {
-                                return color;
-                        }
-                        if (alpha <= 0)
-                        {
-                                return m_background_color;
-                        }
-                        return color + (1 - alpha) * m_background_color;
-                }();
-                unsigned char r = color::linear_float_to_srgb_uint8(c.red());
-                unsigned char g = color::linear_float_to_srgb_uint8(c.green());
-                unsigned char b = color::linear_float_to_srgb_uint8(c.blue());
-
-                std::size_t offset = 3 * m_global_index.compute(pixel);
-                ASSERT(offset + 2 < m_pixels.size());
-                std::byte* ptr = m_pixels.data() + offset;
-                std::memcpy(ptr++, &r, 1);
-                std::memcpy(ptr++, &g, 1);
-                std::memcpy(ptr++, &b, 1);
         }
 
-        void pass_done(image::Image<N>&&) override
+        void pass_done(image::Image<N>&& image) override
         {
+                m_image = std::move(image);
         }
 
         void error_message(const std::string& msg) override
@@ -107,34 +98,33 @@ class Images final : public Notifier<N>
         }
 
 public:
-        Images(const std::array<int, N>& screen_size, const Color& background_color)
-                : m_global_index(screen_size), m_screen_size(screen_size), m_background_color(background_color)
+        explicit Image(const std::string& directory_name, const Color& background_color)
+                : m_path(std::filesystem::temp_directory_path() / path_from_utf8(directory_name)),
+                  m_background_color(background_color)
         {
-                if (!std::all_of(
-                            screen_size.cbegin(), screen_size.cend(),
-                            [](int v)
-                            {
-                                    return v > 0;
-                            }))
+                if (!std::filesystem::create_directory(m_path))
                 {
-                        error("Error screen size " + to_string(screen_size));
-                }
-
-                m_pixels.resize(3 * m_global_index.count(), std::byte(0));
-
-                m_directory = std::filesystem::temp_directory_path() / DIRECTORY_NAME;
-                if (!std::filesystem::create_directory(m_directory))
-                {
-                        error("Error creating directory " + generic_utf8_filename(m_directory));
+                        error("Error creating directory " + generic_utf8_filename(m_path));
                 }
         }
 
-        void write_to_files() const
+        void write_to_files()
         {
-                ProgressRatio progress(nullptr);
-                volume::save_to_images(
-                        m_directory, IMAGE_FILE_FORMAT,
-                        image::ImageView<N>(m_screen_size, image::ColorFormat::R8G8B8_SRGB, m_pixels), &progress);
+                if (!m_image)
+                {
+                        error("No painter image to write to files");
+                }
+
+                try
+                {
+                        save_image(m_path, m_background_color, std::move(*m_image));
+                }
+                catch (...)
+                {
+                        m_image.reset();
+                        throw;
+                }
+                m_image.reset();
         }
 };
 
@@ -171,6 +161,7 @@ template <std::size_t N>
 std::unique_ptr<const mesh::Mesh<N>> sphere_mesh(int facet_count)
 {
         LOG("creating sphere in " + space_name(N) + "...");
+
         std::vector<Vector<N, float>> vertices;
         std::vector<std::array<int, N>> facets;
         geometry::create_sphere(facet_count, &vertices, &facets);
@@ -201,22 +192,22 @@ std::unique_ptr<const mesh::Mesh<N>> file_mesh(const std::string& file_name, Pro
 template <std::size_t N, typename T>
 void test_painter_file(int samples_per_pixel, int thread_count, std::unique_ptr<const Scene<N, T>>&& scene)
 {
-        constexpr int max_pass_count = 1;
-        constexpr bool smooth_normal = true;
+        constexpr int MAX_PASS_COUNT = 1;
+        constexpr bool SMOOTH_NORMAL = true;
 
-        Images images(scene->projector().screen_size(), scene->background_color());
+        Image<N - 1> image(DIRECTORY_NAME, scene->background_color());
 
         LOG("Painting...");
         TimePoint start_time = time();
         {
                 std::unique_ptr<Painter<N, T>> painter = create_painter<N, T>(
-                        &images, samples_per_pixel, max_pass_count, std::move(scene), thread_count, smooth_normal);
+                        &image, samples_per_pixel, MAX_PASS_COUNT, std::move(scene), thread_count, SMOOTH_NORMAL);
                 painter->wait();
         }
         LOG("Painted, " + to_string_fixed(duration_from(start_time), 5) + " s");
 
         LOG("Writing screen images to files...");
-        images.write_to_files();
+        image.write_to_files();
 
         LOG("Done");
 }
@@ -224,7 +215,7 @@ void test_painter_file(int samples_per_pixel, int thread_count, std::unique_ptr<
 template <std::size_t N, typename T>
 void test_painter_window(int samples_per_pixel, int thread_count, std::unique_ptr<const Scene<N, T>>&& scene)
 {
-        constexpr bool smooth_normal = true;
+        constexpr bool SMOOTH_NORMAL = true;
 
         LOG("Window painting...");
 
@@ -233,7 +224,7 @@ void test_painter_window(int samples_per_pixel, int thread_count, std::unique_pt
         std::string name = "Path Tracing In " + to_upper_first_letters(space_name(N));
 
         gui::painter_window::create_painter_window(
-                name, thread_count, samples_per_pixel, smooth_normal, std::move(scene));
+                name, thread_count, samples_per_pixel, SMOOTH_NORMAL, std::move(scene));
 }
 
 enum class PainterTestOutputType
