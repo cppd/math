94bdad21fd43b82ea818c492fa7f92607674e63f
cppd <cppd@users.noreply.github.com>
2021-04-05 09:48:33 +0300

    painter statistics

--- src/painter/painter/statistics.h
+++ src/painter/painter/statistics.h
@@ -33,9 +33,9 @@ class PaintingStatistics
 
         const long long m_screen_pixel_count;
 
-        std::atomic<long long> m_pixel_count;
-        std::atomic<long long> m_ray_count;
-        std::atomic<long long> m_sample_count;
+        std::atomic<long long> m_pixel_counter;
+        std::atomic<long long> m_ray_counter;
+        std::atomic<long long> m_sample_counter;
 
         long long m_pass_number;
         TimePoint m_pass_start_time;
@@ -54,9 +54,9 @@ public:
         {
                 std::lock_guard lg(m_lock);
 
-                m_pixel_count = 0;
-                m_ray_count = 0;
-                m_sample_count = 0;
+                m_pixel_counter = 0;
+                m_ray_counter = 0;
+                m_sample_counter = 0;
 
                 m_pass_number = 1;
                 m_pass_start_time = time();
@@ -66,9 +66,9 @@ public:
 
         void pixel_done(int ray_count, int sample_count)
         {
-                m_pixel_count.fetch_add(1, std::memory_order_relaxed);
-                m_ray_count.fetch_add(ray_count, std::memory_order_relaxed);
-                m_sample_count.fetch_add(sample_count, std::memory_order_relaxed);
+                m_pixel_counter.fetch_add(1, std::memory_order_relaxed);
+                m_ray_counter.fetch_add(ray_count, std::memory_order_relaxed);
+                m_sample_counter.fetch_add(sample_count, std::memory_order_relaxed);
         }
 
         void pass_done()
@@ -83,7 +83,7 @@ public:
                 std::lock_guard lg(m_lock);
                 ++m_pass_number;
                 m_pass_start_time = time();
-                m_pass_start_pixel_count = m_pixel_count;
+                m_pass_start_pixel_count = m_pixel_counter;
         }
 
         Statistics statistics() const
@@ -93,12 +93,13 @@ public:
                 std::lock_guard lg(m_lock);
 
                 s.pass_number = m_pass_number;
-                s.pass_progress = static_cast<double>(m_pixel_count - m_pass_start_pixel_count) / m_screen_pixel_count;
+                s.pass_progress =
+                        static_cast<double>(m_pixel_counter - m_pass_start_pixel_count) / m_screen_pixel_count;
                 s.previous_pass_duration = m_previous_pass_duration;
 
-                s.pixel_count = m_pixel_count;
-                s.ray_count = m_ray_count;
-                s.sample_count = m_sample_count;
+                s.pixel_count = m_pixel_counter;
+                s.ray_count = m_ray_counter;
+                s.sample_count = m_sample_counter;
 
                 return s;
         }
