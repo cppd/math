42416dccf4e72dae40dc89179360d1df5905a32e
cppd <cppd@users.noreply.github.com>
2021-04-28 08:04:44 +0300

    identity

---
+++ src/numerical/identity.h
@@ -0,0 +1,61 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "vec.h"
+
+#include <array>
+#include <utility>
+
+namespace ns::numerical
+{
+namespace identity_implementation
+{
+template <std::size_t ValueIndex, typename T, int... I>
+constexpr Vector<sizeof...(I), T> make_vector_one_value(std::integer_sequence<int, I...>, const T& value)
+{
+        static_assert(ValueIndex >= 0 && ValueIndex < sizeof...(I));
+
+        return Vector<sizeof...(I), T>((I == ValueIndex ? value : 0)...);
+}
+
+template <std::size_t N, typename T, std::size_t ValueIndex>
+constexpr Vector<N, T> make_vector_one_value(const T& value)
+{
+        return make_vector_one_value<ValueIndex>(std::make_integer_sequence<int, N>(), value);
+}
+
+template <typename T, int... I>
+constexpr std::array<Vector<sizeof...(I), T>, sizeof...(I)> make_array_of_vector_one_value(
+        std::integer_sequence<int, I...>,
+        const T& value)
+{
+        return {make_vector_one_value<sizeof...(I), T, I>(value)...};
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<Vector<N, T>, N> make_array_of_vector_one_value(const T& value)
+{
+        return make_array_of_vector_one_value(std::make_integer_sequence<int, N>(), value);
+}
+}
+
+template <std::size_t N, typename T>
+inline constexpr std::array<Vector<N, T>, N> identity_array =
+        identity_implementation::make_array_of_vector_one_value<N, T>(1);
+}
---
+++ src/numerical/test/test_identity.cpp
@@ -0,0 +1,61 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../identity.h"
+
+namespace ns::numerical
+{
+template <std::size_t N, typename T>
+constexpr bool test()
+{
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                for (std::size_t j = 0; j < N; ++j)
+                {
+                        if (i != j)
+                        {
+                                if (!(identity_array<N, T>[i][j] == 0))
+                                {
+                                        return false;
+                                }
+                        }
+                        else
+                        {
+                                if (!(identity_array<N, T>[i][j] == 1))
+                                {
+                                        return false;
+                                }
+                        }
+                }
+        }
+        return true;
+}
+
+template <std::size_t N>
+constexpr bool test()
+{
+        static_assert(test<N, int>());
+        static_assert(test<N, float>());
+        static_assert(test<N, double>());
+        return true;
+}
+
+static_assert(test<2>());
+static_assert(test<3>());
+static_assert(test<4>());
+static_assert(test<5>());
+}
