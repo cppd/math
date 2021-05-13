8407c686dca9999724c0f9d69dba3b5466d4d347
cppd <cppd@users.noreply.github.com>
2021-05-13 09:36:16 +0300

    painter window

--- src/gui/painter_window/process.cpp
+++ src/gui/painter_window/process.cpp
@@ -33,8 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/process/load.h>
 #include <src/progress/progress.h>
 
-#include <cstring>
-
 namespace ns::gui::painter_window
 {
 namespace
@@ -51,6 +49,107 @@ std::array<T, N> to_array(const std::vector<T>& vector)
                 array[i] = vector[i];
         }
         return array;
+}
+
+template <std::size_t N_IMAGE>
+image::Image<N_IMAGE> create_image(
+        const bool delete_alpha,
+        const std::array<int, N_IMAGE>& size,
+        const Color& background,
+        const image::ColorFormat color_format,
+        const dialog::PainterImageParameters& parameters,
+        std::vector<std::byte>&& pixels)
+{
+        static_assert(N_IMAGE >= 2);
+
+        image::Image<N_IMAGE> image;
+        image.size = size;
+        image.color_format = color_format;
+        image.pixels = std::move(pixels);
+
+        ASSERT(image::format_component_count(color_format) == 4);
+
+        if (parameters.with_background && !*parameters.grayscale)
+        {
+                image::blend_alpha(&image.color_format, image.pixels, background);
+                if (delete_alpha)
+                {
+                        image = image::delete_alpha(image);
+                }
+                else
+                {
+                        constexpr float ALPHA = 1;
+                        image::set_alpha(image.color_format, image.pixels, ALPHA);
+                }
+        }
+        else if (parameters.with_background && *parameters.grayscale)
+        {
+                image::blend_alpha(&image.color_format, image.pixels, background);
+                image::make_grayscale(image.color_format, image.pixels);
+                image = image::convert_to_r_component_format(image);
+        }
+        else if (!parameters.with_background && *parameters.grayscale)
+        {
+                image::make_grayscale(image.color_format, image.pixels);
+                image::blend_alpha(&image.color_format, image.pixels, Color(0));
+                image = image::convert_to_r_component_format(image);
+        }
+
+        ASSERT(!(*parameters.grayscale && parameters.convert_to_8_bit));
+        if (parameters.convert_to_8_bit)
+        {
+                image = image::convert_to_8_bit(image);
+        }
+
+        return image;
+}
+
+template <std::size_t N_IMAGE>
+void save_image(
+        const std::array<int, N_IMAGE>& size,
+        const Color& background,
+        const image::ColorFormat color_format,
+        const dialog::PainterImageParameters& parameters,
+        std::vector<std::byte>&& pixels,
+        ProgressRatio* progress)
+{
+        static_assert(N_IMAGE >= 2);
+
+        constexpr bool DELETE_ALPHA = true;
+
+        image::Image<N_IMAGE> image =
+                create_image(DELETE_ALPHA, size, background, color_format, parameters, std::move(pixels));
+
+        image::flip_vertically(&image);
+
+        std::filesystem::path path = path_from_utf8(*parameters.path_string);
+        image::ImageView<N_IMAGE> image_view(image);
+        if constexpr (N_IMAGE == 2)
+        {
+                image::save(path, image_view);
+        }
+        else
+        {
+                volume::save_to_images(path, IMAGE_FILE_FORMAT, image_view, progress);
+        }
+}
+
+template <std::size_t N_IMAGE>
+void add_volume(
+        const std::array<int, N_IMAGE>& size,
+        const Color& background,
+        const image::ColorFormat color_format,
+        const dialog::PainterImageParameters& parameters,
+        std::vector<std::byte>&& pixels)
+{
+        static_assert(N_IMAGE >= 3);
+
+        constexpr bool DELETE_ALPHA = false;
+
+        image::Image<N_IMAGE> image =
+                create_image(DELETE_ALPHA, size, background, color_format, parameters, std::move(pixels));
+
+        process::load_volume<N_IMAGE>("Painter Image", std::move(image));
 }
 }
 
@@ -70,31 +169,17 @@ std::function<void(ProgressRatioList*)> save_image(
         ASSERT(parameters->path_string.has_value());
         ASSERT(!parameters->grayscale.has_value());
 
+        parameters->grayscale = false;
+
         return [=, pixels_ptr = std::make_shared<std::vector<std::byte>>(std::move(pixels)),
                 parameters = std::move(*parameters)](ProgressRatioList* progress_list)
         {
                 ProgressRatio progress(progress_list, "Saving");
                 progress.set(0);
 
-                image::Image<2> image;
-                image.size = {width, height};
-                image.color_format = color_format;
-                image.pixels = std::move(*pixels_ptr);
+                const std::array<int, 2> array_size{width, height};
 
-                ASSERT(image::format_component_count(color_format) == 4);
-                if (parameters.with_background)
-                {
-                        image::blend_alpha(&image.color_format, image.pixels, background);
-                        image = image::delete_alpha(image);
-                }
-
-                if (parameters.convert_to_8_bit)
-                {
-                        image = image::convert_to_8_bit(image);
-                }
-
-                image::flip_vertically(&image);
-                image::save(path_from_utf8(*parameters.path_string), image::ImageView<2>(image));
+                save_image(array_size, background, color_format, parameters, std::move(*pixels_ptr), &progress);
         };
 }
 
@@ -121,9 +206,6 @@ std::function<void(ProgressRatioList*)> save_image(
         return [=, pixels_ptr = std::make_shared<std::vector<std::byte>>(std::move(pixels)),
                 parameters = std::move(*parameters)](ProgressRatioList* progress_list)
         {
-                ProgressRatio progress(progress_list, "Saving");
-                progress.set(0);
-
                 const int N = size.size() + 1;
                 process::apply_for_dimension(
                         N,
@@ -132,41 +214,14 @@ std::function<void(ProgressRatioList*)> save_image(
                                 constexpr int N_IMAGE = N - 1;
                                 if constexpr (N_IMAGE >= 3)
                                 {
-                                        image::Image<N_IMAGE> image;
-                                        image.size = to_array<N_IMAGE, int>(size);
-                                        image.color_format = color_format;
-                                        image.pixels = std::move(*pixels_ptr);
+                                        ProgressRatio progress(progress_list, "Saving");
+                                        progress.set(0);
 
-                                        ASSERT(image::format_component_count(color_format) == 4);
+                                        const std::array<int, N_IMAGE> array_size = to_array<N_IMAGE, int>(size);
 
-                                        if (parameters.with_background && !*parameters.grayscale)
-                                        {
-                                                image::blend_alpha(&image.color_format, image.pixels, background);
-                                                image = image::delete_alpha(image);
-                                        }
-                                        else if (parameters.with_background && *parameters.grayscale)
-                                        {
-                                                image::blend_alpha(&image.color_format, image.pixels, background);
-                                                image::make_grayscale(image.color_format, image.pixels);
-                                                image = image::convert_to_r_component_format(image);
-                                        }
-                                        else if (!parameters.with_background && *parameters.grayscale)
-                                        {
-                                                image::make_grayscale(image.color_format, image.pixels);
-                                                image::blend_alpha(&image.color_format, image.pixels, Color(0));
-                                                image = image::convert_to_r_component_format(image);
-                                        }
-
-                                        ASSERT(!(*parameters.grayscale && parameters.convert_to_8_bit));
-                                        if (parameters.convert_to_8_bit)
-                                        {
-                                                image = image::convert_to_8_bit(image);
-                                        }
-
-                                        image::flip_vertically(&image);
-                                        volume::save_to_images(
-                                                path_from_utf8(*parameters.path_string), IMAGE_FILE_FORMAT,
-                                                image::ImageView<N_IMAGE>(image), &progress);
+                                        save_image(
+                                                array_size, background, color_format, parameters,
+                                                std::move(*pixels_ptr), &progress);
                                 }
                         });
         };
@@ -195,9 +250,6 @@ std::function<void(ProgressRatioList*)> add_volume(
         return [=, pixels_ptr = std::make_shared<std::vector<std::byte>>(std::move(pixels)),
                 parameters = std::move(*parameters)](ProgressRatioList* progress_list)
         {
-                ProgressRatio progress(progress_list, "Adding volume");
-                progress.set(0);
-
                 const int N = size.size() + 1;
                 process::apply_for_dimension(
                         N,
@@ -206,42 +258,16 @@ std::function<void(ProgressRatioList*)> add_volume(
                                 constexpr int N_IMAGE = N - 1;
                                 if constexpr (N_IMAGE >= 3)
                                 {
-                                        image::Image<N_IMAGE> image;
-                                        image.size = to_array<N_IMAGE, int>(size);
-                                        image.color_format = color_format;
-                                        image.pixels = std::move(*pixels_ptr);
+                                        ProgressRatio progress(progress_list, "Adding volume");
+                                        progress.set(0);
 
-                                        ASSERT(image::format_component_count(color_format) == 4);
+                                        const std::array<int, N_IMAGE> array_size = to_array<N_IMAGE, int>(size);
 
-                                        if (parameters.with_background && !*parameters.grayscale)
-                                        {
-                                                image::blend_alpha(&image.color_format, image.pixels, background);
-                                                constexpr float ALPHA = 1;
-                                                image::set_alpha(image.color_format, image.pixels, ALPHA);
-                                        }
-                                        else if (parameters.with_background && *parameters.grayscale)
-                                        {
-                                                image::blend_alpha(&image.color_format, image.pixels, background);
-                                                image::make_grayscale(image.color_format, image.pixels);
-                                                image = image::convert_to_r_component_format(image);
-                                        }
-                                        else if (!parameters.with_background && *parameters.grayscale)
-                                        {
-                                                image::make_grayscale(image.color_format, image.pixels);
-                                                image::blend_alpha(&image.color_format, image.pixels, Color(0));
-                                                image = image::convert_to_r_component_format(image);
-                                        }
-
-                                        ASSERT(!(*parameters.grayscale && parameters.convert_to_8_bit));
-                                        if (parameters.convert_to_8_bit)
-                                        {
-                                                image = image::convert_to_8_bit(image);
-                                        }
-
-                                        process::load_volume<N_IMAGE>("Painter Image", std::move(image));
+                                        add_volume<N_IMAGE>(
+                                                array_size, background, color_format, parameters,
+                                                std::move(*pixels_ptr));
                                 }
                         });
         };
 }
-
 }
