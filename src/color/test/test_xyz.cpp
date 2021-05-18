2b45b7ce00f34d99f40379c7ca3fe6d2b8c37666
cppd <cppd@users.noreply.github.com>
2021-05-18 09:53:24 +0300

    XYZ functions

--- src/color/test/test_xyz.cpp
+++ src/color/test/test_xyz.cpp
@@ -19,70 +19,191 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <src/com/error.h>
 #include <src/com/log.h>
-#include <src/com/print.h>
+#include <src/com/type/limit.h>
 #include <src/test/test.h>
+
+#include <array>
+#include <sstream>
 
 namespace ns::color
 {
 namespace
 {
-template <typename T>
-void check_31(T w, T x, T y, T z)
+struct Entry
 {
-        const auto check = [&w](const char* name, T f, T tab)
-        {
-                T abs_error = std::abs(f - tab);
-                if (!(abs_error < T(0.01)))
-                {
-                        error("XYZ-31, wavelength = " + to_string(w) + ", " + name + " = " + to_string(f)
-                              + ", tab = " + to_string(tab) + ", error = " + to_string(abs_error));
-                }
-        };
+        double w;
+        double x;
+        double y;
+        double z;
+};
 
-        check("x", x_31(w), x);
-        check("y", y_31(w), y);
-        check("z", z_31(w), z);
+// clang-format off
+constexpr std::array DATA_31 = std::to_array<Entry>
+({
+        {380, 0.001368, 0.000039, 0.006450},
+        {400, 0.014310, 0.000396, 0.067850},
+        {420, 0.134380, 0.004000, 0.645600},
+        {440, 0.348280, 0.023000, 1.747060},
+        {460, 0.290800, 0.060000, 1.669200},
+        {480, 0.095640, 0.139020, 0.812950},
+        {500, 0.004900, 0.323000, 0.272000},
+        {520, 0.063270, 0.710000, 0.078250},
+        {540, 0.290400, 0.954000, 0.020300},
+        {560, 0.594500, 0.995000, 0.003900},
+        {580, 0.916300, 0.870000, 0.001650},
+        {600, 1.062200, 0.631000, 0.000800},
+        {620, 0.854450, 0.381000, 0.000190},
+        {640, 0.447900, 0.175000, 0.000020},
+        {660, 0.164900, 0.061000, 0.000000},
+        {680, 0.046770, 0.017000, 0.000000},
+        {700, 0.011359, 0.004102, 0.000000},
+        {720, 0.002899, 0.001047, 0.000000},
+        {740, 0.000690, 0.000249, 0.000000},
+        {760, 0.000166, 0.000060, 0.000000},
+        {780, 0.000042, 0.000015, 0.000000}
+});
+constexpr std::array DATA_64 = std::to_array<Entry>
+({
+        {380, 0.000160, 0.000017, 0.000705},
+        {400, 0.019110, 0.002004, 0.086011},
+        {420, 0.204492, 0.021391, 0.972542},
+        {440, 0.383734, 0.062077, 1.967280},
+        {460, 0.302273, 0.128201, 1.745370},
+        {480, 0.080507, 0.253589, 0.772125},
+        {500, 0.003816, 0.460777, 0.218502},
+        {520, 0.117749, 0.761757, 0.060709},
+        {540, 0.376772, 0.961988, 0.013676},
+        {560, 0.705224, 0.997340, 0.000000},
+        {580, 1.014160, 0.868934, 0.000000},
+        {600, 1.123990, 0.658341, 0.000000},
+        {620, 0.856297, 0.398057, 0.000000},
+        {640, 0.431567, 0.179828, 0.000000},
+        {660, 0.152568, 0.060281, 0.000000},
+        {680, 0.040851, 0.015905, 0.000000},
+        {700, 0.009577, 0.003718, 0.000000},
+        {720, 0.002175, 0.000846, 0.000000},
+        {740, 0.000508, 0.000199, 0.000000},
+        {760, 0.000126, 0.000050, 0.000000},
+        {780, 0.000033, 0.000013, 0.000000}
+});
+// clang-format on
+
+template <XYZ xyz>
+constexpr const auto& data()
+{
+        static_assert(xyz == XYZ_31 || xyz == XYZ_64);
+        switch (xyz)
+        {
+        case XYZ_31:
+                return DATA_31;
+        case XYZ_64:
+                return DATA_64;
+        }
 }
 
-template <typename T>
-void check_31()
+std::ostream& operator<<(std::ostream& os, XYZ xyz)
 {
-        check_31<T>(380, 0.001368, 0.000039, 0.006450);
-        check_31<T>(400, 0.014310, 0.000396, 0.067850);
-        check_31<T>(450, 0.336200, 0.038000, 1.772110);
-        check_31<T>(500, 0.004900, 0.323000, 0.272000);
-        check_31<T>(550, 0.433450, 0.994950, 0.008750);
-        check_31<T>(600, 1.062200, 0.631000, 0.000800);
-        check_31<T>(650, 0.283500, 0.107000, 0.000000);
-        check_31<T>(700, 0.011359, 0.004102, 0.000000);
-        check_31<T>(750, 0.000332, 0.000120, 0.000000);
-        check_31<T>(780, 0.000042, 0.000015, 0.000000);
+        switch (xyz)
+        {
+        case XYZ_31:
+                return (os << "XYZ 31");
+        case XYZ_64:
+                return (os << "XYZ 64");
+        }
+        error_fatal("Unknown XYZ type " + std::to_string(static_cast<long long>(xyz)));
+}
 
+template <XYZ xyz, typename T>
+void check_non_negative()
+{
         for (int w = 3800; w <= 7800; ++w)
         {
                 T t = T(w) / 10;
-                T x = x_31(t);
-                T y = y_31(t);
-                T z = z_31(t);
+                T x = cie_x<xyz>(t);
+                T y = cie_y<xyz>(t);
+                T z = cie_z<xyz>(t);
                 if (!(x >= 0 && y >= 0 && z >= 0))
                 {
-                        error("XYZ-31, approximation is not non-negative, x = " + to_string(x) + ", y = " + to_string(y)
-                              + ", z = " + to_string(z));
+                        std::ostringstream oss;
+                        oss.precision(limits<T>::max_digits10);
+                        oss << xyz << ", approximation is not non-negative";
+                        oss << ", x = " << x;
+                        oss << ", y = " << y;
+                        oss << ", z = " << z;
+                        error(oss.str());
                 }
         }
 }
 
-void test_31()
+template <XYZ xyz, typename T>
+T check(const char* name, T wave, T f, T tab, T max_error)
 {
-        LOG("Test XYZ-31");
+        T abs_error = std::abs(f - tab);
+        if (!(abs_error < max_error))
+        {
+                std::ostringstream oss;
+                oss.precision(limits<T>::max_digits10);
+                oss << xyz;
+                oss << ", wavelength = " << wave;
+                oss << ", " << name << " = " << f;
+                oss << ", tab = " << tab;
+                oss << ", error = " << abs_error;
+                error(oss.str());
+        }
+        return abs_error;
+};
 
-        check_31<float>();
-        check_31<double>();
-        check_31<long double>();
+template <XYZ xyz, typename T>
+void check_mean(const char* name, T mean, T max_error)
+{
+        if (!(mean < max_error))
+        {
+                std::ostringstream oss;
+                oss.precision(limits<T>::max_digits10);
+                oss << xyz << ", mean error " << name << " = " << mean;
+                error(oss.str());
+        }
+};
 
-        LOG("Test XYZ-31 passed");
+template <XYZ xyz, typename T>
+void check(T max_error_x, T max_error_y, T max_error_z, T max_mean_error_x, T max_mean_error_y, T max_mean_error_z)
+{
+        T sum_x = 0;
+        T sum_y = 0;
+        T sum_z = 0;
+
+        for (const Entry& entry : data<xyz>())
+        {
+                sum_x += check<xyz, T>("x", entry.w, cie_x<xyz>(entry.w), entry.x, max_error_x);
+                sum_y += check<xyz, T>("y", entry.w, cie_y<xyz>(entry.w), entry.y, max_error_y);
+                sum_z += check<xyz, T>("z", entry.w, cie_z<xyz>(entry.w), entry.z, max_error_z);
+        }
+
+        check_mean<xyz, T>("x", sum_x / data<xyz>().size(), max_mean_error_x);
+        check_mean<xyz, T>("y", sum_y / data<xyz>().size(), max_mean_error_y);
+        check_mean<xyz, T>("z", sum_z / data<xyz>().size(), max_mean_error_z);
+
+        check_non_negative<xyz, T>();
 }
 
-TEST_SMALL("XYZ-31", test_31)
+template <typename T>
+void check()
+{
+        check<XYZ_31, T>(0.0110, 0.0074, 0.0140, 0.0050, 0.0021, 0.0022);
+        check<XYZ_64, T>(0.0470, 0.0220, 0.0400, 0.0110, 0.0096, 0.0081);
+}
+
+void test()
+{
+        LOG("Test XYZ functions");
+
+        check<float>();
+        check<double>();
+        check<long double>();
+
+        LOG("Test XYZ functions passed");
+}
+
+TEST_SMALL("XYZ Functions", test)
 }
 }
--- src/color/xyz.h
+++ src/color/xyz.h
@@ -35,18 +35,15 @@ T g(T wave_in_nanometers, T m, T t1, T t2)
         T v = (wave_in_nanometers - m) * (wave_in_nanometers < m ? t1 : t2);
         return std::exp(T(-0.5) * v * v);
 }
-}
 
 template <typename T>
 T x_31(T wave_in_nanometers)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        namespace impl = xyz_implementation;
-
-        T g1 = impl::g(wave_in_nanometers, T(442.0), T(0.0624), T(0.0374));
-        T g2 = impl::g(wave_in_nanometers, T(599.8), T(0.0264), T(0.0323));
-        T g3 = impl::g(wave_in_nanometers, T(501.1), T(0.0490), T(0.0382));
+        T g1 = g(wave_in_nanometers, T(442.0), T(0.0624), T(0.0374));
+        T g2 = g(wave_in_nanometers, T(599.8), T(0.0264), T(0.0323));
+        T g3 = g(wave_in_nanometers, T(501.1), T(0.0490), T(0.0382));
         return T(0.362) * g1 + T(1.056) * g2 + T(-0.065) * g3;
 }
 
@@ -55,10 +52,8 @@ T y_31(T wave_in_nanometers)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        namespace impl = xyz_implementation;
-
-        T g1 = impl::g(wave_in_nanometers, T(568.8), T(0.0213), T(0.0247));
-        T g2 = impl::g(wave_in_nanometers, T(530.9), T(0.0613), T(0.0322));
+        T g1 = g(wave_in_nanometers, T(568.8), T(0.0213), T(0.0247));
+        T g2 = g(wave_in_nanometers, T(530.9), T(0.0613), T(0.0322));
         return T(0.821) * g1 + T(0.286) * g2;
 }
 
@@ -67,10 +62,77 @@ T z_31(T wave_in_nanometers)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        namespace impl = xyz_implementation;
-
-        T g1 = impl::g(wave_in_nanometers, T(437.0), T(0.0845), T(0.0278));
-        T g2 = impl::g(wave_in_nanometers, T(459.0), T(0.0385), T(0.0725));
+        T g1 = g(wave_in_nanometers, T(437.0), T(0.0845), T(0.0278));
+        T g2 = g(wave_in_nanometers, T(459.0), T(0.0385), T(0.0725));
         return T(1.217) * g1 + T(0.681) * g2;
+}
+
+//
+
+template <typename T>
+T x_64(T wave_in_nanometers)
+{
+        static_assert(std::is_floating_point_v<T>);
+
+        T t1 = std::log((wave_in_nanometers + T(570.1)) / T(1014));
+        T t2 = std::log((T(1338) - wave_in_nanometers) / T(743.5));
+        return T(0.398) * std::exp(T(-1250) * t1 * t1) + T(1.132) * std::exp(T(-234) * t2 * t2);
+}
+
+template <typename T>
+T y_64(T wave_in_nanometers)
+{
+        static_assert(std::is_floating_point_v<T>);
+
+        T t = (wave_in_nanometers - T(556.1)) / T(46.14);
+        return T(1.011) * std::exp(T(-0.5) * t * t);
+}
+
+template <typename T>
+T z_64(T wave_in_nanometers)
+{
+        static_assert(std::is_floating_point_v<T>);
+
+        T t = std::log((wave_in_nanometers - T(265.8)) / T(180.4));
+        return T(2.060) * std::exp(T(-32) * t * t);
+}
+}
+
+enum XYZ
+{
+        XYZ_31,
+        XYZ_64
+};
+
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_31, T> cie_x(T wave_in_nanometers)
+{
+        return xyz_implementation::x_31(wave_in_nanometers);
+}
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_31, T> cie_y(T wave_in_nanometers)
+{
+        return xyz_implementation::y_31(wave_in_nanometers);
+}
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_31, T> cie_z(T wave_in_nanometers)
+{
+        return xyz_implementation::z_31(wave_in_nanometers);
+}
+
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_64, T> cie_x(T wave_in_nanometers)
+{
+        return xyz_implementation::x_64(wave_in_nanometers);
+}
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_64, T> cie_y(T wave_in_nanometers)
+{
+        return xyz_implementation::y_64(wave_in_nanometers);
+}
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_64, T> cie_z(T wave_in_nanometers)
+{
+        return xyz_implementation::z_64(wave_in_nanometers);
 }
 }
