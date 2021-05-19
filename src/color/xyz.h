956129053191a8e062a8bc53183de280fe00443d
cppd <cppd@users.noreply.github.com>
2021-05-19 09:12:02 +0300

    refactoring

--- src/color/xyz.h
+++ src/color/xyz.h
@@ -32,15 +32,15 @@ namespace ns::color
 namespace xyz_implementation
 {
 template <typename T>
-T g(T wave_in_nanometers, T m, T t1, T t2)
+T g(T wave, T m, T t1, T t2)
 {
-        T v = (wave_in_nanometers - m) * (wave_in_nanometers < m ? t1 : t2);
+        T v = (wave - m) * (wave < m ? t1 : t2);
         return std::exp(T(-0.5) * v * v);
 }
 
 // Integrate[Exp[-1/2*(t*(x-m))^2],{x,a,b}]
 template <typename T>
-T g_integral(T w1, T w2, T m, T t)
+T g_integral(T wave_1, T wave_2, T m, T t)
 {
         // sqrt(2)
         static constexpr T SQRT_2 = 1.4142135623730950488016887242096980785696718753769L;
@@ -48,119 +48,119 @@ T g_integral(T w1, T w2, T m, T t)
         static constexpr T SQRT_PI_2 = 1.2533141373155002512078826424055226265034933703050L;
 
         const T ts = t / SQRT_2;
-        return (SQRT_PI_2 / t) * (std::erf(ts * (w2 - m)) - std::erf(ts * (w1 - m)));
+        return (SQRT_PI_2 / t) * (std::erf(ts * (wave_2 - m)) - std::erf(ts * (wave_1 - m)));
 }
 
 template <typename T>
-T g_integral(T w1, T w2, T m, T t1, T t2)
+T g_integral(T wave_1, T wave_2, T m, T t1, T t2)
 {
-        ASSERT(w1 < w2);
-        if (w2 <= m)
+        ASSERT(wave_1 < wave_2);
+        if (wave_2 <= m)
         {
-                return g_integral(w1, w2, m, t1);
+                return g_integral(wave_1, wave_2, m, t1);
         }
-        if (w1 >= m)
+        if (wave_1 >= m)
         {
-                return g_integral(w1, w2, m, t2);
+                return g_integral(wave_1, wave_2, m, t2);
         }
-        return g_integral(w1, m, m, t1) + g_integral(m, w2, m, t2);
+        return g_integral(wave_1, m, m, t1) + g_integral(m, wave_2, m, t2);
 }
 
 //
 
 template <typename T>
-T x_31(T wave_in_nanometers)
+T x_31(T wave)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T g1 = g(wave_in_nanometers, T(442.0), T(0.0624), T(0.0374));
-        T g2 = g(wave_in_nanometers, T(599.8), T(0.0264), T(0.0323));
-        T g3 = g(wave_in_nanometers, T(501.1), T(0.0490), T(0.0382));
+        T g1 = g(wave, T(442.0), T(0.0624), T(0.0374));
+        T g2 = g(wave, T(599.8), T(0.0264), T(0.0323));
+        T g3 = g(wave, T(501.1), T(0.0490), T(0.0382));
         return T(0.362) * g1 + T(1.056) * g2 + T(-0.065) * g3;
 }
 
 template <typename T>
-T x_31_integral(T w1, T w2)
+T x_31_integral(T wave_1, T wave_2)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T g1 = g_integral(w1, w2, T(442.0), T(0.0624), T(0.0374));
-        T g2 = g_integral(w1, w2, T(599.8), T(0.0264), T(0.0323));
-        T g3 = g_integral(w1, w2, T(501.1), T(0.0490), T(0.0382));
+        T g1 = g_integral(wave_1, wave_2, T(442.0), T(0.0624), T(0.0374));
+        T g2 = g_integral(wave_1, wave_2, T(599.8), T(0.0264), T(0.0323));
+        T g3 = g_integral(wave_1, wave_2, T(501.1), T(0.0490), T(0.0382));
         return T(0.362) * g1 + T(1.056) * g2 + T(-0.065) * g3;
 }
 
 //
 
 template <typename T>
-T y_31(T wave_in_nanometers)
+T y_31(T wave)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T g1 = g(wave_in_nanometers, T(568.8), T(0.0213), T(0.0247));
-        T g2 = g(wave_in_nanometers, T(530.9), T(0.0613), T(0.0322));
+        T g1 = g(wave, T(568.8), T(0.0213), T(0.0247));
+        T g2 = g(wave, T(530.9), T(0.0613), T(0.0322));
         return T(0.821) * g1 + T(0.286) * g2;
 }
 
 template <typename T>
-T y_31_integral(T w1, T w2)
+T y_31_integral(T wave_1, T wave_2)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T g1 = g_integral(w1, w2, T(568.8), T(0.0213), T(0.0247));
-        T g2 = g_integral(w1, w2, T(530.9), T(0.0613), T(0.0322));
+        T g1 = g_integral(wave_1, wave_2, T(568.8), T(0.0213), T(0.0247));
+        T g2 = g_integral(wave_1, wave_2, T(530.9), T(0.0613), T(0.0322));
         return T(0.821) * g1 + T(0.286) * g2;
 }
 
 //
 
 template <typename T>
-T z_31(T wave_in_nanometers)
+T z_31(T wave)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T g1 = g(wave_in_nanometers, T(437.0), T(0.0845), T(0.0278));
-        T g2 = g(wave_in_nanometers, T(459.0), T(0.0385), T(0.0725));
+        T g1 = g(wave, T(437.0), T(0.0845), T(0.0278));
+        T g2 = g(wave, T(459.0), T(0.0385), T(0.0725));
         return T(1.217) * g1 + T(0.681) * g2;
 }
 
 template <typename T>
-T z_31_integral(T w1, T w2)
+T z_31_integral(T wave_1, T wave_2)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T g1 = g_integral(w1, w2, T(437.0), T(0.0845), T(0.0278));
-        T g2 = g_integral(w1, w2, T(459.0), T(0.0385), T(0.0725));
+        T g1 = g_integral(wave_1, wave_2, T(437.0), T(0.0845), T(0.0278));
+        T g2 = g_integral(wave_1, wave_2, T(459.0), T(0.0385), T(0.0725));
         return T(1.217) * g1 + T(0.681) * g2;
 }
 
 //
 
 template <typename T>
-T x_64(T wave_in_nanometers)
+T x_64(T wave)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T t1 = std::log((wave_in_nanometers + T(570.1)) / T(1014));
-        T t2 = std::log((T(1338) - wave_in_nanometers) / T(743.5));
+        T t1 = std::log((wave + T(570.1)) / T(1014));
+        T t2 = std::log((T(1338) - wave) / T(743.5));
         return T(0.398) * std::exp(T(-1250) * t1 * t1) + T(1.132) * std::exp(T(-234) * t2 * t2);
 }
 
 template <typename T>
-T y_64(T wave_in_nanometers)
+T y_64(T wave)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T t = (wave_in_nanometers - T(556.1)) / T(46.14);
+        T t = (wave - T(556.1)) / T(46.14);
         return T(1.011) * std::exp(T(-0.5) * t * t);
 }
 
 template <typename T>
-T z_64(T wave_in_nanometers)
+T z_64(T wave)
 {
         static_assert(std::is_floating_point_v<T>);
 
-        T t = std::log((wave_in_nanometers - T(265.8)) / T(180.4));
+        T t = std::log((wave - T(265.8)) / T(180.4));
         return T(2.060) * std::exp(T(-32) * t * t);
 }
 }
@@ -172,50 +172,50 @@ enum XYZ
 };
 
 template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_31, T> cie_x(T wave_in_nanometers)
+std::enable_if_t<xyz == XYZ_31, T> cie_x(T wave)
 {
-        return xyz_implementation::x_31(wave_in_nanometers);
+        return xyz_implementation::x_31(wave);
 }
 template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_31, T> cie_y(T wave_in_nanometers)
+std::enable_if_t<xyz == XYZ_31, T> cie_y(T wave)
 {
-        return xyz_implementation::y_31(wave_in_nanometers);
+        return xyz_implementation::y_31(wave);
 }
 template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_31, T> cie_z(T wave_in_nanometers)
+std::enable_if_t<xyz == XYZ_31, T> cie_z(T wave)
 {
-        return xyz_implementation::z_31(wave_in_nanometers);
-}
-
-template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_31, T> cie_x_integral(T w1, T w2)
-{
-        return xyz_implementation::x_31_integral(w1, w2);
-}
-template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_31, T> cie_y_integral(T w1, T w2)
-{
-        return xyz_implementation::y_31_integral(w1, w2);
-}
-template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_31, T> cie_z_integral(T w1, T w2)
-{
-        return xyz_implementation::z_31_integral(w1, w2);
+        return xyz_implementation::z_31(wave);
 }
 
 template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_64, T> cie_x(T wave_in_nanometers)
+std::enable_if_t<xyz == XYZ_31, T> cie_x_integral(T wave_1, T wave_2)
 {
-        return xyz_implementation::x_64(wave_in_nanometers);
+        return xyz_implementation::x_31_integral(wave_1, wave_2);
 }
 template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_64, T> cie_y(T wave_in_nanometers)
+std::enable_if_t<xyz == XYZ_31, T> cie_y_integral(T wave_1, T wave_2)
 {
-        return xyz_implementation::y_64(wave_in_nanometers);
+        return xyz_implementation::y_31_integral(wave_1, wave_2);
 }
 template <XYZ xyz, typename T>
-std::enable_if_t<xyz == XYZ_64, T> cie_z(T wave_in_nanometers)
+std::enable_if_t<xyz == XYZ_31, T> cie_z_integral(T wave_1, T wave_2)
 {
-        return xyz_implementation::z_64(wave_in_nanometers);
+        return xyz_implementation::z_31_integral(wave_1, wave_2);
+}
+
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_64, T> cie_x(T wave)
+{
+        return xyz_implementation::x_64(wave);
+}
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_64, T> cie_y(T wave)
+{
+        return xyz_implementation::y_64(wave);
+}
+template <XYZ xyz, typename T>
+std::enable_if_t<xyz == XYZ_64, T> cie_z(T wave)
+{
+        return xyz_implementation::z_64(wave);
 }
 }
