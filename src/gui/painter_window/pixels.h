6d335f94faced63094c84c8eb56f9adaa953c4d9
cppd <cppd@users.noreply.github.com>
2021-06-14 13:59:57 +0300

    painter window

--- src/gui/painter_window/pixels.h
+++ src/gui/painter_window/pixels.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/com/global_index.h>
 #include <src/com/message.h>
 #include <src/com/print.h>
+#include <src/com/time.h>
 #include <src/com/type/limit.h>
 #include <src/com/type/name.h>
 #include <src/image/format.h>
@@ -33,9 +34,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <atomic>
 #include <cstring>
 #include <memory>
-#include <mutex>
 #include <optional>
 #include <span>
+#include <thread>
 #include <vector>
 
 namespace ns::gui::painter_window
@@ -61,7 +62,7 @@ struct Pixels
 
         virtual const std::vector<int>& screen_size() const = 0;
 
-        virtual std::span<const std::byte> slice_r8g8b8a8(long long slice_number) const = 0;
+        virtual std::span<const std::byte> slice_r8g8b8a8(long long slice_number) = 0;
         virtual const std::vector<long long>& busy_indices_2d() const = 0;
         virtual painter::Statistics statistics() const = 0;
 
@@ -74,11 +75,15 @@ class PainterPixels final : public Pixels, public painter::Notifier<N - 1>
 {
         static_assert(N >= 3);
 
+        static constexpr std::chrono::seconds NORMALIZE_INTERVAL{10};
+
         static constexpr std::optional<int> MAX_PASS_COUNT = std::nullopt;
         static constexpr long long NULL_INDEX = -1;
 
-        static constexpr image::ColorFormat RAW_COLOR_FORMAT = image::ColorFormat::R8G8B8A8_SRGB;
-        static constexpr std::size_t RAW_PIXEL_SIZE = image::format_pixel_size_in_bytes(RAW_COLOR_FORMAT);
+        static constexpr image::ColorFormat COLOR_FORMAT = image::ColorFormat::R8G8B8A8_SRGB;
+        static constexpr std::size_t PIXEL_SIZE = image::format_pixel_size_in_bytes(COLOR_FORMAT);
+
+        const std::thread::id m_thread_id = std::this_thread::get_id();
 
         const char* const m_floating_point_name;
         const char* const m_color_name;
@@ -86,10 +91,16 @@ class PainterPixels final : public Pixels, public painter::Notifier<N - 1>
         const GlobalIndex<N - 1, long long> m_global_index;
         const std::vector<int> m_screen_size;
         const long long m_slice_count;
-
-        const std::size_t m_raw_slice_size = RAW_PIXEL_SIZE * m_screen_size[0] * m_screen_size[1];
-        std::vector<std::byte> m_raw_pixels = make_initial_image(m_screen_size, RAW_COLOR_FORMAT);
+        const std::size_t m_slice_size = PIXEL_SIZE * m_screen_size[0] * m_screen_size[1];
         std::vector<long long> m_busy_indices_2d;
+
+        std::vector<std::byte> m_pixels_r8g8b8a8 = make_initial_image(m_screen_size, COLOR_FORMAT);
+
+        std::vector<std::optional<Vector<3, float>>> m_pixels_original{
+                static_cast<std::size_t>(m_global_index.count())};
+        float m_pixel_max_r = 1;
+
+        TimePoint m_last_normalize_time = time();
 
         painter::Images<N - 1> m_painter_images;
         std::atomic_bool m_painter_images_ready = false;
@@ -111,6 +122,59 @@ class PainterPixels final : public Pixels, public painter::Notifier<N - 1>
                 }
         }
 
+        void normalize_pixels()
+        {
+                static_assert(COLOR_FORMAT == image::ColorFormat::R8G8B8A8_SRGB);
+
+                const float max = [this]
+                {
+                        Vector<3, float> m;
+                        m[0] = limits<float>::lowest();
+                        m[1] = limits<float>::lowest();
+                        m[2] = limits<float>::lowest();
+                        for (const std::optional<Vector<3, float>>& pixel : m_pixels_original)
+                        {
+                                if (pixel)
+                                {
+                                        for (std::size_t i = 0; i < 3; ++i)
+                                        {
+                                                if (is_finite((*pixel)[i]))
+                                                {
+                                                        m[i] = std::max(m[i], (*pixel)[i]);
+                                                }
+                                        }
+                                }
+                        }
+                        return std::max(std::max(m[0], m[1]), m[2]);
+                }();
+
+                if (!(max > 0 && max != 1))
+                {
+                        return;
+                }
+
+                const float max_r = 1 / max;
+                m_pixel_max_r = max_r;
+
+                std::byte* ptr = m_pixels_r8g8b8a8.data();
+                ASSERT(m_pixels_r8g8b8a8.size() % PIXEL_SIZE == 0);
+                for (std::size_t i = 0; i < m_pixels_original.size(); ++i)
+                {
+                        if (m_pixels_original[i])
+                        {
+                                std::array<uint8_t, 4> rgba8;
+                                static_assert(std::span(rgba8).size_bytes() == PIXEL_SIZE);
+                                RGB8 rgb8 = make_rgb8(*m_pixels_original[i] * max_r);
+                                rgba8[0] = rgb8.red;
+                                rgba8[1] = rgb8.green;
+                                rgba8[2] = rgb8.blue;
+                                std::memcpy(ptr, rgba8.data(), PIXEL_SIZE);
+                        }
+                        ptr += PIXEL_SIZE;
+                }
+                ASSERT(ptr == m_pixels_r8g8b8a8.data() + m_pixels_r8g8b8a8.size());
+        }
+
         // PainterNotifier
 
         void thread_busy(unsigned thread_number, const std::array<int, N - 1>& pixel) override
@@ -127,17 +191,17 @@ class PainterPixels final : public Pixels, public painter::Notifier<N - 1>
 
         void pixel_set(const std::array<int, N - 1>& pixel, const Vector<3, float>& rgb) override
         {
-                static_assert(RAW_COLOR_FORMAT == image::ColorFormat::R8G8B8A8_SRGB);
+                static_assert(COLOR_FORMAT == image::ColorFormat::R8G8B8A8_SRGB);
 
                 static constexpr uint8_t ALPHA = limits<uint8_t>::max();
 
-                const RGB8 rgb8 = make_rgb8(rgb);
-                const std::array<uint8_t, 4> rgba8{rgb8.red, rgb8.green, rgb8.blue, ALPHA};
-
                 const long long index = m_global_index.compute(flip_vertically(pixel));
 
-                static_assert(std::span(rgba8).size_bytes() == RAW_PIXEL_SIZE);
-                std::memcpy(&m_raw_pixels[RAW_PIXEL_SIZE * index], rgba8.data(), RAW_PIXEL_SIZE);
+                m_pixels_original[index] = rgb;
+                const RGB8 rgb8 = make_rgb8(rgb * m_pixel_max_r);
+                const std::array<uint8_t, 4> rgba8{rgb8.red, rgb8.green, rgb8.blue, ALPHA};
+                static_assert(std::span(rgba8).size_bytes() == PIXEL_SIZE);
+                std::memcpy(&m_pixels_r8g8b8a8[PIXEL_SIZE * index], rgba8.data(), PIXEL_SIZE);
         }
 
         painter::Images<N - 1>* images(long long /*pass_number*/) override
@@ -182,11 +246,19 @@ class PainterPixels final : public Pixels, public painter::Notifier<N - 1>
                 return m_painter->statistics();
         }
 
-        std::span<const std::byte> slice_r8g8b8a8(long long slice_number) const override
+        std::span<const std::byte> slice_r8g8b8a8(long long slice_number) override
         {
+                ASSERT(std::this_thread::get_id() == m_thread_id);
+
                 check_slice_number(slice_number);
 
-                return std::span(&m_raw_pixels[slice_number * m_raw_slice_size], m_raw_slice_size);
+                if (time() - m_last_normalize_time >= NORMALIZE_INTERVAL)
+                {
+                        normalize_pixels();
+                        m_last_normalize_time = time();
+                }
+
+                return std::span(&m_pixels_r8g8b8a8[slice_number * m_slice_size], m_slice_size);
         }
 
         std::optional<Images> slice(long long slice_number) const override
