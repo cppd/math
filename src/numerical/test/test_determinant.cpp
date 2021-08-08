17c7c404bbe0d432e47583ed70f161e8f6e8e69c
cppd <cppd@users.noreply.github.com>
2021-08-08 10:31:10 +0300

    determinant tests

--- src/numerical/test/test_determinant.cpp
+++ src/numerical/test/test_determinant.cpp
@@ -16,6 +16,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "../determinant.h"
+#include "../gauss.h"
+#include "../matrix.h"
+
+#include <src/com/error.h>
+#include <src/com/log.h>
+#include <src/com/print.h>
+#include <src/com/random/engine.h>
+#include <src/com/time.h>
+#include <src/com/type/name.h>
+#include <src/test/test.h>
+
+#include <random>
 
 namespace ns::numerical
 {
@@ -45,5 +57,98 @@ static_assert(DETERMINANT == determinant(VECTORS<__float128>));
 
 static_assert(
         -28 == determinant(VECTORS<int>, std::to_array<unsigned char>({2, 4}), std::to_array<unsigned char>({3, 5})));
+
+//
+
+template <typename T>
+bool are_equal(const T& a, const T& b, const T& precision)
+{
+        if (a == b)
+        {
+                return true;
+        }
+        const T rel = std::abs(a - b) / std::max(std::abs(a), std::abs(b));
+        return (rel < precision);
+}
+
+template <std::size_t N, typename T>
+std::vector<std::array<Vector<N, T>, N>> random_matrices(const int count)
+{
+        std::mt19937_64 random_engine = create_engine<std::mt19937_64>();
+        std::uniform_real_distribution<T> urd(-10, 10);
+        std::vector<std::array<Vector<N, T>, N>> res(count);
+        for (int i = 0; i < count; ++i)
+        {
+                for (std::size_t j = 0; j < N; ++j)
+                {
+                        for (std::size_t k = 0; k < N; ++k)
+                        {
+                                res[i][j][k] = urd(random_engine);
+                        }
+                }
+        }
+        return res;
+}
+
+template <std::size_t N, typename T>
+void test_determinant(const int count, const std::type_identity_t<T>& precision)
+{
+        LOG("Test determinant, " + to_string(N) + ", " + type_name<T>());
+        const std::vector<std::array<Vector<N, T>, N>> matrices = random_matrices<N, T>(count);
+
+        std::vector<T> cofactor_expansion(count);
+        std::vector<T> row_reduction(count);
+
+        {
+                TimePoint start_time = time();
+                for (int i = 0; i < count; ++i)
+                {
+                        cofactor_expansion[i] = determinant(matrices[i]);
+                }
+                LOG("Time = " + to_string_fixed(duration_from(start_time), 5) + " s, cofactor expansion");
+        }
+
+        {
+                TimePoint start_time = time();
+                for (int i = 0; i < count; ++i)
+                {
+                        Matrix<N, N, T> matrix(matrices[i]);
+                        row_reduction[i] = determinant_gauss(&matrix);
+                }
+                LOG("Time = " + to_string_fixed(duration_from(start_time), 5) + " s, row reduction");
+        }
+
+        for (int i = 0; i < count; ++i)
+        {
+                if (!are_equal(cofactor_expansion[i], row_reduction[i], precision))
+                {
+                        error("Determinants are not equal:\ncofactor_expansion = " + to_string(cofactor_expansion[i])
+                              + "\nrow_reduction = " + to_string(row_reduction[i]) + "\n" + to_string(matrices[i]));
+                }
+        }
+}
+
+template <typename T>
+void test_determinant(const int count, const std::type_identity_t<T>& precision)
+{
+        test_determinant<1, T>(count, precision);
+        test_determinant<2, T>(count, precision);
+        test_determinant<3, T>(count, precision);
+        test_determinant<4, T>(count, precision);
+        test_determinant<5, T>(count, precision);
+        test_determinant<6, T>(count, precision);
+        test_determinant<7, T>(count, precision);
+        test_determinant<8, T>(count, precision);
+}
+
+void test()
+{
+        LOG("Test determinant");
+        test_determinant<double>(1'000, 1e-8);
+        test_determinant<long double>(100, 1e-11);
+        LOG("Test determinant passed");
+}
+
+TEST_SMALL("Determinant", test)
 }
 }
