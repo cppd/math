40274d7a0480bccc2421ebdaadac0b98b75f72cb
cppd <cppd@users.noreply.github.com>
2021-08-13 08:48:31 +0300

    solve

--- src/numerical/solve.h
+++ src/numerical/solve.h
@@ -15,6 +15,16 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+Steven J. Leon.
+Linear Algebra with Applications. Ninth Edition.
+Pearson Education, 2015.
+
+2.3 Additional Topics and Applications
+The Adjoint of a Matrix
+Cramer’s Rule
+*/
+
 #pragma once
 
 #include "gauss.h"
@@ -27,19 +37,134 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace ns::numerical
 {
+namespace solve_implementation
+{
+template <std::size_t N, typename T>
+constexpr Vector<N, T> solve_cofactor_expansion(const std::array<Vector<N, T>, N>& a, const Vector<N, T>& b)
+{
+        static_assert(N >= 1 && N <= 3);
+
+        // Cramer’s rule
+
+        if constexpr (N == 1)
+        {
+                return Vector<1, T>(b[0] / a[0][0]);
+        }
+
+        if constexpr (N == 2)
+        {
+                const T d = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+                Vector<2, T> res;
+                res[0] = (b[0] * a[1][1] - b[1] * a[0][1]) / d;
+                res[1] = (b[1] * a[0][0] - b[0] * a[1][0]) / d;
+                return res;
+        }
+
+        if constexpr (N == 3)
+        {
+                const T c00 = a[1][1] * a[2][2] - a[1][2] * a[2][1];
+                const T c10 = a[0][2] * a[2][1] - a[0][1] * a[2][2];
+                const T c20 = a[0][1] * a[1][2] - a[0][2] * a[1][1];
+
+                const T d = a[0][0] * c00 + a[1][0] * c10 + a[2][0] * c20;
+
+                Vector<3, T> res;
+
+                res[0] = (b[0] * c00 + b[1] * c10 + b[2] * c20) / d;
+
+                const T c01 = a[1][2] * a[2][0] - a[1][0] * a[2][2];
+                const T c11 = a[0][0] * a[2][2] - a[0][2] * a[2][0];
+                const T c21 = a[0][2] * a[1][0] - a[0][0] * a[1][2];
+                res[1] = (b[0] * c01 + b[1] * c11 + b[2] * c21) / d;
+
+                const T c02 = a[1][0] * a[2][1] - a[1][1] * a[2][0];
+                const T c12 = a[0][1] * a[2][0] - a[0][0] * a[2][1];
+                const T c22 = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+                res[2] = (b[0] * c02 + b[1] * c12 + b[2] * c22) / d;
+
+                return res;
+        }
+}
+
+template <std::size_t N, typename T>
+constexpr std::array<Vector<N, T>, N> inverse_cofactor_expansion(const std::array<Vector<N, T>, N>& a)
+{
+        static_assert(N >= 1 && N <= 3);
+
+        // Adjoint
+
+        if constexpr (N == 1)
+        {
+                return {Vector<1, T>(T(1) / a[0][0])};
+        }
+
+        if constexpr (N == 2)
+        {
+                const T d = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+                std::array<Vector<2, T>, 2> res;
+                res[0][0] = a[1][1] / d;
+                res[0][1] = -a[0][1] / d;
+                res[1][0] = -a[1][0] / d;
+                res[1][1] = a[0][0] / d;
+                return res;
+        }
+
+        if constexpr (N == 3)
+        {
+                const T c00 = a[1][1] * a[2][2] - a[1][2] * a[2][1];
+                const T c10 = a[0][2] * a[2][1] - a[0][1] * a[2][2];
+                const T c20 = a[0][1] * a[1][2] - a[0][2] * a[1][1];
+
+                const T d = a[0][0] * c00 + a[1][0] * c10 + a[2][0] * c20;
+
+                std::array<Vector<3, T>, 3> res;
+
+                res[0][0] = c00 / d;
+                res[0][1] = c10 / d;
+                res[0][2] = c20 / d;
+
+                res[1][0] = (a[1][2] * a[2][0] - a[1][0] * a[2][2]) / d;
+                res[1][1] = (a[0][0] * a[2][2] - a[0][2] * a[2][0]) / d;
+                res[1][2] = (a[0][2] * a[1][0] - a[0][0] * a[1][2]) / d;
+
+                res[2][0] = (a[1][0] * a[2][1] - a[1][1] * a[2][0]) / d;
+                res[2][1] = (a[0][1] * a[2][0] - a[0][0] * a[2][1]) / d;
+                res[2][2] = (a[0][0] * a[1][1] - a[0][1] * a[1][0]) / d;
+
+                return res;
+        }
+}
+}
+
 template <std::size_t N, typename T>
 [[nodiscard]] constexpr Vector<N, T> linear_solve(const std::array<Vector<N, T>, N>& a, const Vector<N, T>& b)
 {
         static_assert(is_floating_point<T>);
+        static_assert(N > 0);
 
-        return solve_gauss(a, b);
+        if constexpr (N <= 3)
+        {
+                return solve_implementation::solve_cofactor_expansion(a, b);
+        }
+        else
+        {
+                return solve_gauss(a, b);
+        }
 }
 
 template <std::size_t N, typename T>
 [[nodiscard]] constexpr std::array<Vector<N, T>, N> inverse(const std::array<Vector<N, T>, N>& a)
 {
         static_assert(is_floating_point<T>);
+        static_assert(N > 0);
 
-        return solve_gauss(a, identity_array<N, T>);
+        if constexpr (N <= 3)
+        {
+                return solve_implementation::inverse_cofactor_expansion(a);
+        }
+        else
+        {
+                return solve_gauss(a, identity_array<N, T>);
+        }
 }
 }
