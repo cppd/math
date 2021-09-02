e4e84e434b39ce602f5b5971116a88e765fbd41c
cppd <cppd@users.noreply.github.com>
2021-09-02 09:51:37 +0300

    min max

--- src/com/min_max.h
+++ src/com/min_max.h
@@ -19,12 +19,62 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "error.h"
 
+#include <algorithm>
+#include <array>
 #include <span>
 
 namespace ns
 {
 namespace min_max_implementation
 {
+template <bool COMPUTE_MIN, unsigned COUNT, typename T>
+constexpr T min_max_value_array(const std::span<const T>& p)
+{
+        ASSERT(!p.empty());
+        ASSERT(p.size() % COUNT == 0);
+
+        const T* ptr = p.data();
+
+        std::array<T, COUNT> m;
+        for (unsigned i = 0; i < COUNT; ++i)
+        {
+                m[i] = ptr[i];
+        }
+        ptr += COUNT;
+
+        const T* const end = p.data() + p.size();
+        while (ptr != end)
+        {
+                for (unsigned i = 0; i < COUNT; ++i)
+                {
+                        if constexpr (COMPUTE_MIN)
+                        {
+                                m[i] = std::min(m[i], ptr[i]);
+                        }
+                        else
+                        {
+                                m[i] = std::max(m[i], ptr[i]);
+                        }
+                }
+                ptr += COUNT;
+        }
+
+        T cmp = m[0];
+        for (unsigned i = 1; i < COUNT; ++i)
+        {
+                if constexpr (COMPUTE_MIN)
+                {
+                        cmp = std::min(cmp, m[i]);
+                }
+                else
+                {
+                        cmp = std::max(cmp, m[i]);
+                }
+        }
+
+        return cmp;
+}
+
 template <bool COMPUTE_MIN, typename T>
 constexpr T min_max_value(const std::span<const T>& p)
 {
@@ -38,49 +88,16 @@ constexpr T min_max_value(const std::span<const T>& p)
 
         if (p.size() >= 2 * COUNT)
         {
-                std::array<T, COUNT> m;
-                for (unsigned i = 0; i < COUNT; ++i)
-                {
-                        m[i] = ptr[i];
-                }
-                ptr += COUNT;
-
-                const T* end = p.data() + (p.size() / COUNT) * COUNT;
-                while (ptr != end)
-                {
-                        for (unsigned i = 0; i < COUNT; ++i)
-                        {
-                                if constexpr (COMPUTE_MIN)
-                                {
-                                        m[i] = std::min(m[i], ptr[i]);
-                                }
-                                else
-                                {
-                                        m[i] = std::max(m[i], ptr[i]);
-                                }
-                        }
-                        ptr += COUNT;
-                }
-
-                cmp = m[0];
-                for (unsigned i = 1; i < COUNT; ++i)
-                {
-                        if constexpr (COMPUTE_MIN)
-                        {
-                                cmp = std::min(cmp, m[i]);
-                        }
-                        else
-                        {
-                                cmp = std::max(cmp, m[i]);
-                        }
-                }
+                const std::size_t count = (p.size() / COUNT) * COUNT;
+                cmp = min_max_value_array<COMPUTE_MIN, COUNT>(std::span<const T>(p.data(), count));
+                ptr += count;
         }
         else
         {
                 cmp = *ptr++;
         }
 
-        const T* end = p.data() + p.size();
+        const T* const end = p.data() + p.size();
         while (ptr != end)
         {
                 if constexpr (COMPUTE_MIN)
