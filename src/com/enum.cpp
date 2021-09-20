fc026269c8fda78c67129d238db87d592a21db5a
cppd <cppd@users.noreply.github.com>
2021-09-20 07:23:09 +0300

    enumeration function

---
+++ src/com/enum.cpp
@@ -0,0 +1,49 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "enum.h"
+
+namespace ns
+{
+namespace
+{
+template <typename Underlying, Underlying VALUE, typename Result>
+struct Test
+{
+        enum class Enum : Underlying
+        {
+                E = VALUE
+        };
+        static_assert(std::is_same_v<Result, decltype(enum_to_int(Enum::E))>);
+        static_assert(enum_to_int(Enum::E) == VALUE);
+};
+
+template struct Test<signed char, 100, int>;
+template struct Test<signed char, -100, int>;
+template struct Test<unsigned char, 100, unsigned>;
+template struct Test<char, 100, std::conditional_t<std::is_signed_v<char>, int, unsigned>>;
+template struct Test<int, 1'000'000'000, int>;
+template struct Test<int, -1'000'000'000, int>;
+template struct Test<unsigned, 1'000'000'000, unsigned>;
+template struct Test<long long, 1'000'000'000'000, long long>;
+template struct Test<long long, -1'000'000'000'000, long long>;
+template struct Test<unsigned long long, 1'000'000'000'000, unsigned long long>;
+template struct Test<__int128, __int128(1e12) * __int128(1e12), __int128>;
+template struct Test<__int128, __int128(-1e12) * __int128(1e12), __int128>;
+template struct Test<unsigned __int128, __int128(1e12) * __int128(1e12), unsigned __int128>;
+}
+}
---
+++ src/com/enum.h
@@ -0,0 +1,36 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <type_traits>
+
+namespace ns
+{
+template <typename T>
+constexpr auto enum_to_int(const T v) requires(std::is_enum_v<T>)
+{
+        if constexpr (std::is_signed_v<std::underlying_type_t<T>>)
+        {
+                return static_cast<std::common_type_t<int, std::underlying_type_t<T>>>(v);
+        }
+        else
+        {
+                return static_cast<std::common_type_t<unsigned, std::underlying_type_t<T>>>(v);
+        }
+}
+}
