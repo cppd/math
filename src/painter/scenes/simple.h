8c6a973c4b552a60219b270e81e6aa604a7094cb
cppd <cppd@users.noreply.github.com>
2021-10-07 09:21:57 +0300

    painter scenes

--- src/painter/scenes/simple.h
+++ src/painter/scenes/simple.h
@@ -19,14 +19,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "storage_scene.h"
 
-#include "../lights/distant_light.h"
+#include "../lights/ball_light.h"
 #include "../objects.h"
 #include "../projectors/parallel_projector.h"
 #include "../shapes/shape.h"
 
 #include <src/color/color.h>
 #include <src/com/error.h>
-#include <src/com/type/limit.h>
+#include <src/com/print.h>
 
 #include <algorithm>
 #include <cmath>
@@ -34,16 +34,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace ns::painter
 {
-template <std::size_t N, typename T, typename Color>
-std::unique_ptr<const Scene<N, T, Color>> create_simple_scene(
-        const Color& light,
-        const Color& background_light,
-        int min_screen_size,
-        int max_screen_size,
-        std::unique_ptr<const Shape<N, T, Color>>&& shape)
+namespace simple_scene_implementation
 {
-        ASSERT(shape);
-
+inline void check_screen_size(const int min_screen_size, const int max_screen_size)
+{
         if (min_screen_size < 3)
         {
                 error("Min screen size (" + to_string(min_screen_size) + ") is too small");
@@ -54,59 +48,120 @@ std::unique_ptr<const Scene<N, T, Color>> create_simple_scene(
                 error("Wrong min and max screen sizes: min = " + to_string(min_screen_size)
                       + ", max = " + to_string(max_screen_size));
         }
+}
 
-        const geometry::BoundingBox<N, T> bb = shape->bounding_box();
-        const Vector<N, T> object_size = bb.max - bb.min;
-        const Vector<N, T> center = bb.min + object_size / T(2);
+template <std::size_t N, typename T>
+std::unique_ptr<const Projector<N, T>> create_projector(
+        const geometry::BoundingBox<N, T>& bounding_box,
+        const int min_screen_size,
+        const int max_screen_size)
+{
+        const Vector<N, T> object_size = bounding_box.max - bounding_box.min;
+        const Vector<N, T> center = bounding_box.min + object_size / T(2);
 
-        //
-
-        T max_projected_object_size = Limits<T>::lowest();
-        // excluding camera direction N - 1
-        for (unsigned i = 0; i < N - 1; ++i)
+        const T max_projected_object_size = [&]
         {
-                max_projected_object_size = std::max(object_size[i], max_projected_object_size);
-        }
-        if (max_projected_object_size == 0)
-        {
-                error("Object is a point on the screen");
-        }
-
-        std::array<int, N - 1> screen_size;
-        for (unsigned i = 0; i < N - 1; ++i)
-        {
-                int size_in_pixels = std::lround((object_size[i] / max_projected_object_size) * max_screen_size);
-                screen_size[i] = std::clamp(size_in_pixels, min_screen_size, max_screen_size);
-        }
-
-        Vector<N, T> camera_position(center);
-        camera_position[N - 1] = bb.max[N - 1] + object_size.norm();
-
-        Vector<N, T> camera_direction(0);
-        camera_direction[N - 1] = -1;
-
-        std::array<Vector<N, T>, N - 1> screen_axes;
-        for (unsigned i = 0; i < N - 1; ++i)
-        {
-                for (unsigned n = 0; n < N; ++n)
+                static_assert(N >= 2);
+                T res = object_size[0];
+                // excluding camera direction N - 1
+                for (unsigned i = 1; i < N - 1; ++i)
                 {
-                        screen_axes[i][n] = (i != n) ? 0 : 1;
+                        res = std::max(object_size[i], res);
                 }
-        }
+                if (!(res > 0))
+                {
+                        error("Object maximum projection size " + to_string(res) + " is not positive");
+                }
+                return res;
+        }();
 
-        T units_per_pixel = max_projected_object_size / max_screen_size;
+        const std::array<int, N - 1> screen_size = [&]
+        {
+                std::array<int, N - 1> res;
+                for (unsigned i = 0; i < N - 1; ++i)
+                {
+                        int size_in_pixels =
+                                std::lround((object_size[i] / max_projected_object_size) * max_screen_size);
+                        res[i] = std::clamp(size_in_pixels, min_screen_size, max_screen_size);
+                }
+                return res;
+        }();
 
-        std::unique_ptr<const Projector<N, T>> projector = std::make_unique<const ParallelProjector<N, T>>(
+        const Vector<N, T> camera_position = [&]
+        {
+                Vector<N, T> res(center);
+                res[N - 1] = bounding_box.max[N - 1] + object_size.norm();
+                return res;
+        }();
+
+        const Vector<N, T> camera_direction = []
+        {
+                Vector<N, T> res(0);
+                res[N - 1] = -1;
+                return res;
+        }();
+
+        const std::array<Vector<N, T>, N - 1> screen_axes = []
+        {
+                std::array<Vector<N, T>, N - 1> res;
+                for (unsigned i = 0; i < N - 1; ++i)
+                {
+                        for (unsigned n = 0; n < N; ++n)
+                        {
+                                res[i][n] = (i != n) ? 0 : 1;
+                        }
+                }
+                return res;
+        }();
+
+        const T units_per_pixel = max_projected_object_size / max_screen_size;
+
+        return std::make_unique<const ParallelProjector<N, T>>(
                 camera_position, camera_direction, screen_axes, units_per_pixel, screen_size);
+}
 
-        //
+template <std::size_t N, typename T, typename Color>
+std::unique_ptr<const painter::LightSource<N, T, Color>> create_light_source(
+        const geometry::BoundingBox<N, T>& bounding_box,
+        const Color& color)
+{
+        const Vector<N, T> box_direction = bounding_box.max - bounding_box.min;
+        const Vector<N, T> center = bounding_box.min + box_direction / T(2);
+        const T object_size = box_direction.norm();
 
-        Vector<N, T> light_direction(bb.max - center);
+        static constexpr T DISTANCE = 100;
+        static constexpr T RADIUS = DISTANCE / 100;
+
+        const T distance = object_size * DISTANCE;
+        const T radius = object_size * RADIUS;
+        const Vector<N, T> position = center + box_direction.normalized() * distance;
+
+        return std::make_unique<const painter::BallLight<N, T, Color>>(
+                position, box_direction, radius, color, distance);
+}
+}
+
+template <std::size_t N, typename T, typename Color>
+std::unique_ptr<const Scene<N, T, Color>> create_simple_scene(
+        const Color& light,
+        const Color& background_light,
+        const int min_screen_size,
+        const int max_screen_size,
+        std::unique_ptr<const Shape<N, T, Color>>&& shape)
+{
+        namespace impl = simple_scene_implementation;
+
+        ASSERT(shape);
+
+        impl::check_screen_size(min_screen_size, max_screen_size);
+
+        const geometry::BoundingBox<N, T> bounding_box = shape->bounding_box();
+
+        std::unique_ptr<const Projector<N, T>> projector =
+                impl::create_projector(bounding_box, min_screen_size, max_screen_size);
 
         std::vector<std::unique_ptr<const LightSource<N, T, Color>>> light_sources;
-        light_sources.push_back(std::make_unique<const DistantLight<N, T, Color>>(light_direction, light));
-
-        //
+        light_sources.push_back(impl::create_light_source(bounding_box, light));
 
         std::vector<std::unique_ptr<const Shape<N, T, Color>>> shapes;
         shapes.push_back(std::move(shape));
