687bf07820b78650470c95d6eaabe3df47e01175
cppd <cppd@users.noreply.github.com>
2021-10-19 08:40:22 +0300

    bounding box tests

--- src/geometry/spatial/test/test_bounding_box.cpp
+++ src/geometry/spatial/test/test_bounding_box.cpp
@@ -15,7 +15,19 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "parallelotope_points.h"
+
 #include "../bounding_box.h"
+
+#include <src/com/error.h>
+#include <src/com/log.h>
+#include <src/com/print.h>
+#include <src/com/random/engine.h>
+#include <src/sampling/sphere_uniform.h>
+#include <src/test/test.h>
+
+#include <cmath>
+#include <random>
 
 namespace ns::geometry::spatial::test
 {
@@ -63,5 +75,153 @@ struct Test
 template struct Test<float>;
 template struct Test<double>;
 template struct Test<long double>;
+
+//
+
+template <std::size_t N, typename T>
+BoundingBox<N, T> create_random_box(std::mt19937_64& engine)
+{
+        std::uniform_real_distribution<T> urd(-5, 5);
+        Vector<N, T> p1;
+        Vector<N, T> p2;
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                do
+                {
+                        p1[i] = urd(engine);
+                        p2[i] = urd(engine);
+                } while (!(std::abs(p1[i] - p2[i]) >= T(0.5)));
+        }
+        return {p1, p2};
+}
+
+template <std::size_t N, typename T>
+std::array<Vector<N, T>, N> box_vectors(const BoundingBox<N, T>& box)
+{
+        const Vector<N, T> diagonal = box.diagonal();
+        std::array<Vector<N, T>, N> res;
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                for (std::size_t j = 0; j < N; ++j)
+                {
+                        res[i][j] = 0;
+                }
+                res[i][i] = diagonal[i];
+        }
+        return res;
+}
+
+template <std::size_t N, typename T>
+bool is_on_box(const BoundingBox<N, T>& box, const Vector<N, T>& point, const T& precision)
+{
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                if (std::abs(point[i] - box.min()[i]) < precision)
+                {
+                        return true;
+                }
+                if (std::abs(point[i] - box.max()[i]) < precision)
+                {
+                        return true;
+                }
+        }
+        return false;
+}
+
+template <std::size_t N, typename T>
+void test_intersections(
+        const BoundingBox<N, T>& box,
+        const int point_count,
+        const T& precision,
+        std::mt19937_64& engine)
+{
+        const T length = box.diagonal().norm();
+        const T move_distance = 2 * length;
+        const T move_min = length;
+        const T move_max = 2 * length;
+
+        for (const Vector<N, T>& point : internal_points(box.min(), box_vectors(box), point_count, engine))
+        {
+                const Ray<N, T> ray(point, sampling::uniform_on_sphere<N, T>(engine));
+
+                {
+                        const Ray<N, T> r = ray;
+                        const auto t = box.intersect(r);
+                        if (!t)
+                        {
+                                error("Ray must intersect, inside\nbox " + to_string(box) + "\nray " + to_string(r));
+                        }
+                        if (!(*t > 0 && *t < length))
+                        {
+                                error("Intersection out of bounding box, inside\ndistance = " + to_string(*t) + ", "
+                                      + "max distance = " + to_string(length) + "\nbox " + to_string(box) + "\nray "
+                                      + to_string(r));
+                        }
+
+                        const Vector<N, T> p = r.point(*t);
+                        if (!is_on_box(box, p, precision))
+                        {
+                                error("Intersection out of bounding box, inside\nintersection point = " + to_string(p)
+                                      + "\nbox " + to_string(box) + "\nray " + to_string(r));
+                        }
+                }
+                {
+                        const Ray<N, T> r = ray.moved(-move_distance);
+                        const auto t = box.intersect(r);
+                        if (!t)
+                        {
+                                error("Ray must intersect, outside\nbox " + to_string(box) + "\nray " + to_string(r));
+                        }
+                        if (!(*t > move_min && *t < move_max))
+                        {
+                                error("Intersection out of bounding box, outside\ndistance = " + to_string(*t) + ", "
+                                      + "min distance = " + to_string(move_min) + ", " + "max distance = "
+                                      + to_string(move_max) + "\nbox " + to_string(box) + "\nray " + to_string(r));
+                        }
+                        const Vector<N, T> p = r.point(*t);
+                        if (!is_on_box(box, p, precision))
+                        {
+                                error("Intersection out of bounding box, outside\nintersection point = " + to_string(p)
+                                      + "\nbox " + to_string(box) + "\nray " + to_string(r));
+                        }
+                }
+                {
+                        const Ray<N, T> r = ray.moved(move_distance);
+                        const auto t = box.intersect(r);
+                        if (t)
+                        {
+                                error("Ray must not intersect\n" + to_string(r));
+                        }
+                }
+        }
+}
+
+template <std::size_t N, typename T>
+void test(const int point_count, const T& precision, std::mt19937_64& engine)
+{
+        test_intersections(create_random_box<N, T>(engine), point_count, precision, engine);
+}
+
+template <typename T>
+void test(const int point_count, const T& precision, std::mt19937_64& engine)
+{
+        test<2, T>(point_count, precision, engine);
+        test<3, T>(point_count, precision, engine);
+        test<4, T>(point_count, precision, engine);
+        test<5, T>(point_count, precision, engine);
+}
+
+void test_bounding_box()
+{
+        std::mt19937_64 engine = create_engine<std::mt19937_64>();
+
+        LOG("Test bounding box");
+        test<float>(10'000, 1e-5, engine);
+        test<double>(10'000, 1e-14, engine);
+        test<long double>(1'000, 1e-17, engine);
+        LOG("Test bounding box passed");
+}
+
+TEST_SMALL("Bounding box", test_bounding_box)
 }
 }
