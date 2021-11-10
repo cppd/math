14502853225e1c564d859be24fbd36122f3922f1
cppd <cppd@users.noreply.github.com>
2021-11-10 09:18:22 +0300

    reference

---
+++ src/com/reference.h
@@ -0,0 +1,53 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <utility>
+
+namespace ns
+{
+namespace reference_implementation
+{
+template <typename T>
+concept Indirection = requires(T&& v)
+{
+        *v;
+};
+}
+
+template <typename T>
+T& to_ref(T* const object)
+{
+        return *object;
+}
+
+template <typename T>
+T&& to_ref(T&& object) requires(!reference_implementation::Indirection<T>)
+{
+        return std::forward<T>(object);
+}
+
+template <typename T>
+decltype(auto) to_ref(T& object) requires(reference_implementation::Indirection<T>)
+{
+        return *object;
+}
+
+template <typename T>
+void to_ref(const T&& object) requires(reference_implementation::Indirection<T>) = delete;
+}
