1ad4d8db788377118b65be9ec45522c9cd2e89ba
cppd <cppd@users.noreply.github.com>
2021-12-30 08:05:22 +0300

    shading

--- src/shading/fresnel.h
+++ src/shading/fresnel.h
@@ -26,6 +26,7 @@ CRC Press, 2018.
 
 #pragma once
 
+#include <src/com/constant.h>
 #include <src/com/error.h>
 #include <src/com/exponent.h>
 
@@ -39,5 +40,45 @@ Color fresnel(const Color& f0, const T h_l)
         ASSERT(h_l >= 0);
         static constexpr Color WHITE(1);
         return interpolation(f0, WHITE, power<5>(1 - h_l));
+}
+
+// Integrate[Power[Sin[x],n-2]*Cos[x],{x,0,Pi/2},Assumptions->n\[Element]Integers&&n>=2]
+// 1 / (n - 1)
+// Simplify[(n-1)*Integrate[Power[Sin[x],n-2]*Cos[x]*(f0+(1-f0)*Power[1-Cos[x],5]),{x,0,Pi/2}]]
+template <std::size_t N, typename Color>
+constexpr Color fresnel_cosine_weighted_average(const Color& f0)
+{
+        using T = typename Color::DataType;
+
+        static_assert(N >= 3 && N <= 9);
+
+        if constexpr (N == 3)
+        {
+                return f0 * (T(20) / 21) + Color(T(1) / 21);
+        }
+        if constexpr (N == 4)
+        {
+                return f0 * (495 * PI<T> / 256 - T(36) / 7) + Color(T(43) / 7 - 495 * PI<T> / 256);
+        }
+        if constexpr (N == 5)
+        {
+                return f0 * (T(115) / 126) + Color(T(11) / 126);
+        }
+        if constexpr (N == 6)
+        {
+                return f0 * (715 * PI<T> / 512 - T(220) / 63) + Color(T(283) / 63 - 715 * PI<T> / 512);
+        }
+        if constexpr (N == 7)
+        {
+                return f0 * (T(29) / 33) + Color(T(4) / 33);
+        }
+        if constexpr (N == 8)
+        {
+                return f0 * (2275 * PI<T> / 2048 - T(260) / 99) + Color(T(359) / 99 - 2275 * PI<T> / 2048);
+        }
+        if constexpr (N == 9)
+        {
+                return f0 * (T(1093) / 1287) + Color(T(194) / 1287);
+        }
 }
 }
---
+++ src/shading/test/test_fresnel.cpp
@@ -0,0 +1,51 @@
+/*
+Copyright (C) 2017-2021 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../fresnel.h"
+
+#include <src/color/color.h>
+
+namespace ns::shading::test
+{
+namespace
+{
+// f0=1/2;
+// n=3;
+// N[(n-1)*Integrate[Power[Sin[x],n-2]*Cos[x]*(f0+(1-f0)*Power[1-Cos[x],5]),{x,0,Pi/2}],50]
+template <typename Color>
+struct Test
+{
+        template <std::size_t N>
+        static constexpr bool equal(const Color& a, const Color& b)
+        {
+                const Color v = fresnel_cosine_weighted_average<N>(a);
+                return v.equal_to_relative(b, 1e-6) && v.equal_to_absolute(b, 1e-7);
+        }
+
+        static_assert(equal<3>(Color(0.5), Color(0.52380952380952380952380952380952380952380952380952L)));
+        static_assert(equal<4>(Color(0.5), Color(0.53414661141500179372961514423452078857611831226709L)));
+        static_assert(equal<5>(Color(0.5), Color(0.54365079365079365079365079365079365079365079365079L)));
+        static_assert(equal<6>(Color(0.5), Color(0.55243921935527907324916649305826501397164100330401L)));
+        static_assert(equal<7>(Color(0.5), Color(0.56060606060606060606060606060606060606060606060606L)));
+        static_assert(equal<8>(Color(0.5), Color(0.56822816691139623250880668008422595808350231323425L)));
+        static_assert(equal<9>(Color(0.5), Color(0.57536907536907536907536907536907536907536907536908L)));
+};
+
+template struct Test<color::Color>;
+template struct Test<color::Spectrum>;
+}
+}
