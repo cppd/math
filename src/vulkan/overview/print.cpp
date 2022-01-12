1adb57bbb4cbe116d1d42803e17ffd135495f85e
cppd <cppd@users.noreply.github.com>
2022-01-12 07:39:08 +0300

    Vulkan information

--- src/vulkan/overview/print.cpp
+++ src/vulkan/overview/print.cpp
@@ -24,25 +24,33 @@ namespace ns::vulkan
 namespace
 {
 template <typename Flags, typename Flag>
-void add_flags(std::string* const s, Flags* const flags, const Flag& flag, const char* const name)
+void add_flags(
+        std::string* const s,
+        const Flags flags,
+        Flags* const flags_clear,
+        const Flag test_flags,
+        const std::string_view name)
 {
-        if ((*flags & flag) == flag)
+        if ((flags & test_flags) == test_flags)
         {
                 if (!s->empty())
                 {
                         *s += ", ";
                 }
                 *s += name;
-                *flags &= ~flag;
+                *flags_clear &= ~test_flags;
         }
 }
 
 template <typename Flags>
-void add_flags_unknown(std::string* const s, const Flags& flags)
+void add_flags_unknown(std::string* const s, const Flags flags)
 {
-        if (flags != 0)
+        if (flags)
         {
-                *s += s->empty() ? "" : ", ";
+                if (!s->empty())
+                {
+                        *s += ", ";
+                }
                 *s += "UNKNOWN (";
                 *s += to_string_binary(flags, "0b");
                 *s += ")";
@@ -50,119 +58,153 @@ void add_flags_unknown(std::string* const s, const Flags& flags)
 }
 }
 
-std::string samples_to_string(VkSampleCountFlags flags)
+std::string samples_to_string(const VkSampleCountFlags flags)
 {
         if (!flags)
         {
                 return "NONE";
         }
+
         std::string s;
-        add_flags(&s, &flags, VK_SAMPLE_COUNT_1_BIT, "1");
-        add_flags(&s, &flags, VK_SAMPLE_COUNT_2_BIT, "2");
-        add_flags(&s, &flags, VK_SAMPLE_COUNT_4_BIT, "4");
-        add_flags(&s, &flags, VK_SAMPLE_COUNT_8_BIT, "8");
-        add_flags(&s, &flags, VK_SAMPLE_COUNT_16_BIT, "16");
-        add_flags(&s, &flags, VK_SAMPLE_COUNT_32_BIT, "32");
-        add_flags(&s, &flags, VK_SAMPLE_COUNT_64_BIT, "64");
-        add_flags_unknown(&s, flags);
+        VkSampleCountFlags flags_clear = flags;
+
+        add_flags(&s, flags, &flags_clear, VK_SAMPLE_COUNT_1_BIT, "1");
+        add_flags(&s, flags, &flags_clear, VK_SAMPLE_COUNT_2_BIT, "2");
+        add_flags(&s, flags, &flags_clear, VK_SAMPLE_COUNT_4_BIT, "4");
+        add_flags(&s, flags, &flags_clear, VK_SAMPLE_COUNT_8_BIT, "8");
+        add_flags(&s, flags, &flags_clear, VK_SAMPLE_COUNT_16_BIT, "16");
+        add_flags(&s, flags, &flags_clear, VK_SAMPLE_COUNT_32_BIT, "32");
+        add_flags(&s, flags, &flags_clear, VK_SAMPLE_COUNT_64_BIT, "64");
+
+        add_flags_unknown(&s, flags_clear);
+
         return s;
 }
 
-std::string resolve_modes_to_string(VkResolveModeFlags flags)
+std::string resolve_modes_to_string(const VkResolveModeFlags flags)
 {
         if (!flags)
         {
                 return "NONE";
         }
+
         std::string s;
-        add_flags(&s, &flags, VK_RESOLVE_MODE_SAMPLE_ZERO_BIT, "SAMPLE_ZERO");
-        add_flags(&s, &flags, VK_RESOLVE_MODE_AVERAGE_BIT, "AVERAGE");
-        add_flags(&s, &flags, VK_RESOLVE_MODE_MIN_BIT, "MIN");
-        add_flags(&s, &flags, VK_RESOLVE_MODE_MAX_BIT, "MAX");
-        add_flags_unknown(&s, flags);
+        VkSampleCountFlags flags_clear = flags;
+
+        add_flags(&s, flags, &flags_clear, VK_RESOLVE_MODE_SAMPLE_ZERO_BIT, "SAMPLE_ZERO");
+        add_flags(&s, flags, &flags_clear, VK_RESOLVE_MODE_AVERAGE_BIT, "AVERAGE");
+        add_flags(&s, flags, &flags_clear, VK_RESOLVE_MODE_MIN_BIT, "MIN");
+        add_flags(&s, flags, &flags_clear, VK_RESOLVE_MODE_MAX_BIT, "MAX");
+
+        add_flags_unknown(&s, flags_clear);
+
         return s;
 }
 
-std::string shader_stages_to_string(VkShaderStageFlags flags)
+std::string shader_stages_to_string(const VkShaderStageFlags flags)
 {
         if (!flags)
         {
                 return "NONE";
         }
+
         std::string s;
-        add_flags(&s, &flags, VK_SHADER_STAGE_VERTEX_BIT, "VERTEX");
-        add_flags(&s, &flags, VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT, "TESSELLATION_CONTROL");
-        add_flags(&s, &flags, VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT, "TESSELLATION_EVALUATION");
-        add_flags(&s, &flags, VK_SHADER_STAGE_GEOMETRY_BIT, "GEOMETRY");
-        add_flags(&s, &flags, VK_SHADER_STAGE_FRAGMENT_BIT, "FRAGMENT");
-        add_flags(&s, &flags, VK_SHADER_STAGE_COMPUTE_BIT, "COMPUTE");
-        add_flags_unknown(&s, flags);
+        VkSampleCountFlags flags_clear = flags;
+
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_VERTEX_BIT, "VERTEX");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT, "TESSELLATION_CONTROL");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT, "TESSELLATION_EVALUATION");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_GEOMETRY_BIT, "GEOMETRY");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_FRAGMENT_BIT, "FRAGMENT");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_COMPUTE_BIT, "COMPUTE");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_RAYGEN_BIT_KHR, "RAYGEN");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_ANY_HIT_BIT_KHR, "ANY_HIT");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR, "CLOSEST_HIT");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_MISS_BIT_KHR, "MISS");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_INTERSECTION_BIT_KHR, "INTERSECTION");
+        add_flags(&s, flags, &flags_clear, VK_SHADER_STAGE_CALLABLE_BIT_KHR, "CALLABLE");
+
+        add_flags_unknown(&s, flags_clear);
+
         return s;
 }
 
-std::string subgroup_features_to_string(VkSubgroupFeatureFlags flags)
+std::string subgroup_features_to_string(const VkSubgroupFeatureFlags flags)
 {
         if (!flags)
         {
                 return "NONE";
         }
+
         std::string s;
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_BASIC_BIT, "BASIC");
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_VOTE_BIT, "VOTE");
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_ARITHMETIC_BIT, "ARITHMETIC");
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_BALLOT_BIT, "BALLOT");
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_SHUFFLE_BIT, "SHUFFLE");
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_SHUFFLE_RELATIVE_BIT, "SHUFFLE_RELATIVE");
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_CLUSTERED_BIT, "CLUSTERED");
-        add_flags(&s, &flags, VK_SUBGROUP_FEATURE_QUAD_BIT, "QUAD");
-        add_flags_unknown(&s, flags);
+        VkSampleCountFlags flags_clear = flags;
+
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_BASIC_BIT, "BASIC");
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_VOTE_BIT, "VOTE");
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_ARITHMETIC_BIT, "ARITHMETIC");
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_BALLOT_BIT, "BALLOT");
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_SHUFFLE_BIT, "SHUFFLE");
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_SHUFFLE_RELATIVE_BIT, "SHUFFLE_RELATIVE");
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_CLUSTERED_BIT, "CLUSTERED");
+        add_flags(&s, flags, &flags_clear, VK_SUBGROUP_FEATURE_QUAD_BIT, "QUAD");
+
+        add_flags_unknown(&s, flags_clear);
+
         return s;
 }
 
-std::string queues_to_string(VkQueueFlags flags)
+std::string queues_to_string(const VkQueueFlags flags)
 {
         if (!flags)
         {
                 return "NONE";
         }
+
         std::string s;
-        add_flags(&s, &flags, VK_QUEUE_GRAPHICS_BIT, "GRAPHICS");
-        add_flags(&s, &flags, VK_QUEUE_COMPUTE_BIT, "COMPUTE");
-        add_flags(&s, &flags, VK_QUEUE_TRANSFER_BIT, "TRANSFER");
-        add_flags(&s, &flags, VK_QUEUE_SPARSE_BINDING_BIT, "SPARSE_BINDING");
-        add_flags(&s, &flags, VK_QUEUE_PROTECTED_BIT, "PROTECTED");
-        add_flags_unknown(&s, flags);
+        VkSampleCountFlags flags_clear = flags;
+
+        add_flags(&s, flags, &flags_clear, VK_QUEUE_GRAPHICS_BIT, "GRAPHICS");
+        add_flags(&s, flags, &flags_clear, VK_QUEUE_COMPUTE_BIT, "COMPUTE");
+        add_flags(&s, flags, &flags_clear, VK_QUEUE_TRANSFER_BIT, "TRANSFER");
+        add_flags(&s, flags, &flags_clear, VK_QUEUE_SPARSE_BINDING_BIT, "SPARSE_BINDING");
+        add_flags(&s, flags, &flags_clear, VK_QUEUE_PROTECTED_BIT, "PROTECTED");
+
+        add_flags_unknown(&s, flags_clear);
+
         return s;
 }
 
 std::string shader_float_controls_independence_to_string(const VkShaderFloatControlsIndependence v)
 {
-        if (v == VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_32_BIT_ONLY)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wswitch"
+        switch (v)
         {
+        case VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_32_BIT_ONLY:
                 return "32_BIT_ONLY";
-        }
-
-        if (v == VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_ALL)
-        {
+        case VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_ALL:
                 return "ALL";
-        }
-        if (v == VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_NONE)
-        {
+        case VK_SHADER_FLOAT_CONTROLS_INDEPENDENCE_NONE:
                 return "NONE";
         }
+#pragma GCC diagnostic pop
+
         return "UNKNOWN";
 }
 
 std::string point_clipping_behavior_to_string(const VkPointClippingBehavior v)
 {
-        if (v == VK_POINT_CLIPPING_BEHAVIOR_ALL_CLIP_PLANES)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wswitch"
+        switch (v)
         {
+        case VK_POINT_CLIPPING_BEHAVIOR_ALL_CLIP_PLANES:
                 return "ALL_CLIP_PLANES";
-        }
-        if (v == VK_POINT_CLIPPING_BEHAVIOR_USER_CLIP_PLANES_ONLY)
-        {
+        case VK_POINT_CLIPPING_BEHAVIOR_USER_CLIP_PLANES_ONLY:
                 return "USER_CLIP_PLANES_ONLY";
         }
+#pragma GCC diagnostic pop
+
         return "UNKNOWN";
 }
 }
