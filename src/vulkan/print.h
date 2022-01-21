3d12a5bbdb60656c605b74b44ad044901b785521
cppd <cppd@users.noreply.github.com>
2022-01-21 09:25:44 +0300

    Vulkan print

--- src/vulkan/print.h
+++ src/vulkan/print.h
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "print/image_layout.h"
 #include "print/image_type.h"
 #include "print/physical_device_type.h"
+#include "print/present_mode.h"
 #include "print/result.h"
 
 #include <algorithm>
---
+++ src/vulkan/print/present_mode.cpp
@@ -0,0 +1,46 @@
+/*
+Copyright (C) 2017-2022 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "present_mode.h"
+
+#include <src/com/enum.h>
+#include <src/com/print.h>
+
+#define CASE(format) \
+        case format: \
+                return #format;
+
+namespace ns::vulkan
+{
+std::string present_mode_to_string(const VkPresentModeKHR mode)
+{
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wswitch"
+        switch (mode)
+        {
+                CASE(VK_PRESENT_MODE_IMMEDIATE_KHR)
+                CASE(VK_PRESENT_MODE_MAILBOX_KHR)
+                CASE(VK_PRESENT_MODE_FIFO_KHR)
+                CASE(VK_PRESENT_MODE_FIFO_RELAXED_KHR)
+                CASE(VK_PRESENT_MODE_SHARED_DEMAND_REFRESH_KHR)
+                CASE(VK_PRESENT_MODE_SHARED_CONTINUOUS_REFRESH_KHR)
+        }
+#pragma GCC diagnostic pop
+
+        return "Unknown VkPresentModeKHR " + to_string(enum_to_int(mode));
+}
+}
