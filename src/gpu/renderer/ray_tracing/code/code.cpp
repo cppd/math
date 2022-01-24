1291b261c0c4decacfff484ff462bba460f9e175
cppd <cppd@users.noreply.github.com>
2022-01-24 09:07:28 +0300

    ray tracing shaders

---
+++ src/gpu/renderer/ray_tracing/code/code.cpp
@@ -0,0 +1,45 @@
+/*
+Copyright (C) 2017-2022 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "code.h"
+
+namespace ns::gpu::renderer
+{
+std::vector<std::uint32_t> code_ray_closest_hit_rchit()
+{
+        static constexpr std::uint32_t CODE[] = {
+#include "renderer_ray_closest_hit.rchit.spr"
+        };
+        return {std::cbegin(CODE), std::cend(CODE)};
+}
+
+std::vector<std::uint32_t> code_ray_generation_rgen()
+{
+        static constexpr std::uint32_t CODE[] = {
+#include "renderer_ray_generation.rgen.spr"
+        };
+        return {std::cbegin(CODE), std::cend(CODE)};
+}
+
+std::vector<std::uint32_t> code_ray_miss_rmiss()
+{
+        static constexpr std::uint32_t CODE[] = {
+#include "renderer_ray_miss.rmiss.spr"
+        };
+        return {std::cbegin(CODE), std::cend(CODE)};
+}
+}
---
+++ src/gpu/renderer/ray_tracing/code/renderer_ray_closest_hit.rchit
@@ -0,0 +1,28 @@
+/*
+Copyright (C) 2017-2022 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#version 460
+
+#extension GL_EXT_ray_tracing : enable
+
+layout(location = 0) rayPayloadInEXT vec3 hit_value;
+hitAttributeEXT vec2 hit_attribute;
+
+void main()
+{
+        hit_value = vec3(hit_attribute.x, hit_attribute.y, 1 - hit_attribute.x - hit_attribute.y);
+}
