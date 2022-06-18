d24b91d4e94e5a0c8ba65c9f4c9ed68d622f6432
cppd <cppd@users.noreply.github.com>
2022-06-18 09:58:10 +0300

    settings instantiation

---
+++ src/settings/instantiation.h
@@ -0,0 +1,74 @@
+/*
+Copyright (C) 2017-2022 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define TEMPLATE_INSTANTIATION_N(TEMPLATE_N) \
+        TEMPLATE_N(3)                        \
+        TEMPLATE_N(4)                        \
+        TEMPLATE_N(5)                        \
+        TEMPLATE_N(6)
+
+#define TEMPLATE_INSTANTIATION_N_2(TEMPLATE_N) \
+        TEMPLATE_N(2)                          \
+        TEMPLATE_N(3)                          \
+        TEMPLATE_N(4)                          \
+        TEMPLATE_N(5)                          \
+        TEMPLATE_N(6)
+
+#define TEMPLATE_INSTANTIATION_N_2_A(TEMPLATE_N) \
+        TEMPLATE_N(2)                            \
+        TEMPLATE_N(3)                            \
+        TEMPLATE_N(4)                            \
+        TEMPLATE_N(5)                            \
+        TEMPLATE_N(6)                            \
+        TEMPLATE_N(7)
+
+//
+
+#define TEMPLATE_INSTANTIATION_N_T_IMPL_N(N, TEMPLATE_N_T) \
+        TEMPLATE_N_T((N), float)                           \
+        TEMPLATE_N_T((N), double)
+
+#define TEMPLATE_INSTANTIATION_N_T(TEMPLATE_N_T)           \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(3, TEMPLATE_N_T) \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(4, TEMPLATE_N_T) \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(5, TEMPLATE_N_T) \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(6, TEMPLATE_N_T)
+
+#define TEMPLATE_INSTANTIATION_N_T_2(TEMPLATE_N_T)         \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(2, TEMPLATE_N_T) \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(3, TEMPLATE_N_T) \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(4, TEMPLATE_N_T) \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(5, TEMPLATE_N_T) \
+        TEMPLATE_INSTANTIATION_N_T_IMPL_N(6, TEMPLATE_N_T)
+
+//
+
+#define TEMPLATE_INSTANTIATION_N_T_C_IMPL_N_T(N, T, TEMPLATE_N_T_C) \
+        TEMPLATE_N_T_C((N), T, ::ns::color::Color)                  \
+        TEMPLATE_N_T_C((N), T, ::ns::color::Spectrum)
+
+#define TEMPLATE_INSTANTIATION_N_T_C_IMPL_N(N, TEMPLATE_N_T_C)            \
+        TEMPLATE_INSTANTIATION_N_T_C_IMPL_N_T((N), float, TEMPLATE_N_T_C) \
+        TEMPLATE_INSTANTIATION_N_T_C_IMPL_N_T((N), double, TEMPLATE_N_T_C)
+
+#define TEMPLATE_INSTANTIATION_N_T_C(TEMPLATE_N_T_C)           \
+        TEMPLATE_INSTANTIATION_N_T_C_IMPL_N(3, TEMPLATE_N_T_C) \
+        TEMPLATE_INSTANTIATION_N_T_C_IMPL_N(4, TEMPLATE_N_T_C) \
+        TEMPLATE_INSTANTIATION_N_T_C_IMPL_N(5, TEMPLATE_N_T_C) \
+        TEMPLATE_INSTANTIATION_N_T_C_IMPL_N(6, TEMPLATE_N_T_C)
