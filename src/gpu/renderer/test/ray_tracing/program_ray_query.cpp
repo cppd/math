615011b516a1471eeff9fbcb53651a798cf4f714
cppd <cppd@users.noreply.github.com>
2022-11-13 11:49:46 +0300

    renderer tests

--- src/gpu/renderer/test/ray_tracing/program_ray_query.cpp
+++ src/gpu/renderer/test/ray_tracing/program_ray_query.cpp
@@ -25,55 +25,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/vulkan/error.h>
 #include <src/vulkan/pipeline_compute.h>
 
+#include <array>
+
 namespace ns::gpu::renderer::test
 {
 namespace
 {
-class Constant final
+class SpecializationConstants final
 {
         struct Data final
         {
                 std::int32_t local_size_x;
                 std::int32_t local_size_y;
-        } data_;
+        };
 
-        std::vector<VkSpecializationMapEntry> entries_;
+        static constexpr std::array<VkSpecializationMapEntry, 2> ENTRIES{
+                {{0, offsetof(Data, local_size_x), sizeof(Data::local_size_x)},
+                 {1, offsetof(Data, local_size_y), sizeof(Data::local_size_y)}}
+        };
+
+        Data data_;
+
+        VkSpecializationInfo info_{
+                .mapEntryCount = ENTRIES.size(),
+                .pMapEntries = ENTRIES.data(),
+                .dataSize = sizeof(data_),
+                .pData = &data_};
 
 public:
-        Constant();
+        explicit SpecializationConstants(const std::int32_t local_size)
+                : data_{.local_size_x = local_size, .local_size_y = local_size}
+        {
+        }
 
-        void set(std::int32_t size);
+        SpecializationConstants(const SpecializationConstants&) = delete;
+        SpecializationConstants& operator=(const SpecializationConstants&) = delete;
 
-        [[nodiscard]] VkSpecializationInfo info() const;
+        [[nodiscard]] const VkSpecializationInfo& info() const
+        {
+                return info_;
+        }
 };
-
-Constant::Constant()
-{
-        entries_.resize(2);
-
-        entries_[0].constantID = 0;
-        entries_[0].offset = offsetof(Data, local_size_x);
-        entries_[0].size = sizeof(Data::local_size_x);
-
-        entries_[1].constantID = 1;
-        entries_[1].offset = offsetof(Data, local_size_y);
-        entries_[1].size = sizeof(Data::local_size_y);
-}
-
-void Constant::set(const std::int32_t size)
-{
-        data_ = {.local_size_x = size, .local_size_y = size};
-}
-
-VkSpecializationInfo Constant::info() const
-{
-        VkSpecializationInfo info = {};
-        info.mapEntryCount = entries_.size();
-        info.pMapEntries = entries_.data();
-        info.dataSize = sizeof(data_);
-        info.pData = &data_;
-        return info;
-}
 }
 
 std::vector<VkDescriptorSetLayoutBinding> RayQueryProgram::descriptor_set_layout_bindings()
@@ -89,16 +81,13 @@ RayQueryProgram::RayQueryProgram(const VkDevice device, const unsigned local_siz
 {
         const vulkan::Shader shader(device, code_ray_query_comp(), VK_SHADER_STAGE_COMPUTE_BIT);
 
-        Constant constant;
-        constant.set(local_size);
-
-        const VkSpecializationInfo constant_info = constant.info();
+        const SpecializationConstants constants(local_size);
 
         vulkan::ComputePipelineCreateInfo compute_info;
         compute_info.device = device;
         compute_info.pipeline_layout = pipeline_layout_;
         compute_info.shader = &shader;
-        compute_info.constants = &constant_info;
+        compute_info.constants = &constants.info();
         pipeline_ = vulkan::create_compute_pipeline(compute_info);
 }
 
