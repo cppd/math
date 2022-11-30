b0e6faff88dbdba6a8a7eea823f5ff2cb99fb1d3
cppd <cppd@users.noreply.github.com>
2022-11-30 09:48:02 +0300

    refactoring

--- src/com/alg.h
+++ src/com/alg.h
@@ -33,14 +33,15 @@ void sort_and_unique(T* const v)
         v->erase(std::unique(v->begin(), v->end()), v->end());
 }
 
-template <typename T, typename Less, typename Equal>
-void sort_and_unique(T* const v, const Less& less, const Equal& equal)
-{
-        std::sort(v->begin(), v->end(), less);
-        v->erase(std::unique(v->begin(), v->end(), equal), v->end());
-}
+// template <typename T, typename Less, typename Equal>
+// void sort_and_unique(T* const v, const Less& less, const Equal& equal)
+// {
+//         std::sort(v->begin(), v->end(), less);
+//         v->erase(std::unique(v->begin(), v->end(), equal), v->end());
+// }
 
 template <typename T>
+        requires(!std::is_pointer_v<T>)
 [[nodiscard]] T sort_and_unique(T v)
 {
         sort_and_unique(&v);
@@ -80,89 +81,89 @@ template <typename T>
                 });
 }
 
-template <typename T, typename V>
-[[nodiscard]] constexpr T multiply_all(const V& v)
+template <typename Result, typename Data>
+[[nodiscard]] constexpr Result multiply_all(const Data& data)
 {
         static_assert(
-                (Integral<typename V::value_type> && Integral<T>)
-                || (FloatingPoint<typename V::value_type> && FloatingPoint<T>));
-        static_assert(Signed<typename V::value_type> == Signed<T>);
-        static_assert(Limits<typename V::value_type>::digits() <= Limits<T>::digits());
+                (Integral<typename Data::value_type> && Integral<Result>)
+                || (FloatingPoint<typename Data::value_type> && FloatingPoint<Result>));
+        static_assert(Signed<typename Data::value_type> == Signed<Result>);
+        static_assert(Limits<typename Data::value_type>::digits() <= Limits<Result>::digits());
 
-        if (v.empty())
+        if (data.empty())
         {
                 error("Empty container for multiply all");
         }
 
-        T value = v[0];
-        for (std::size_t i = 1; i < v.size(); ++i)
+        Result value = data[0];
+        for (std::size_t i = 1; i < data.size(); ++i)
         {
-                value *= v[i];
+                value *= data[i];
         }
 
         return value;
 }
 
-template <typename T, typename V>
-[[nodiscard]] constexpr T add_all(const V& v)
+template <typename Result, typename Data>
+[[nodiscard]] constexpr Result add_all(const Data& data)
 {
         static_assert(
-                (Integral<typename V::value_type> && Integral<T>)
-                || (FloatingPoint<typename V::value_type> && FloatingPoint<T>));
-        static_assert(Signed<typename V::value_type> == Signed<T>);
-        static_assert(Limits<typename V::value_type>::digits() <= Limits<T>::digits());
+                (Integral<typename Data::value_type> && Integral<Result>)
+                || (FloatingPoint<typename Data::value_type> && FloatingPoint<Result>));
+        static_assert(Signed<typename Data::value_type> == Signed<Result>);
+        static_assert(Limits<typename Data::value_type>::digits() <= Limits<Result>::digits());
 
-        if (v.empty())
+        if (data.empty())
         {
                 error("Empty container for add all");
         }
 
-        T value = v[0];
-        for (std::size_t i = 1; i < v.size(); ++i)
+        Result value = data[0];
+        for (std::size_t i = 1; i < data.size(); ++i)
         {
-                value += v[i];
+                value += data[i];
         }
 
         return value;
 }
 
-template <typename T1, typename T2>
-[[nodiscard]] bool intersect(T1 t1, T2 t2)
-{
-        std::sort(t1.begin(), t1.end());
-        std::sort(t2.begin(), t2.end());
-
-        auto i1 = std::cbegin(t1);
-        auto i2 = std::cbegin(t2);
-
-        while (true)
-        {
-                if (i1 == std::cend(t1))
-                {
-                        return false;
-                }
-
-                if (i2 == std::cend(t2))
-                {
-                        return false;
-                }
-
-                if (*i1 < *i2)
-                {
-                        ++i1;
-                }
-                else if (*i1 > *i2)
-                {
-                        ++i2;
-                }
-                else if (!(*i1 == *i2))
-                {
-                        error("Failed to find intersection. Not (a < b) and not (a > b) and not (a == b)");
-                }
-                else
-                {
-                        return true;
-                }
-        }
-}
+// template <typename T1, typename T2>
+// [[nodiscard]] bool intersect(T1 t1, T2 t2)
+// {
+//         std::sort(t1.begin(), t1.end());
+//         std::sort(t2.begin(), t2.end());
+//
+//         auto i1 = std::cbegin(t1);
+//         auto i2 = std::cbegin(t2);
+//
+//         while (true)
+//         {
+//                 if (i1 == std::cend(t1))
+//                 {
+//                         return false;
+//                 }
+//
+//                 if (i2 == std::cend(t2))
+//                 {
+//                         return false;
+//                 }
+//
+//                 if (*i1 < *i2)
+//                 {
+//                         ++i1;
+//                 }
+//                 else if (*i1 > *i2)
+//                 {
+//                         ++i2;
+//                 }
+//                 else if (!(*i1 == *i2))
+//                 {
+//                         error("Failed to find intersection. Not (a < b) and not (a > b) and not (a == b)");
+//                 }
+//                 else
+//                 {
+//                         return true;
+//                 }
+//         }
+// }
 }
