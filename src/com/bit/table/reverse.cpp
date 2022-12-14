efb727fd071cce8cac121b25bfd97aee82d62ca5
cppd <cppd@users.noreply.github.com>
2022-12-14 09:37:01 +0300

    bit reverse table

--- src/com/bit/table/reverse.cpp
+++ src/com/bit/table/reverse.cpp
@@ -17,28 +17,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "reverse.h"
 
-#if 0
+#include "../reverse.h"
+
 #include <iomanip>
 #include <sstream>
 
-namespace ns
+namespace ns::bit::table
 {
-std::string lookup_table_bit_reverse()
+std::string bit_reverse_lookup_table()
 {
+        const std::string new_line = '\n' + std::string(8, ' ');
+
         std::ostringstream oss;
         oss << std::hex << std::setfill('0');
+
         oss << "// clang-format off\n";
         oss << "inline constexpr std::array<std::uint8_t, 256> BIT_REVERSE_TABLE =\n";
         oss << "{";
         for (int i = 0; i <= 255; ++i)
         {
                 oss << ((i != 0) ? "," : "");
-                oss << (((i & 0b111) != 0) ? " " : "\n" + std::string(8, ' '));
+                oss << (((i % 8) != 0) ? " " : new_line);
                 oss << "0x" << std::setw(2) << bit_reverse(8, i);
         }
         oss << "\n};\n";
         oss << "// clang-format on\n";
+
         return oss.str();
 }
 }
-#endif
