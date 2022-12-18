58d98432d6297b0ac3294411817d77e6998cc404
cppd <cppd@users.noreply.github.com>
2022-12-18 11:31:34 +0300

    noise

--- src/numerical/noise.cpp
+++ src/numerical/noise.cpp
@@ -18,11 +18,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "noise.h"
 
 #include <src/com/error.h>
+#include <src/com/interpolation_smooth.h>
 #include <src/com/random/pcg.h>
 #include <src/sampling/sphere_uniform.h>
 #include <src/settings/instantiation.h>
 
 #include <algorithm>
+#include <array>
+#include <bit>
+#include <cmath>
 #include <numeric>
 #include <vector>
 
@@ -31,7 +35,11 @@ namespace ns::numerical
 namespace
 {
 constexpr unsigned SIZE = 256;
+constexpr Smooth INTERPOLATION_TYPE{Smooth::N_2};
 constexpr PCG::result_type PCG_INIT_VALUE{12345};
+
+static_assert(std::has_single_bit(SIZE));
+constexpr unsigned SIZE_M = SIZE - 1;
 
 template <typename RandomEngine>
 std::vector<unsigned> permutation_table(const unsigned size, RandomEngine& engine)
@@ -64,6 +72,9 @@ std::vector<Vector<N, T>> gradients(const unsigned size, RandomEngine& engine)
 template <std::size_t N, typename T>
 class Noise final
 {
+        static_assert(N > 0);
+        static_assert(std::is_floating_point_v<T>);
+
         std::vector<unsigned> perm_;
         std::vector<Vector<N, T>> gradients_;
 
@@ -77,9 +88,38 @@ public:
                 ASSERT(gradients_.size() == SIZE);
         }
 
-        [[nodiscard]] T compute(const Vector<N, T>& /*p*/) const
+        [[nodiscard]] T compute(const Vector<N, T>& p) const
         {
-                error("not implemented");
+                std::array<std::array<T, N>, 2> s;
+                std::array<std::array<unsigned, N>, 2> cell;
+
+                for (std::size_t i = 0; i < N; ++i)
+                {
+                        const T floor = std::floor(p[i]);
+
+                        s[0][i] = p[i] - floor;
+                        s[1][i] = s[0][i] - 1;
+
+                        cell[0][i] = static_cast<long long>(floor) & SIZE_M;
+                        cell[1][i] = cell[0][i] + 1;
+                }
+
+                std::array<T, (1 << N)> data;
+
+                for (std::size_t i = 0; i < data.size(); ++i)
+                {
+                        Vector<N, T> v;
+                        unsigned hash = 0;
+                        for (std::size_t n = 0; n < N; ++n)
+                        {
+                                const bool bit = ((1 << n) & i) != 0;
+                                v[n] = s[bit][n];
+                                hash = perm_[cell[bit][n] + hash];
+                        }
+                        data[i] = dot(gradients_[hash], v);
+                }
+
+                return interpolation<INTERPOLATION_TYPE>(data, s[0]);
         }
 };
 }
