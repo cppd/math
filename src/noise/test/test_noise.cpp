6f1eeeb3d95b8e2d6f834c181f49dcd7fdf1d9b9
cppd <cppd@users.noreply.github.com>
2022-12-23 09:20:42 +0300

    noise tests

--- src/noise/test/test_noise.cpp
+++ src/noise/test/test_noise.cpp
@@ -21,11 +21,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/com/file/path.h>
 #include <src/com/log.h>
 #include <src/com/print.h>
+#include <src/com/random/pcg.h>
+#include <src/com/type/name.h>
 #include <src/image/file_save.h>
 #include <src/numerical/vector.h>
+#include <src/settings/dimensions.h>
 #include <src/test/test.h>
 
+#include <algorithm>
+#include <array>
 #include <filesystem>
+#include <numeric>
+#include <random>
 #include <vector>
 
 namespace ns::noise
@@ -45,41 +52,43 @@ T noise_coordinate(const int x)
 }
 
 template <std::size_t N, typename T>
-        requires (N == 2)
-T compute_noise(const int i, const int j)
+class Noise final
 {
-        Vector<2, T> p;
-        p[0] = noise_coordinate<T>(i);
-        p[1] = noise_coordinate<T>(j);
-        return noise(p);
-}
+        Vector<N, T> vector_;
+        std::array<int, 2> indices_;
 
-template <std::size_t N, typename T>
-        requires (N == 3)
-T compute_noise(const int i, const int j)
-{
-        Vector<3, T> p;
-        p[0] = noise_coordinate<T>(i);
-        p[1] = 3.1;
-        p[2] = noise_coordinate<T>(j);
-        return noise(p);
-}
+public:
+        Noise()
+        {
+                PCG pcg(N * 12345);
 
-template <std::size_t N, typename T>
-        requires (N == 4)
-T compute_noise(const int i, const int j)
-{
-        Vector<4, T> p;
-        p[0] = 2.4;
-        p[1] = noise_coordinate<T>(i);
-        p[2] = -5.7;
-        p[3] = noise_coordinate<T>(j);
-        return noise(p);
-}
+                static_assert(std::is_same_v<T, float> || std::is_same_v<T, double>);
+                // using double to make identical numbers for different types
+                std::uniform_real_distribution<double> urd(-10, 10);
+                for (std::size_t i = 0; i < N; ++i)
+                {
+                        vector_[i] = static_cast<T>(urd(pcg));
+                }
+
+                std::array<int, N> indices;
+                std::iota(indices.begin(), indices.end(), 0);
+                std::sample(indices.cbegin(), indices.cend(), indices_.begin(), indices_.size(), pcg);
+        }
+
+        [[nodiscard]] T compute(const int i, const int j)
+        {
+                Vector<N, T> p = vector_;
+                p[indices_[0]] = noise_coordinate<T>(i);
+                p[indices_[1]] = noise_coordinate<T>(j);
+                return noise(p);
+        }
+};
 
 template <std::size_t N, typename T>
 void test()
 {
+        Noise<N, T> noise;
+
         std::vector<float> pixels(static_cast<unsigned long long>(IMAGE_SIZE) * IMAGE_SIZE);
 
         unsigned long long index = -1;
@@ -87,7 +96,7 @@ void test()
         {
                 for (int j = 0; j < IMAGE_SIZE; ++j)
                 {
-                        const T n = compute_noise<N, T>(i, j);
+                        const T n = noise.compute(i, j);
                         if (!(n > T{-1.001} && n < T{1.001}))
                         {
                                 error("Noise value " + to_string(n) + " is not in the range [-1, 1]");
@@ -96,27 +105,26 @@ void test()
                 }
         }
 
+        const std::string file_name = std::string(FILE_NAME) + '_' + std::to_string(N) + "d_" + type_name<T>();
+
         image::save(
-                std::filesystem::temp_directory_path()
-                        / path_from_utf8(std::string(FILE_NAME) + '_' + std::to_string(N)),
+                std::filesystem::temp_directory_path() / path_from_utf8(file_name),
                 image::ImageView<2>(
                         {IMAGE_SIZE, IMAGE_SIZE}, image::ColorFormat::R32,
                         std::as_bytes(std::span(pixels.cbegin(), pixels.cend()))));
 }
 
-template <typename T>
-void test()
+template <std::size_t... I>
+void test(std::index_sequence<I...>&&)
 {
-        test<2, T>();
-        test<3, T>();
-        test<4, T>();
+        (test<I, float>(), ...);
+        (test<I, double>(), ...);
 }
 
 void test_noise()
 {
         LOG("Test noise");
-        test<float>();
-        test<double>();
+        test(settings::Dimensions2());
         LOG("Test noise passed");
 }
 
