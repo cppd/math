1405540170b2b2eb551298995d451f03030a4810
cppd <cppd@users.noreply.github.com>
2023-01-05 06:54:42 +0300

    window manage

--- src/window/obsolete/manage.cpp
+++ src/window/obsolete/manage.cpp
@@ -37,7 +37,7 @@ namespace ns::window
 {
 namespace
 {
-int error_handler(Display*, XErrorEvent* e)
+int error_handler(Display*, XErrorEvent* const e)
 {
         constexpr int BUF_SIZE = 1000;
         std::array<char, BUF_SIZE> buf;
@@ -47,13 +47,13 @@ int error_handler(Display*, XErrorEvent* e)
 
 Display* g_display = nullptr;
 
-//constexpr Atom xa_atom()
-//{
-//#pragma GCC diagnostic push
-//#pragma GCC diagnostic ignored "-Wold-style-cast"
-//        return XA_ATOM;
-//#pragma GCC diagnostic pop
-//}
+// constexpr Atom xa_atom()
+// {
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wold-style-cast"
+//         return XA_ATOM;
+// #pragma GCC diagnostic pop
+// }
 }
 
 WindowInit::WindowInit()
@@ -89,71 +89,71 @@ WindowInit::~WindowInit()
         XSetErrorHandler(nullptr);
 }
 
-//void move_window_to_parent(WindowID window, WindowID parent)
-//{
-//        // https://standards.freedesktop.org/wm-spec/wm-spec-1.5.html
+// void move_window_to_parent(const WindowID window, const WindowID parent)
+// {
+//         // https://standards.freedesktop.org/wm-spec/wm-spec-1.5.html
+//
+//         Display* const display = g_display;
+//
+//         XUnmapWindow(display, window);
+//
+// #if 1
+//         const Atom wm_window_type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
+//         Atom wm_window_type_toolbar = XInternAtom(display, "_NET_WM_WINDOW_TYPE_TOOLBAR", False);
+//         const unsigned char* const toolbar = reinterpret_cast<const unsigned char*>(&wm_window_type_toolbar);
+//         XChangeProperty(display, window, wm_window_type, xa_atom(), 32, PropModeAppend, toolbar, 1);
+// #else
+//         const Atom wm_state = XInternAtom(display, "_NET_WM_STATE", False);
+//         Atom wm_state_skip_taskbar = XInternAtom(display, "_NET_WM_STATE_SKIP_TASKBAR", False);
+//         Atom wm_state_skip_pager = XInternAtom(display, "_NET_WM_STATE_SKIP_PAGER", False);
+//         const unsigned char* const skip_taskbar = reinterpret_cast<const unsigned char*>(&wm_state_skip_taskbar);
+//         const unsigned char* const skip_pager = reinterpret_cast<const unsigned char*>(&wm_state_skip_pager);
+//         XChangeProperty(display, window, wm_state, xa_atom(), 32, PropModeAppend, skip_taskbar, 1);
+//         XChangeProperty(display, window, wm_state, xa_atom(), 32, PropModeAppend, skip_pager, 1);
+// #endif
+//
+//         XReparentWindow(display, window, parent, 0, 0);
+//
+//         XWindowAttributes parent_attr;
+//         XGetWindowAttributes(display, parent, &parent_attr);
+//         XResizeWindow(display, window, parent_attr.width, parent_attr.height);
+//
+//         XMapWindow(display, window);
+// }
 
-//        Display* display = g_display;
+// void make_window_fullscreen(const WindowID window)
+// {
+//         // https://standards.freedesktop.org/wm-spec/wm-spec-1.5.html
+//
+//         Display* const display = g_display;
+//
+//         XUnmapWindow(display, window);
+//
+//         const Atom wm_state = XInternAtom(display, "_NET_WM_STATE", False);
+//         Atom wm_state_fullscreen = XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", False);
+//         const unsigned char* const fullscreen = reinterpret_cast<const unsigned char*>(&wm_state_fullscreen);
+//         XChangeProperty(display, window, wm_state, xa_atom(), 32, PropModeAppend, fullscreen, 1);
+//
+//         XReparentWindow(display, window, XDefaultRootWindow(display), 0, 0);
+//
+//         XMapWindow(display, window);
+// }
 
-//        XUnmapWindow(display, window);
+// void set_focus(WindowID window)
+// {
+//         Display* const display = g_display;
+//
+//         XSetInputFocus(display, window, RevertToParent, CurrentTime);
+// }
 
-//#if 1
-//        Atom wm_window_type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
-//        Atom wm_window_type_toolbar = XInternAtom(display, "_NET_WM_WINDOW_TYPE_TOOLBAR", False);
-//        const unsigned char* toolbar = reinterpret_cast<const unsigned char*>(&wm_window_type_toolbar);
-//        XChangeProperty(display, window, wm_window_type, xa_atom(), 32, PropModeAppend, toolbar, 1);
-//#else
-//        Atom wm_state = XInternAtom(display, "_NET_WM_STATE", False);
-//        Atom wm_state_skip_taskbar = XInternAtom(display, "_NET_WM_STATE_SKIP_TASKBAR", False);
-//        Atom wm_state_skip_pager = XInternAtom(display, "_NET_WM_STATE_SKIP_PAGER", False);
-//        const unsigned char* skip_taskbar = reinterpret_cast<const unsigned char*>(&wm_state_skip_taskbar);
-//        const unsigned char* skip_pager = reinterpret_cast<const unsigned char*>(&wm_state_skip_pager);
-//        XChangeProperty(display, window, wm_state, xa_atom(), 32, PropModeAppend, skip_taskbar, 1);
-//        XChangeProperty(display, window, wm_state, xa_atom(), 32, PropModeAppend, skip_pager, 1);
-//#endif
-
-//        XReparentWindow(display, window, parent, 0, 0);
-
-//        XWindowAttributes parent_attr;
-//        XGetWindowAttributes(display, parent, &parent_attr);
-//        XResizeWindow(display, window, parent_attr.width, parent_attr.height);
-
-//        XMapWindow(display, window);
-//}
-
-//void make_window_fullscreen(WindowID window)
-//{
-//        // https://standards.freedesktop.org/wm-spec/wm-spec-1.5.html
-
-//        Display* display = g_display;
-
-//        XUnmapWindow(display, window);
-
-//        Atom wm_state = XInternAtom(display, "_NET_WM_STATE", False);
-//        Atom wm_state_fullscreen = XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", False);
-//        const unsigned char* fullscreen = reinterpret_cast<const unsigned char*>(&wm_state_fullscreen);
-//        XChangeProperty(display, window, wm_state, xa_atom(), 32, PropModeAppend, fullscreen, 1);
-
-//        XReparentWindow(display, window, XDefaultRootWindow(display), 0, 0);
-
-//        XMapWindow(display, window);
-//}
-
-//void set_focus(WindowID window)
-//{
-//        Display* display = g_display;
-
-//        XSetInputFocus(display, window, RevertToParent, CurrentTime);
-//}
-
-//void set_size_to_parent(WindowID window, WindowID parent)
-//{
-//        Display* display = g_display;
-
-//        XWindowAttributes parent_attr;
-//        XGetWindowAttributes(display, parent, &parent_attr);
-//        XResizeWindow(display, window, parent_attr.width, parent_attr.height);
-//}
+// void set_size_to_parent(WindowID window, WindowID parent)
+// {
+//         Display* const display = g_display;
+//
+//         XWindowAttributes parent_attr;
+//         XGetWindowAttributes(display, parent, &parent_attr);
+//         XResizeWindow(display, window, parent_attr.width, parent_attr.height);
+// }
 
 std::string vulkan_create_surface_extension()
 {
@@ -162,7 +162,7 @@ std::string vulkan_create_surface_extension()
 
 VkSurfaceKHR vulkan_create_surface(WindowID window, VkInstance instance)
 {
-        Display* display = g_display;
+        Display* const display = g_display;
 
         VkXlibSurfaceCreateInfoKHR info = {};
         info.sType = VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR;
@@ -194,44 +194,44 @@ WindowInit::~WindowInit()
 {
 }
 
-//void move_window_to_parent(WindowID window, WindowID parent)
-//{
-//        SetWindowLongPtr(window, GWL_STYLE, WS_CHILD | WS_VISIBLE);
+// void move_window_to_parent(WindowID window, WindowID parent)
+// {
+//         SetWindowLongPtr(window, GWL_STYLE, WS_CHILD | WS_VISIBLE);
+//
+//         SetParent(window, parent);
+//
+//         RECT rect;
+//         GetClientRect(parent, &rect);
+//         SetWindowPos(window, HWND_TOP, 0, 0, rect.right - rect.left, rect.bottom - rect.top, 0);
+//
+//         SetFocus(parent);
+// }
 
-//        SetParent(window, parent);
+// void change_window_style_not_child(WindowID window)
+// {
+//         SetWindowLongPtr(window, GWL_STYLE, WS_POPUP);
+// }
 
-//        RECT rect;
-//        GetClientRect(parent, &rect);
-//        SetWindowPos(window, HWND_TOP, 0, 0, rect.right - rect.left, rect.bottom - rect.top, 0);
+// void make_window_fullscreen(WindowID window)
+// {
+//         SetWindowLongPtr(window, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+//
+//         SetParent(window, nullptr);
+//
+//         SetWindowPos(window, HWND_TOPMOST, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), 0);
+// }
 
-//        SetFocus(parent);
-//}
+// void set_focus(WindowID window)
+// {
+//         SetFocus(window);
+// }
 
-//void change_window_style_not_child(WindowID window)
-//{
-//        SetWindowLongPtr(window, GWL_STYLE, WS_POPUP);
-//}
-
-//void make_window_fullscreen(WindowID window)
-//{
-//        SetWindowLongPtr(window, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-
-//        SetParent(window, nullptr);
-
-//        SetWindowPos(window, HWND_TOPMOST, 0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), 0);
-//}
-
-//void set_focus(WindowID window)
-//{
-//        SetFocus(window);
-//}
-
-//void set_size_to_parent(WindowID window, WindowID parent)
-//{
-//        RECT rect;
-//        GetClientRect(parent, &rect);
-//        SetWindowPos(window, HWND_TOP, 0, 0, rect.right - rect.left, rect.bottom - rect.top, 0);
-//}
+// void set_size_to_parent(WindowID window, WindowID parent)
+// {
+//         RECT rect;
+//         GetClientRect(parent, &rect);
+//         SetWindowPos(window, HWND_TOP, 0, 0, rect.right - rect.left, rect.bottom - rect.top, 0);
+// }
 
 std::string vulkan_create_surface_extension()
 {
