598ed53a9e31cffe62b4a3462071df57c14aeca3
cppd <cppd@users.noreply.github.com>
2023-02-02 09:19:41 +0300

    pixels samples create

---
+++ src/painter/pixels/samples/com/create.h
@@ -0,0 +1,63 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <src/com/error.h>
+
+namespace ns::painter::pixels::samples::com
+{
+template <typename T, typename Copy>
+void create_without_sum(const std::size_t count, const Copy copy)
+{
+        ASSERT(count > 0 && count <= T::size());
+
+        for (std::size_t i = 0; i < count; ++i)
+        {
+                copy(i);
+        }
+}
+
+template <typename T, typename Copy, typename Sum>
+void create_with_sum(const std::size_t count, const Copy copy, const Sum sum)
+{
+        static constexpr std::size_t COUNT = T::size();
+        static_assert(COUNT % 2 == 0);
+
+        ASSERT(count > COUNT);
+
+        std::size_t sample_i = 0;
+
+        for (std::size_t i = 0; i < COUNT / 2; ++i, ++sample_i)
+        {
+                copy(i, sample_i);
+        }
+
+        const std::size_t sum_end = count - COUNT / 2;
+        for (; sample_i < sum_end; ++sample_i)
+        {
+                sum(sample_i);
+        }
+
+        for (std::size_t i = COUNT / 2; i < COUNT; ++i, ++sample_i)
+        {
+                copy(i, sample_i);
+        }
+
+        ASSERT(sample_i == count);
+}
+}
