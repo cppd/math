4a6eec90ba3a8537536dc0cb65bfd5478d7e05b9
cppd <cppd@users.noreply.github.com>
2023-02-13 08:44:16 +0300

    edit DFT

--- src/gpu/dft/dft.cpp
+++ src/gpu/dft/dft.cpp
@@ -26,6 +26,7 @@ CRC Press LLC, 2000.
 #include "barriers.h"
 #include "buffer.h"
 #include "fft.h"
+#include "function.h"
 
 #include "shaders/mul.h"
 #include "shaders/mul_d.h"
@@ -39,6 +40,85 @@ namespace ns::gpu::dft
 {
 namespace
 {
+class Diagonals final
+{
+        static void run_fft(
+                const ComplexNumberBuffer& fwd,
+                const ComplexNumberBuffer& inv,
+                const int n,
+                const vulkan::Device& device,
+                const vulkan::CommandPool& compute_command_pool,
+                const vulkan::Queue& compute_queue)
+        {
+                const std::unique_ptr<Fft> fft = create_fft(device, {compute_command_pool.family_index()}, 1, n);
+
+                fft->run_for_data(false, fwd, device.handle(), compute_command_pool.handle(), compute_queue.handle());
+                fft->run_for_data(true, inv, device.handle(), compute_command_pool.handle(), compute_queue.handle());
+        }
+
+        std::optional<ComplexNumberBuffer> d1_fwd_;
+        std::optional<ComplexNumberBuffer> d1_inv_;
+        std::optional<ComplexNumberBuffer> d2_fwd_;
+        std::optional<ComplexNumberBuffer> d2_inv_;
+
+public:
+        Diagonals(
+                const int n_1,
+                const int n_2,
+                const int m_1,
+                const int m_2,
+                const vulkan::Device& device,
+                const vulkan::CommandPool& compute_command_pool,
+                const vulkan::Queue& compute_queue,
+                const vulkan::CommandPool& transfer_command_pool,
+                const vulkan::Queue& transfer_queue,
+                const std::uint32_t family_index)
+        {
+                // Compute the diagonal D in Lemma 13.2: use the radix-2 FFT
+                // 13.13, 13.26.
+
+                // Coefficients for inverse DFT, because N is not equal to M
+                const double d1_coef = static_cast<double>(m_1) / n_1;
+                const double d2_coef = static_cast<double>(m_2) / n_2;
+
+                const std::vector<std::complex<double>> d1_fwd = compute_h2(n_1, m_1, compute_h(n_1, false, 1));
+                const std::vector<std::complex<double>> d1_inv = compute_h2(n_1, m_1, compute_h(n_1, true, d1_coef));
+                const std::vector<std::complex<double>> d2_fwd = compute_h2(n_2, m_2, compute_h(n_2, false, 1));
+                const std::vector<std::complex<double>> d2_inv = compute_h2(n_2, m_2, compute_h(n_2, true, d2_coef));
+
+                const std::vector<std::uint32_t> family_indices = {
+                        family_index, compute_command_pool.family_index(), transfer_command_pool.family_index()};
+
+                d1_fwd_.emplace(device, transfer_command_pool, transfer_queue, family_indices, d1_fwd);
+                d1_inv_.emplace(device, transfer_command_pool, transfer_queue, family_indices, d1_inv);
+                d2_fwd_.emplace(device, transfer_command_pool, transfer_queue, family_indices, d2_fwd);
+                d2_inv_.emplace(device, transfer_command_pool, transfer_queue, family_indices, d2_inv);
+
+                run_fft(*d1_fwd_, *d1_inv_, m_1, device, compute_command_pool, compute_queue);
+                run_fft(*d2_fwd_, *d2_inv_, m_2, device, compute_command_pool, compute_queue);
+        }
+
+        [[nodiscard]] const vulkan::Buffer& d1_fwd() const
+        {
+                return d1_fwd_->buffer();
+        }
+
+        [[nodiscard]] const vulkan::Buffer& d1_inv() const
+        {
+                return d1_inv_->buffer();
+        }
+
+        [[nodiscard]] const vulkan::Buffer& d2_fwd() const
+        {
+                return d2_fwd_->buffer();
+        }
+
+        [[nodiscard]] const vulkan::Buffer& d2_inv() const
+        {
+                return d2_inv_->buffer();
+        }
+};
+
 class Impl final : public Dft
 {
         const std::thread::id thread_id_ = std::this_thread::get_id();
@@ -72,15 +152,10 @@ class Impl final : public Dft
         std::unique_ptr<Fft> fft_n2_m1_;
         std::unique_ptr<Fft> fft_n1_m2_;
 
-        int n1_ = -1;
-        int n2_ = -1;
-        int m1_ = -1;
-        int m2_ = -1;
+        unsigned width_;
+        unsigned height_;
 
-        std::optional<ComplexNumberBuffer> d1_fwd_;
-        std::optional<ComplexNumberBuffer> d1_inv_;
-        std::optional<ComplexNumberBuffer> d2_fwd_;
-        std::optional<ComplexNumberBuffer> d2_inv_;
+        std::optional<Diagonals> diagonals_;
         std::optional<ComplexNumberBuffer> x_d_;
         std::optional<ComplexNumberBuffer> buffer_;
 
@@ -162,105 +237,58 @@ class Impl final : public Dft
                 buffer_barrier(command_buffer, x_d_->buffer().handle());
         }
 
-        void create_diagonals(const std::uint32_t family_index)
-        {
-                // Compute the diagonal D in Lemma 13.2: use the radix-2 FFT
-                // 13.13, 13.26.
-
-                // Coefficients for inverse DFT, because N is not equal to M.
-                const double m1_div_n1 = static_cast<double>(m1_) / n1_;
-                const double m2_div_n2 = static_cast<double>(m2_) / n2_;
-
-                const std::vector<std::complex<double>> d1_fwd = compute_h2(n1_, m1_, compute_h(n1_, false, 1.0));
-                const std::vector<std::complex<double>> d1_inv = compute_h2(n1_, m1_, compute_h(n1_, true, m1_div_n1));
-                const std::vector<std::complex<double>> d2_fwd = compute_h2(n2_, m2_, compute_h(n2_, false, 1.0));
-                const std::vector<std::complex<double>> d2_inv = compute_h2(n2_, m2_, compute_h(n2_, true, m2_div_n2));
-
-                //
-
-                const std::vector<std::uint32_t> family_indices = {
-                        family_index, compute_command_pool_->family_index(), transfer_command_pool_->family_index()};
-
-                d1_fwd_.emplace(*device_, *transfer_command_pool_, *transfer_queue_, family_indices, d1_fwd);
-                d1_inv_.emplace(*device_, *transfer_command_pool_, *transfer_queue_, family_indices, d1_inv);
-                d2_fwd_.emplace(*device_, *transfer_command_pool_, *transfer_queue_, family_indices, d2_fwd);
-                d2_inv_.emplace(*device_, *transfer_command_pool_, *transfer_queue_, family_indices, d2_inv);
-
-                {
-                        const std::unique_ptr<Fft> fft =
-                                create_fft(*device_, {compute_command_pool_->family_index()}, 1, m1_);
-                        fft->run_for_data(
-                                false, *d1_fwd_, device_->handle(), compute_command_pool_->handle(),
-                                compute_queue_->handle());
-                        fft->run_for_data(
-                                true, *d1_inv_, device_->handle(), compute_command_pool_->handle(),
-                                compute_queue_->handle());
-                }
-                {
-                        const std::unique_ptr<Fft> fft =
-                                create_fft(*device_, {compute_command_pool_->family_index()}, 1, m2_);
-                        fft->run_for_data(
-                                false, *d2_fwd_, device_->handle(), compute_command_pool_->handle(),
-                                compute_queue_->handle());
-                        fft->run_for_data(
-                                true, *d2_inv_, device_->handle(), compute_command_pool_->handle(),
-                                compute_queue_->handle());
-                }
-        }
-
-        //
-
         void create_buffers(const unsigned width, const unsigned height, const std::uint32_t family_index) override
         {
                 ASSERT(thread_id_ == std::this_thread::get_id());
 
-                //
-
                 ASSERT(width > 0 && height > 0);
 
-                n1_ = width;
-                n2_ = height;
-                m1_ = compute_m(n1_);
-                m2_ = compute_m(n2_);
+                width_ = width;
+                height_ = height;
 
-                create_diagonals(family_index);
+                const int n_1 = width;
+                const int n_2 = height;
+                const int m_1 = compute_m(n_1);
+                const int m_2 = compute_m(n_2);
+
+                diagonals_.emplace(
+                        n_1, n_2, m_1, m_2, *device_, *compute_command_pool_, *compute_queue_, *transfer_command_pool_,
+                        *transfer_queue_, family_index);
 
                 const std::vector<std::uint32_t> family_indices = {family_index};
 
-                x_d_.emplace(*device_, family_indices, n1_ * n2_, buffer_memory_type_);
+                x_d_.emplace(*device_, family_indices, n_1 * n_2, buffer_memory_type_);
                 buffer_.emplace(
-                        *device_, family_indices, std::max(m1_ * n2_, m2_ * n1_),
+                        *device_, family_indices, std::max(m_1 * n_2, m_2 * n_1),
                         vulkan::BufferMemoryType::DEVICE_LOCAL);
 
                 fft_n2_m1_.reset();
-                fft_n2_m1_ = create_fft(*device_, family_indices, n2_, m1_);
+                fft_n2_m1_ = create_fft(*device_, family_indices, n_2, m_1);
                 fft_n2_m1_->set_data(*buffer_);
 
                 fft_n1_m2_.reset();
-                fft_n1_m2_ = create_fft(*device_, family_indices, n1_, m2_);
+                fft_n1_m2_ = create_fft(*device_, family_indices, n_1, m_2);
                 fft_n1_m2_->set_data(*buffer_);
 
                 mul_memory_.set(x_d_->buffer(), buffer_->buffer());
-                mul_program_.create_pipelines(n1_, n2_, m1_, m2_, group_size_[0], group_size_[1]);
-                mul_rows_to_buffer_groups_ = group_count({m1_, n2_}, group_size_);
-                mul_rows_from_buffer_groups_ = group_count({n1_, n2_}, group_size_);
-                mul_columns_to_buffer_groups_ = group_count({n1_, m2_}, group_size_);
-                mul_columns_from_buffer_groups_ = group_count({n1_, n2_}, group_size_);
+                mul_program_.create_pipelines(n_1, n_2, m_1, m_2, group_size_[0], group_size_[1]);
+                mul_rows_to_buffer_groups_ = group_count({m_1, n_2}, group_size_);
+                mul_rows_from_buffer_groups_ = group_count({n_1, n_2}, group_size_);
+                mul_columns_to_buffer_groups_ = group_count({n_1, m_2}, group_size_);
+                mul_columns_from_buffer_groups_ = group_count({n_1, n_2}, group_size_);
 
-                mul_d_d1_fwd_.set(d1_fwd_->buffer(), buffer_->buffer());
-                mul_d_d1_inv_.set(d1_inv_->buffer(), buffer_->buffer());
-                mul_d_d2_fwd_.set(d2_fwd_->buffer(), buffer_->buffer());
-                mul_d_d2_inv_.set(d2_inv_->buffer(), buffer_->buffer());
-                mul_d_program_.create_pipelines(n1_, n2_, m1_, m2_, group_size_[0], group_size_[1]);
-                mul_d_row_groups_ = group_count({m1_, n2_}, group_size_);
-                mul_d_column_groups_ = group_count({m2_, n1_}, group_size_);
+                mul_d_d1_fwd_.set(diagonals_->d1_fwd(), buffer_->buffer());
+                mul_d_d1_inv_.set(diagonals_->d1_inv(), buffer_->buffer());
+                mul_d_d2_fwd_.set(diagonals_->d2_fwd(), buffer_->buffer());
+                mul_d_d2_inv_.set(diagonals_->d2_inv(), buffer_->buffer());
+                mul_d_program_.create_pipelines(n_1, n_2, m_1, m_2, group_size_[0], group_size_[1]);
+                mul_d_row_groups_ = group_count({m_1, n_2}, group_size_);
+                mul_d_column_groups_ = group_count({m_2, n_1}, group_size_);
         }
 
         void delete_buffers() override
         {
                 ASSERT(thread_id_ == std::this_thread::get_id());
-
-                //
 
                 mul_program_.delete_pipelines();
                 mul_d_program_.delete_pipelines();
@@ -268,23 +296,16 @@ class Impl final : public Dft
                 fft_n2_m1_.reset();
                 fft_n1_m2_.reset();
 
-                d1_fwd_.reset();
-                d1_inv_.reset();
-                d2_fwd_.reset();
-                d2_inv_.reset();
+                diagonals_.reset();
                 x_d_.reset();
                 buffer_.reset();
-
-                n1_ = n2_ = m1_ = m2_ = -1;
         }
 
         void compute_commands(const VkCommandBuffer command_buffer, const bool inverse) const override
         {
                 ASSERT(std::this_thread::get_id() == thread_id_);
 
-                //
-
-                if (n1_ > 1)
+                if (width_ > 1)
                 {
                         rows_to_buffer(command_buffer, inverse);
                         fft_n2_m1_->commands(command_buffer, inverse);
@@ -292,7 +313,8 @@ class Impl final : public Dft
                         fft_n2_m1_->commands(command_buffer, !inverse);
                         rows_from_buffer(command_buffer, inverse);
                 }
-                if (n2_ > 1)
+
+                if (height_ > 1)
                 {
                         columns_to_buffer(command_buffer, inverse);
                         fft_n1_m2_->commands(command_buffer, inverse);
