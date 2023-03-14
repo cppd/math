123a373f21d600953bb5eb51c45be55f30f4fed9
cppd <cppd@users.noreply.github.com>
2023-03-14 08:57:59 +0300

    painter integrators

--- src/painter/integrators/bpt/vertex/infinite_light.h
+++ src/painter/integrators/bpt/vertex/infinite_light.h
@@ -27,67 +27,71 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace ns::painter::integrators::bpt::vertex
 {
+namespace infinite_light_implementation
+{
+template <std::size_t N, typename T, typename Color>
+[[nodiscard]] T angle_pdf_origin(
+        const Scene<N, T, Color>& scene,
+        const LightDistribution<N, T, Color>& light_distribution,
+        const Ray<N, T>& ray_to_light)
+{
+        T sum = 0;
+        T weight_sum = 0;
+
+        for (const LightSource<N, T, Color>* const light : scene.light_sources())
+        {
+                if (!light->is_infinite_area())
+                {
+                        continue;
+                }
+
+                const T pdf_dir = light->leave_pdf_dir(-ray_to_light.dir());
+                const T distribution_pdf = light_distribution.pdf(light);
+                sum += pdf_dir * distribution_pdf;
+                weight_sum += distribution_pdf;
+        }
+
+        if (weight_sum > 0)
+        {
+                return sum / weight_sum;
+        }
+        return 0;
+}
+
+template <std::size_t N, typename T, typename Color, typename Normal>
+[[nodiscard]] T area_pdf(
+        const Scene<N, T, Color>& scene,
+        const LightDistribution<N, T, Color>& light_distribution,
+        const Vector<N, T>& light_dir,
+        const Normal& next_normal)
+{
+        T sum = 0;
+        T weight_sum = 0;
+
+        for (const LightSource<N, T, Color>* const light : scene.light_sources())
+        {
+                if (!light->is_infinite_area())
+                {
+                        continue;
+                }
+
+                const T pdf = light->leave_pdf_pos(light_dir);
+                const T distribution_pdf = light_distribution.pdf(light);
+                sum += pdf * distribution_pdf;
+                weight_sum += distribution_pdf;
+        }
+
+        if (weight_sum > 0)
+        {
+                return pos_pdf_to_area_pdf(sum / weight_sum, light_dir, next_normal);
+        }
+        return 0;
+}
+}
+
 template <std::size_t N, typename T, typename Color>
 class InfiniteLight final
 {
-        [[nodiscard]] static T angle_pdf_origin(
-                const Scene<N, T, Color>& scene,
-                const LightDistribution<N, T, Color>& light_distribution,
-                const Ray<N, T>& ray_to_light)
-        {
-                T sum = 0;
-                T weight_sum = 0;
-
-                for (const LightSource<N, T, Color>* const light : scene.light_sources())
-                {
-                        if (!light->is_infinite_area())
-                        {
-                                continue;
-                        }
-
-                        const T pdf_dir = light->leave_pdf_dir(-ray_to_light.dir());
-                        const T distribution_pdf = light_distribution.pdf(light);
-                        sum += pdf_dir * distribution_pdf;
-                        weight_sum += distribution_pdf;
-                }
-
-                if (weight_sum > 0)
-                {
-                        return sum / weight_sum;
-                }
-                return 0;
-        }
-
-        template <typename Normal>
-        [[nodiscard]] static T area_pdf(
-                const Scene<N, T, Color>& scene,
-                const LightDistribution<N, T, Color>& light_distribution,
-                const Vector<N, T>& light_dir,
-                const Normal& next_normal)
-        {
-                T sum = 0;
-                T weight_sum = 0;
-
-                for (const LightSource<N, T, Color>* const light : scene.light_sources())
-                {
-                        if (!light->is_infinite_area())
-                        {
-                                continue;
-                        }
-
-                        const T pdf = light->leave_pdf_pos(light_dir);
-                        const T distribution_pdf = light_distribution.pdf(light);
-                        sum += pdf * distribution_pdf;
-                        weight_sum += distribution_pdf;
-                }
-
-                if (weight_sum > 0)
-                {
-                        return pos_pdf_to_area_pdf(sum / weight_sum, light_dir, next_normal);
-                }
-                return 0;
-        }
-
         const Scene<N, T, Color>* scene_;
         const LightDistribution<N, T, Color>* light_distribution_;
         Vector<N, T> dir_;
@@ -107,7 +111,8 @@ public:
                   dir_(-ray_to_light.dir()),
                   beta_(beta),
                   angle_pdf_forward_(angle_pdf_forward),
-                  angle_pdf_origin_(angle_pdf_origin(*scene, *light_distribution, ray_to_light))
+                  angle_pdf_origin_(
+                          infinite_light_implementation::angle_pdf_origin(*scene, *light_distribution, ray_to_light))
         {
         }
 
@@ -124,7 +129,7 @@ public:
         template <typename Normal>
         [[nodiscard]] T area_pdf(const Normal& next_normal) const
         {
-                return area_pdf(*scene_, *light_distribution_, dir_, next_normal);
+                return infinite_light_implementation::area_pdf(*scene_, *light_distribution_, dir_, next_normal);
         }
 
         [[nodiscard]] bool is_connectible() const
