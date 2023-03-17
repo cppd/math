ecde3d070c07eeb36f56a98e1413d95fdfab7148
cppd <cppd@users.noreply.github.com>
2023-03-17 09:42:46 +0300

    code points

--- src/text/code_points.cpp
+++ src/text/code_points.cpp
@@ -19,98 +19,50 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <src/com/error.h>
 
-#include <tuple>
+#include <array>
 
 namespace ns::text
 {
 namespace
 {
-std::vector<std::tuple<char32_t, char32_t>> code_point_ranges()
+constexpr std::array RANGES = std::to_array<std::array<char32_t, 2>>({
+        {   0x0,   0x7F}, // C0 Controls and Basic Latin
+        {  0x80,   0xFF}, // C1 Controls and Latin-1 Supplement
+        { 0x100,  0x17F}, // Latin Extended-A
+        { 0x180,  0x24F}, // Latin Extended-B
+        {0x1E00, 0x1EFF}, // Latin Extended Additional
+        {0x2C60, 0x2C7F}, // Latin Extended-C
+        {0xA720, 0xA7FF}, // Latin Extended-D
+        {0xAB30, 0xAB6F}, // Latin Extended-E
+        { 0x250,  0x2AF}, // IPA Extensions
+        { 0x2B0,  0x2FF}, // Spacing Modifier Letters
+        { 0x370,  0x3FF}, // Greek and Coptic
+        {0x1F00, 0x1FFF}, // Greek Extended
+        { 0x400,  0x4FF}, // Cyrillic
+        { 0x500,  0x52F}, // Cyrillic Supplement
+        {0x2DE0, 0x2DFF}, // Cyrillic Extended-A
+        {0xA640, 0xA69F}, // Cyrillic Extended-B
+        {0x1C80, 0x1C8F}, // Cyrillic Extended-C
+        {0xFFFD, 0xFFFD}  // Replacement character
+});
+
+constexpr std::size_t COUNT = []
 {
-        std::vector<std::tuple<char32_t, char32_t>> res;
-        res.reserve(19);
-
-        // C0 Controls and Basic Latin
-        res.emplace_back(0x0, 0x7F);
-
-        // C1 Controls and Latin-1 Supplement
-        res.emplace_back(0x80, 0xFF);
-
-        // Latin Extended-A
-        res.emplace_back(0x100, 0x17F);
-
-        // Latin Extended-B
-        res.emplace_back(0x180, 0x24F);
-
-        // Latin Extended Additional
-        res.emplace_back(0x1E00, 0x1EFF);
-
-        // Latin Extended-C
-        res.emplace_back(0x2C60, 0x2C7F);
-
-        // Latin Extended-D
-        res.emplace_back(0xA720, 0xA7FF);
-
-        // Latin Extended-E
-        res.emplace_back(0xAB30, 0xAB6F);
-
-        // IPA Extensions
-        res.emplace_back(0x250, 0x2AF);
-
-        // Spacing Modifier Letters
-        res.emplace_back(0x2B0, 0x2FF);
-
-#if 0
-        // Combining Diacritical Marks
-        res.emplace_back(0x300, 0x36F);
-#endif
-
-        // Greek and Coptic
-        res.emplace_back(0x370, 0x3FF);
-
-        // Greek Extended
-        res.emplace_back(0x1F00, 0x1FFF);
-
-        // Cyrillic
-        res.emplace_back(0x400, 0x4FF);
-
-        // Cyrillic Supplement
-        res.emplace_back(0x500, 0x52F);
-
-        // Cyrillic Extended-A
-        res.emplace_back(0x2DE0, 0x2DFF);
-
-        // Cyrillic Extended-B
-        res.emplace_back(0xA640, 0xA69F);
-
-        // Cyrillic Extended-C
-        res.emplace_back(0x1C80, 0x1C8F);
-
-        // Replacement character
-        res.emplace_back(0xFFFD, 0xFFFD);
-
+        std::size_t res = RANGES.size();
+        for (const auto [min, max] : RANGES)
+        {
+                ASSERT(max >= min);
+                res += max - min;
+        }
         return res;
-}
+}();
 }
 
 std::vector<char32_t> supported_code_points()
 {
-        const std::vector<std::tuple<char32_t, char32_t>>& ranges = code_point_ranges();
-
-        const std::size_t count = [&]
-        {
-                std::size_t res = ranges.size();
-                for (const auto& [min, max] : ranges)
-                {
-                        ASSERT(max >= min);
-                        res += max - min;
-                }
-                return res;
-        }();
-
         std::vector<char32_t> res;
-        res.reserve(count);
-        for (const auto& [min, max] : ranges)
+        res.reserve(COUNT);
+        for (const auto [min, max] : RANGES)
         {
                 for (char32_t i = min; i <= max; ++i)
                 {
