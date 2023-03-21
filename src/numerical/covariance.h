aac06a14b4734c6425cda5a8551428d481953ee5
cppd <cppd@users.noreply.github.com>
2023-03-21 09:40:31 +0300

    covariance matrix

---
+++ src/numerical/covariance.h
@@ -0,0 +1,95 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "matrix.h"
+#include "vector.h"
+
+#include <vector>
+
+namespace ns::numerical
+{
+namespace covariance_implementation
+{
+template <std::size_t N, typename T>
+Vector<N, T> average(const std::vector<Vector<N, T>>& data)
+{
+        Vector<N, T> res(0);
+        for (const Vector<N, T>& p : data)
+        {
+                res += p;
+        }
+        return res / static_cast<T>(data.size());
+}
+}
+
+template <std::size_t N, typename T>
+Matrix<N, N, T> covariance_matrix_simple(const std::vector<Vector<N, T>>& data)
+{
+        namespace impl = covariance_implementation;
+
+        const Vector<N, T> average = impl::average(data);
+
+        Matrix<N, N, T> res;
+
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                res.row(i) = Vector<N, T>(0);
+        }
+
+        for (const Vector<N, T>& d : data)
+        {
+                const Vector<N, T> v = d - average;
+                for (std::size_t i = 0; i < N; ++i)
+                {
+                        for (std::size_t j = i; j < N; ++j)
+                        {
+                                res(i, j) += v[i] * v[j];
+                        }
+                }
+        }
+
+        return res;
+}
+
+template <std::size_t N, typename T>
+Matrix<N, N, T> covariance_matrix_full(const std::vector<Vector<N, T>>& data)
+{
+        Matrix<N, N, T> res = covariance_matrix_simple(data);
+
+        const T size = data.size();
+
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                for (std::size_t j = i; j < N; ++j)
+                {
+                        res(i, j) /= size;
+                }
+        }
+
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                for (std::size_t j = 0; j < i; ++j)
+                {
+                        res(i, j) = res(j, i);
+                }
+        }
+
+        return res;
+}
+}
---
+++ src/numerical/test/test_covariance.cpp
@@ -0,0 +1,93 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../covariance.h"
+
+#include <src/com/error.h>
+#include <src/com/print.h>
+#include <src/test/test.h>
+
+#include <vector>
+
+namespace ns::numerical
+{
+namespace
+{
+template <typename T>
+void test_equal(const T a, const T b)
+{
+        if (!(a == b))
+        {
+                error(to_string(a) + " is not equal to " + to_string(b));
+        }
+}
+
+template <typename T>
+void test_simple()
+{
+        const std::vector<Vector<2, T>> data{
+                {5, 10},
+                {7, 11},
+                {4,  8},
+                {3,  6},
+                {6, 10}
+        };
+
+        const Matrix<2, 2, T> m = covariance_matrix_simple(data);
+
+        test_equal(m(0, 0), T{10});
+        test_equal(m(0, 1), T{12});
+        test_equal(m(1, 0), T{0});
+        test_equal(m(1, 1), T{16});
+}
+
+template <typename T>
+void test_full()
+{
+        const std::vector<Vector<2, T>> data{
+                {5, 10},
+                {7, 11},
+                {4,  8},
+                {3,  6},
+                {6, 10}
+        };
+
+        const Matrix<2, 2, T> m = covariance_matrix_full(data);
+
+        test_equal(m(0, 0), T{2.0L});
+        test_equal(m(0, 1), T{2.4L});
+        test_equal(m(1, 0), T{2.4L});
+        test_equal(m(1, 1), T{3.2L});
+}
+
+template <typename T>
+void test()
+{
+        test_simple<T>();
+        test_full<T>();
+}
+
+void test_covariance()
+{
+        test<float>();
+        test<double>();
+        test<long double>();
+}
+
+TEST_SMALL("Covariance", test_covariance)
+}
+}
