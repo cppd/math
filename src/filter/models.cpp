5624f869d2543898c4106e109bae6ca7fdd65d37
cppd <cppd@users.noreply.github.com>
2023-04-06 09:51:13 +0300

    filter models

---
+++ src/filter/models.cpp
@@ -0,0 +1,91 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+Yaakov Bar-Shalom, X.-Rong Li, Thiagalingam Kirubarajan.
+Estimation with Applications To Tracking and Navigation.
+John Wiley & Sons, 2001.
+
+6.3.2 Discrete White Noise Acceleration Model
+6.3.3 Discrete Wiener Process Acceleration Model
+*/
+
+/*
+Roger R Labbe Jr.
+Kalman and Bayesian Filters in Python.
+
+7.3.2 Piecewise White Noise Model
+*/
+
+#include "models.h"
+
+#include <src/com/exponent.h>
+
+namespace ns::filter
+{
+// For this model, standard deviation (sigma = sqrt(variance))
+// should be of the order of the maximum acceleration magnitude (a).
+// A practical range is a/2 <= sigma <= a.
+template <std::size_t N, typename T>
+        requires (N == 2)
+Matrix<N, N, T> discrete_white_noise(const T dt, const T variance)
+{
+        const T dt_2 = power<2>(dt);
+        const T dt_3 = power<3>(dt);
+        const T dt_4 = power<4>(dt);
+        const T v = variance;
+
+        // clang-format off
+        return Matrix<N, N, T>
+        {{{
+                {dt_4 / 4 * v, dt_3 / 2 * v},
+                {dt_3 / 2 * v,     dt_2 * v}
+        }}};
+        // clang-format on
+}
+
+// For this model, standard deviation (sigma = sqrt(variance))
+// should be of the order of the magnitude of the maximum
+// acceleration increment over a sampling period (d).
+// A practical range is d/2 <= sigma <= d.
+template <std::size_t N, typename T>
+        requires (N == 3)
+Matrix<N, N, T> discrete_white_noise(const T dt, const T variance)
+{
+        const T dt_2 = power<2>(dt);
+        const T dt_3 = power<3>(dt);
+        const T dt_4 = power<4>(dt);
+        const T v = variance;
+
+        // clang-format off
+        return Matrix<N, N, T>
+        {{{
+                {dt_4 / 4 * v, dt_3 / 2 * v, dt_2 / 2 * v},
+                {dt_3 / 2 * v,     dt_2 * v,       dt * v},
+                {dt_2 / 2 * v,       dt * v,        1 * v}
+        }}};
+        // clang-format on
+}
+
+#define INSTANTIATION(T)                                     \
+        template Matrix<2, 2, T> discrete_white_noise(T, T); \
+        template Matrix<3, 3, T> discrete_white_noise(T, T);
+
+INSTANTIATION(float)
+INSTANTIATION(double)
+INSTANTIATION(long double)
+}
