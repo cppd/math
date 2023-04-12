737071dc40f9fd6825eceb1717958d0651a41213
cppd <cppd@users.noreply.github.com>
2023-04-12 09:56:06 +0300

    edit filter tests

--- src/filter/test/test_filter_1d.cpp
+++ src/filter/test/test_filter_1d.cpp
@@ -23,9 +23,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/com/file/path.h>
 #include <src/com/print.h>
 #include <src/com/random/pcg.h>
+#include <src/com/type/name.h>
 #include <src/settings/directory.h>
 #include <src/test/test.h>
 
+#include <cctype>
 #include <cmath>
 #include <fstream>
 #include <random>
@@ -35,22 +37,38 @@ namespace ns::filter
 {
 namespace
 {
+std::string replace_space(const std::string_view s)
+{
+        std::string res;
+        res.reserve(s.size());
+        for (const char c : s)
+        {
+                res += !std::isspace(static_cast<unsigned char>(c)) ? c : '_';
+        }
+        return res;
+}
+
 std::filesystem::path file_path(const std::string_view name)
 {
         return settings::test_directory() / path_from_utf8(name);
 }
 
 template <typename T>
-[[nodiscard]] bool equal(const T a, const T b, const T precision)
+void compare(const T a, const T b, const T precision)
 {
         static_assert(std::is_floating_point_v<T>);
 
         if (a == b)
         {
-                return true;
+                return;
         }
-        const T rel = std::abs(a - b) / std::max(std::abs(a), std::abs(b));
-        return (rel < precision);
+
+        const T abs = std::abs(a - b);
+        if (!(abs < precision))
+        {
+                error(to_string(a) + " is not equal to " + to_string(b) + "; absolute " + to_string(abs)
+                      + "; required precision " + to_string(precision));
+        }
 }
 
 template <typename T>
@@ -117,10 +135,9 @@ void write_to_file(
         }
 }
 
-void test()
+template <typename T>
+void test_impl(const T precision)
 {
-        using T = double;
-
         constexpr std::size_t N = 2;
         constexpr std::size_t M = 1;
 
@@ -128,8 +145,6 @@ void test()
         constexpr T VELOCITY_MEAN = 1;
         constexpr T VELOCITY_VARIANCE = power<2>(0.1);
         constexpr T MEASUREMENT_VARIANCE = power<2>(3);
-
-        constexpr auto ENGINE_INIT = 11111;
 
         constexpr Vector<N, T> X(10, 5);
         constexpr Matrix<N, N, T> P{
@@ -148,8 +163,8 @@ void test()
 
         constexpr std::size_t COUNT = 50;
 
-        const std::vector<ProcessData<T>> process_data = generate_random_data<double>(
-                COUNT, DT, VELOCITY_MEAN, VELOCITY_VARIANCE, MEASUREMENT_VARIANCE, PCG(ENGINE_INIT));
+        const std::vector<ProcessData<T>> process_data =
+                generate_random_data<T>(COUNT, DT, VELOCITY_MEAN, VELOCITY_VARIANCE, MEASUREMENT_VARIANCE, PCG());
 
         Filter<N, M, T> filter;
         filter.set_x(X);
@@ -170,15 +185,19 @@ void test()
                 result_data.push_back({.filter = f_x, .standard_deviation = f_stddev});
         }
 
-        write_to_file("filter.txt", process_data, result_data);
+        write_to_file("filter_1d_" + replace_space(type_name<T>()) + ".txt", process_data, result_data);
 
-        if (!equal(result_data.back().filter, 48.481651625145673, 0.0)
-            || !equal(result_data.back().standard_deviation, 1.4306605516486153, 0.0))
-        {
-                error("Filter test failed");
-        }
+        compare(result_data.back().standard_deviation, T{1.43066055164861537772L}, precision);
+        compare(process_data.back().x, result_data.back().filter, 4 * result_data.back().standard_deviation);
 }
 
-TEST_SMALL("Filter", test)
+void test()
+{
+        test_impl<float>(2e-7);
+        test_impl<double>(0);
+        test_impl<long double>(0);
+}
+
+TEST_SMALL("Filter 1D", test)
 }
 }
