71c2052edb7e4300a197d5b4fdb8d9ddb387be70
cppd <cppd@users.noreply.github.com>
2023-04-30 12:03:56 +0300

    test filter

--- src/filter/test/test_filter_1d.cpp
+++ src/filter/test/test_filter_1d.cpp
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../filter.h"
 #include "../models.h"
+#include "../nees.h"
 
 #include <src/com/error.h>
 #include <src/com/exponent.h>
@@ -175,55 +176,123 @@ void check_distribution(
 }
 
 template <typename T>
+class TestFilter
+{
+        const Matrix<2, 2, T> f_;
+        const Matrix<2, 2, T> f_t_ = f_.transposed();
+        const Matrix<2, 2, T> q_;
+
+        const Matrix<1, 2, T> h_{
+                {1, 0}
+        };
+        const Matrix<2, 1, T> h_t_ = h_.transposed();
+        const Matrix<1, 1, T> r_;
+
+        Filter<2, T> filter_;
+
+public:
+        TestFilter(
+                const std::type_identity_t<T> dt,
+                const std::type_identity_t<T> process_variance,
+                const std::type_identity_t<T> measurement_variance,
+                const Vector<2, T>& x,
+                const Matrix<2, 2, T> p)
+                : f_{{1, dt}, {0, 1}},
+                  q_(discrete_white_noise<2, T>(dt, process_variance)),
+                  r_({{measurement_variance}}),
+                  filter_(x, p)
+        {
+        }
+
+        void process(const T measurement)
+        {
+                filter_.predict(f_, f_t_, q_);
+                filter_.update(h_, h_t_, r_, Vector<1, T>(measurement));
+        }
+
+        void process_ext(const T measurement)
+        {
+                // measurement = x[0]
+                // Jacobian matrix
+                //  1 0
+                const auto h = [](const Vector<2, T>& x)
+                {
+                        return Vector<1, T>(x[0]);
+                };
+                const auto h_jacobian = [](const Vector<2, T>& /*x*/)
+                {
+                        return Matrix<1, 2, T>{
+                                {1, 0}
+                        };
+                };
+                filter_.predict(f_, f_t_, q_);
+                filter_.update(h, h_jacobian, r_, Vector<1, T>(measurement));
+        }
+
+        [[nodiscard]] T x() const
+        {
+                return filter_.x()[0];
+        }
+
+        [[nodiscard]] T variance() const
+        {
+                return filter_.p()(0, 0);
+        }
+};
+
+template <typename T, bool EXT>
 void test_impl()
 {
-        constexpr std::size_t N = 2;
-        constexpr std::size_t M = 1;
-
         constexpr T DT = 1;
         constexpr T VELOCITY_MEAN = 1;
         constexpr T VELOCITY_VARIANCE = power<2>(0.1);
         constexpr T MEASUREMENT_VARIANCE = power<2>(3);
-
-        constexpr Vector<N, T> X(10, 5);
-        constexpr Matrix<N, N, T> P{
-                {500,  0},
-                {  0, 50}
-        };
-        constexpr Matrix<N, N, T> F{
-                {1, DT},
-                {0,  1}
-        };
-        constexpr Matrix<N, N, T> F_T = F.transposed();
-        constexpr Matrix<N, N, T> Q{discrete_white_noise<N, T>(DT, VELOCITY_VARIANCE)};
-
-        constexpr Matrix<M, N, T> H{
-                {1, 0}
-        };
-        constexpr Matrix<N, M, T> H_T = H.transposed();
-        constexpr Matrix<M, M, T> R{{MEASUREMENT_VARIANCE}};
 
         constexpr std::size_t COUNT = 1000;
 
         const std::vector<ProcessData<T>> process_data =
                 generate_random_data<T>(COUNT, DT, VELOCITY_MEAN, VELOCITY_VARIANCE, MEASUREMENT_VARIANCE, PCG());
 
-        Filter<N, T> filter(X, P);
+        constexpr Vector<2, T> X(10, 5);
+        constexpr Matrix<2, 2, T> P{
+                {500,  0},
+                {  0, 50}
+        };
+
+        TestFilter<T> filter(DT, VELOCITY_VARIANCE, MEASUREMENT_VARIANCE, X, P);
 
         std::unordered_map<int, unsigned> distribution;
+
+        std::vector<Vector<1, T>> values;
+        values.reserve(process_data.size());
+        std::vector<Vector<1, T>> estimates;
+        estimates.reserve(process_data.size());
+        std::vector<Matrix<1, 1, T>> covariances;
+        covariances.reserve(process_data.size());
 
         std::vector<ResultData<T>> result_data;
         result_data.reserve(process_data.size());
         for (const ProcessData<T>& process : process_data)
         {
-                filter.predict(F, F_T, Q);
-                filter.update(H, H_T, R, Vector<M, T>(process.z));
+                if constexpr (EXT)
+                {
+                        filter.process_ext(process.z);
+                }
+                else
+                {
+                        filter.process(process.z);
+                }
 
-                const T f_x = filter.x()[0];
-                const T f_stddev = std::sqrt(filter.p()(0, 0));
+                const T x = filter.x();
+                const T variance = filter.variance();
+                const T stddev = std::sqrt(variance);
 
-                result_data.push_back({.x = f_x, .standard_deviation = f_stddev});
-                ++distribution[static_cast<int>((f_x - process.x) / f_stddev)];
+                result_data.push_back({.x = x, .standard_deviation = stddev});
+                ++distribution[static_cast<int>((x - process.x) / stddev)];
+
+                values.emplace_back(process.x);
+                estimates.emplace_back(x);
+                covariances.push_back(Matrix<1, 1, T>{{variance}});
         }
 
         write_to_file("filter_1d_" + replace_space(type_name<T>()) + ".txt", process_data, result_data);
@@ -231,8 +300,22 @@ void test_impl()
         compare(result_data.back().standard_deviation, T{1.4306576889002234962L}, T{0});
         compare(process_data.back().x, result_data.back().x, 5 * result_data.back().standard_deviation);
 
+        const T nees = nees_average(values, estimates, covariances);
+        if (!(nees < T{1.2}))
+        {
+                error(std::string("NEES average <") + type_name<T>() + "> = " + to_string(nees)
+                      + "; 1 degree of freedom; failed");
+        }
+
         constexpr std::array EXPECTED_DISTRIBUTION = std::to_array<unsigned>({610, 230, 60, 15, 7, 2, 0, 0, 0, 0});
         check_distribution(distribution, EXPECTED_DISTRIBUTION);
+}
+
+template <typename T>
+void test_impl()
+{
+        test_impl<T, false>();
+        test_impl<T, true>();
 }
 
 void test()
