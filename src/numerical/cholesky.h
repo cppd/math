e31e72223d25138eeb430ab7d6b9282a201702d2
cppd <cppd@users.noreply.github.com>
2023-05-11 09:55:39 +0300

    Cholesky decomposition

---
+++ src/numerical/cholesky.h
@@ -0,0 +1,92 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+Steven C. Chapra, Raymond P. Canale.
+Numerical Methods for Engineers. Seventh edition.
+McGraw-Hill Education, 2015.
+
+11.1.2 Cholesky Decomposition
+*/
+
+#pragma once
+
+#include "matrix.h"
+
+#include <src/com/print.h>
+#include <src/com/type/limit.h>
+
+#include <algorithm>
+#include <cmath>
+#include <exception>
+#include <string>
+
+namespace ns::numerical
+{
+class CholeskyException final : public std::exception
+{
+        std::string msg_;
+
+public:
+        explicit CholeskyException(const std::string& msg)
+                : msg_("The Cholesky decomposition: matrix is not positive definite, " + msg)
+        {
+        }
+
+        [[nodiscard]] const char* what() const noexcept override
+        {
+                return msg_.c_str();
+        }
+};
+
+template <std::size_t N, typename T>
+[[nodiscard]] Matrix<N, N, T> cholesky_decomposition_lower_triangular(const Matrix<N, N, T>& a)
+{
+        Matrix<N, N, T> l(0);
+
+        for (std::size_t k = 0; k < N; ++k)
+        {
+                for (std::size_t i = 0; i < k; ++i)
+                {
+                        T sum = 0;
+                        for (std::size_t j = 0; j < i; ++j)
+                        {
+                                sum += l(i, j) * l(k, j);
+                        }
+                        l(k, i) = (a(k, i) - sum) / l(i, i);
+                }
+
+                T sum = 0;
+                for (std::size_t j = 0; j < k; ++j)
+                {
+                        const T v = l(k, j);
+                        sum += v * v;
+                }
+
+                const T v = a(k, k) - sum;
+
+                if (!(v > -10 * Limits<T>::epsilon()))
+                {
+                        throw CholeskyException("sqrt(" + to_string(v) + ")");
+                }
+
+                l(k, k) = std::sqrt(std::max(T{0}, v));
+        }
+
+        return l;
+}
+}
---
+++ src/numerical/test/test_cholesky.cpp
@@ -0,0 +1,157 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../cholesky.h"
+
+#include <src/com/error.h>
+#include <src/com/log.h>
+#include <src/com/random/pcg.h>
+#include <src/test/test.h>
+
+#include <random>
+#include <utility>
+
+namespace ns::numerical
+{
+namespace
+{
+template <typename T>
+[[nodiscard]] bool equal(const T a, const T b, const T precision)
+{
+        static_assert(std::is_floating_point_v<T>);
+
+        if (a == b)
+        {
+                return true;
+        }
+        const T abs = std::abs(a - b);
+        if (abs < precision)
+        {
+                return true;
+        }
+        const T rel = abs / std::max(std::abs(a), std::abs(b));
+        return (rel < precision);
+}
+
+template <std::size_t N, typename T>
+[[nodiscard]] bool equal(const Matrix<N, N, T>& a, const Matrix<N, N, T>& b, const T precision)
+{
+        for (std::size_t r = 0; r < N; ++r)
+        {
+                for (std::size_t c = 0; c < N; ++c)
+                {
+                        if (!equal(a(r, c), b(r, c), precision))
+                        {
+                                return false;
+                        }
+                }
+        }
+        return true;
+}
+
+template <std::size_t N, typename T>
+[[nodiscard]] bool lower_triangular(const Matrix<N, N, T>& a)
+{
+        for (std::size_t r = 0; r < N; ++r)
+        {
+                for (std::size_t c = r + 1; c < N; ++c)
+                {
+                        if (!(a(r, c) == 0))
+                        {
+                                return false;
+                        }
+                }
+        }
+        return true;
+}
+
+template <std::size_t N, typename T>
+void test(const Matrix<N, N, T>& matrix, const std::type_identity_t<T> precision)
+{
+        const Matrix<N, N, T> decomposition = cholesky_decomposition_lower_triangular(matrix);
+
+        if (!lower_triangular(decomposition))
+        {
+                error("Decomposition is not lower triangular\n" + to_string(decomposition));
+        }
+
+        const Matrix<N, N, T> check = decomposition * decomposition.transposed();
+
+        if (!equal(check, matrix, precision))
+        {
+                error("Failed to decompose\nmatrix\n" + to_string(check) + "\nis not equal to the original matrix\n"
+                      + to_string(matrix));
+        }
+}
+
+template <std::size_t N, typename T>
+[[nodiscard]] Matrix<N, N, T> random_matrix(PCG& pcg)
+{
+        std::uniform_real_distribution<T> urd(-10, 10);
+        Matrix<N, N, T> res;
+        for (std::size_t r = 0; r < N; ++r)
+        {
+                for (std::size_t c = 0; c < N; ++c)
+                {
+                        res(r, c) = urd(pcg);
+                }
+        }
+        return res;
+}
+
+template <std::size_t N, typename T>
+[[nodiscard]] Matrix<N, N, T> positive_diagonal_matrix(PCG& pcg)
+{
+        std::uniform_real_distribution<T> urd(1, 10);
+        Matrix<N, N, T> res(0);
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                res(i, i) = urd(pcg);
+        }
+        return res;
+}
+
+template <std::size_t N, typename T>
+[[nodiscard]] Matrix<N, N, T> positive_definite_matrix(PCG& pcg)
+{
+        const Matrix<N, N, T> r = random_matrix<N, T>(pcg);
+        const Matrix<N, N, T> d = positive_diagonal_matrix<N, T>(pcg);
+        return r * d * r.transposed();
+}
+
+template <typename T>
+void test(const std::type_identity_t<T> precision, PCG& pcg)
+{
+        [&]<unsigned... I>(std::integer_sequence<unsigned, I...> &&)
+        {
+                (test(positive_definite_matrix<1 + I, T>(pcg), precision), ...);
+        }
+        (std::make_integer_sequence<unsigned, 10>());
+}
+
+void test_cholesky()
+{
+        LOG("Test the Cholesky decomposition");
+        PCG pcg;
+        test<double>(1e-12, pcg);
+        test<long double>(1e-15, pcg);
+        LOG("Test the Cholesky decomposition passed");
+}
+
+TEST_SMALL("Cholesky Decomposition", test_cholesky)
+}
+}
