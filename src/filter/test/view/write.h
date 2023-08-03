52e519d4b74c3662a3fb3d8580bb46c441231ce0
cppd <cppd@users.noreply.github.com>
2023-08-03 08:47:45 +0300

    filter view

--- src/filter/test/view/write.h
+++ src/filter/test/view/write.h
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "../simulator.h"
+#include "../measurement.h"
 
 #include <src/color/rgb8.h>
 #include <src/numerical/vector.h>
