c1f9e865b08d27ccd20fe56ac0001230a620229a
cppd <cppd@users.noreply.github.com>
2023-08-11 09:49:48 +0300

    position variance

---
+++ src/filter/test/position_variance.cpp
@@ -0,0 +1,315 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "position_variance.h"
+
+#include <src/com/error.h>
+#include <src/com/log.h>
+#include <src/com/type/name.h>
+
+namespace ns::filter::test
+{
+namespace
+{
+template <std::size_t N, typename T>
+constexpr Vector<N, T> VARIANCE_DEFAULT{square(T{1})};
+}
+
+template <std::size_t N, typename T>
+PositionVariance<N, T>::PositionVariance(
+        std::string name,
+        const color::RGB8 color,
+        const T reset_dt,
+        const T linear_dt,
+        std::unique_ptr<PositionFilter<N, T>>&& filter)
+        : name_(std::move(name)),
+          color_(color),
+          reset_dt_(reset_dt),
+          linear_dt_(linear_dt),
+          filter_(std::move(filter))
+{
+        ASSERT(filter_);
+}
+
+template <std::size_t N, typename T>
+void PositionVariance<N, T>::save_results(const T time)
+{
+        positions_.push_back({.time = time, .point = filter_->position()});
+        positions_p_.push_back({.time = time, .point = filter_->position_p().diagonal()});
+        speeds_.push_back({.time = time, .point = Vector<1, T>(filter_->speed())});
+        speeds_p_.push_back({.time = time, .point = Vector<1, T>(filter_->speed_p())});
+}
+
+template <std::size_t N, typename T>
+void PositionVariance<N, T>::add_nees_checks(const TrueData<N, T>& true_data)
+{
+        nees_position_.add(true_data.position - filter_->position(), filter_->position_p());
+        if (const T speed_p = filter_->speed_p(); is_finite(speed_p))
+        {
+                nees_speed_.add(true_data.speed - filter_->speed(), speed_p);
+        }
+}
+
+template <std::size_t N, typename T>
+void PositionVariance<N, T>::check_time(const T time) const
+{
+        if (last_predict_time_ && !(*last_predict_time_ < time))
+        {
+                error("Measurement time does not increase; from " + to_string(*last_predict_time_) + " to "
+                      + to_string(time));
+        }
+
+        if (last_update_time_ && !(*last_update_time_ < time))
+        {
+                error("Measurement time does not increase; from " + to_string(*last_update_time_) + " to "
+                      + to_string(time));
+        }
+}
+
+template <std::size_t N, typename T>
+bool PositionVariance<N, T>::prepare_position_variance(const Measurements<N, T>& m)
+{
+        ASSERT(m.position);
+        ASSERT(last_predict_time_);
+        ASSERT(last_update_time_);
+
+        const T dt = m.time - *last_update_time_;
+        if (!(dt <= linear_dt_))
+        {
+                error("Variance computations require dt " + to_string(dt) + " to be less than or equal to "
+                      + to_string(linear_dt_));
+        }
+
+        if (last_position_variance_)
+        {
+                return true;
+        }
+
+        ASSERT(!position_variance_.has_variance());
+
+        filter_->predict(m.time - *last_predict_time_);
+        const auto update = filter_->update(m.position->value, VARIANCE_DEFAULT<N, T>, /*use_gate=*/false);
+        ASSERT(update);
+        last_predict_time_ = m.time;
+        last_update_time_ = m.time;
+
+        position_variance_.push(update->residual);
+        LOG(to_string(m.time) + "; " + name_ + "; Residual = " + to_string(update->residual));
+        const auto new_variance = position_variance_.compute();
+        if (!new_variance)
+        {
+                return false;
+        }
+
+        filter_->reset(m.position->value, *new_variance);
+        last_position_variance_ = *new_variance;
+        const auto standard_deviation = position_variance_.standard_deviation();
+        ASSERT(standard_deviation);
+        LOG(to_string(m.time) + "; " + name_ + "; Initial Standard Deviation = " + to_string(*standard_deviation));
+
+        return false;
+}
+
+template <std::size_t N, typename T>
+void PositionVariance<N, T>::update_position_variance(
+        const Measurements<N, T>& m,
+        const PositionFilterUpdate<N, T>& update)
+{
+        position_variance_.push(update.residual);
+
+        const auto standard_deviation = position_variance_.standard_deviation();
+        ASSERT(standard_deviation);
+        LOG(to_string(m.time) + "; " + name_ + "; Standard Deviation = " + to_string(*standard_deviation));
+
+        const auto new_variance = position_variance_.compute();
+        ASSERT(new_variance);
+        last_position_variance_ = *new_variance;
+}
+
+template <std::size_t N, typename T>
+void PositionVariance<N, T>::update_position(const Measurements<N, T>& m)
+{
+        check_time(m.time);
+
+        if (!m.position)
+        {
+                return;
+        }
+
+        if (!last_predict_time_ || !last_update_time_ || !(m.time - *last_update_time_ < reset_dt_))
+        {
+                filter_->reset(m.position->value, VARIANCE_DEFAULT<N, T>);
+                last_predict_time_ = m.time;
+                last_update_time_ = m.time;
+                save_results(m.time);
+                add_nees_checks(m.true_data);
+                return;
+        }
+
+        if (!prepare_position_variance(m))
+        {
+                return;
+        }
+
+        ASSERT(last_position_variance_);
+
+        filter_->predict(m.time - *last_predict_time_);
+        last_predict_time_ = m.time;
+
+        const auto update = filter_->update(m.position->value, *last_position_variance_, /*use_gate=*/true);
+        if (!update)
+        {
+                save_results(m.time);
+                add_nees_checks(m.true_data);
+                return;
+        }
+        const T update_dt = m.time - *last_update_time_;
+        last_update_time_ = m.time;
+
+        update_position_variance(m, *update);
+
+        save_results(m.time);
+        add_nees_checks(m.true_data);
+        if (update_dt <= linear_dt_)
+        {
+                nis_.add(update->residual, update->r);
+        }
+}
+
+template <std::size_t N, typename T>
+const std::string& PositionVariance<N, T>::name() const
+{
+        return name_;
+}
+
+template <std::size_t N, typename T>
+color::RGB8 PositionVariance<N, T>::color() const
+{
+        return color_;
+}
+
+template <std::size_t N, typename T>
+const std::optional<Vector<N, T>>& PositionVariance<N, T>::last_position_variance() const
+{
+        return last_position_variance_;
+}
+
+template <std::size_t N, typename T>
+[[nodiscard]] Vector<N, T> PositionVariance<N, T>::velocity() const
+{
+        return filter_->velocity();
+}
+
+template <std::size_t N, typename T>
+[[nodiscard]] Matrix<N, N, T> PositionVariance<N, T>::velocity_p() const
+{
+        return filter_->velocity_p();
+}
+
+template <std::size_t N, typename T>
+Vector<3 * N, T> PositionVariance<N, T>::position_velocity_acceleration() const
+{
+        return filter_->position_velocity_acceleration();
+}
+
+template <std::size_t N, typename T>
+Matrix<3 * N, 3 * N, T> PositionVariance<N, T>::position_velocity_acceleration_p() const
+{
+        return filter_->position_velocity_acceleration_p();
+}
+
+template <std::size_t N, typename T>
+std::string PositionVariance<N, T>::consistency_string() const
+{
+        const std::string name = std::string("Position<") + type_name<T>() + "> " + name_;
+
+        std::string s;
+
+        const auto new_line = [&]()
+        {
+                if (!s.empty())
+                {
+                        s += '\n';
+                }
+                s += name;
+        };
+
+        if (!nees_position_.empty())
+        {
+                new_line();
+                s += "; NEES Position; " + nees_position_.check_string();
+        }
+
+        if (!nees_speed_.empty())
+        {
+                new_line();
+                s += "; NEES Speed; " + nees_speed_.check_string();
+        }
+
+        if (!nis_.empty())
+        {
+                new_line();
+                s += "; NIS Position; " + nis_.check_string();
+        }
+
+        if (const auto& mean = position_variance_.mean())
+        {
+                new_line();
+                s += "; Mean " + to_string(*mean);
+        }
+
+        if (const auto& standard_deviation = position_variance_.standard_deviation())
+        {
+                new_line();
+                s += "; Standard Deviation " + to_string(*standard_deviation);
+        }
+
+        return s;
+}
+
+template <std::size_t N, typename T>
+const std::vector<Point<N, T>>& PositionVariance<N, T>::positions() const
+{
+        return positions_;
+}
+
+template <std::size_t N, typename T>
+const std::vector<Point<N, T>>& PositionVariance<N, T>::positions_p() const
+{
+        return positions_p_;
+}
+
+template <std::size_t N, typename T>
+const std::vector<Point<1, T>>& PositionVariance<N, T>::speeds() const
+{
+        return speeds_;
+}
+
+template <std::size_t N, typename T>
+const std::vector<Point<1, T>>& PositionVariance<N, T>::speeds_p() const
+{
+        return speeds_p_;
+}
+
+#define TEMPLATE_N_T(N, T) template class PositionVariance<(N), T>;
+
+#define TEMPLATE_T(T) TEMPLATE_N_T(1, T) TEMPLATE_N_T(2, T) TEMPLATE_N_T(3, T)
+
+TEMPLATE_T(float)
+TEMPLATE_T(double)
+TEMPLATE_T(long double)
+}
---
+++ src/filter/test/position_variance.h
@@ -0,0 +1,94 @@
+/*
+Copyright (C) 2017-2023 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "measurement.h"
+#include "point.h"
+#include "position_filter.h"
+#include "variance.h"
+
+#include "../consistency.h"
+
+#include <src/color/rgb8.h>
+#include <src/numerical/vector.h>
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace ns::filter::test
+{
+template <std::size_t N, typename T>
+class PositionVariance final
+{
+        std::string name_;
+        color::RGB8 color_;
+        T reset_dt_;
+        T linear_dt_;
+        std::unique_ptr<PositionFilter<N, T>> filter_;
+
+        std::vector<Point<N, T>> positions_;
+        std::vector<Point<N, T>> positions_p_;
+        std::vector<Point<1, T>> speeds_;
+        std::vector<Point<1, T>> speeds_p_;
+
+        NormalizedSquared<N, T> nees_position_;
+        NormalizedSquared<1, T> nees_speed_;
+        NormalizedSquared<N, T> nis_;
+
+        MovingVariance<N, T> position_variance_;
+        std::optional<Vector<N, T>> last_position_variance_;
+
+        std::optional<T> last_predict_time_;
+        std::optional<T> last_update_time_;
+
+        void save_results(T time);
+        void add_nees_checks(const TrueData<N, T>& true_data);
+
+        void check_time(T time) const;
+
+        [[nodiscard]] bool prepare_position_variance(const Measurements<N, T>& m);
+        void update_position_variance(const Measurements<N, T>& m, const PositionFilterUpdate<N, T>& update);
+
+public:
+        PositionVariance(
+                std::string name,
+                color::RGB8 color,
+                T reset_dt,
+                T linear_dt,
+                std::unique_ptr<PositionFilter<N, T>>&& filter);
+
+        void update_position(const Measurements<N, T>& m);
+
+        [[nodiscard]] const std::string& name() const;
+        [[nodiscard]] color::RGB8 color() const;
+
+        [[nodiscard]] const std::optional<Vector<N, T>>& last_position_variance() const;
+        [[nodiscard]] Vector<N, T> velocity() const;
+        [[nodiscard]] Matrix<N, N, T> velocity_p() const;
+        [[nodiscard]] Vector<3 * N, T> position_velocity_acceleration() const;
+        [[nodiscard]] Matrix<3 * N, 3 * N, T> position_velocity_acceleration_p() const;
+
+        [[nodiscard]] std::string consistency_string() const;
+        [[nodiscard]] const std::vector<Point<N, T>>& positions() const;
+        [[nodiscard]] const std::vector<Point<N, T>>& positions_p() const;
+        [[nodiscard]] const std::vector<Point<1, T>>& speeds() const;
+        [[nodiscard]] const std::vector<Point<1, T>>& speeds_p() const;
+};
+}
