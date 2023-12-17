cf3ab1cc8862d95175a6bb7a795c403c7de278e4
cppd <cppd@users.noreply.github.com>
2023-12-17 08:47:50 +0300

    refactoring

--- src/gpu/render_buffers.h
+++ src/gpu/render_buffers.h
@@ -18,7 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include <src/numerical/region.h>
-#include <src/numerical/vector.h>
 #include <src/vulkan/objects.h>
 
 #include <vector>
