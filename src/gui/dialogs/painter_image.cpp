2e1561bcbf617692be594bced944333d3cd50923
cppd <cppd@users.noreply.github.com>
2024-01-01 11:32:39 +0300

    update copyright years

--- src/gui/dialogs/painter_image.cpp
+++ src/gui/dialogs/painter_image.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2017-2023 Topological Manifold
+Copyright (C) 2017-2024 Topological Manifold
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
