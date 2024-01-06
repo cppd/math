52931ce8dce58dc3870bee28ee3ec821155b0b13
cppd <cppd@users.noreply.github.com>
2024-01-06 06:32:18 +0300

    text

--- src/text/test/test_unicode.cpp
+++ src/text/test/test_unicode.cpp
@@ -15,12 +15,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../unicode.h"
-
 #include <src/com/error.h>
 #include <src/com/log.h>
 #include <src/com/print.h>
 #include <src/test/test.h>
+#include <src/text/unicode.h>
 
 #include <cstddef>
 #include <string>
