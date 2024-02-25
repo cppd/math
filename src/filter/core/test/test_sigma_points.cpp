3c2c6042cd5b973ecacf3b8fb136b90dfe42beb0
cppd <cppd@users.noreply.github.com>
2024-02-25 11:13:19 +0300

    sigma points

--- src/filter/core/test/test_sigma_points.cpp
+++ src/filter/core/test/test_sigma_points.cpp
@@ -65,7 +65,7 @@ template <std::size_t N, typename T>
 template <typename T>
 void test_impl(const T precision)
 {
-        const auto cmp = [&](const auto& a, const auto& b)
+        const auto cmp = [&]<std::size_t N>(const numerical::Vector<N, T>& a, const numerical::Vector<N, T>& b)
         {
                 if (!(equal(a, b, precision)))
                 {
@@ -78,14 +78,12 @@ void test_impl(const T precision)
         const SigmaPoints<2, T> sigma_points = create_sigma_points<2, T>(SIGMA_POINTS_ALPHA);
 
         cmp(sigma_points.wm(),
-            numerical::Vector<5, T>(
-                    -65.6666666666666666644L, 16.6666666666666666661L, 16.6666666666666666661L, 16.6666666666666666661L,
-                    16.6666666666666666661L));
+            {-65.6666666666666666644L, 16.6666666666666666661L, 16.6666666666666666661L, 16.6666666666666666661L,
+             16.6666666666666666661L});
 
         cmp(sigma_points.wc(),
-            numerical::Vector<5, T>(
-                    -62.6766666666666666663L, 16.6666666666666666661L, 16.6666666666666666661L, 16.6666666666666666661L,
-                    16.6666666666666666661L));
+            {-62.6766666666666666663L, 16.6666666666666666661L, 16.6666666666666666661L, 16.6666666666666666661L,
+             16.6666666666666666661L});
 
         {
                 const numerical::Vector<2, T> x(-1, 2);
@@ -94,11 +92,11 @@ void test_impl(const T precision)
                         {0.1L,    1}
                 };
                 const std::array<numerical::Vector<2, T>, 5> s = sigma_points.points(x, p, std::plus(), std::minus());
-                cmp(s[0], numerical::Vector<2, T>(-1, 2));
-                cmp(s[1], numerical::Vector<2, T>(-0.826794919243112270664L, 2.01732050807568877304L));
-                cmp(s[2], numerical::Vector<2, T>(-1, 2.17233687939614085981L));
-                cmp(s[3], numerical::Vector<2, T>(-1.17320508075688772934L, 1.98267949192431122707L));
-                cmp(s[4], numerical::Vector<2, T>(-1, 1.82766312060385914019L));
+                cmp(s[0], {-1, 2});
+                cmp(s[1], {-0.826794919243112270664L, 2.01732050807568877304L});
+                cmp(s[2], {-1, 2.17233687939614085981L});
+                cmp(s[3], {-1.17320508075688772934L, 1.98267949192431122707L});
+                cmp(s[4], {-1, 1.82766312060385914019L});
         }
 
         {
@@ -108,11 +106,11 @@ void test_impl(const T precision)
                         {-0.2L,     1}
                 };
                 const std::array<numerical::Vector<2, T>, 5> s = sigma_points.points(x, p, std::plus(), std::minus());
-                cmp(s[0], numerical::Vector<2, T>(1.1L, -2.2L));
-                cmp(s[1], numerical::Vector<2, T>(1.27320508075688772936L, -2.23464101615137754591L));
-                cmp(s[2], numerical::Vector<2, T>(1.1L, -2.03029437251522859413L));
-                cmp(s[3], numerical::Vector<2, T>(0.926794919243112270686L, -2.16535898384862245418L));
-                cmp(s[4], numerical::Vector<2, T>(1.1L, -2.36970562748477140596L));
+                cmp(s[0], {1.1L, -2.2L});
+                cmp(s[1], {1.27320508075688772936L, -2.23464101615137754591L});
+                cmp(s[2], {1.1L, -2.03029437251522859413L});
+                cmp(s[3], {0.926794919243112270686L, -2.16535898384862245418L});
+                cmp(s[4], {1.1L, -2.36970562748477140596L});
         }
 }
 
