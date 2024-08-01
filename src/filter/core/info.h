62e0acbec90f70cb92e95863df72cf468220eec5
cppd <cppd@users.noreply.github.com>
2024-08-01 09:31:31 +0300

    filter tests

---
+++ src/filter/core/info.h
@@ -0,0 +1,114 @@
+/*
+Copyright (C) 2017-2024 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <src/numerical/matrix.h>
+#include <src/numerical/vector.h>
+
+#include <cstddef>
+#include <optional>
+
+namespace ns::filter::core
+{
+template <std::size_t N, typename T>
+class Info final
+{
+        // State mean
+        numerical::Vector<N, T> x_;
+
+        // State information
+        numerical::Matrix<N, N, T> i_;
+
+        // State covariance
+        std::optional<numerical::Matrix<N, N, T>> p_;
+
+public:
+        Info(const numerical::Vector<N, T>& x, const numerical::Matrix<N, N, T>& i)
+                : x_(x),
+                  i_(i)
+        {
+        }
+
+        [[nodiscard]] const numerical::Vector<N, T>& x() const
+        {
+                return x_;
+        }
+
+        [[nodiscard]] const numerical::Matrix<N, N, T>& i() const
+        {
+                return i_;
+        }
+
+        [[nodiscard]] const std::optional<numerical::Matrix<N, N, T>>& p() const
+        {
+                return p_;
+        }
+
+        template <typename F, typename FJ>
+        void predict(
+                // State transition function
+                // numerical::Vector<N, T> f(const numerical::Vector<N, T>& x)
+                const F f,
+                // State transition function Jacobian
+                // numerical::Matrix<N, N, T> f(const numerical::Vector<N, T>& x)
+                const FJ fj,
+                // Process covariance
+                const numerical::Matrix<N, N, T>& q)
+        {
+                x_ = f(x_);
+
+                const numerical::Matrix<N, N, T> fjx = fj(x_);
+                const numerical::Matrix<N, N, T> fjx_t = fjx.transposed();
+                const numerical::Matrix<N, N, T> q_inv = q.inversed();
+
+                i_ = q_inv - q_inv * fjx * (i_ + fjx_t * q_inv * fjx).inversed() * fjx_t * q_inv;
+        }
+
+        template <std::size_t M, typename H, typename HJ, typename AddX, typename ResidualZ>
+        void update(
+                // Measurement function
+                // numerical::Vector<M, T> f(const numerical::Vector<N, T>& x)
+                const H h,
+                // Measurement function Jacobian
+                // numerical::Matrix<M, N, T> f(const numerical::Vector<N, T>& x)
+                const HJ hj,
+                // Measurement covariance
+                const numerical::Matrix<M, M, T>& r,
+                // Measurement
+                const numerical::Vector<M, T>& z,
+                // The sum of the two state vectors
+                // numerical::Vector<N, T> f(const numerical::Vector<N, T>& a, const numerical::Vector<N, T>& b)
+                const AddX add_x,
+                // The residual between the two measurement vectors
+                // numerical::Vector<M, T> f(const numerical::Vector<M, T>& a, const numerical::Vector<M, T>& b)
+                const ResidualZ residual_z)
+        {
+                const numerical::Matrix<M, N, T> hjx = hj(x_);
+                const numerical::Matrix<N, M, T> hjx_t = hjx.transposed();
+                const numerical::Matrix<M, M, T> r_inv = r.inversed();
+
+                i_ = i_ + hjx_t * r_inv * hjx;
+                p_ = i_.inversed();
+
+                const numerical::Matrix<N, M, T> k = *p_ * hjx_t * r_inv;
+                const numerical::Vector<M, T> residual = residual_z(z, h(x_));
+
+                x_ = add_x(x_, k * residual);
+        }
+};
+}
