e747cca04764897dad02530b1b3b721eff9da58e
cppd <cppd@users.noreply.github.com>
2024-08-03 10:01:09 +0300

    filter tests

---
+++ src/filter/core/test/filters/info.cpp
@@ -0,0 +1,163 @@
+/*
+Copyright (C) 2017-2024 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "info.h"
+
+#include "ekf_model.h"
+#include "noise_model.h"
+
+#include <src/com/error.h>
+#include <src/filter/core/info.h>
+#include <src/numerical/matrix.h>
+#include <src/numerical/vector.h>
+
+#include <memory>
+#include <optional>
+
+namespace ns::filter::core::test::filters
+{
+namespace
+{
+template <typename T>
+class Filter final : public FilterInfo<T>
+{
+        std::optional<Info<2, T>> filter_;
+
+        void filter_update(const auto& h, const auto& hj, const auto& r, const auto& z)
+        {
+                ASSERT(filter_);
+
+                filter_->update(h, hj, r, z, ekf_model::Add(), ekf_model::Residual());
+        }
+
+        void reset(const numerical::Vector<2, T>& x, const numerical::Matrix<2, 2, T>& i) override
+        {
+                filter_.emplace(x, i);
+        }
+
+        void predict(const T dt, const NoiseModel<T>& noise_model) override
+        {
+                ASSERT(filter_);
+
+                const numerical::Matrix<2, 2, T> f_matrix = ekf_model::f(dt);
+
+                filter_->predict(
+                        [&](const numerical::Vector<2, T>& x)
+                        {
+                                return f_matrix * x;
+                        },
+                        [&](const numerical::Vector<2, T>& /*x*/)
+                        {
+                                return f_matrix;
+                        },
+                        ekf_model::q(dt, noise_model));
+        }
+
+        void update_position(const T position, const T position_variance) override
+        {
+                ASSERT(filter_);
+
+                filter_update(
+                        ekf_model::position_h<T>, ekf_model::position_hj<T>,
+                        ekf_model::position_r<T>(position_variance), numerical::Vector<1, T>(position));
+        }
+
+        void update_position_speed(const T position, const T position_variance, const T speed, const T speed_variance)
+                override
+        {
+                ASSERT(filter_);
+
+                filter_update(
+                        ekf_model::position_speed_h<T>, ekf_model::position_speed_hj<T>,
+                        ekf_model::position_speed_r<T>(position_variance, speed_variance),
+                        numerical::Vector<2, T>(position, speed));
+        }
+
+        void update_speed(const T speed, const T speed_variance) override
+        {
+                ASSERT(filter_);
+
+                filter_update(
+                        ekf_model::speed_h<T>, ekf_model::speed_hj<T>, ekf_model::speed_r<T>(speed_variance),
+                        numerical::Vector<1, T>(speed));
+        }
+
+        [[nodiscard]] T position() const override
+        {
+                ASSERT(filter_);
+
+                return filter_->x()[0];
+        }
+
+        [[nodiscard]] T position_p() const override
+        {
+                ASSERT(filter_);
+
+                const auto& p = filter_->p();
+                ASSERT(p);
+                return (*p)[0, 0];
+        }
+
+        [[nodiscard]] numerical::Vector<2, T> position_speed() const override
+        {
+                ASSERT(filter_);
+
+                return filter_->x();
+        }
+
+        [[nodiscard]] numerical::Matrix<2, 2, T> position_speed_p() const override
+        {
+                ASSERT(filter_);
+
+                const auto& p = filter_->p();
+                ASSERT(p);
+                return *p;
+        }
+
+        [[nodiscard]] T speed() const override
+        {
+                ASSERT(filter_);
+
+                return filter_->x()[1];
+        }
+
+        [[nodiscard]] T speed_p() const override
+        {
+                ASSERT(filter_);
+
+                const auto& p = filter_->p();
+                ASSERT(p);
+                return (*p)[1, 1];
+        }
+
+public:
+        Filter() = default;
+};
+}
+
+template <typename T>
+[[nodiscard]] std::unique_ptr<FilterInfo<T>> create_filter_info()
+{
+        return std::make_unique<Filter<T>>();
+}
+
+#define INSTANTIATION(T) template std::unique_ptr<FilterInfo<T>> create_filter_info();
+
+INSTANTIATION(float)
+INSTANTIATION(double)
+INSTANTIATION(long double)
+}
---
+++ src/filter/core/test/filters/info.h
@@ -0,0 +1,59 @@
+/*
+Copyright (C) 2017-2024 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "noise_model.h"
+
+#include <src/numerical/matrix.h>
+#include <src/numerical/vector.h>
+
+#include <memory>
+
+namespace ns::filter::core::test::filters
+{
+template <typename T>
+class FilterInfo
+{
+public:
+        using Type = T;
+
+        virtual ~FilterInfo() = default;
+
+        virtual void reset(const numerical::Vector<2, T>& x, const numerical::Matrix<2, 2, T>& i) = 0;
+
+        virtual void predict(T dt, const NoiseModel<T>& noise_model) = 0;
+
+        virtual void update_position(T position, T position_variance) = 0;
+
+        virtual void update_position_speed(T position, T position_variance, T speed, T speed_variance) = 0;
+
+        virtual void update_speed(T speed, T speed_variance) = 0;
+
+        [[nodiscard]] virtual T position() const = 0;
+        [[nodiscard]] virtual T position_p() const = 0;
+
+        [[nodiscard]] virtual numerical::Vector<2, T> position_speed() const = 0;
+        [[nodiscard]] virtual numerical::Matrix<2, 2, T> position_speed_p() const = 0;
+
+        [[nodiscard]] virtual T speed() const = 0;
+        [[nodiscard]] virtual T speed_p() const = 0;
+};
+
+template <typename T>
+[[nodiscard]] std::unique_ptr<FilterInfo<T>> create_filter_info();
+}
