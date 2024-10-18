979c1fc0b7dad59c0df1d9c671bb0d284ea26db1
cppd <cppd@users.noreply.github.com>
2024-10-18 09:10:35 +0300

    test quaternion

--- src/numerical/test/test_quaternion.cpp
+++ src/numerical/test/test_quaternion.cpp
@@ -33,25 +33,25 @@ namespace
 template <typename T>
 struct Test final
 {
-        static constexpr Quaternion<T> A{1, 2, 3, 4};
+        static constexpr Quaternion<T> A{2, 3, 4, 5};
         static constexpr Quaternion<T> B{11, 12, 13, 14};
 
-        static_assert(A == Quaternion<T>(1, {2, 3, 4}));
-        static_assert(A == Quaternion<T>(Vector<4, T>(1, 2, 3, 4)));
-        static_assert(A[0] == 1);
-        static_assert(A[1] == 2);
-        static_assert(A[2] == 3);
-        static_assert(A[3] == 4);
-        static_assert(A.data() == Vector<4, T>(1, 2, 3, 4));
-        static_assert(A.imag() == Vector<3, T>(2, 3, 4));
-        static_assert(A.conjugate() == Quaternion<T>(1, -2, -3, -4));
-        static_assert(A * T{3} == Quaternion<T>(3, 6, 9, 12));
-        static_assert(T{3} * A == Quaternion<T>(3, 6, 9, 12));
-        static_assert(A / T{2} == Quaternion<T>(T{1} / 2, 1, T{3} / 2, 2));
-        static_assert(A + B == Quaternion<T>(12, 14, 16, 18));
-        static_assert(A - B == Quaternion<T>(-10, -10, -10, -10));
-        static_assert(A * B == Quaternion<T>(-108, 24, 66, 48));
-        static_assert(A * Vector<3, T>(11, 12, 13) == Quaternion<T>(-110, 2, 30, 4));
+        static_assert(A == Quaternion<T>(2, {3, 4, 5}));
+        static_assert(A == Quaternion<T>(Vector<4, T>(2, 3, 4, 5)));
+        static_assert(A[0] == 2);
+        static_assert(A[1] == 3);
+        static_assert(A[2] == 4);
+        static_assert(A[3] == 5);
+        static_assert(A.data() == Vector<4, T>(2, 3, 4, 5));
+        static_assert(A.imag() == Vector<3, T>(3, 4, 5));
+        static_assert(A.conjugate() == Quaternion<T>(2, -3, -4, -5));
+        static_assert(A * T{3} == Quaternion<T>(6, 9, 12, 15));
+        static_assert(T{3} * A == Quaternion<T>(6, 9, 12, 15));
+        static_assert(A / T{2} == Quaternion<T>(1, T{3} / 2, 2, T{5} / 2));
+        static_assert(A + B == Quaternion<T>(13, 15, 17, 19));
+        static_assert(A - B == Quaternion<T>(-9, -9, -9, -9));
+        static_assert(A * B == Quaternion<T>(-136, 48, 88, 74));
+        static_assert(A * Vector<3, T>(11, 12, 13) == Quaternion<T>(-146, 14, 40, 18));
 };
 
 template struct Test<float>;
@@ -76,10 +76,10 @@ bool equal(const T a, const T b, const T precision)
         return (rel < precision);
 }
 
-template <typename T>
-void test_equal(const Quaternion<T>& a, const Quaternion<T>& b, const T precision)
+template <typename T, typename P>
+void test_equal(const T& a, const T& b, const P precision)
 {
-        for (std::size_t i = 0; i < 4; ++i)
+        for (std::size_t i = 0; i < std::tuple_size_v<T>; ++i)
         {
                 if (!equal(a[i], b[i], precision))
                 {
@@ -96,32 +96,39 @@ void test(const T precision)
                 q.normalize();
                 test_equal(
                         q,
-                        Quaternion<T>(
-                                0.272165526975908677584L, 0.408248290463863016363L, 0.544331053951817355168L,
-                                0.680413817439771693974L),
+                        {0.272165526975908677584L, 0.408248290463863016363L, 0.544331053951817355168L,
+                         0.680413817439771693974L},
                         precision);
         }
 
         test_equal(
                 Quaternion<T>(2, 4, 3, 5).normalized(),
-                Quaternion<T>(
-                        0.272165526975908677584L, 0.544331053951817355168L, 0.408248290463863016363L,
-                        0.680413817439771693974L),
+                {0.272165526975908677584L, 0.544331053951817355168L, 0.408248290463863016363L,
+                 0.680413817439771693974L},
                 precision);
 
         test_equal(
                 Quaternion<T>(3, -7, 2, -8).inversed(),
-                Quaternion<T>(
-                        0.0238095238095238095235L, 0.0555555555555555555548L, -0.0158730158730158730157L,
-                        0.0634920634920634920626L),
+                {0.0238095238095238095235L, 0.0555555555555555555548L, -0.0158730158730158730157L,
+                 0.0634920634920634920626L},
                 precision);
+
+        test_equal(
+                quaternion_for_rotation({4, -5, 6}, T{2}),
+                {0.540302305868139717414L, 0.383578074011068530816L, -0.479472592513835663554L,
+                 0.57536711101660279621L},
+                precision);
+
+        test_equal(
+                rotate_vector({-5, 6, 4}, T{2}, {3, -5, 2}),
+                {5.46996008744151012305L, 0.27754662586912375613L, -2.82886982950179797927L}, precision);
 }
 
 void test_quaternion()
 {
         LOG("Test quaternion");
-        test<float>(1e-7);
-        test<double>(0);
+        test<float>(1e-6);
+        test<double>(1e-15);
         test<long double>(0);
         LOG("Test quaternion passed");
 }
