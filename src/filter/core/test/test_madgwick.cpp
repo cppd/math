acf00f61d0958efeb2edee5bb8ea8ad9f407d2b3
cppd <cppd@users.noreply.github.com>
2024-10-29 08:28:03 +0300

    filter tests

--- src/filter/core/test/test_madgwick.cpp
+++ src/filter/core/test/test_madgwick.cpp
@@ -59,44 +59,70 @@ void test_equal(const T& a, const T& b, const P precision)
 }
 
 template <typename T>
-void test_impl(const T precision)
+void test_m(const T precision)
 {
+        numerical::Quaternion<T> q;
+
+        const auto cmp = [&](const T w, const T x, const T y, const T z)
+        {
+                test_equal(q, {w, x, y, z}, precision);
+        };
+
+        const T beta = madgwick_beta<T>(1);
+        Madgwick<T> m;
+
+        for (int i = 0; i < 10; ++i)
+        {
+                q = m.update({0.01, 0.02, 0.03}, {3, 4, 5}, beta, 0.1, 1);
+        }
+        cmp(0.896883542158740787844L, 0.353210147162852465801L, -0.265415656397648120859L, 0.0199256890719475578416L);
+
+        q = m.update({0.01, 0.02, 0.03}, {0, 0, 0}, beta, 0.1, 1);
+        cmp(0.896940894562757041824L, 0.353239921590941529961L, -0.265038161415321047061L, 0.0217568942860156602685L);
+}
+
+template <typename T>
+void test_mm(const T precision)
+{
+        numerical::Quaternion<T> q;
+
+        const auto cmp = [&](const T w, const T x, const T y, const T z)
+        {
+                test_equal(q, {w, x, y, z}, precision);
+        };
+
         const T beta = madgwick_beta<T>(1);
         const T zeta = madgwick_zeta<T>(0.1);
+        MadgwickMarg<T> m;
 
+        for (int i = 0; i < 10; ++i)
         {
-                Madgwick<T> m;
-                numerical::Quaternion<T> q;
-                for (int i = 0; i <= 10; ++i)
-                {
-                        q = m.update({0.01, 0.02, 0.03}, {3, 4, 5}, beta, 0.1, 1);
-                }
-                test_equal(
-                        q,
-                        {0.925845007236751315932L, 0.306664637927562528535L, -0.22005178876596579639L,
-                         0.0185750553279297316612L},
-                        precision);
+                q = m.update({0.01, 0.02, 0.03}, {3, 4, 5}, {2, -3, 4}, beta, zeta, 0.1, 1, 1);
         }
-        {
-                MadgwickMarg<T> m;
-                numerical::Quaternion<T> q;
-                for (int i = 0; i < 10; ++i)
-                {
-                        q = m.update({0.01, 0.02, 0.03}, {3, 4, 5}, {2, -3, 4}, beta, zeta, 0.1, 1, 1);
-                }
-                test_equal(
-                        q,
-                        {0.620363456233866526746L, 0.199558167398623417972L, 0.663642023000996199224L,
-                         0.367294140031146965319L},
-                        precision);
-        }
+        cmp(0.620363456233866526746L, 0.199558167398623417972L, 0.663642023000996199224L, 0.367294140031146965319L);
+
+        q = m.update({0.01, 0.02, 0.03}, {0, 0, 0}, {2, -3, 4}, beta, zeta, 0.1, 1, 1);
+        cmp(0.612400673687452607309L, 0.201650341705407511953L, 0.668516208234102618762L, 0.370659727898168788243L);
+
+        q = m.update({0.01, 0.02, 0.03}, {3, 4, 5}, {0, 0, 0}, beta, zeta, 0.1, 1, 1);
+        cmp(0.611696622987349623412L, 0.220555175307468252527L, 0.640826584894761140925L, 0.40807345437198153405L);
+
+        q = m.update({0.01, 0.02, 0.03}, {0, 0, 0}, {0, 0, 0}, beta, zeta, 0.1, 1, 1);
+        cmp(0.603648959038190618021L, 0.222166385960238515492L, 0.6456164487307030759L, 0.411618066087763750726L);
+}
+
+template <typename T>
+void test_impl(const T precision)
+{
+        test_m<T>(precision);
+        test_mm<T>(precision);
 }
 
 void test()
 {
         LOG("Test Madgwick filter");
-        test_impl<float>(1e-6);
-        test_impl<double>(1e-15);
+        test_impl<float>(1e-5);
+        test_impl<double>(1e-14);
         test_impl<long double>(0);
         LOG("Test Madgwick filter passed");
 }
