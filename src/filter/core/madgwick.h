1b0416b67384513f1b13086b822a61e459629498
cppd <cppd@users.noreply.github.com>
2024-10-30 08:06:16 +0300

    filter

--- src/filter/core/madgwick.h
+++ src/filter/core/madgwick.h
@@ -35,6 +35,64 @@ namespace ns::filter::core
 namespace madgwick_implementation
 {
 template <typename T>
+[[nodiscard]] numerical::Quaternion<T> compute_gn(const numerical::Quaternion<T> q, const numerical::Vector<3, T> an)
+{
+        // (25)
+        // f_g
+        const T f_0 = 2 * q[1] * q[3] - 2 * q[2] * q[0] - an[0];
+        const T f_1 = 2 * q[1] * q[0] + 2 * q[2] * q[3] - an[1];
+        const T f_2 = 1 - 2 * q[1] * q[1] - 2 * q[2] * q[2] - an[2];
+
+        // (20) (26)
+        // transpose(J_g) * f_g
+        const numerical::Vector<3, T> f(2 * f_0, 2 * f_1, 4 * f_2);
+        numerical::Quaternion<T> r;
+        r[0] = q[1] * f[1] - q[2] * f[0];
+        r[1] = q[3] * f[0] + q[0] * f[1] - q[1] * f[2];
+        r[2] = q[3] * f[1] - q[0] * f[0] - q[2] * f[2];
+        r[3] = q[1] * f[0] + q[2] * f[1];
+        return r.normalized();
+}
+
+template <typename T>
+[[nodiscard]] numerical::Quaternion<T> compute_gn(
+        const numerical::Quaternion<T> q,
+        const numerical::Vector<3, T> an,
+        const numerical::Vector<3, T> mn,
+        const T b_x,
+        const T b_z)
+{
+        // (25)
+        // f_g
+        const T f_0 = 2 * q[1] * q[3] - 2 * q[2] * q[0] - an[0];
+        const T f_1 = 2 * q[1] * q[0] + 2 * q[2] * q[3] - an[1];
+        const T f_2 = 1 - 2 * q[1] * q[1] - 2 * q[2] * q[2] - an[2];
+
+        // (29)
+        // f_b
+        const T f_3 = 2 * b_x * (T{0.5} - q[2] * q[2] - q[3] * q[3]) + 2 * b_z * (q[1] * q[3] - q[0] * q[2]) - mn[0];
+        const T f_4 = 2 * b_x * (q[1] * q[2] - q[0] * q[3]) + 2 * b_z * (q[0] * q[1] + q[2] * q[3]) - mn[1];
+        const T f_5 = 2 * b_x * (q[0] * q[2] + q[1] * q[3]) + 2 * b_z * (T{0.5} - q[1] * q[1] - q[2] * q[2]) - mn[2];
+
+        // (20) (26) (30) (34) (44)
+        // normalize(transpose(J_g / J_b) * (f_g / f_b))
+
+        const numerical::Vector<6, T> f(2 * f_0, 2 * f_1, 4 * f_2, 2 * f_3, 2 * f_4, 2 * f_5);
+
+        const numerical::Quaternion<T> bxq = b_x * q;
+        const numerical::Quaternion<T> bzq = b_z * q;
+
+        numerical::Quaternion<T> r;
+        r[0] = q[1] * f[1] - q[2] * f[0] - bzq[2] * f[3] - (bxq[3] - bzq[1]) * f[4] + bxq[2] * f[5];
+        r[1] = q[3] * f[0] + q[0] * f[1] - q[1] * f[2] + bzq[3] * f[3] + (bxq[2] + bzq[0]) * f[4]
+               + (bxq[3] - 2 * bzq[1]) * f[5];
+        r[2] = q[3] * f[1] - -q[0] * f[0] - q[2] * f[2] - (2 * bxq[2] + bzq[0]) * f[3] + (bxq[1] + bzq[3]) * f[4]
+               + (bxq[0] - 2 * bzq[2]) * f[5];
+        r[3] = q[1] * f[0] + q[2] * f[1] - (2 * bxq[3] - bzq[1]) * f[3] - (bxq[0] - bzq[2]) * f[4] + bxq[1] * f[5];
+        return r.normalized();
+}
+
+template <typename T>
 [[nodiscard]] numerical::Quaternion<T> update(
         const numerical::Quaternion<T> q,
         const numerical::Vector<3, T> w,
@@ -53,24 +111,9 @@ template <typename T>
                 return (q + d * dt).normalized();
         }
 
-        const numerical::Quaternion<T> gn = [&]
-        {
-                // (25)
-                // f_g
-                const T f_0 = 2 * q[1] * q[3] - 2 * q[2] * q[0] - a[0] / a_norm;
-                const T f_1 = 2 * q[1] * q[0] + 2 * q[2] * q[3] - a[1] / a_norm;
-                const T f_2 = 1 - 2 * q[1] * q[1] - 2 * q[2] * q[2] - a[2] / a_norm;
+        const numerical::Vector<3, T> an = a / a_norm;
 
-                // (20) (26)
-                // transpose(J_g) * f_g
-                const numerical::Vector<3, T> f(2 * f_0, 2 * f_1, 4 * f_2);
-                numerical::Quaternion<T> r;
-                r[0] = q[1] * f[1] - q[2] * f[0];
-                r[1] = q[3] * f[0] + q[0] * f[1] - q[1] * f[2];
-                r[2] = q[3] * f[1] - q[0] * f[0] - q[2] * f[2];
-                r[3] = q[1] * f[0] + q[2] * f[1];
-                return r.normalized();
-        }();
+        const numerical::Quaternion<T> gn = compute_gn(q, an);
 
         // (42) (43) (44)
         return (q + (d - beta * gn) * dt).normalized();
@@ -135,10 +178,11 @@ public:
                 const T min_accelerometer,
                 const T min_magnetometer)
         {
+                namespace impl = madgwick_implementation;
+
                 const T m_norm = m.norm();
                 if (!(m_norm >= min_magnetometer))
                 {
-                        namespace impl = madgwick_implementation;
                         q_ = impl::update(q_, w - wb_, a, beta, dt, min_accelerometer);
                         return q_;
                 }
@@ -153,40 +197,10 @@ public:
                         return q_;
                 }
 
+                const numerical::Vector<3, T> an = a / a_norm;
                 const numerical::Vector<3, T> mn = m / m_norm;
 
-                const numerical::Quaternion<T> gn = [&]
-                {
-                        // (25)
-                        // f_g
-                        const T f_0 = 2 * q_[1] * q_[3] - 2 * q_[2] * q_[0] - a[0] / a_norm;
-                        const T f_1 = 2 * q_[1] * q_[0] + 2 * q_[2] * q_[3] - a[1] / a_norm;
-                        const T f_2 = 1 - 2 * q_[1] * q_[1] - 2 * q_[2] * q_[2] - a[2] / a_norm;
-
-                        // (29)
-                        // f_b
-                        const T f_3 = 2 * b_x_ * (T{0.5} - q_[2] * q_[2] - q_[3] * q_[3])
-                                      + 2 * b_z_ * (q_[1] * q_[3] - q_[0] * q_[2]) - mn[0];
-                        const T f_4 = 2 * b_x_ * (q_[1] * q_[2] - q_[0] * q_[3])
-                                      + 2 * b_z_ * (q_[0] * q_[1] + q_[2] * q_[3]) - mn[1];
-                        const T f_5 = 2 * b_x_ * (q_[0] * q_[2] + q_[1] * q_[3])
-                                      + 2 * b_z_ * (T{0.5} - q_[1] * q_[1] - q_[2] * q_[2]) - mn[2];
-
-                        // (20) (26) (30) (34) (44)
-                        // normalize(transpose(J_g / J_b) * (f_g / f_b))
-                        const numerical::Quaternion<T> bxq = b_x_ * q_;
-                        const numerical::Quaternion<T> bzq = b_z_ * q_;
-                        const numerical::Vector<6, T> f(2 * f_0, 2 * f_1, 4 * f_2, 2 * f_3, 2 * f_4, 2 * f_5);
-                        numerical::Quaternion<T> r;
-                        r[0] = q_[1] * f[1] - q_[2] * f[0] - bzq[2] * f[3] - (bxq[3] - bzq[1]) * f[4] + bxq[2] * f[5];
-                        r[1] = q_[3] * f[0] + q_[0] * f[1] - q_[1] * f[2] + bzq[3] * f[3] + (bxq[2] + bzq[0]) * f[4]
-                               + (bxq[3] - 2 * bzq[1]) * f[5];
-                        r[2] = q_[3] * f[1] - -q_[0] * f[0] - q_[2] * f[2] - (2 * bxq[2] + bzq[0]) * f[3]
-                               + (bxq[1] + bzq[3]) * f[4] + (bxq[0] - 2 * bzq[2]) * f[5];
-                        r[3] = q_[1] * f[0] + q_[2] * f[1] - (2 * bxq[3] - bzq[1]) * f[3] - (bxq[0] - bzq[2]) * f[4]
-                               + bxq[1] * f[5];
-                        return r.normalized();
-                }();
+                const numerical::Quaternion<T> gn = impl::compute_gn(q_, an, mn, b_x_, b_z_);
 
                 // (47)
                 const numerical::Vector<3, T> w_err = T{2} * numerical::multiply_vec(q_.conjugate(), gn);
