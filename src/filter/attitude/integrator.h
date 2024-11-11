ea3d0a4c3101379d5fb05b14d9d8b29f596518f0
cppd <cppd@users.noreply.github.com>
2024-11-11 09:49:37 +0300

    integrator

---
+++ src/filter/attitude/integrator.h
@@ -0,0 +1,57 @@
+/*
+Copyright (C) 2017-2024 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "quaternion.h"
+
+#include <src/numerical/vector.h>
+
+#include <cmath>
+
+namespace ns::filter::attitude
+{
+template <typename T>
+[[nodiscard]] Quaternion<T> zeroth_order_quaternion_integrator(
+        const Quaternion<T>& q,
+        const numerical::Vector<3, T>& w,
+        const T dt)
+{
+        const T wn = w.norm();
+        const T k = wn * dt / 2;
+        const Quaternion<T> theta{(std::sin(k) / wn) * w, std::cos(k)};
+
+        return theta * q;
+}
+
+template <typename T>
+[[nodiscard]] Quaternion<T> first_order_quaternion_integrator(
+        const Quaternion<T>& q,
+        const numerical::Vector<3, T>& w0,
+        const numerical::Vector<3, T>& w1,
+        const T dt)
+{
+        const numerical::Vector<3, T> wa = (w0 + w1) / T{2};
+        const Quaternion<T> q1 = zeroth_order_quaternion_integrator(q, wa, dt);
+
+        const Quaternion<T> qw0 = Quaternion<T>(w0, 0);
+        const Quaternion<T> qw1 = Quaternion<T>(w1, 0);
+        const Quaternion<T> q2 = (qw1 * qw0 - qw0 * qw1) * (dt * dt / 48);
+
+        return (q1 + q2) * q;
+}
+}
