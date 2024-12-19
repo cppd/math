44f777b2edf1ece11f354b7467ae25598bc05d90
cppd <cppd@users.noreply.github.com>
2024-12-19 09:47:44 +0300

    filter tests

--- src/filter/attitude/ekf/test/test_matrix.cpp
+++ src/filter/attitude/ekf/test/test_matrix.cpp
@@ -16,6 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <src/com/log.h>
+#include <src/filter/attitude/ekf/constant.h>
 #include <src/filter/attitude/ekf/matrix.h>
 #include <src/numerical/matrix.h>
 #include <src/numerical/vector.h>
@@ -64,12 +65,18 @@ void test_equal(const numerical::Matrix<R, C, T>& a, const numerical::Matrix<R, 
 {
         for (std::size_t i = 0; i < R; ++i)
         {
-                test_equal(a.row(i), b.row(i), precision);
+                for (std::size_t j = 0; j < C; ++j)
+                {
+                        if (!equal(a[i, j], b[i, j], precision))
+                        {
+                                error(to_string(a) + " is not equal to " + to_string(b));
+                        }
+                }
         }
 }
 
 template <typename T>
-void test_impl(const T precision)
+void test_cross(const T precision)
 {
         const numerical::Vector<3, T> v1{1, 2, 3};
         const numerical::Vector<3, T> v2{3, -2, 1};
@@ -84,11 +91,100 @@ void test_impl(const T precision)
         test_equal(m * m * m * m * m * m, cross_matrix<6>(v1), precision);
 }
 
+template <typename T>
+void test_state(const T precision)
+{
+        using Matrix3 = numerical::Matrix<3, 3, T>;
+        using Matrix6 = numerical::Matrix<6, 6, T>;
+
+        constexpr numerical::Vector<3, T> W(W_THRESHOLD<T>, W_THRESHOLD<T> / T{2}, W_THRESHOLD<T> * T{2});
+
+        {
+                const Matrix3 m = state_transition_matrix_3(T{1000} * W, T{0.01});
+                const Matrix3 c = {
+                        {    0.999999978750000092988L,  0.000200002498249989085633L, -4.99899995625437641756e-05L},
+                        {-0.000199997498250010964594L,     0.999999975000000109382L,  0.000100004999124978133608L},
+                        { 5.00099995624562483812e-05L, -9.99949991250218915054e-05L,     0.999999993750000027359L}
+                };
+                test_equal(m, c, precision);
+        }
+        {
+                const Matrix3 m = state_transition_matrix_3(W / T{10}, T{0.1});
+                const Matrix3 c = {
+                        {    0.999999999999978750017L,  2.00000002500000027457e-07L, -4.99999900000000068669e-08L},
+                        {-1.99999997500000027466e-07L,     0.999999999999974999978L,  1.00000005000000013729e-07L},
+                        {  5.0000010000000006864e-08L, -9.99999950000000137333e-08L,     0.999999999999993750008L}
+                };
+                test_equal(m, c, precision);
+        }
+        {
+                const Matrix6 m = state_transition_matrix_6(T{1000} * W, T{0.01});
+                const Matrix3 c00 = {
+                        {    0.999999978750000092988L,  0.000200002498249989085633L, -4.99899995625437641756e-05L},
+                        {-0.000199997498250010964594L,     0.999999975000000109382L,  0.000100004999124978133608L},
+                        { 5.00099995624562483812e-05L, -9.99949991250218915054e-05L,     0.999999993750000027359L}
+                };
+                const Matrix3 c01 = {
+                        { -0.00999999992916666706103L, -1.00000832895752153731e-06L,  2.49966665572806654057e-07L},
+                        { 9.99991662290898605391e-07L,  -0.00999999991666666709319L, -5.00016664478727967538e-07L},
+                        {-2.50033332239298381617e-07L,   4.9998333114548210381e-07L,  -0.00999999997916666692984L}
+                };
+                const Matrix3 c10 = {
+                        {0, 0, 0},
+                        {0, 0, 0},
+                        {0, 0, 0}
+                };
+                const Matrix3 c11 = {
+                        {1, 0, 0},
+                        {0, 1, 0},
+                        {0, 0, 1}
+                };
+                test_equal(numerical::block<0, 0, 3, 3>(m), c00, precision);
+                test_equal(numerical::block<0, 3, 3, 3>(m), c01, precision);
+                test_equal(numerical::block<3, 0, 3, 3>(m), c10, precision);
+                test_equal(numerical::block<3, 3, 3, 3>(m), c11, precision);
+        }
+        {
+                const Matrix6 m = state_transition_matrix_6(W / T{10}, T{0.1});
+                const Matrix3 c00 = {
+                        {    0.999999999999978750017L,  2.00000002500000027457e-07L, -4.99999900000000068669e-08L},
+                        {-1.99999997500000027466e-07L,     0.999999999999974999978L,  1.00000005000000013729e-07L},
+                        {  5.0000010000000006864e-08L, -9.99999950000000137333e-08L,     0.999999999999993750008L}
+                };
+                const Matrix3 c01 = {
+                        {  -0.0999999999999992972147L, -1.00000000833333352612e-08L,  2.49999966666666714875e-09L},
+                        { 9.99999991666666859524e-09L,   -0.0999999999999991722198L, -5.00000016666666763086e-09L},
+                        {-2.50000033333333381535e-09L,  4.99999983333333429736e-09L,   -0.0999999999999997972149L}
+                };
+                const Matrix3 c10 = {
+                        {0, 0, 0},
+                        {0, 0, 0},
+                        {0, 0, 0}
+                };
+                const Matrix3 c11 = {
+                        {1, 0, 0},
+                        {0, 1, 0},
+                        {0, 0, 1}
+                };
+                test_equal(numerical::block<0, 0, 3, 3>(m), c00, precision);
+                test_equal(numerical::block<0, 3, 3, 3>(m), c01, precision);
+                test_equal(numerical::block<3, 0, 3, 3>(m), c10, precision);
+                test_equal(numerical::block<3, 3, 3, 3>(m), c11, precision);
+        }
+}
+
+template <typename T>
+void test_impl(const T precision)
+{
+        test_cross<T>(precision);
+        test_state<T>(precision);
+}
+
 void test()
 {
         LOG("Test attitude EKF matrix");
-        test_impl<float>(0);
-        test_impl<double>(0);
+        test_impl<float>(1e-5);
+        test_impl<double>(1e-14);
         test_impl<long double>(0);
         LOG("Test attitude EKF matrix passed");
 }
