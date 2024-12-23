57fd53a8525239ce66f9acaa5fd323ea61031577
cppd <cppd@users.noreply.github.com>
2024-12-23 09:13:09 +0300

    test utility

---
+++ src/filter/attitude/ekf/test/test_utility.cpp
@@ -0,0 +1,142 @@
+/*
+Copyright (C) 2017-2024 Topological Manifold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received q copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <src/com/log.h>
+#include <src/filter/attitude/ekf/quaternion.h>
+#include <src/filter/attitude/ekf/utility.h>
+#include <src/numerical/vector.h>
+#include <src/test/test.h>
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <type_traits>
+
+namespace ns::filter::attitude::ekf::test
+{
+namespace
+{
+template <typename T>
+bool equal(const T a, const T b, const T precision)
+{
+        static_assert(std::is_floating_point_v<T>);
+
+        if (a == b)
+        {
+                return true;
+        }
+        const T abs = std::abs(a - b);
+        if (abs < precision)
+        {
+                return true;
+        }
+        const T rel = abs / std::max(std::abs(a), std::abs(b));
+        return (rel < precision);
+}
+
+template <std::size_t N, typename T>
+bool equal(const numerical::Vector<N, T>& a, const numerical::Vector<N, T>& b, const T precision)
+{
+        for (std::size_t i = 0; i < N; ++i)
+        {
+                if (!equal(a[i], b[i], precision))
+                {
+                        return false;
+                }
+        }
+        return true;
+}
+
+template <typename T>
+numerical::Vector<4, T> to_vector(const Quaternion<T>& q)
+{
+        return {q.w(), q.x(), q.y(), q.z()};
+}
+
+template <std::size_t N, typename T>
+void test_equal(const numerical::Vector<N, T>& a, const numerical::Vector<N, T>& b, const T precision)
+{
+        if (!equal(a, b, precision))
+        {
+                error(to_string(a) + " is not equal to " + to_string(b));
+        }
+}
+
+template <typename T>
+void test_equal(const Quaternion<T>& a, const Quaternion<T>& b, const T precision)
+{
+        if (!equal(to_vector(a), to_vector(b), precision))
+        {
+                error(to_string(a) + " is not equal to " + to_string(b));
+        }
+}
+
+template <typename T>
+void test_impl(const T precision)
+{
+        {
+                const numerical::Vector<3, T> a(-2, 3, -4);
+                const Quaternion<T> q = initial_quaternion(a);
+                const Quaternion<T> c(
+                        0.0927149414594679982437L,
+                        {0.701002096737354575291L, -0.616430642265564459194L, -0.346429304874838346569L});
+                test_equal(q, c, precision);
+        }
+        {
+                const numerical::Vector<3, T> a(2, -3, 4);
+                const numerical::Vector<3, T> m(-5, 6, -3);
+                const Quaternion<T> q = initial_quaternion(a, m);
+                const Quaternion<T> c(
+                        0.858736994555715913943L,
+                        {-0.352495235158186814633L, -0.0660032789842357141936L, -0.366007446011034129433L});
+                test_equal(q, c, precision);
+        }
+        {
+                const numerical::Vector<3, T> v(-0.2L, 0.3L, -0.4L);
+                const Quaternion<T> q = delta_quaternion(v);
+                const Quaternion<T> c(0.842614977317635863055L, {-0.2L, 0.3L, -0.4L});
+                test_equal(q, c, precision);
+        }
+        {
+                const numerical::Vector<3, T> v(-1.2, 2.3, -3.4);
+                const Quaternion<T> q = delta_quaternion(v);
+                const Quaternion<T> c(
+                        0.227684720124393468466L,
+                        {-0.273221664149272152055L, 0.523674856286104937033L, -0.774128048422937772562L});
+                test_equal(q, c, precision);
+        }
+        {
+                const Quaternion<T> q = Quaternion<T>(-2, {5.2, -3.3, 4.4}).normalized();
+                const numerical::Vector<3, T> v = global_to_local(q, {2.1, -3.2, 4.3});
+                const numerical::Vector<3, T> c(
+                        5.0222059063468757101L, -2.42440854951868102712L, 1.4281775167237726246L);
+                test_equal(v, c, precision);
+        }
+}
+
+void test()
+{
+        LOG("Test attitude EKF utility");
+        test_impl<float>(1e-6);
+        test_impl<double>(1e-15);
+        test_impl<long double>(0);
+        LOG("Test attitude EKF utility passed");
+}
+
+TEST_SMALL("Attitude EKF Utility", test)
+}
+}
