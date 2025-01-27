b56cad706972820ca155a65070a4b5d59b2afc4f
cppd <cppd@users.noreply.github.com>
2025-01-27 08:05:08 +0300

    filter tests

--- src/filter/attitude/kalman/test/test_ukf_utility.cpp
+++ src/filter/attitude/kalman/test/test_ukf_utility.cpp
@@ -45,6 +45,23 @@ void test_impl(const T precision)
                         {0.0968541929606670444315L, -0.145281289441000553207L, 0.193708385921334088863L});
                 test_equal(q, c, precision);
         }
+        {
+                const numerical::Vector<3, T> v(0.2, -0.3, 0.4);
+                const Quaternion<T> center = Quaternion<T>(0.3, {-0.2, 0.1, -0.5}).normalized();
+                const Quaternion<T> q = error_to_quaternion(v, center);
+                const Quaternion<T> c(
+                        0.673133918337381338399L,
+                        {-0.347946905362277922438L, 0.0692871417083943385982L, -0.648862829129900554946L});
+                test_equal(q, c, precision);
+        }
+        {
+                const Quaternion<T> q = Quaternion<T>(1.5, {-0.4, 0.1, 0.6}).normalized();
+                const Quaternion<T> center_inversed = Quaternion<T>(-2.1, {0.1, -0.5, 0.3}).normalized();
+                const numerical::Vector<3, T> v = quaternion_to_error(q, center_inversed);
+                const numerical::Vector<3, T> c(
+                        -0.402911489752197424512L, 0.695938027753795459499L, 0.610471954169996024718L);
+                test_equal(v, c, precision);
+        }
 }
 
 void test()
