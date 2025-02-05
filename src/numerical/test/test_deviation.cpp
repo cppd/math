e5ad621ba736e21f7044c22a6f9bb9a9e2a3110a
cppd <cppd@users.noreply.github.com>
2025-02-05 08:54:28 +0300

    test deviation

--- src/numerical/test/test_deviation.cpp
+++ src/numerical/test/test_deviation.cpp
@@ -18,10 +18,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <src/com/error.h>
 #include <src/com/log.h>
 #include <src/com/print.h>
+#include <src/com/random/pcg.h>
 #include <src/numerical/deviation.h>
 #include <src/test/test.h>
 
 #include <cmath>
+#include <cstddef>
+#include <random>
 #include <vector>
 
 namespace ns::numerical
@@ -29,35 +32,85 @@ namespace ns::numerical
 namespace
 {
 template <typename T>
-        requires (std::is_floating_point_v<T>)
-void compare(const T a, const T b, const T precision)
+void compare(const T a, const T b)
 {
-        if (!(std::abs(a - b) <= precision))
+        if (!(a == b))
         {
                 error(to_string(a) + " is not equal to " + to_string(b));
         }
 }
 
 template <typename T>
-void test(const T& precision)
+void test_constant()
 {
         const std::vector<T> data{-2, 3, 7, -15, -6, 0, 1, 3, 19};
 
         const numerical::MedianAbsoluteDeviation<T> mad = median_absolute_deviation(data);
-        compare(mad.median, T{1}, precision);
-        compare(mad.deviation, T{3}, precision);
+        compare(mad.median, T{1});
+        compare(mad.deviation, T{3});
 
         const T sd = numerical::standard_deviation(mad);
-        compare(sd, T{4.44780665551680558147L}, precision);
+        compare(sd, T{4.44780665551680558147L});
+}
+
+template <typename T>
+void test_random()
+{
+        constexpr std::size_t COUNT = 10'000;
+        constexpr std::size_t ERROR_COUNT = 10;
+        constexpr T MEAN = -1;
+        constexpr T STD_DEV = 10;
+
+        PCG engine;
+
+        std::normal_distribution<T> nd(MEAN, STD_DEV);
+
+        std::vector<T> data;
+        data.reserve(COUNT + ERROR_COUNT);
+
+        for (std::size_t i = 0; i < COUNT; ++i)
+        {
+                data.push_back(nd(engine));
+        }
+
+        for (std::size_t i = 0; i < ERROR_COUNT; ++i)
+        {
+                data.push_back(MEAN + T{10'000} * i * STD_DEV);
+        }
+
+        const numerical::MedianAbsoluteDeviation<T> mad = median_absolute_deviation(data);
+        const T sd = numerical::standard_deviation(mad);
+
+        if (!(mad.median > -2 && mad.median < 0))
+        {
+                error("Median " + to_string(mad.median) + " is out of range");
+        }
+
+        if (!(mad.deviation > 6 && mad.deviation < 7.5))
+        {
+                error("Deviation " + to_string(mad.deviation) + " is out of range");
+        }
+
+        if (!(sd > 9 && mad.deviation < 11))
+        {
+                error("Standard deviation " + to_string(sd) + " is out of range");
+        }
+}
+
+template <typename T>
+void test()
+{
+        test_constant<T>();
+        test_random<T>();
 }
 
 void test_deviation()
 {
         LOG("Test deviation");
 
-        test<float>(0);
-        test<double>(0);
-        test<long double>(0);
+        test<float>();
+        test<double>();
+        test<long double>();
 
         LOG("Test deviation passed");
 }
