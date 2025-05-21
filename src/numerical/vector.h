57cdc06188b37c4590339ba05d61bc9c4b5ea149
cppd <cppd@users.noreply.github.com>
2025-05-21 08:39:46 +0300

    vector

--- src/numerical/vector.h
+++ src/numerical/vector.h
@@ -289,9 +289,9 @@ template <typename T>
 }
 
 using Vector2d = Vector<2, double>;
-using Vector3d = Vector<3, double>;
-using Vector4d = Vector<4, double>;
 using Vector2f = Vector<2, float>;
+using Vector3d = Vector<3, double>;
 using Vector3f = Vector<3, float>;
+using Vector4d = Vector<4, double>;
 using Vector4f = Vector<4, float>;
 }
