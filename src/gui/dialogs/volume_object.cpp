ba84d2819577a19ae7a3d22d5b4ab395534bc378
cppd <cppd@users.noreply.github.com>
2025-09-07 10:31:08 +0300

    volume object dialog

--- src/gui/dialogs/volume_object.cpp
+++ src/gui/dialogs/volume_object.cpp
@@ -30,6 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <algorithm>
 #include <optional>
 #include <string>
+#include <utility>
 
 namespace ns::gui::dialogs
 {
