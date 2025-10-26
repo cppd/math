a2e8c94fa6a173ffacf0500d914c7513601a2ca8
cppd <cppd@users.noreply.github.com>
2025-10-26 09:24:54 +0300

    Vulkan strings

--- src/vulkan/strings/flags.cpp
+++ src/vulkan/strings/flags.cpp
@@ -22,9 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <vulkan/vulkan_core.h>
 
 #include <bit>
+#include <cstdint>
 #include <ios>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace ns::vulkan::strings
@@ -62,23 +64,23 @@ std::string to_hex_flags(const Flags flags)
         return oss.str();
 }
 
-template <typename Flags, typename Flag>
-void add_flags(
-        std::vector<std::string>* const strings,
-        const Flags flags,
-        Flags* const flags_clear,
-        const Flag test_flags,
-        const std::string& name)
+template <std::uintmax_t TEST_FLAGS, typename Flags>
+void add_flags(const std::string_view name, std::vector<std::string>* const strings, Flags* const flags)
 {
-        if ((flags & test_flags) == test_flags)
+        static_assert(std::is_unsigned_v<Flags>);
+        static_assert(TEST_FLAGS > 0);
+
+        constexpr Flags TEST{TEST_FLAGS};
+
+        if ((*flags & TEST) == TEST)
         {
-                strings->push_back(name);
-                *flags_clear &= ~test_flags;
+                strings->emplace_back(name);
+                *flags &= ~TEST;
         }
 }
 
 template <typename Flags>
-void add_flags_unknown(std::vector<std::string>* const strings, const Flags flags)
+void add_flags_unknown(const Flags flags, std::vector<std::string>* const strings)
 {
         if (!flags)
         {
@@ -97,124 +99,94 @@ void add_flags_unknown(std::vector<std::string>* const strings, const Flags flag
 }
 }
 
-std::vector<std::string> sample_counts_to_strings(const VkSampleCountFlags flags)
+std::vector<std::string> sample_counts_to_strings(VkSampleCountFlags flags)
 {
-        if (!flags)
-        {
-                return {};
-        }
-
         std::vector<std::string> res;
-        auto flags_clear = flags;
 
-        add_flags(&res, flags, &flags_clear, VK_SAMPLE_COUNT_1_BIT, "1");
-        add_flags(&res, flags, &flags_clear, VK_SAMPLE_COUNT_2_BIT, "2");
-        add_flags(&res, flags, &flags_clear, VK_SAMPLE_COUNT_4_BIT, "4");
-        add_flags(&res, flags, &flags_clear, VK_SAMPLE_COUNT_8_BIT, "8");
-        add_flags(&res, flags, &flags_clear, VK_SAMPLE_COUNT_16_BIT, "16");
-        add_flags(&res, flags, &flags_clear, VK_SAMPLE_COUNT_32_BIT, "32");
-        add_flags(&res, flags, &flags_clear, VK_SAMPLE_COUNT_64_BIT, "64");
+        add_flags<VK_SAMPLE_COUNT_1_BIT>("1", &res, &flags);
+        add_flags<VK_SAMPLE_COUNT_2_BIT>("2", &res, &flags);
+        add_flags<VK_SAMPLE_COUNT_4_BIT>("4", &res, &flags);
+        add_flags<VK_SAMPLE_COUNT_8_BIT>("8", &res, &flags);
+        add_flags<VK_SAMPLE_COUNT_16_BIT>("16", &res, &flags);
+        add_flags<VK_SAMPLE_COUNT_32_BIT>("32", &res, &flags);
+        add_flags<VK_SAMPLE_COUNT_64_BIT>("64", &res, &flags);
 
-        add_flags_unknown(&res, flags_clear);
+        add_flags_unknown(flags, &res);
 
         return res;
 }
 
-std::vector<std::string> resolve_modes_to_strings(const VkResolveModeFlags flags)
+std::vector<std::string> resolve_modes_to_strings(VkResolveModeFlags flags)
 {
-        if (!flags)
-        {
-                return {};
-        }
-
         std::vector<std::string> res;
-        auto flags_clear = flags;
 
-        add_flags(&res, flags, &flags_clear, VK_RESOLVE_MODE_SAMPLE_ZERO_BIT, "SAMPLE_ZERO");
-        add_flags(&res, flags, &flags_clear, VK_RESOLVE_MODE_AVERAGE_BIT, "AVERAGE");
-        add_flags(&res, flags, &flags_clear, VK_RESOLVE_MODE_MIN_BIT, "MIN");
-        add_flags(&res, flags, &flags_clear, VK_RESOLVE_MODE_MAX_BIT, "MAX");
+        add_flags<VK_RESOLVE_MODE_SAMPLE_ZERO_BIT>("SAMPLE_ZERO", &res, &flags);
+        add_flags<VK_RESOLVE_MODE_AVERAGE_BIT>("AVERAGE", &res, &flags);
+        add_flags<VK_RESOLVE_MODE_MIN_BIT>("MIN", &res, &flags);
+        add_flags<VK_RESOLVE_MODE_MAX_BIT>("MAX", &res, &flags);
 
-        add_flags_unknown(&res, flags_clear);
+        add_flags_unknown(flags, &res);
 
         return res;
 }
 
-std::vector<std::string> shader_stages_to_strings(const VkShaderStageFlags flags)
+std::vector<std::string> shader_stages_to_strings(VkShaderStageFlags flags)
 {
-        if (!flags)
-        {
-                return {};
-        }
-
         std::vector<std::string> res;
-        auto flags_clear = flags;
 
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_VERTEX_BIT, "VERTEX");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT, "TESSELLATION_CONTROL");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT, "TESSELLATION_EVALUATION");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_GEOMETRY_BIT, "GEOMETRY");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_FRAGMENT_BIT, "FRAGMENT");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_COMPUTE_BIT, "COMPUTE");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_RAYGEN_BIT_KHR, "RAYGEN");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_ANY_HIT_BIT_KHR, "ANY_HIT");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR, "CLOSEST_HIT");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_MISS_BIT_KHR, "MISS");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_INTERSECTION_BIT_KHR, "INTERSECTION");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_CALLABLE_BIT_KHR, "CALLABLE");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_TASK_BIT_EXT, "TASK");
-        add_flags(&res, flags, &flags_clear, VK_SHADER_STAGE_MESH_BIT_EXT, "MESH");
+        add_flags<VK_SHADER_STAGE_VERTEX_BIT>("VERTEX", &res, &flags);
+        add_flags<VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT>("TESSELLATION_CONTROL", &res, &flags);
+        add_flags<VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT>("TESSELLATION_EVALUATION", &res, &flags);
+        add_flags<VK_SHADER_STAGE_GEOMETRY_BIT>("GEOMETRY", &res, &flags);
+        add_flags<VK_SHADER_STAGE_FRAGMENT_BIT>("FRAGMENT", &res, &flags);
+        add_flags<VK_SHADER_STAGE_COMPUTE_BIT>("COMPUTE", &res, &flags);
+        add_flags<VK_SHADER_STAGE_RAYGEN_BIT_KHR>("RAYGEN", &res, &flags);
+        add_flags<VK_SHADER_STAGE_ANY_HIT_BIT_KHR>("ANY_HIT", &res, &flags);
+        add_flags<VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR>("CLOSEST_HIT", &res, &flags);
+        add_flags<VK_SHADER_STAGE_MISS_BIT_KHR>("MISS", &res, &flags);
+        add_flags<VK_SHADER_STAGE_INTERSECTION_BIT_KHR>("INTERSECTION", &res, &flags);
+        add_flags<VK_SHADER_STAGE_CALLABLE_BIT_KHR>("CALLABLE", &res, &flags);
+        add_flags<VK_SHADER_STAGE_TASK_BIT_EXT>("TASK", &res, &flags);
+        add_flags<VK_SHADER_STAGE_MESH_BIT_EXT>("MESH", &res, &flags);
 
-        add_flags_unknown(&res, flags_clear);
+        add_flags_unknown(flags, &res);
 
         return res;
 }
 
-std::vector<std::string> subgroup_features_to_strings(const VkSubgroupFeatureFlags flags)
+std::vector<std::string> subgroup_features_to_strings(VkSubgroupFeatureFlags flags)
 {
-        if (!flags)
-        {
-                return {};
-        }
-
         std::vector<std::string> res;
-        auto flags_clear = flags;
 
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_BASIC_BIT, "BASIC");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_VOTE_BIT, "VOTE");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_ARITHMETIC_BIT, "ARITHMETIC");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_BALLOT_BIT, "BALLOT");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_SHUFFLE_BIT, "SHUFFLE");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_SHUFFLE_RELATIVE_BIT, "SHUFFLE_RELATIVE");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_CLUSTERED_BIT, "CLUSTERED");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_QUAD_BIT, "QUAD");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_ROTATE_BIT, "ROTATE");
-        add_flags(&res, flags, &flags_clear, VK_SUBGROUP_FEATURE_ROTATE_CLUSTERED_BIT, "ROTATE_CLUSTERED");
+        add_flags<VK_SUBGROUP_FEATURE_BASIC_BIT>("BASIC", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_VOTE_BIT>("VOTE", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_ARITHMETIC_BIT>("ARITHMETIC", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_BALLOT_BIT>("BALLOT", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_SHUFFLE_BIT>("SHUFFLE", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_SHUFFLE_RELATIVE_BIT>("SHUFFLE_RELATIVE", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_CLUSTERED_BIT>("CLUSTERED", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_QUAD_BIT>("QUAD", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_ROTATE_BIT>("ROTATE", &res, &flags);
+        add_flags<VK_SUBGROUP_FEATURE_ROTATE_CLUSTERED_BIT>("ROTATE_CLUSTERED", &res, &flags);
 
-        add_flags_unknown(&res, flags_clear);
+        add_flags_unknown(flags, &res);
 
         return res;
 }
 
-std::vector<std::string> queues_to_strings(const VkQueueFlags flags)
+std::vector<std::string> queues_to_strings(VkQueueFlags flags)
 {
-        if (!flags)
-        {
-                return {};
-        }
-
         std::vector<std::string> res;
-        auto flags_clear = flags;
 
-        add_flags(&res, flags, &flags_clear, VK_QUEUE_GRAPHICS_BIT, "GRAPHICS");
-        add_flags(&res, flags, &flags_clear, VK_QUEUE_COMPUTE_BIT, "COMPUTE");
-        add_flags(&res, flags, &flags_clear, VK_QUEUE_TRANSFER_BIT, "TRANSFER");
-        add_flags(&res, flags, &flags_clear, VK_QUEUE_SPARSE_BINDING_BIT, "SPARSE_BINDING");
-        add_flags(&res, flags, &flags_clear, VK_QUEUE_PROTECTED_BIT, "PROTECTED");
-        add_flags(&res, flags, &flags_clear, VK_QUEUE_VIDEO_DECODE_BIT_KHR, "VIDEO_DECODE");
-        add_flags(&res, flags, &flags_clear, VK_QUEUE_VIDEO_ENCODE_BIT_KHR, "VIDEO_ENCODE");
+        add_flags<VK_QUEUE_GRAPHICS_BIT>("GRAPHICS", &res, &flags);
+        add_flags<VK_QUEUE_COMPUTE_BIT>("COMPUTE", &res, &flags);
+        add_flags<VK_QUEUE_TRANSFER_BIT>("TRANSFER", &res, &flags);
+        add_flags<VK_QUEUE_SPARSE_BINDING_BIT>("SPARSE_BINDING", &res, &flags);
+        add_flags<VK_QUEUE_PROTECTED_BIT>("PROTECTED", &res, &flags);
+        add_flags<VK_QUEUE_VIDEO_DECODE_BIT_KHR>("VIDEO_DECODE", &res, &flags);
+        add_flags<VK_QUEUE_VIDEO_ENCODE_BIT_KHR>("VIDEO_ENCODE", &res, &flags);
 
-        add_flags_unknown(&res, flags_clear);
+        add_flags_unknown(flags, &res);
 
         return res;
 }
