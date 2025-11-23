89e284bb7381fc0b6dadcd69b42c61f5455babcd
cppd <cppd@users.noreply.github.com>
2025-11-23 10:21:18 +0300

    filter tests

--- src/filter/attitude/limit.h
+++ src/filter/attitude/limit.h
@@ -17,65 +17,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include <src/com/exponent.h>
-#include <src/numerical/vector.h>
-
 namespace ns::filter::attitude
 {
-namespace limit_implementation
-{
-template <typename T>
-inline constexpr T ACCELERATION_MIN = 9.0; // m/(s*s)
-
-template <typename T>
-inline constexpr T ACCELERATION_MAX = 10.6; // m/(s*s)
-
-template <typename T>
-inline constexpr T MAGNETIC_FIELD_MIN = 10; // uT
-
-template <typename T>
-inline constexpr T MAGNETIC_FIELD_MAX = 90; // uT
-}
-
 template <typename T>
 [[nodiscard]] bool acc_suitable(const T acc)
 {
-        namespace impl = limit_implementation;
+        constexpr T ACCELERATION_MIN = 9.0; // m/(s*s)
+        constexpr T ACCELERATION_MAX = 10.6; // m/(s*s)
 
-        return acc >= impl::ACCELERATION_MIN<T> && acc <= impl::ACCELERATION_MAX<T>;
-}
-
-template <typename T>
-[[nodiscard]] bool acc_suitable(const numerical::Vector<3, T>& acc)
-{
-        namespace impl = limit_implementation;
-
-        constexpr T MIN_2 = square(impl::ACCELERATION_MIN<T>);
-        constexpr T MAX_2 = square(impl::ACCELERATION_MAX<T>);
-
-        const T n2 = acc.norm_squared();
-
-        return n2 >= MIN_2 && n2 <= MAX_2;
+        return acc >= ACCELERATION_MIN && acc <= ACCELERATION_MAX;
 }
 
 template <typename T>
 [[nodiscard]] bool mag_suitable(const T mag)
 {
-        namespace impl = limit_implementation;
+        constexpr T MAGNETIC_FIELD_MIN = 10; // uT
+        constexpr T MAGNETIC_FIELD_MAX = 90; // uT
 
-        return mag >= impl::MAGNETIC_FIELD_MIN<T> && mag <= impl::MAGNETIC_FIELD_MAX<T>;
-}
-
-template <typename T>
-[[nodiscard]] bool mag_suitable(const numerical::Vector<3, T>& mag)
-{
-        namespace impl = limit_implementation;
-
-        constexpr T MIN_2 = square(impl::MAGNETIC_FIELD_MIN<T>);
-        constexpr T MAX_2 = square(impl::MAGNETIC_FIELD_MAX<T>);
-
-        const T n2 = mag.norm_squared();
-
-        return n2 >= MIN_2 && n2 <= MAX_2;
+        return mag >= MAGNETIC_FIELD_MIN && mag <= MAGNETIC_FIELD_MAX;
 }
 }
