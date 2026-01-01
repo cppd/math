e06b98ae0812d26301735588c3a8a4aeeea419e5
cppd <cppd@users.noreply.github.com>
2026-01-01 12:09:57 +0300

    update copyright years

--- src/filter/test/view/show_2d.py
+++ src/filter/test/view/show_2d.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2025 Topological Manifold
+# Copyright (C) 2017-2026 Topological Manifold
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
