d555d0f72b76e0e436f643caf944c08ec8cb3a20
cppd <cppd@users.noreply.github.com>
2026-01-15 08:52:54 +0300

    test filter

--- src/filter/attitude/determination/test/test_quest.cpp
+++ src/filter/attitude/determination/test/test_quest.cpp
@@ -18,15 +18,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "cmp.h"
 
 #include <src/com/log.h>
+#include <src/com/random/pcg.h>
 #include <src/filter/attitude/determination/quest.h>
+#include <src/numerical/complement.h>
 #include <src/numerical/quaternion.h>
 #include <src/numerical/vector.h>
+#include <src/sampling/sphere_uniform.h>
 #include <src/test/test.h>
+
+#include <array>
 
 namespace ns::filter::attitude::determination::test
 {
 namespace
 {
+template <typename T>
+numerical::Vector<3, T> add_error(const numerical::Vector<3, T>& v, const T error, PCG& pcg)
+{
+        const std::array<numerical::Vector<3, T>, 2> c =
+                numerical::orthogonal_complement_of_unit_vector(v.normalized());
+
+        const numerical::Vector<2, T> r = sampling::uniform_on_sphere<2, T>(pcg);
+
+        return v + error * (c[0] * r[0] + c[1] * r[1]);
+}
+
 template <typename T>
 void test_impl(const T precision)
 {
@@ -55,6 +71,35 @@ void test_impl(const T precision)
                         quest_attitude<T>({s1, s2, s3}, {r1, r2, r3}, {0.5, 0.5, 0.5});
 
                 test_equal(a, q, precision);
+        }
+
+        {
+                PCG pcg;
+
+                const numerical::Vector<3, T> r1(-2, 3, -4);
+                const numerical::Vector<3, T> r2(2, 3, -4);
+                const numerical::Vector<3, T> r3(-2, -3, -4);
+
+                const numerical::Vector<3, T> s1 = numerical::rotate_vector(q, r1);
+                const numerical::Vector<3, T> s2 = numerical::rotate_vector(q, r2);
+                const numerical::Vector<3, T> s3 = numerical::rotate_vector(q, r3);
+
+                const T e1 = 0.01 * r1.norm();
+                const T e2 = 0.02 * r2.norm();
+                const T e3 = 0.1 * r3.norm();
+
+                const numerical::Vector<3, T> s1e = add_error(s1, e1, pcg);
+                const numerical::Vector<3, T> s2e = add_error(s2, e2, pcg);
+                const numerical::Vector<3, T> s3e = add_error(s3, e3, pcg);
+
+                const T w1 = r1.norm() / e1;
+                const T w2 = r2.norm() / e2;
+                const T w3 = r3.norm() / e3;
+
+                const numerical::QuaternionHJ<T, true> a =
+                        quest_attitude<T>({s1e, s2e, s3e}, {r1, r2, r3}, {w1, w2, w3});
+
+                test_equal(a, q, T{0.02});
         }
 }
 
