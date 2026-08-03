ce90c68be587b578fde26d3ff4d92e57caa94dc3
cppd <cppd@users.noreply.github.com>
2026-08-03 08:54:30 +0300

    color

--- src/color/test/test_conversion.cpp
+++ src/color/test/test_conversion.cpp
@@ -41,6 +41,7 @@ constexpr bool check_1(const numerical::Vector<3, T>& v, const T& precision)
 {
         const numerical::Vector<3, T> rgb = xyz_to_linear_srgb<T>(v[0], v[1], v[2]);
         const numerical::Vector<3, T> xyz = linear_srgb_to_xyz<T>(rgb[0], rgb[1], rgb[2]);
+
         return compare<T>(v, xyz, precision);
 }
 
@@ -49,29 +50,30 @@ constexpr bool check_2(const numerical::Vector<3, T>& v, const T& precision)
 {
         const numerical::Vector<3, T> xyz = linear_srgb_to_xyz<T>(v[0], v[1], v[2]);
         const numerical::Vector<3, T> rgb = xyz_to_linear_srgb<T>(xyz[0], xyz[1], xyz[2]);
+
         return compare<T>(v, rgb, precision);
 }
 
 template <int I, int MAX, typename T>
+        requires (I == 3)
 constexpr bool check(numerical::Vector<3, T>& v, const T& precision)
 {
-        static_assert(I >= 0 && I <= 3);
-        if constexpr (I == 3)
+        return check_1<T>(v, precision) && check_2<T>(v, precision);
+}
+
+template <int I, int MAX, typename T>
+        requires (I >= 0 && I < 3)
+constexpr bool check(numerical::Vector<3, T>& v, const T& precision)
+{
+        for (int i = 0; i <= MAX; ++i)
         {
-                return check_1<T>(v, precision) && check_2<T>(v, precision);
-        }
-        else
-        {
-                for (int i = 0; i <= MAX; ++i)
+                v[I] = static_cast<T>(i) / MAX;
+                if (!check<I + 1, MAX, T>(v, precision))
                 {
-                        v[I] = static_cast<T>(i) / MAX;
-                        if (!check<I + 1, MAX, T>(v, precision))
-                        {
-                                return false;
-                        }
+                        return false;
                 }
-                return true;
         }
+        return true;
 }
 
 template <typename T>
@@ -79,6 +81,7 @@ constexpr bool check(const T& precision)
 {
         constexpr int MAX = 4;
         numerical::Vector<3, T> v;
+
         return check<0, MAX, T>(v, precision);
 }
 
@@ -88,6 +91,7 @@ struct Check final
         static constexpr T D65_X = 0.9505;
         static constexpr T D65_Y = 1;
         static constexpr T D65_Z = 1.089;
+
         static_assert(compare<T>(xyz_to_linear_srgb<T>(D65_X, D65_Y, D65_Z), {1, 1, 1}, 1e-6));
         static_assert(compare<T>(linear_srgb_to_xyz<T>(1, 1, 1), {D65_X, D65_Y, D65_Z}, 1e-16));
 
